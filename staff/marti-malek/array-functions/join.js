86345fdbc26e96922549166b823ffb2d5a62ebee
martimalek <martimalekdot@hotmail.es>
2019-01-15 16:14:01 +0100

    reverse not finished

--- staff/marti-malek/array-functions/join.js
+++ staff/marti-malek/array-functions/join.js
@@ -1,12 +1,21 @@
-/* Function join: Join takes the elements of the array "arr" and adds them into a string which returns */
-function join(arr) {
+/**
+ * Abstraction of join.
+ * 
+ * Joins the elements of an array with the desired param.
+ * 
+ * @param {Array} arr 
+ * @param {*} elem
+ */
+function join(arr, elem) {
     var res = '';
+
+    elem = elem === undefined ? ',' : elem;
+    if (arguments.length > 2) throw Error('too many arguments');
+
     for (var i = 0; i < arr.length; i++) {
         res += arr[i];
+        if (i == arr.length-1) return res;
+        res += elem;
     }
     return res;
 }
-
-var a = ['hello', 'world'];
-
-join(a);
--- staff/marti-malek/array-functions/reverse.js
+++ staff/marti-malek/array-functions/reverse.js
@@ -1,14 +1,15 @@
-function reverse(arr) {
-    var res = [];
-    var j = 0;
-    for (var i = arr.length; i > 1; i--) {
-        res[j] = arr[i];
-        j++;
-    }
-    console.log(res);
-}
-
 var a = [1,2,3];
 
-reverse(a);
+function reverse(arr) {
+    var res = [];
+    for (var i = arr.length; i > 0; i--) {
+        res[arr.length-i] = arr[i-1];    
+    }
+    arr = Object.assign(res);
+    return arr;
+}
+
+
+var rev = reverse(a);
+console.log(rev);
 console.log(a);
