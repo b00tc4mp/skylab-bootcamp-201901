33e6a523f2245b1fc05a644199d8a8271977780b
Serena <serena@example.com>
2019-01-15 13:07:57 +0100

    Changes fill

--- staff/serena-mejias/array-functions/fill.js
+++ staff/serena-mejias/array-functions/fill.js
@@ -1,14 +1,65 @@
-var array = [1, 2, 3, 4];
+/**
+ * Abstraction of fill.
+ *
+ * Fills an array from one position to other.
+ *
+ * @param {Array} array
+ * @param {*} value
+ * @param {number} start
+ * @param {number} end
+ * 
+ * @throws {Error} - If too many arguments.
+ * @throws {TypeError} - If array is not an array.
+ * 
+ */
+var array = [1, 2, 3, 4, 5];
 
-function fill(arr, num) {
-  var newArr = [];
-  for (var i=0; i<arr.length; i++) {
-    newArr[i] = num;
-    return newArr;
+//use case 1
+
+function fill(arr, num, start, end) {
+  if (!(arr instanceof Array)) {
+    throw new TypeError("is not an array"); 
+  }
+
+  end = end === undefined ? arr.length : end < 0 ? arr.length + end : end; //ternario (primera parte pregunta (wue devuelve un bolean)?, segunda true :, tercera false).
+  start = start === undefined ? 0 : start < 0 ? arr.length + start : start;
+  for (var i = start; i < end; i++) {
+    arr[i] = num;
+    return arr;
   }
 }
 
-console.log(fill(array, 5));
+console.log(fill(array, 0, 0, 2));
+//[0,0,3,4,5]
 
+//use case 2
 
+console.log(fill(array, 0, 2));
 
+//use case 3
+
+console.log(fill(array, 0));
+
+//use case 4
+
+console.log(fill(array, -3, -2)); //[1,2,0,4,5]
+
+//use case 5
+
+console.log(fill(array,0,-3,4));
+
+//use case 6
+
+try {
+  fill({}, 0);
+} catch (err) {
+  error = err;
+}
+
+//use case 7
+
+try {
+  fill(1, 0);
+} catch (err) {
+  error = err;
+}
