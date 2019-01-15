7cc6c119fda7404807a442f014174d4ce6a1c43f
Àlex Barba <alexander.barba@gmail.com>
2019-01-15 19:42:39 +0100

    v004

--- staff/alex-barba/array-functions/fill.js
+++ staff/alex-barba/array-functions/fill.js
@@ -1,7 +1,7 @@
 /**
  * Abstraction of fill.
  * 
- * Fills an array from one position to other.
+ *  returns the first index at which a given element can be found in the array, or -1 if it is not present.
  * 
  * @param {Array} array 
  * @param {*} value 
