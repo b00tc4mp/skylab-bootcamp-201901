a3ad98e3f89dfa1deeac6a0c98a61a2a72d9f836
jordi <jordi.pujolg@gmail.com>
2019-01-15 16:11:41 +0100

    more changes

--- staff/jordi-pujol/array-functions/find.js
+++ staff/jordi-pujol/array-functions/find.js
@@ -1,16 +1,26 @@
-/*
-Abstractation of find
+/**
+ *Abstractation of find
+ *The find() method returns the value of the first element in the array that satisfies the provided testing function.
+ *Otherwise undefined is returned.
+ * 
+ * @param {*} arr 
+ * @param {*} func 
+ */
 
-The find() method returns the value of the first element in the array that satisfies the provided testing function.
-Otherwise undefined is returned.*/
 
-
-var inventory = ["apples","bananas","cheeries"];
-
-function myfind (fruit){
-    
+function find(arr, val, func){
+    var res;
+    for (var i=0; i<arr.length; i++)
+    if (arr[i] === val){
+        res = i
+    }
+    return res
 }
 
+// start = start === undefined ? 0 : (start < 0 ? array.length + start : start);
 
+var a = [1, 2, 3, 4, 5];
 
-console.log(inventory.myfind(func,arr));
+var res = find(a,function(v) { return v + 10});
+
+console.log(res);
