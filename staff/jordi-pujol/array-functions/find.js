4a563d5d626f09b45572897c51bdead66365ce5b
jordi <jordi.pujolg@gmail.com>
2019-01-15 13:07:10 +0100

    3rd change

---
+++ staff/jordi-pujol/array-functions/find.js
@@ -0,0 +1,16 @@
+/*
+Abstractation of find
+
+The find() method returns the value of the first element in the array that satisfies the provided testing function.
+Otherwise undefined is returned.*/
+
+
+var inventory = ["apples","bananas","cheeries"];
+
+function myfind (fruit){
+    
+}
+
+
+
+console.log(inventory.myfind(func,arr));
