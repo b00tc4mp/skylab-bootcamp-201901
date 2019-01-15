a0a44f8eb4d12ed1ad127362d496367359844842
Emma Cowdery <e2maaa@gmail.com>
2019-01-15 10:32:57 +0100

    delete old files

--- staff/emma-cowdery/array-higher-order-functions/fill.js
+++
@@ -1,7 +0,0 @@
-/*The fill() method fills all the elements of an array from a start index to an end index with a static value. The end index is not included.
-    Syntax: arr.fill(value[, start[, end]])
-        value: value to fill an array
-        start: optional, defaults to 0
-        end: optional, defaults to this.lenght*/
-
-
