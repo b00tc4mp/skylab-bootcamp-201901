154dd340debf95593f73d88e646d41cb526bd3b1
Pedro <pe->
2019-01-16 17:15:05 +0100

    update

--- staff/pedro-garriganogues/array-functions/find.js
+++ staff/pedro-garriganogues/array-functions/find.js
@@ -9,33 +9,20 @@
  * 
  * @throws {TypeError} - If array is not an array
  */
-var arr = ["1","2","3","4","5"];
+var arr = [1, 2, 3, 4, 5];
+var expected = []
 
-function buscar(array, word){    
-for(var i = 1; i < array.length+1;i++){
+function finder(array, callback) {
     if (!(array instanceof Array))
-    throw new TypeError(ele + ' is not an array');
+        throw new TypeError(array + ' is not an array');
 
-        if(array[i] === word){
-            
-        console.log(word + ' is element num ' + (i+1) + ' at the array.')
+    for (var i = 0; i < array.length; i++) {
+        if (callback(array[i])) {
+            return array[i];
         }
+
     }
 };
 
-buscar(arr, "4")
 
 
-// use case 1
-
-test('use case 1', function () {
-    var arr = [1, 2, 3, 4, 5];
-
-    var res = fill(arr, 0, 0, 2);
-
-    var expected = [0, 0, 3, 4, 5];
-
-    if (res !== arr) throw Error('array and result should be the same');
-    if (res.toString() !== expected.toString()) throw Error('result should be the one expected');
-    if (arr.toString() !== expected.toString()) throw Error('array should have been changed to the one expected');
-});
--- staff/pedro-garriganogues/array-functions/find.test.js
+++ staff/pedro-garriganogues/array-functions/find.test.js
@@ -1,29 +1,91 @@
-/**
- * Abstraction of fill.
- * 
- * Finds a word.
- * 
- * 
- * @param {Word} - word to find
- * @param {Array} array
- * 
- * @throws {TypeError} - If array is not an array
- */
-var arr = ["1","2","3","4","5"];
+suite('TEST find');
 
-function buscar(array, word){    
-for(var i = 1; i < array.length+1;i++){
-    if (!(array instanceof Array))
-    throw new TypeError(ele + ' is not an array');
 
-        if(array[i] === word){
-            
-        console.log(word + ' is element num ' + (i+1) + ' at the array.')
-        }
+// use case 1
+
+test('CASE word coincidence', function () {
+
+    var arr = [1, 2, 3, 4, 5];
+    var expected = 4;
+    var res = finder(arr, function (element) {
+        return element > 3;
+    });
+
+    if (res !== expected) {
+        throw Error('parameter and result should be the same')
+    };
+
+});
+
+// use case 2   
+
+test('CASE object inside', function () {
+    var arr = {};
+
+    try {
+        finder(arr, function (element) {
+            return element;
+        });
+    } catch (err) {
+        error = err;
     }
-};
+    if (arr === Object) throw Error('object inside');
 
-buscar(arr, "4")
+});
+
+// use case 3   
+
+test('CASE text inside', function () {
+    var arr = {};
+
+    try {
+        finder(arr, function (element) {
+            return element;
+        });
+    } catch (err) {
+        error = err;
+    }
+    if (arr === Text) throw Error('object inside');
+
+});
+
+// use case 4
+
+test('CASE empty inside', function () {
+    var arr = {};
+
+    try {
+        finder('', function (element) {
+            return element;
+        });
+    } catch (err) {
+        error = err;
+    }
+    if (arr === '') throw Error('object inside');
+
+});
+
+// use case 5 
+
+test('CASE empty inside', function () {
+    var arr = {};
+
+    try {
+        finder('', function (element) {
+            return element;
+        });
+    } catch (err) {
+        error = err;
+    }
+    if (arr === '') throw Error('object inside');
+
+});
+
+
+
+
+
+
 
 
 
