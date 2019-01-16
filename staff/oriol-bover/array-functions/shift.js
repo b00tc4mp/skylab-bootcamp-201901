1ea06e6c19bafc665e9ca20c71f74ef3ee15a361
Oriol Bover Vila <boversauros@Oriols-MacBook-Pro.local>
2019-01-16 11:25:50 +0100

    update test machine and shift realese

---
+++ staff/oriol-bover/array-functions/shift.js
@@ -0,0 +1,25 @@
+/**
+ * Abstraction of shift
+ * 
+ * Iterate an array and removes the first element from an array and returns that removed element. 
+ * This method changes the length of the array.
+ * 
+ * @param {Array} array 
+ * 
+ * @returns {*} - The removed element from the array; undefined if the array is empty.
+ */
+
+function shift(array){
+    if(!(array instanceof Array)) throw Error('the array isn\'t an Array');
+
+    var res = array[0];
+    var array2 = Object.assign([], array);
+
+    for (var i = 0; i < array.length-1; i++) {
+        array[i] = array2[i+1];
+    }
+
+    array.length = array.length-1;
+    return res;
+
+}
