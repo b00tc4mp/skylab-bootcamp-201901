e6bfcf0c74a504b1d0ba68bcc680a940862563c6
martimalek <martimalekdot@hotmail.es>
2019-01-16 19:48:27 +0100

    finished all but testing

--- staff/marti-malek/e-commerce-protos/scripts/cart.js
+++ staff/marti-malek/e-commerce-protos/scripts/cart.js
@@ -1,3 +1,9 @@
+/**
+ * Demo of an e-commerce.
+ * 
+ * @param {Array} cartList 
+ */
+
 function Cart(cartList) {
     this.cartList = [];
 
@@ -15,6 +21,42 @@ function Cart(cartList) {
         var items = this.cartList.length;
 
         return items;
-    }
+    };
+    this.mostExpensive = function () {
+        var item = 0;
+        for (var i = 0; i < this.cartList.length; i++) {
+            if (this.cartList[i].price > this.cartList[item].price) {
+                item = i;
+            }
+        }
+        return this.cartList[item];
+    };
+    this.cheapest = function () {
+        var item = 0;
+        for (var i = 0; i < this.cartList.length; i++) {
+            if (this.cartList[i].price < this.cartList[item].price) {
+                item = i;
+            }
+        }
+        return this.cartList[item];
+    };
+    this.numberOf = function (type) {
+        var num = 0;
+        for (var i = 0; i < this.cartList.length; i++) {
+            if (this.cartList[i] instanceof type) {
+                num++;
+            }
+        }
+        return num;
+    };
+    this.productsByPriceRange = function () {
+        var products = [];
+        for (var i = 0; i < this.cartList.length; i++) {
+            if (30 < this.cartList[i].price && this.cartList[i].price < 120) {
+                products.push(this.cartList[i]);
+            }
+        }
+        return products;
+    };
 };
 
