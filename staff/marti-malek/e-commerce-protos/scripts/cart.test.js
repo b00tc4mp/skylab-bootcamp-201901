9f4975a1692cf3cf3e87db46262d091e8f73b46b
martimalek <martimalekdot@hotmail.es>
2019-01-16 20:27:04 +0100

    continued testing

--- staff/marti-malek/e-commerce-protos/scripts/cart.test.js
+++ staff/marti-malek/e-commerce-protos/scripts/cart.test.js
@@ -1,10 +1,29 @@
 suite('totalPrice');
 
 test('Correct', function () {
+    var cart = new Cart;
+
+    var tanga = new Tanga('Wicked Weasel', 32, 'red', 29.95);
+    cart.add(tanga);
 
     var res = cart.totalPrice();
 
-    var expected = 4085.92;
+    var expected = 29.95;
 
-    assert(res.toString() === expected.toString(), 'should have returned the correct value')
-})
+    assert(res.toString() === expected.toString(), 'should have returned the correct value');
+});
+
+test('Fail too many arguments', function () {
+    var cart = new Cart;
+
+    var tanga = new Tanga('Wicked Weasel', 32, 'red', 29.95);
+    cart.add(tanga);
+
+    try{
+        cart.totalPrice();
+    }catch (err) {
+        error = err;
+    }
+
+    assert(Error, 'should have returned the correct value');
+});
