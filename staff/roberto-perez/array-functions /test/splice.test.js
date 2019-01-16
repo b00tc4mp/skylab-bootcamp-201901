40a0dc685f40390e8520125df6908da4bbc58289
robertz <robertz.perez1985@gmail.com>
2019-01-16 22:30:33 +0100

    some array-higher

--- staff/roberto-perez/array-functions /test/splice.test.js
+++ staff/roberto-perez/array-functions /test/splice.test.js
@@ -1,48 +1,89 @@
 suite("splice");
 
-test('Slice arr - DELETE', function() {
+// test('Slice arr - DELETE', function() {
     
-    var arr = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
-
-    var expected = ["drum", "mandarin"];
-
-    var removed = splice(arr, 2, 2);
-    console.log('---------------');
-    console.log(removed);
-    console.log(arr);
-
-    if (removed.toString() !== expected.toString())
-    throw Error("The new array " + removed+ " does not match expected " + expected);
-
-});
-
-// test('Slice arr - DELETE with last param > arr.length', function() {
-
 //     var arr = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
-//     //console.log(arr);
 
-//     var expected = ["drum", "mandarin", "sturgeon"];
+//     var expected = ["angel", "clown"];
 
-//     var removed = splice(arr, 2, 4);
+//     var removed = splice(arr, 1, 2);
 
-//     //console.log(removed);
-//     //console.log(arr);
+//     console.log('DELETED', removed);
+//     console.log('MUTATED:', arr);
 
 //     if (removed.toString() !== expected.toString())
 //     throw Error("The new array " + removed+ " does not match expected " + expected);
 
 // });
 
+// test('Slice arr - DELETE', function() {
+    
+//     var arr = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
+
+//     var expected = ["drum", "mandarin"];
+
+//     var removed = splice(arr, 2, 2);
+
+//     if (removed.toString() !== expected.toString())
+//     throw Error("The new array " + removed+ " does not match expected " + expected);
+
+// });
+
+// test('Slice arr - DELETE with last param > arr.length', function() {
+
+//     var arr = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
+
+//     var expected = ["drum", "mandarin", "sturgeon"];
+
+//     var removed = splice(arr, 2, 4);
+
+//     if (removed.toString() !== expected.toString())
+//     throw Error("The new array " + removed+ " does not match expected " + expected);
+
+// });
+
+
+// test('Slice arr - start > arr.length', function() {
+
+//     var arr = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
+
+//     var expected = [];
+
+//     var removed = splice(arr, 12, 4);
+
+//     if (removed.toString() !== expected.toString())
+//     throw Error("The new array " + removed+ " does not match expected " + expected);
+
+// });
+
+
+test('Slice arr - ADD', function() {
+
+    var arr = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
+
+    var expected = [];
+
+    var removed = splice(arr, 1, 0,"drum");
+
+    console.log("DELETED: ", removed);
+    console.log("MUTATED", arr);
+
+    if (removed.toString() !== expected.toString())
+    throw Error("The new array " + removed+ " does not match expected " + expected);
+
+});
+
+
 // test('Slice arr - ADD', function() {
 
 //     var arr = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
-//     console.log(arr);
 
 //     var expected = [];
 
 //     var removed = splice(arr, 2, 0, 'drum');
 
-//     console.log(arr);
+//     console.log('DELETED', removed);
+//     console.log('MUTATED:', arr);
 
 //     if (removed.toString() !== expected.toString())
 //     throw Error("The new array " + removed+ " does not match expected " + expected);
