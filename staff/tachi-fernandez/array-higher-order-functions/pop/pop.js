a974fa520655e1badc79976cdacafabb442aed48
Tachi <jrfluna8@gmail.com>
2019-01-16 11:29:40 +0100

    changes

--- staff/tachi-fernandez/array-higher-order-functions/pop/pop.js
+++ staff/tachi-fernandez/array-higher-order-functions/pop/pop.js
@@ -1,9 +1,16 @@
 function pop(array) {
-    for (var i = 0; i < array.length; i++) {
-       
-        var res = array[i] - 
-        return res
+    var lastNum = array[array.length - 1]
+    //console.log(lastNum)
+    var temporal = []
+    for (var i = 0; i < array.length - 1; i++) {
+        temporal[i] = array[i]
     }
+    array = []
+    for (var i = 0; i < temporal.length; i++) {
+        array[i] = temporal[i]
+    }
+    return lastNum
 
 }
+
 
--- staff/tachi-fernandez/array-higher-order-functions/pop/pop.test.js
+++ staff/tachi-fernandez/array-higher-order-functions/pop/pop.test.js
@@ -5,6 +5,7 @@ var array = [1,2,3,4,5]
 var res = pop(array)
 
 console.log(res)
+console.log(array)
 
 
 
