5d443baa2bf01e5046f045d5f89e0b523be7b5c1
Ury18 <ury_98@hotmail.com>
2019-01-16 11:44:32 +0100

    indexof and join

--- staff/oriol-escolar/array-higher-order-functions/index-of.test.js
+++ staff/oriol-escolar/array-higher-order-functions/index-of.test.js
@@ -5,7 +5,7 @@ suite('Index-of')
 
 //#region Use Case 1
 
-test('Case1', function () {
+test('Use Case1', function () {
 
     var array = ['pepino', 'piedra', 'tomate'];
 
@@ -24,11 +24,11 @@ test('Case1', function () {
 
 //#region Use Case 2
 
-test('Case2', function () {
+test('Use Case2', function () {
 
     var array = ['pepino', 'pepino', 'tomate'];
 
-    var expected = [0, 1];
+    var expected = 0;
 
     var indexes = indexof(array, 'pepino');
 
@@ -44,11 +44,11 @@ test('Case2', function () {
 
 //#region Use Case 3
 
-test('Case3', function () {
+test('Use Case3', function () {
 
     var array = ['pepino', 'piedra', 'pepino'];
 
-    var expected = [0, 2];
+    var expected = 0;
 
     var indexes = indexof(array, 'pepino');
 
@@ -64,13 +64,14 @@ test('Case3', function () {
 
 //#region Use Case 4
 
-test('Case3', function () {
+test('Use Case4', function () {
 
     var array = ['patata', 'piedra', 'patata'];
 
-    var expected = undefined;
+    var expected = -1;
 
     var indexes = indexof(array,);
+
 
     if (indexes !== expected) { throw Error('Indexes is no what it is expected') }
 
