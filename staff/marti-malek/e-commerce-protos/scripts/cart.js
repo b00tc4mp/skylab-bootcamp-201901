0ec95fdc4b7ffc0be9dc5b84278f48c87aa63164
martimalek <martimalekdot@hotmail.es>
2019-01-16 20:27:04 +0100

    continued testing

--- staff/marti-malek/e-commerce-protos/scripts/cart.js
+++ staff/marti-malek/e-commerce-protos/scripts/cart.js
@@ -18,7 +18,7 @@ function Cart(cartList) {
      * @throws {Error} - If too many arguments
      */
     this.totalPrice = function () {
-        if (arguments.length > 0) throw Error ('too many arguments')
+        if (arguments.length > 0) throw Error ('too many arguments');
 
         price2 = 0;
         for(var i = 0; i < this.cartList.length; i++) {
@@ -26,12 +26,30 @@ function Cart(cartList) {
         }
         return price2;
     };
+    /**
+     * Function that calculates the total number of items added to the cart.
+     * 
+     * @returns {number} 
+     * 
+     * @throws {Error} - If too many arguments
+     */
     this.numberOfItems = function () {
+        if (arguments.length > 0) throw Error ('too many arguments');
+
         var items = this.cartList.length;
 
         return items;
     };
+    /**
+     * Function that calculates the most expensive product of the ones added to the cart.
+     * 
+     * @returns {Object} 
+     * 
+     * @throws {Error} - If too many arguments
+     */
     this.mostExpensive = function () {
+        if (arguments.length > 0) throw Error ('too many arguments');
+
         var item = 0;
         for (var i = 0; i < this.cartList.length; i++) {
             if (this.cartList[i].price > this.cartList[item].price) {
@@ -40,7 +58,16 @@ function Cart(cartList) {
         }
         return this.cartList[item];
     };
+    /**
+     * Function that calculates the cheapest product of the ones added to the cart.
+     * 
+     * @returns {Object} 
+     * 
+     * @throws {Error} - If too many arguments
+     */
     this.cheapest = function () {
+        if (arguments.length > 0) throw Error ('too many arguments');
+
         var item = 0;
         for (var i = 0; i < this.cartList.length; i++) {
             if (this.cartList[i].price < this.cartList[item].price) {
@@ -49,7 +76,18 @@ function Cart(cartList) {
         }
         return this.cartList[item];
     };
+    /**
+     * Function that calculates the total number of items of a kind.
+     * 
+     * @param {} type
+     * 
+     * @returns {Object} 
+     * 
+     * @throws {Error} - If too many arguments
+     */
     this.numberOf = function (type) {
+        if (arguments.length > 1) throw Error ('too many arguments');
+
         var num = 0;
         for (var i = 0; i < this.cartList.length; i++) {
             if (this.cartList[i] instanceof type) {
@@ -58,7 +96,16 @@ function Cart(cartList) {
         }
         return num;
     };
+    /**
+     * Function that returns the products inside a price range.
+     * 
+     * @returns {Array} 
+     * 
+     * @throws {Error} - If too many arguments
+     */
     this.productsByPriceRange = function () {
+        if (arguments.length > 0) throw Error ('too many arguments');
+
         var products = [];
         for (var i = 0; i < this.cartList.length; i++) {
             if (30 < this.cartList[i].price && this.cartList[i].price < 120) {
