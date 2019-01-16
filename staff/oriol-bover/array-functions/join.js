21b96e57a0cfc3b54d0e6bf0ddd82cf68f11f4e1
Oriol Bover Vila <boversauros@Oriols-MacBook-Pro.local>
2019-01-16 09:06:20 +0100

    6 array functions tested

---
+++ staff/oriol-bover/array-functions/join.js
@@ -0,0 +1,31 @@
+/**
+ * Abstraction of join
+ * 
+ * iterates an array and creates and returns a new string by concatenating all of the elements.
+ * By default separates by commas.
+ * 
+ * @param {Array} array - array to iterate
+ * @param {*} separator - specify element seperator
+ * 
+ * @returns {String}
+ */
+
+function join(array, separator){
+    if(arguments.length > 2)
+        throw Error('too many arguments');
+    
+    if (!(array instanceof Array))
+        throw new TypeError(array + ' is not an array');
+
+    var string = '';
+
+    separator = separator === undefined ? ',' : separator;
+
+    for (var i = 0; i < array.length; i++) {
+        var value = array[i];
+
+      string += i === array.length-1 ? value : value + separator;
+    }
+
+    return string;
+}
---
+++ staff/oriol-bover/array-functions/reverse.test.js
@@ -0,0 +1,8 @@
+suite('reverse');
+
+
+test('functional', function(){
+    var arr = [1,2,3];
+
+    reverse(arr);
+});
