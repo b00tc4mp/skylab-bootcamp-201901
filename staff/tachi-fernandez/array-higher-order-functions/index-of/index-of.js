bbf4eb6ce271d3e954c9b7b1b9927ea82c8f577e
Tachi <jrfluna8@gmail.com>
2019-01-16 11:28:44 +0100

    changes

--- staff/tachi-fernandez/array-higher-order-functions/index-of/index-of.js
+++ staff/tachi-fernandez/array-higher-order-functions/index-of/index-of.js
@@ -1,4 +1,25 @@
+/**
+ * Abstraction of index-of.
+ * 
+ * Index-of shows the position of serchValue in the array
+ * 
+ * @param {Array} array 
+ * @param {*} searchValue 
+ * 
+ * 
+ * @throws {Error} - If too many arguments (> 4)
+ * @throws {TypeError} - If array is not an array
+ */
+
+
 function indexOf(array, searchValue) {
+
+    if (arguments.length > 2) throw Error('too many arguments');
+    if (!(array instanceof Array))
+    throw new TypeError(array + ' is not an array');
+
+
+
     for (var i = 0; i < array.length; i++) 
         if (searchValue === array[i]) return i
     return -1 
--- staff/tachi-fernandez/array-higher-order-functions/index-of/index-of.test.js
+++ staff/tachi-fernandez/array-higher-order-functions/index-of/index-of.test.js
@@ -1,7 +1,87 @@
-// use case 1
+suite("Test Index-of")
 
-var array = [1,2,3,4,5]
+// Case 1:
 
-var res = indexOf(array,4)
+test("all arguments" , function(){
 
-console.log(res)
+var array = [1,2,3,4,5];
+var res = indexOf(array,4);
+var expected = 3;
+
+if (res.toString() !== expected.toString()) throw Error('result should be the one expected');
+});
+
+//Case:2
+
+test("searchValue in an string" , function(){
+
+
+    var array = [1,2,3,4,5];
+    var res = indexOf(array,"cuatro")
+    var expected = -1
+
+    if (res.toString() !== expected.toString()) throw Error('result should be the one expected');
+
+
+    
+});
+
+//Case:3
+
+test("searchValue is negative" , function(){
+
+    
+
+    var array = [1,2,3,4,5];
+    var res = indexOf(array,-3)
+    var expected = -1
+
+    if (res.toString() !== expected.toString()) throw Error('result should be the one expected');
+
+
+    
+});
+
+//Case:4
+
+test("more arguments" , function(){
+
+    var error;
+
+    try {
+        indexOf(array, 0,3);
+    } catch (err) {
+        error = err;
+    }
+
+    if (!error) throw Error('should have thrown an error');
+    if (!(error instanceof Error)) throw Error('should have thrown TypeError');
+});
+
+//Case:5
+
+test("argument is a booleano" , function(){
+
+
+    var error;
+
+    try {
+        fill(1,false);
+    } catch (err) {
+        error = err;
+    }
+
+    if (!error) throw Error('should have thrown an error');
+    if (!(error instanceof Error)) throw Error('should have thrown TypeError');
+});
+
+
+
+
+
+
+
+
+
+
+
