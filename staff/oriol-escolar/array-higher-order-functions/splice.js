1d93cf6702cebe25c6eabf84c58507e45c76bd01
Ury18 <ury_98@hotmail.com>
2019-01-17 20:54:12 +0100

    slice and splice

---
+++ staff/oriol-escolar/array-higher-order-functions/splice.js
@@ -0,0 +1,31 @@
+function splice(array,begin,end){
+
+if(!(array instanceof Array)) throw TypeError (array + ' is not an array')
+begin =  typeof begin === 'number' ? (begin >= 0 ? (begin > array.length? 0: begin): arr.length+begin):0 
+end =  typeof end === 'number' ? (end>=0 ? (end>array.length?array.length:end ): array.length+end ): array.length;
+
+var sliced = [];
+
+for(i=begin;i<end;i++){
+
+    if(sliced.length == 0)
+    {
+        sliced[0]= array[i];
+    }else{
+        sliced[sliced.length] = array[i]
+    }
+}
+
+array.length = sliced.length;
+for(i=0;i<array.length;i++){
+
+array[i]=sliced[i]
+
+
+}
+
+return array;
+
+}
+
+
