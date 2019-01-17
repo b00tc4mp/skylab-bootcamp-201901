ece1faa456b5547eac342763c48c5775a06fcc60
quinwacca <37936498+quinwacca@users.noreply.github.com>
2019-01-17 18:52:49 +0100

    minor changes

--- staff/luis-garrido/e-commerce-protos/scripts/cart.test.js
+++ staff/luis-garrido/e-commerce-protos/scripts/cart.test.js
@@ -83,39 +83,9 @@ suite("cart", function () {
   describe("most expensive and cheapest items", function() {
     it("should return the most expensive item in cart", function () {
 
-      var cart = new Cart();
+      var cart=getReadyToTest();
 
-      var tshirt = new TShirt(399, "Supreme", "white", "M");
-      var sweater = new Sweater(149, "Diesel", "black", "M");
-      var laptop = new Laptop(1599, "Apple", "MacBook Pro", 15);
-      var desktop = new Desktop(420, "HP", "1800", 20);
-      var mobile = new Mobile(999, "Apple", "iPhone X", "space-gray");
-      var mobile2 = new Mobile(119, "Xiaomi", "5X", "space-rose");
-      var socks = new Socks(9.99, "Calvin Klein", "black", 42);
-      var tanga = new Tanga(29.95, "Wicked Weasel", "red", 32);
-      var bra = new Bra(39, "Calvin Klein", "cream", 80, "push-up");
-      var slips = new Slips(13.99, "Abanderado", "purple", 42);
-      var jeans = new Jeans(199, "Diesel", "denim", 42, "regular");
-      var shorts = new Shorts(35, "Adidas", "blue", 42, "Sport");
-      var cap = new Cap(29, "Obey", "black", "M");
-      var hat = new Hat(44.99, "Jack & Jones", "red", undefined, "Cup"); 
-
-      cart.add(socks);  
-      cart.add(tanga);
-      cart.add(bra);
-      cart.add(slips);
-      cart.add(mobile); 
-      cart.add(mobile2);
-      cart.add(laptop);
-      cart.add(desktop);
-      cart.add(hat);
-      cart.add(jeans);
-      cart.add(tshirt);
-      cart.add(cap);
-      cart.add(shorts);
-      cart.add(sweater);
-
-      expectedValue = laptop;
+      expectedValue = cart.products[6];
 
       mostExpensiveItem = cart.mostExpensive();
 
@@ -128,39 +98,9 @@ suite("cart", function () {
 
     it("should return the cheapest item in cart", function () {
 
-      var cart = new Cart();
+      var cart=getReadyToTest();
 
-      var tshirt = new TShirt(399, "Supreme", "white", "M");
-      var sweater = new Sweater(149, "Diesel", "black", "M");
-      var laptop = new Laptop(1599, "Apple", "MacBook Pro", 15);
-      var desktop = new Desktop(420, "HP", "1800", 20);
-      var mobile = new Mobile(999, "Apple", "iPhone X", "space-gray");
-      var mobile2 = new Mobile(119, "Xiaomi", "5X", "space-rose");
-      var socks = new Socks(9.99, "Calvin Klein", "black", 42);
-      var tanga = new Tanga(29.95, "Wicked Weasel", "red", 32);
-      var bra = new Bra(39, "Calvin Klein", "cream", 80, "push-up");
-      var slips = new Slips(13.99, "Abanderado", "purple", 42);
-      var jeans = new Jeans(199, "Diesel", "denim", 42, "regular");
-      var shorts = new Shorts(35, "Adidas", "blue", 42, "Sport");
-      var cap = new Cap(29, "Obey", "black", "M");
-      var hat = new Hat(44.99, "Jack & Jones", "red", undefined, "Cup");
-
-      cart.add(socks);
-      cart.add(tanga);
-      cart.add(bra);
-      cart.add(slips);
-      cart.add(mobile);
-      cart.add(mobile2);
-      cart.add(laptop);
-      cart.add(desktop);
-      cart.add(hat);
-      cart.add(jeans);
-      cart.add(tshirt);
-      cart.add(cap);
-      cart.add(shorts);
-      cart.add(sweater);
-
-      expectedValue = socks;
+      expectedValue = cart.products[0];
 
       cheapestItem = cart.cheapest();
 
@@ -208,41 +148,11 @@ suite("cart", function () {
   describe("products between prices", function() {
     it("should return an array with products in between prices", function () {
 
-      var cart = new Cart();
+      var cart = getReadyToTest();
 
-      var tshirt = new TShirt(399, "Supreme", "white", "M");
-      var sweater = new Sweater(149, "Diesel", "black", "M");
-      var laptop = new Laptop(1599, "Apple", "MacBook Pro", 15);
-      var desktop = new Desktop(420, "HP", "1800", 20);
-      var mobile = new Mobile(999, "Apple", "iPhone X", "space-gray");
-      var mobile2 = new Mobile(119, "Xiaomi", "5X", "space-rose");
-      var socks = new Socks(9.99, "Calvin Klein", "black", 42);
-      var tanga = new Tanga(29.95, "Wicked Weasel", "red", 32);
-      var bra = new Bra(39, "Calvin Klein", "cream", 80, "push-up");
-      var slips = new Slips(13.99, "Abanderado", "purple", 42);
-      var jeans = new Jeans(199, "Diesel", "denim", 42, "regular");
-      var shorts = new Shorts(35, "Adidas", "blue", 42, "Sport");
-      var cap = new Cap(29, "Obey", "black", "M");
-      var hat = new Hat(44.99, "Jack & Jones", "red", undefined, "Cup");
-
-      cart.add(socks);
-      cart.add(tanga);
-      cart.add(bra);
-      cart.add(slips);
-      cart.add(mobile);
-      cart.add(mobile2);
-      cart.add(laptop);
-      cart.add(desktop);
-      cart.add(hat);
-      cart.add(jeans);
-      cart.add(tshirt);
-      cart.add(cap);
-      cart.add(shorts);
-      cart.add(sweater);
-
-      expectedValue = [bra, mobile2, hat, shorts];
+      expectedValue = [cart.products[2], cart.products[5], cart.products[8], cart.products[12]];
       productsInBetween = cart.productsByPriceRange(30, 120);
-      console.log(cart);
+
       expect(productsInBetween.toString() === expectedValue.toString(), "number of products between 30 and 120 prices must be 4");
     });
   });
