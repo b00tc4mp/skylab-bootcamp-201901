fa7d98c448fd6afd80e10088b41480cf014637a8
Scostta <sergio.cs04@gmail.com>
2019-01-17 13:42:15 +0100

    update

--- staff/sergio-costa/e-commerce-protos/scripts/cart.js
+++ staff/sergio-costa/e-commerce-protos/scripts/cart.js
@@ -2,83 +2,59 @@ function Cart(){
 
   this.products=[];
 
-  this.add = function(items){
-    this.products.push(items);
-  };
+}
 
-  // this.totalPrice = function(){
-  //   acc=0;
-  //   for(var i = 0; i<this.products.length; i++){
-  //     acc += this.products[i].price;
-  //   }
-  //   return acc;
-  // }
 
-  this.totalPrice = function(){
-    return this.products.reduce(function(a, item){
-      return a + item.price;
-    },0);
-  }
+Cart.prototype.add = function(items){
+  this.products.push(items);
+};
 
-  this.numberOfItems = function(){
-    return this.products.length;
-  }
+Cart.prototype.totalPrice = function(){
+  return this.products.reduce(function(accum, item){
+    return accum + item.price;
+  },0);
+}
 
-  // this.mostExpensive = function(){
-  //   var expensiveItem = this.products[0];
-  //   for(var i = 0; i<this.products.length; i++){
-  //       if(this.products[i].price > expensiveItem.price){
-  //           expensiveItem = this.products[i];
-  //       }
-  //   }
-  //   return expensiveItem;
-  // }
+Cart.prototype.mostExpensive = function(){
+  return this.products.reduce(function(accum, item){
+    // if(item.price>a.price){
+    //   return item;
+    // } 
+    // return a;
+    return item.price > accum.price ? item : accum;
+  });
+}
 
-  this.mostExpensive = function(){
-    return this.products.reduce(function(a,item){
-      if(item.price>a.price){
-        return item;
-      } 
-      return a;
-    });
-  }
+Cart.prototype.numberOfItems = function(){
+  return this.products.length;
+}
 
-  // this.cheapest = function(){
-  //   var cheapestItem = this.products[0];
-  //   for(var i = 0; i<this.products.length; i++){
-  //       if(this.products[i].price < cheapestItem.price){
-  //           cheapestItem = this.products[i];
-  //       }
-  //   }
-  //   return cheapestItem;
-  // }
+Cart.prototype.cheapest = function(){
+  return this.products.reduce(function(accum, item){
+    return item.price < accum.price ? item : accum;
+  });
+}
 
-  this.cheapest = function(){
-    return this.products.reduce(function(a, item){
-      if(a.price>item.price){
-        return item;
-      }
-      return  a;
-    });
-  }
-
-  this.numberOf = function(proto){
-    var sum =0;
-    for(var i = 0; i<this.products.length; i++){
-      if(this.products[i] instanceof proto){
-        sum +=1;
-      }
+Cart.prototype.numberOf = function(proto){
+  var sum =0;
+  for(var i = 0; i<this.products.length; i++){
+    if(this.products[i] instanceof proto){
+      sum +=1;
     }
-    return sum;
   }
+  return sum;
+}
 
-  this.productsByPriceRange = function(min, max){
-    var rangeProducts = [];
-    for(var i = 0; i<this.products.length; i++){
-      if(this.products[i].price>=min && this.products[i].price<=max){
-        rangeProducts.push(this.products[i]);
-      }
-    }
-    return rangeProducts;
-  }
+Cart.prototype.productsByPriceRange = function(min, max){
+  // var rangeProducts = [];
+  // for(var i = 0; i<this.products.length; i++){
+  //   if(this.products[i].price>=min && this.products[i].price<=max){
+  //     rangeProducts.push(this.products[i]);
+  //   }
+  // }
+  // return rangeProducts;
+
+  return this.products.filter(function(item){
+    return item.price>= min && item.price <=max;
+  })
 }
