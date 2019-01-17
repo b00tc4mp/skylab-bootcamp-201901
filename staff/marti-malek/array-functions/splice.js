751c383ee806efadac3f18935a306aaa49aa5ade
martimalek <martimalekdot@hotmail.es>
2019-01-17 12:19:27 +0100

    array function finished

--- staff/marti-malek/array-functions/splice.js
+++ staff/marti-malek/array-functions/splice.js
@@ -1,21 +1,69 @@
-function splice(arr, start) {
-    var res = Object.assign([], arr);
-    var arr2 = [];
-    j = 0;
-    for (var i = start; i < arr.length; i++) {
-        if (j === arr2.length) {
-            arr2[j] =  arr[i];
-            j++;
+/**
+ * 
+ * @param {Array} arr 
+ * @param {number} start 
+ * @param {number} deleteCount 
+ * @param {*} item 
+ * 
+ * @returns {Array} - If deleted part of arr
+ * 
+ * @throws {Error} - If too few arguments
+ * @throws {TypeError} - If arr is not an array
+ */
+function splice(arr, start, deleteCount, item) {
+
+    if (arguments.length < 1) throw Error ('too few arguments');
+    if (!(arr instanceof Array)) throw TypeError ('arr should be an array'); 
+
+    var initial = [];
+    var res = [];
+    var final = [];
+    var items = [];
+    var def = [];
+
+    if (arguments.length > 3) {
+        for (var i = 3; i < arguments.length; i++) {
+            items[items.length] = arguments[i];
         }
     }
-    res.length = start;
-    return arr2;
 
+    //Taking the pieces to build arr
+
+    for (var i = 0; i < start; i++) {
+        initial[initial.length] = arr[i];
+    };
+    for (var i = start; i < start+deleteCount; i++) {
+        res[res.length] = arr[i];
+    };
+    for (var i = initial.length+res.length; i < arr.length; i++) {
+        final[final.length] = arr[i];
+    };
+
+    //Building def
+
+    for (var i = 0; i < initial.length; i++) {
+        def[def.length] = initial[i];
+    };
+    for (var i = 0; i < items.length; i++) {
+        def[def.length] = items[i];
+    };
+    for (var i = 0; i < final.length; i++) {
+        def[def.length] = final[i];
+    };
+
+    //Copying def into arr
+    arr.length = def.length;
+    for (var i = 0; i < def.length; i++) {
+        arr[i] = def[i];
+    }
+
+    return res;
 };
 
-var a = [1,2,3,4];
+var a = [1,2,3,4,5,6];
 
-splice(a, 2);
+console.log(splice(a,1,2,'hola'));
 
 console.log(a);
+
 
