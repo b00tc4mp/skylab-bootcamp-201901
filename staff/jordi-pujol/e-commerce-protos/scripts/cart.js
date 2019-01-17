e609a25e0ad022dbcc08e8e691a8e20a6469727a
jordi <jordi.pujolg@gmail.com>
2019-01-17 17:43:55 +0100

    test changes 2

--- staff/jordi-pujol/e-commerce-protos/scripts/cart.js
+++ staff/jordi-pujol/e-commerce-protos/scripts/cart.js
@@ -1,7 +1,18 @@
+/**
+ * Abstraction of Cart
+ * 
+ * @method (add)
+ *  @param {Object} object
+ *  @throws {TypeError}
+ *  @returns {Number} Array length
+ * 
+ */
+
 function Cart() {
     this.cartItems = [];
+}
     
-    this.add = function (item){
+    Cart.prototype.add = function (item){
 
         if (!(item instanceof Product)) throw TypeError ('Should be an object');
 
@@ -9,7 +20,7 @@ function Cart() {
         return this.cartItems.length
     };
 
-    this.totalPrice = function(){
+    Cart.prototype.totalPrice = function(){
         var sumP = 0;
         for (let i = 0; i< this.cartItems.length; i++){
            sumP += this.cartItems[i].price
@@ -17,7 +28,7 @@ function Cart() {
         return sumP
     };
 
-    this.numberOfItems = function (){
+    Cart.prototype.numberOfItems = function (){
         sumItems = 0;
         for (let i =0; i< this.cartItems.length; i++){
             sumItems ++
@@ -25,31 +36,36 @@ function Cart() {
         return sumItems
     }
 
-    this.mostExpensive = function() {
+    Cart.prototype.mostExpensive = function() {
         var exp = this.cartItems[0].price;
         var pro = {}
         for (let i=0; i< this.cartItems.length; i++){
             if (exp < this.cartItems[i].price) {
                 exp = this.cartItems[i].price
-                pro = this.cartItems[i]
+                pro = this.cartItems[i].constructor.name
             }
         }
         return pro
     }
 
-    this.cheapest = function (){
+    Cart.prototype.cheapest = function (){
+
         var cheap = this.cartItems[0].price;
-        var pro = this.cartItems[0]
+        var pro = this.cartItems[0].constructor.name
         for (let i=0; i< this.cartItems.length; i++){
             if (cheap > this.cartItems[i].price) {
                 cheap = this.cartItems[i].price
-                pro = this.cartItems[i]
+                pro = this.cartItems[i].constructor.name
             }
         }
         return pro
     }
 
-    this.numberOf = function (item){
+    Cart.prototype.numberOf = function (item){
+
+        if (typeof item !== 'function') throw TypeError(item + ' is not a function');
+        if (item !== Product && !(item.prototype instanceof Product)) throw TypeError(item + ' is not a product type')
+
         sum = 0;
         for (let i = 0; i< this.cartItems.length; i++){
             if (this.cartItems[i].constructor.name === item || this.cartItems[i] instanceof item){
@@ -59,7 +75,7 @@ function Cart() {
         return sum
     }
 
-    this.productsByPriceRange = function(min, max){
+    Cart.prototype.productsByPriceRange = function(min, max){
         var list = []
         for (var i =0; i< this.cartItems.length; i++){
             if (this.cartItems[i].price > min && this.cartItems[i].price < max){
@@ -68,4 +84,3 @@ function Cart() {
         }
         return list
     }
-}
--- staff/jordi-pujol/e-commerce-protos/scripts/cart.test.js
+++ staff/jordi-pujol/e-commerce-protos/scripts/cart.test.js
@@ -1,46 +1,140 @@
-/**
- * Abstraction of Cart
- * 
- * @method (add)
- *  @param {Object} object
- *  @throws {TypeError}
- *  @returns {Number} Array length
- * 
- */
-
 var cart = new Cart;
 
-suite('cart')
 
-// .Add()
+var socks = new Socks('Calvin Klein', 42, 'black', 9.99);
+var tanga = new Tanga('Wicked Weasel', 32, 'red', 29.95);
+var bra = new Bra('Calvin Klein', 'push-up', 80, 'cream', 39); // model
+var slips = new Slips('Abanderado', 42, 'purple', 13.99);
 
-test('adding one element', function(){
-    var socks = new Socks('Calvin Klein', 42, 'black', 9.99);
+cart.add(socks)
+cart.add(tanga)
+cart.add(bra)
+cart.add(slips)
+
+suite('cart', function(){
+
+    // .Add()
+
+    describe ('adding elements', function(){
     
-    var res = cart.add(socks)
+        it ('should add one element', function(){
+            var socks = new Socks('Calvin Klein', 42, 'black', 9.99);
+            
+            var res = cart.add(socks)
 
-    var expected = 1;
+            var expected = 5;
 
-    assert (res === expected,'expected and result should be the same')
-});
+            expect (res === expected,'expected and result should be the same')
+        });
+    })
 
+    describe ('fails adding', function() {
 
-test ('adding an item that is not an instance of product & when no param is passed', function (){
+        it ('should add an item that is not an instance of product & when no param is passed', function (){
+            
+            var error;
+
+            try {
+                cart.add(5)
+            } catch (err){
+                error = err
+            }
+            expect (error,'expected and result should be the same')
+        })
+    });
+
+        // .totalPrice()
+
+    describe('testing cart.totalPrice()', function(){
+
+        it ('should calculate total price ', function(){
+
+            var res = cart.totalPrice()
+            var expected = 102.91999999999999
+
+            expect (res === expected), 'expected and result should be the same'
+        });
+
+    })
+
+    describe('testing numberOfItems()', function(){
+
+        it ('should calculate the total number of items', function(){
+
+            var res = cart.numberOfItems()
+
+            var expected = 5
+
+            expect (res === expected), 'expected and result should be the same'
+        })
+    })
+
+    describe('testing mostExpensive()', function(){
+
+        it ('should return the most expensive product', function(){
+
+            var res = cart.mostExpensive()
+
+            var expected = "Bra"
+
+            expect (res === expected), 'expected and result should be the same'
+        })
+    })
+
+    describe('testing cheapest()', function(){
+
+        it('should return the cheapest product', function(){
+
+            var res = cart.cheapest();
+
+            var expected = "Socks";
+
+            expect (res === expected), 'expected and result should be the same'
+
+        })
+    })
+
+    describe ('testing numberOf(string)', function(){
+
+        it('should return the number of objects of the argument', function(productType){
+            var res = cart.numberOf(Clothing);
+
+            var expected = 5
+
+            expect (res === expected), 'expected and result should be the same'
+        })
     
-    var error;
+        it('should fail when the param is not a function', function(){
 
-    try {
-        cart.add(5)
-    } catch (err){
-        error = err
-    }
-    assert (error,'expected and result should be the same')
+            var error;
+
+            try{
+                var res = cart.numberOf('patata')
+            } catch (err){
+                error = err
+            }
+
+            expect (error, 'there should be an error!')
+        })
+    })
+
+    describe('testing productsByPriceRange()', function(){
+
+        it ('should return the product', function(){
+        
+            var res = cart.productsByPriceRange(10, 20);
+
+            var expected = [slips]
+
+            expect (res[0] === expected[0] , 'result and expected should be the same')
+        })
+    })
 })
 
-// .totalPrice()
-
-test('calculating total price ', function(){
 
 
 
-});
+
+
+
+
