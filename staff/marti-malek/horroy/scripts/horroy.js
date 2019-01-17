09cbf52c1a176c65035d999fd287d12a8e9cf28f
martimalek <martimalekdot@hotmail.es>
2019-01-17 20:21:36 +0100

    finished horroy

--- staff/marti-malek/horroy/scripts/horroy.js
+++ staff/marti-malek/horroy/scripts/horroy.js
@@ -49,7 +49,16 @@ Horroy.prototype.pop = function() {
     this.length = this.length-1;
     return res;
 };
-
+/**
+ * Abstraction of fill.
+ * 
+ * Fills an horroy from one position to other.
+ * 
+ * @param {*} value 
+ * @param {number} start 
+ * @param {number} end 
+ *
+ */
 Horroy.prototype.fill = function(value, start, end) {
 
     start = start === undefined ? 0 : (start < 0 ? this.length + start : start);
@@ -60,9 +69,18 @@ Horroy.prototype.fill = function(value, start, end) {
 
     return this;
 };
-
+/**
+ * 
+ * Abstraction of filter.
+ * 
+ * Returns the elements of an horroy that are validated througha function.
+ * 
+ * @param {Function} func 
+ * 
+ * @returns {thisay}
+ */
 Horroy.prototype.filter = function(func) {
-    var res = [];
+    var res = new Horroy();
     var j = 0;
 
     if (arguments.length > 2) throw Error ('too many arguments');
@@ -75,7 +93,15 @@ Horroy.prototype.filter = function(func) {
     }
     return res;
 };
-
+/**
+ * Abstraction of find.
+ * 
+ * Finds an element in an horroy satisfying an expression.
+ * 
+ * @param {Function} callback - The expression to evaluate.
+ * 
+ * @returns {*} - An item if found, otherwise undefined.
+ */
 Horroy.prototype.find = function(callback) {
 
     for (var i = 0; i < this.length; i++) {
@@ -84,8 +110,257 @@ Horroy.prototype.find = function(callback) {
         if(callback(value)) return value;
     }
 };
+/**
+ * 
+ * Abstraction of indexOf.
+ * 
+ * Returns the first position in the horroy where the searched item is found.
+ * 
+ * @param {*} value 
+ * @param {Number} start
+ * 
+ * @returns {Number}
+ * 
+ */
+Horroy.prototype.indexOf = function(value, start) {
 
-Horroy.prototype.forEach = function(callback) {
-
-    for (var i = 0; i < this.length; i++) callback(this[i]);
+    start = start? start : 0;
+    for (var i = start; i < this.length; i++) {
+        if (this[i] === value) {
+            return i;
+        }
+    }
+    return -1;
 };
+/**
+ * Abstraction of join.
+ * 
+ * Joins the elements of an horroy with the desired param.
+ * 
+ * @param {*} elem
+ * 
+ */
+Horroy.prototype.join = function(elem) {
+    var res = '';
+
+    elem = elem === undefined ? ',' : elem;
+
+    for (var i = 0; i < this.length; i++) {
+        res += this[i];
+        if (i == this.length-1) return res;
+        res += elem;
+    }
+    return res;
+};
+/**
+ * Abstraction of map.
+ * 
+ * Iterates an horroy evaluating an expression on each of its values. 
+ * The result is located and returned in a new thisay.
+ * 
+ * @param {Function} func - The expression to evaluate.
+ * 
+ * @returns {thisay} - A new thisay with the resulting values.
+ * 
+ */
+Horroy.prototype.map = function(func) {
+    var res = new Horroy();
+
+    for (var i = 0; i < this.length; i++) res[i] = func(this[i]);
+
+    return res;
+};
+/**
+ * Reduces an array into a value.
+ * 
+ * @param {Function} callback - The expression to evaluate.
+ * @param {*} accumulator - The accumulator of the reduction value.
+ * 
+ * @returns {*} - The reduction value.
+ */
+Horroy.prototype.reduce = function(callback, accumulator) {
+
+    var i = 0;
+
+    if (accumulator === undefined) {
+        accumulator = this[0];
+
+        i = 1;
+    }
+
+    for (; i < this.length; i++) {
+        var item = this[i];
+
+        accumulator = callback(accumulator, item);
+    }
+
+    return accumulator;
+};
+/**
+ * 
+ * Abstraction of reverse.
+ * 
+ * Reverses the elements of an thisay (modifying it!).
+ * 
+ * @returns {thisay}
+ * 
+ */
+Horroy.prototype.reverse = function() {
+
+    var res = Object.assign([], this);;
+    for (var i = this.length; i > 0; i--) {
+        this[i-1] = res[this.length-i];    
+    }
+    return this;
+};
+
+/**
+ * 
+ * Abstraction of shift.
+ * 
+ * Deletes the first element of an horroy and returns it (modifying the horroy).
+ * 
+ * @returns {*}
+ * 
+ */
+Horroy.prototype.shift = function() {
+
+    var res = this[0];
+    var this2 = Object.assign(this);
+    for (var i = 0; i < this.length; i++) {
+        this2[i] = this2[i+1];
+    };
+    this.length = this.length-1;
+    return res;
+};
+/**
+ * 
+ * Abstraction of slice.
+ * 
+ * Returns a copy of a portion of an horroy.
+ * 
+ * @param {number} start 
+ * @param {number} end 
+ * 
+ * @returns {thisay}
+ * 
+ */
+Horroy.prototype.slice = function(start, end) {
+
+    var res = new Horroy();
+    var j = 0;
+    for (var i = start; i < end; i++) {
+        if (j === res.length) {
+            res[j] =  this[i];
+            j++;
+        }
+    }
+    return res;
+};
+/**
+ * 
+ * Abstraction of some.
+ * 
+ * Evaluates if any element in an horroy fullfills a condition.
+ * 
+ * @param {function} func 
+ * 
+ * @returns {boolean}
+ * 
+ */
+Horroy.prototype.some = function(func) {
+
+    for (var i = 0; i < this.length; i++) {
+        if (func(this[i])) {
+            return true;
+        }
+    }
+    return false;
+};
+/**
+ * Abstraction of splice.
+ * 
+ * Does an alteration on an horroy (deleting, slicing, ....)
+ * @param {number} start 
+ * @param {number} deleteCount 
+ * @param {*} item 
+ * 
+ * @returns {thisay} - If deleted part of thisay
+ * 
+ */
+Horroy.prototype.splice = function(start, deleteCount, item) {
+
+    var initial = new Horroy();
+    var res = new Horroy();
+    var final = new Horroy();
+    var items = new Horroy();
+    var def = new Horroy();
+
+    if (arguments.length > 3) {
+        for (var i = 3; i < arguments.length; i++) {
+            items[items.length] = arguments[i];
+        }
+    }
+
+    //Taking the pieces to build this
+
+    for (var i = 0; i < start; i++) {
+        initial[initial.length] = this[i];
+    };
+    for (var i = start; i < start+deleteCount; i++) {
+        res[res.length] = this[i];
+    };
+    for (var i = initial.length+res.length; i < this.length; i++) {
+        final[final.length] = this[i];
+    };
+
+    //Building def
+
+    for (var i = 0; i < initial.length; i++) {
+        def[def.length] = initial[i];
+    };
+    for (var i = 0; i < items.length; i++) {
+        def[def.length] = items[i];
+    };
+    for (var i = 0; i < final.length; i++) {
+        def[def.length] = final[i];
+    };
+
+    //Copying def into this
+    this.length = def.length;
+    for (var i = 0; i < def.length; i++) {
+        this[i] = def[i];
+    }
+
+    return res;
+};
+/**
+ * Abstraction of unshift.
+ * 
+ * Puts the desired elements at the beginning of the array.
+ * 
+ * @returns {number} - The length of the array
+ * 
+ */
+Horroy.prototype.unshift = function (element) {
+    
+    var copy = new Horroy();
+    
+
+    for (var i = 1; i < arguments.length; i++) {
+        copy[copy.length] = arguments[i];
+        copy.length++;
+    };
+    for (var i = 0; i < this.length; i++) {
+        copy[copy.length] = this[i];
+        copy.length++;
+    };
+
+    this.length = copy.length;
+
+    for(var i = 0; i < copy.length; i++) {
+        this[i] = copy[i];
+    };   
+
+    return this.length;
+};  
