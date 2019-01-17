ef8d238cec7692058e851dbcac2dc71420eb0277
martimalek <martimalekdot@hotmail.es>
2019-01-17 10:27:05 +0100

    unshift finished

--- staff/marti-malek/array-functions/unshift.js
+++ staff/marti-malek/array-functions/unshift.js
@@ -1,28 +1,39 @@
-function unshift(arr, elem) {
+/**
+ * Abstraction of unshift.
+ * 
+ * Puts the desired elements at the beginning of the array.
+ * 
+ * @returns {number} - The length of the array
+ * 
+ * @throws {TypeError} - If the first argument is not an array
+ * 
+ */
+function unshift() {
 
-    var array = [];
-
+    if (!(arguments[0] instanceof Array)) throw TypeError (arguments[0] + 'should be an array');
+    
+    var arr = arguments[0];
     var res = Object.assign([], arr);
+    var elem = [];
+    var j = 0;
+    var m = 0;
 
-    if (arguments.length > 2) {
-        array[0] = elem;
-        for (var i = 1; i < arguments.length-1; i++) {
-            if (i == arr.length-1) {
-                array[i] = elem;
-            }
+    for (var i = 1; i < arguments.length; i++) {
+        elem[j] = arguments[i];
+        j++;
+    };
+    for (var k = 0; k < res.length+elem.length; k++) {
+        if (k < elem.length) {
+            arr[k] = elem[k];
+            arr.length = elem.length;
+        } else {
+            arr[k] = res[m];
+            m++;
         }
     }
+    return arr.length;
+};
 
-    /*With 1 elem argument*/
-    for (i = 0; i < arguments.length-1; i++) {
-        for (j = 0; j < res.length; j++) {
-            arr[j+1] = res[j];
-        }
-        arr[i] = elem;
-    }
-    return arr;
-}
+var a = [1,2,3,4];
 
-var a = [1,2,3,4,5];
-
-unshift(a,0,1,3,6,8,3);
+unshift(a,0,1,2,3);
