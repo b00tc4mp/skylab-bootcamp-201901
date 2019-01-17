38feab7d22512351b73ff14bc941a900ff52d931
Waldo Geraldo Faldo <gcelma@MacBook-Air-de-Waldo.local>
2019-01-17 20:18:05 +0100

    added shift

---
+++ staff/gerard-celma/array-functions/reverse.test.js
@@ -0,0 +1,24 @@
+suite('TEST reverse');
+
+test('array as parameter, all ok', function() {
+    var arr = [1,2,3]
+
+    var res = reverse(arr);
+
+    var expected = [3,2,1];
+
+    if(res.toString() !== expected.toString()) throw Error ("Expected result '" + expected + "' not matched");
+});
+
+test("parameter not array", function () {
+    var error;
+
+    try {
+        reverse(1);
+    } catch (err) {
+        error = err;
+    }
+
+    if(!error) throw Error ('should have thrown an error');
+    if(!(error instanceof TypeError)) throw Error ('should have thrown TypeError');
+});
