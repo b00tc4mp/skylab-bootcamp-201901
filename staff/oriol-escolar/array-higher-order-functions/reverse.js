c46100153d328d9cd84e9a86df13a0eb7836b342
Ury18 <ury_98@hotmail.com>
2019-01-17 12:21:10 +0100

    update

--- staff/oriol-escolar/array-higher-order-functions/reverse.js
+++ staff/oriol-escolar/array-higher-order-functions/reverse.js
@@ -1,23 +1,35 @@
 
 
 
-function fill(arr,func) {
+function reverse(array) {
 
-    var res =[];
+    var res = [];
     
-    for (var i = 0; i < arr.length; i++) 
     
-    res[i] = func(arr[i]);
-    return res;
+    for (var i = 0; i < array.length; i++){
+
+        
+        res[i] = array[array.length -i-1];
+
+
+    } 
+
+    for(var i=0; i<array.length;i++)
+    {
+
+        array[i]=res[i];
+
+
+    }
     
+    
+    
+    //return array;
 }
 
 
-var cars = ['ferrari', 'porsche', 'mercedes'];
+var cars = ['ferrari', 'porsche', 'mercedes','patata'];
 
-console.log(cars);
+reverse(cars);
 
-cars = fill(cars,function(v){ return v='ferrari'  });
-
-console.log(cars);
 
