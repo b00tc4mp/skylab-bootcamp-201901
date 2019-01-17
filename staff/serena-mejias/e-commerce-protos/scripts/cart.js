3819c84d84b2a9540ca17efe46e9f6420f559ebb
Serena <serena@example.com>
2019-01-17 19:27:48 +0100

    Changes test

--- staff/serena-mejias/e-commerce-protos/scripts/cart.js
+++ staff/serena-mejias/e-commerce-protos/scripts/cart.js
@@ -1,39 +1,38 @@
-function Cart(){
-    this.list = [];
+function Cart() {
+  this.list = [];
+}
+
+Cart.prototype.add = function(product) {
+  if (!(product instanceof Product))
+    throw TypeError(product + " is not a product");
+  this.list.push(product);
 };
 
-Cart.prototype.add = function(prod){
-    this.list.push(prod);
+Cart.prototype.totalPrice = function() {
+  return this.list.reduce(function(previousValue, currentValue, index, array) {
+    return previousValue + currentValue.price;
+  }, 0);
+};
+Cart.prototype.numberOfItems = function() {
+  return this.list.length;
 };
 
-Cart.prototype.totalPrice = function(){
-    
-    return this.list.reduce(function(previousValue, currentValue, index, array){
-        return previousValue + currentValue.price;
-    },0);
-};
-Cart.prototype.numberOfItems = function(){
-    return this.list.length;
-
-};
-
-Cart.prototype.mostExpensive = function(){
-    var max = 0;
+Cart.prototype.mostExpensive = function() {
+  /*var max = 0;
     for(var i=0; i<cart.list.length; i++){
         if(cart.list[i].price>max){
             max = cart.list[i].price;
             var value = cart.list[i];
         }
-    }
-   return value;
-};
-
-    /*return this.products.reduce(function(accum,product){
-        return accum <product.price? product:accum;
     }*/
 
-Cart.prototype.cheapest = function(){
-    var min=cart.list[0].price;
+  return this.list.reduce(function(accum, product) {
+    return accum.price < product.price ? product : accum;
+  });
+};
+
+Cart.prototype.cheapest = function() {
+  /*var min=cart.list[0].price;
     for(var i=1; i<cart.list.length; i++){
         if(cart.list[i].price<min){
             min = cart.list[i].price;
@@ -42,19 +41,35 @@ Cart.prototype.cheapest = function(){
             value = min;
         }
     }
-   return value;
+   return value;*/
+  return this.list.reduce(function(accum, product) {
+    return accum.price > product.price ? product : accum;
+  });
 };
 
-Cart.prototype.numberOf = function(item){
-    if (typeof item !== 'function') throw TypeError (item + 'is not a function')
-    if (item !== Product && !(type.prototype instanceof Product)) throw TypeError (item + 'is not a function')
-    var result = (cart.list).filter(function(listItem){
-        
-        return listItem instanceof item;  
-    })
-    return result.length
+Cart.prototype.numberOf = function(item) {
+  /*if (typeof item !== "function") throw TypeError(item + "is not a function");
+  if (item !== Product && !(item.prototype instanceof Product))
+    throw TypeError(item + "is not a function");
+
+  var result = cart.list.filter(function(listItem) {
+    return listItem instanceof item;
+  });
+  return result.length;*/
+  if (typeof item !== "function") throw TypeError(item + " is not a function");
+  if (item !== Product && !(item.prototype instanceof Product))
+    throw TypeError(item + " is not a product type");
+
+  return this.list.reduce(function(accum, product) {
+    return product instanceof item ? ++accum : accum;
+  }, 0);
 };
 
-Cart.prototype.numberOf = function(min,max){
-    var result = 
-}
+Cart.prototype.productsByPriceRange = function(min, max) {
+  if (typeof min !== "number") throw TypeError(min + " is not a number");
+  if (typeof max !== "number") throw TypeError(max + " is not a number");
+
+  return this.list.filter(function(product) {
+    return product.price >= min && product.price <= max;
+  });
+};
