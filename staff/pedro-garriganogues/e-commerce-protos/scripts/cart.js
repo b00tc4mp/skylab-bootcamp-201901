a921e7380757c940b9fe593bc7bd8db9c2390fc4
Pedro <pe->
2019-01-17 15:08:01 +0100

    finished task

--- staff/pedro-garriganogues/e-commerce-protos/scripts/cart.js
+++ staff/pedro-garriganogues/e-commerce-protos/scripts/cart.js
@@ -27,25 +27,31 @@ Cart.prototype.numberOfItems = function(){
 
 // console.log('most expensive', cart.mostExpensive());
 
-Cart.prototype.totalPrice = function(){
-
-    return this.products.reduce(function(acumm, product){        
-        return accum.price < product.price? product : acumm;
+Cart.prototype.mostExpensive = function () {
+    return this.products.reduce(function (accum, product) {
+        return accum.price < product.price ? product : accum;
     });
 };
 
 // console.log('cheapest', cart.cheapest());
 
-Cart.prototype.cart.cheapest = function(){
-
-    return this.products.reduce(function(acumm, product){        
-        return aproduct instanceof type? ++accum : accum;
+Cart.prototype.cheapest = function () {
+    return this.products.reduce(function (accum, product) {
+        return accum.price > product.price ? product : accum;
     });
 };
 
 // console.log('number of clothing items', cart.numberOf(Clothing));
 
 
+Cart.prototype.numberOf = function (type) {
+    if (typeof type !== 'function') throw TypeError(type + ' is not a function');
+    if (type !== Product && !(type.prototype instanceof Product)) throw TypeError(type + ' is not a product type');
+
+    return this.products.reduce(function (accum, product) {
+        return product instanceof type ? ++accum : accum;
+    }, 0);
+};
 
 // console.log('number of electronics items', cart.numberOf(Electronics));
 
@@ -53,13 +59,11 @@ Cart.prototype.cart.cheapest = function(){
 
 // console.log('products in between prices', cart.productsByPriceRange(30, 120));
 
-Cart.prototype.productsByPriceRange = function(min, max){
-    if (typeof min !== 'number') throw TypeError (min + ' is not a number');
-    if (typeof max !== 'number') throw TypeError (max + ' is not a number');
+Cart.prototype.productsByPriceRange = function(min, max) {
+    if (typeof min !== 'number') throw TypeError(min + ' is not a number');
+    if (typeof max !== 'number') throw TypeError(max + ' is not a number');
 
-    return this.products
-
-
-
-    
-}
+    return this.products.filter(function(product) {
+        return product.price >= min && product.price <= max;
+    })
+};
