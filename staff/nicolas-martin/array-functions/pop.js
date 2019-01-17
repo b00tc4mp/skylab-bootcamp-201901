8eb78ba466655fb818c1142d032b2cdd38ff318f
nico <nico@somoslaclave.com>
2019-01-17 22:41:59 +0100

    pop push and minor changes

---
+++ staff/nicolas-martin/array-functions/pop.js
@@ -0,0 +1,15 @@
+/**
+ * Removes the last element from an array and returns that element
+ *  
+ * @param {Array} array 
+ * @param {*} element 
+ * 
+ * @returns {number} - the new length of the array or undefined if the array is empty
+ */
+
+function pop(array) {
+    if (!(array instanceof Array)) throw TypeError(array + ' is not an array');  
+    var lastElement = array[array.length-1];
+    array.length = array.length - 1;
+    return lastElement;
+}
--- staff/nicolas-martin/array-functions/push.js
+++ staff/nicolas-martin/array-functions/push.js
@@ -1,4 +1,5 @@
 /**
+ * Add one or more elements at the end of the array
  * 
  * @param {Array} array 
  * @param {*} element 
@@ -6,8 +7,11 @@
  * @returns {number} - the new length of the array
  */
 
-function push(array, element) {
-    assert(array instanceof Array, array + ' is not an array');
-    array[array.length] = element;
+function push() {
+    var array = arguments[0];
+    if (!(array instanceof Array)) throw TypeError(array + ' is not an array');
+    for (var i = 1; i < arguments.length; i++) {
+        array[array.length] = arguments[i];   
+    }
     return array.length;
 }
