96fcb1242d5df2be83163f990e563a4dfe16f5c1
martimalek <martimalekdot@hotmail.es>
2019-01-17 12:19:27 +0100

    array function finished

--- staff/marti-malek/array-functions/reduce.js
+++ staff/marti-malek/array-functions/reduce.js
@@ -1,15 +1,29 @@
-function reduce(arr, func) {
-    var res;
-    for (var i = 0; i < arr.length; i++) {
-        res += func(arr);
+/**
+ * Reduces an array into a value.
+ * 
+ * @param {Array} array - The array to operate on.
+ * @param {Function} callback - The expression to evaluate.
+ * @param {*} accumulator - The accumulator of the reduction value.
+ * 
+ * @returns {*} - The reduction value.
+ */
+function reduce(array, callback, accumulator) {
+    if (!(array instanceof Array)) throw TypeError(array + ' is not an array');
+    if (!(callback instanceof Function)) throw TypeError(callback + ' is not an function');
+
+    var i = 0;
+
+    if (accumulator === undefined) {
+        accumulator = array[0];
+
+        i = 1;
     }
-    return res;
+
+    for (; i < array.length; i++) {
+        var item = array[i];
+
+        accumulator = callback(accumulator, item);
+    }
+
+    return accumulator;
 }
-
-var reducer = function (accumulator, currentValue) {
-    return accumulator + currentValue;
-} 
-
-var a = [1,2,3,4];
-
-console.log(reduce(a,reducer));
