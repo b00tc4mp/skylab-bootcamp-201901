f3cffdaa3cc9ceda4aa7dd09c8a5b326f671e295
nico <nico@somoslaclave.com>
2019-01-17 18:03:59 +0100

    new test-machine-2.0.0

--- staff/nicolas-martin/e-commerce-protos/scripts/cart.test.js
+++ staff/nicolas-martin/e-commerce-protos/scripts/cart.test.js
@@ -1,7 +1,5 @@
-suite("cart");
-
 function createSetOfProducts() {
-  var cart = new Cart();
+  var cart = new Cart;
 
   var tshirt = new TShirt(120, "Supreme", "white", "M");
   cart.add(tshirt);
@@ -19,83 +17,121 @@ function createSetOfProducts() {
   return cart;
 }
 
-// use case 1
-test("cart should add 1 product expected", function() {
-  var cart = new Cart();
-  var tshirt = new TShirt(399, "Supreme", "white", "M");
-  cart.add(tshirt);
+suite("cart", function(){
 
-  assert(cart.products.length === 1, "products should be 1 length");
-  assert(
-    cart.products[0] === tshirt,
-    "expected product " + tshirt + " was not added to cart products"
-  );
-});
-
-// use case 2
-test("succeed on get the total price of the cart", function() {
-  var cart = createSetOfProducts();
-  var totalPrice = cart.totalPrice();
-  var expectedValue = 232.21;
-  assert(
-          expectedValue === totalPrice,
-          "total price " + totalPrice + "is not the expected value " + expectedValue
-  );
-});
-
-// use case 3
-test("succeed on get the number of products in cart", function() {
-  var cart = createSetOfProducts(); 
-  var result = cart.numberOfItems();
-  var expectedNumberOfItems = 6;
-
-  assert((typeof expectedNumberOfItems === "number"), 'The result: ' + expectedNumberOfItems + ' is not from type of number');
-  assert(result === expectedNumberOfItems, 'The number of products in the cart should be ' + expectedNumberOfItems + ' and we get ' + result);
-});
-
-// use case 4
-test("succeed on get the product most expensive", function() {
-  var cart = createSetOfProducts(); 
-  var mostExpensiveItem = cart.mostExpensive(); // Laptop {screenSize: 15, model: "MacBook Pro", price: 1599, brand: "Apple"}
-  var mostExpensiveItemExpected = {screenSize: 15, model: "MacBook Pro", price: 1599, brand: "Apple"};
-
-  assert((mostExpensiveItem instanceof Object), 'The most expensive product:' + mostExpensiveItem + ' is not type of Object');
-  assert(mostExpensiveItem.toString() === mostExpensiveItemExpected.toString(), 'The product most expensive is not the expected one');
-});
-
-// use case 5
-test("succeed on get the cheapest product", function() {
-  var cart = createSetOfProducts(); 
-  var mostCheapestItem = cart.cheapest(); // Socks {color: "black", size: 42, price: 9.99, brand: "Calvin Klein"}
-  var mostCheapestItemExpected = {color: "black", size: 42, price: 9.99, brand: "Calvin Klein"};
-
-  assert((mostCheapestItem instanceof Object), 'The cheapest product:' + mostCheapestItem + ' is not type of Object');
-  assert(mostCheapestItem.toString() === mostCheapestItemExpected.toString(), 'The cheapest product is not the expected one');
-});
-
-// use case 6
-test("succeed on get the Nº of products from a category", function() {
-  var cart = createSetOfProducts();
-  var category = Electronics;
-  var productsResult = cart.numberOf(category);
-
-  var expected = 4; 
-
-  assert(productsResult === expected, 'the result: ' + productsResult.length + ' should be ' + expected);
-
-});
-
-// use case 7
-test("succeed on get the products by price range", function() {
-    var cart = createSetOfProducts();
-    var min = 5;
-    var max = 10;
-
-    var productsByPriceRange = cart.productsByPriceRange(min, max);
-
-    var expected = [{screenSize: 20, model: "1800", price: 9.99, brand: "HP"}];
-
+    describe('*** manage products from Cart ***', function(){
+      it("should add 1 product", function() {
+        var cart = new Cart();
+        var tshirt = new TShirt(399, "Supreme", "white", "M");
+        cart.add(tshirt);
+      
+        expect(cart.products.length === 1, "products should be 1 length");
+        expect(
+          cart.products[0] === tshirt,
+          "expected product " + tshirt + " was not added to cart products"
+        );
+      });
     
-    assert(productsByPriceRange.toString() === expected.toString(), 'result value: ' + productsByPriceRange + ' is different from expected: ' + expected);
+    // use case 2
+    it("should get the total price of the cart", function() {
+      var cart = createSetOfProducts();
+      var totalPrice = cart.totalPrice();
+      var expectedValue = 232.21;
+      expect(
+              expectedValue === totalPrice,
+              "total price " + totalPrice + "is not the expected value " + expectedValue
+      );
+    });
+    
+    // use case 3
+    it("should get the number of products in cart", function() {
+      var cart = createSetOfProducts(); 
+      var result = cart.numberOfItems();
+      var expectedNumberOfItems = 6;
+    
+      expect((typeof expectedNumberOfItems === "number"), 'The result: ' + expectedNumberOfItems + ' is not from type of number');
+      expect(result === expectedNumberOfItems, 'The number of products in the cart should be ' + expectedNumberOfItems + ' and we get ' + result);
+    });
+    
+    // use case 4
+    it("should get the product most expensive", function() {
+      var cart = createSetOfProducts(); 
+      var mostExpensiveItem = cart.mostExpensive(); // Laptop {screenSize: 15, model: "MacBook Pro", price: 1599, brand: "Apple"}
+      var mostExpensiveItemExpected = {screenSize: 15, model: "MacBook Pro", price: 1599, brand: "Apple"};
+    
+      expect((mostExpensiveItem instanceof Object), 'The most expensive product:' + mostExpensiveItem + ' is not type of Object');
+      expect(mostExpensiveItem.toString() === mostExpensiveItemExpected.toString(), 'The product most expensive is not the expected one');
+    });
+    
+    // use case 5
+    it("should get the cheapest product", function() {
+      var cart = createSetOfProducts(); 
+      var mostCheapestItem = cart.cheapest(); // Socks {color: "black", size: 42, price: 9.99, brand: "Calvin Klein"}
+      var mostCheapestItemExpected = {color: "black", size: 42, price: 9.99, brand: "Calvin Klein"};
+    
+      expect((mostCheapestItem instanceof Object), 'The cheapest product:' + mostCheapestItem + ' is not type of Object');
+      expect(mostCheapestItem.toString() === mostCheapestItemExpected.toString(), 'The cheapest product is not the expected one');
+    });
+    
+    // use case 6
+    it("should get the Nº of products from a category", function() {
+      var cart = createSetOfProducts();
+      var category = Electronics;
+      var productsResult = cart.numberOf(category);
+    
+      var expected = 4; 
+    
+      expect(productsResult === expected, 'the result: ' + productsResult.length + ' should be ' + expected);
+    });
+    
+    // use case 7
+    it("should get the products by price range", function() {
+        var cart = createSetOfProducts();
+        var min = 5;
+        var max = 10;
+    
+        var productsByPriceRange = cart.productsByPriceRange(min, max);
+    
+        var expected = [{screenSize: 20, model: "1800", price: 9.99, brand: "HP"}];
+    
+        
+        expect(productsByPriceRange.toString() === expected.toString(), 'result value: ' + productsByPriceRange + ' is different from expected: ' + expected);
+    });
+  });
 
+  describe('*** input data validation ***', function(){
+    it("should thrown an error when search products by range with not a number in productsByPriceRange", function() {
+      var cart = createSetOfProducts();
+      var min = '';
+      var max = 10;
+      var error;
+
+      try {
+        var productsByPriceRange = cart.productsByPriceRange(min, max);
+      } catch (err) {
+        error = err;
+      }
+
+      expect(error, 'should thrown an error'); 
+    });
+
+
+
+    it("should thrown an error when using not Product object in productsByPriceRange", function() {
+      var cart = new Cart;
+      var searchObject = Product;
+      var error;
+
+      try {
+        var productsInCategory = cart.numberOf(searchObject);
+      } catch (err) {
+        error = err;
+      }
+      expect(error, 'should thrown an error'); 
+      expect(error instanceof TypeError, 'should thrown a TypeError');
+    });
+  });
+
+  
+   
 });
--- staff/nicolas-martin/e-commerce-protos/scripts/hat.js
+++ staff/nicolas-martin/e-commerce-protos/scripts/hat.js
@@ -1,6 +1,8 @@
-function Hat(price, brand, color, size, style) {
+function Hat(price, brand, color, style) {
   this.style = style;
-  Clothing.call(this, price, brand, color, size);
+  // Clothing.call(this, price, brand, color, undefined);
+  // also we can put this like this:
+  Clothing.call(this, price, brand, color, undefined);
 }
 
 Hat.prototype = Object.create(Clothing.prototype);
