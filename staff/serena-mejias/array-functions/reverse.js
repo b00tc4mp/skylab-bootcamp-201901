90da4659434fa76dfde527697847ae6e093323fb
Serena <serena@example.com>
2019-01-18 12:30:53 +0100

    Add reverse and push

--- staff/serena-mejias/array-functions/reverse.js
+++ staff/serena-mejias/array-functions/reverse.js
@@ -1,9 +1,13 @@
-var array = ['one', 'two', 'three'];
+//var array = ["one", "two", "three","four","five"];
+var array = [{a:1,b:2},{a:2,b:3},{a:4,b:5}]
 
-function reverse(array){
-    for(var i=array.length; i<array.length; i--){
-        arrar += array[i];
-    }
-    return array;
+function reverse(array) {
+  for (var i = 0; i < Math.floor(array.length/2); i++) {
+    var n = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = n;
+    
+  }
+  return array;
 }
 reverse(array);
--- staff/serena-mejias/array-functions/reverse.test.js
+++ staff/serena-mejias/array-functions/reverse.test.js
@@ -8,3 +8,23 @@ test("change the order of an array", function() {
 
   if(reversed !== expected) return 'reverse should be the same as expected.' 
 });
+
+test("change the order of an array of objects", function() {
+  var array = [{a:1,b:2},{a:2,b:3},{a:4,b:5}];
+
+  var reversed = reverse(array);
+  var expected = [{a:4,b:5},{a:2,b:3},{a:1,b:2}] ;
+
+  if(reversed !== expected) return 'reverse should be the same as expected.' 
+});
+
+test("the argument is not an array", function(){
+  var error;
+  try {
+    reverse('serena');
+  } catch (err){
+    error=err;
+  }
+  if(error) "it should be an error"; 
+})
+
