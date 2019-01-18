54a7a8b7f95b23f3d4c5274e5fc7e1aa61d6d6ff
Àlex Barba <alexander.barba@gmail.com>
2019-01-18 09:48:03 +0100

    Revert "v001"
    This reverts commit 1f28e6355f5d74bf118060a8be21d8d9c7d69090.

--- staff/alex-barba/horroy/horroy.test.js
+++ staff/alex-barba/horroy/horroy.test.js
@@ -1,32 +1,4 @@
 suite('horroy', function(){
-    describe('Horroy function', function(){
-        it('creates horroy', function () {
-            var res = new Horroy(1,2,3,4,5);
-        
-            var expected = [1,2,3,4,5];
-        
-            expect(res.toString() === expected.toString(), 'horroy and result should be the same');
-
-        });
-
-        it('creates horroy length', function () {
-            var res = new Horroy(1,2,3,4,5);
-
-            var expected = 5;
-        
-            expect(res.length === expected, 'horroy and result should be the same');
-        });
-
-        it('creates empty horroy with no length if no arguments', function () {
-            var res = new Horroy();
-
-            var expected = [];
-            
-            expect(res.toString() === expected.toString(), 'horroy and result should be the same');
-            expect(res.length === expected.length, 'horroy and result should be the same');
-        });
-    });
-
     describe('.fill function', function(){
         it('all arguments', function () {
             var hor = new Horroy(1,2,3,4,5)
@@ -34,39 +6,6 @@ suite('horroy', function(){
             var res = hor.fill(0, 0, 2);
         
             var expected = [0,0,3,4,5];
-        
-            expect(res.toString() === expected.toString(), 'horroy and result should be the same');
-
-        });
-
-        it('2 arguments', function () {
-            var hor = new Horroy(1,2,3,4,5)
-        
-            var res = hor.fill(0, 0);
-        
-            var expected = [0,0,0,0,0];
-        
-            expect(res.toString() === expected.toString(), 'horroy and result should be the same');
-
-        });
-
-        it('1 argument', function () {
-            var hor = new Horroy(1,2,3,4,5)
-        
-            var res = hor.fill(0);
-        
-            var expected = [0,0,0,0,0];
-        
-            expect(res.toString() === expected.toString(), 'horroy and result should be the same');
-
-        });
-
-        it('start and end have with negative values', function () {
-            var hor = new Horroy(1,2,3,4,5)
-        
-            var res = hor.fill(0,-3, -2);
-        
-            var expected = [1,2,0,4,5];
         
             expect(res.toString() === expected.toString(), 'horroy and result should be the same');
 
@@ -86,34 +25,6 @@ suite('horroy', function(){
             var expected = [12, 130, 44];
         
             expect(res.toString() === expected.toString(), 'horroy and result should be the same');           
-        });
-
-        it('all arguments with no result', function () {
-            var hor = new Horroy(12, 5, 8, 130, 44);
-        
-            function isBigEnough(value) {
-            return value > 150;
-            };
-        
-            var res = hor.filter(isBigEnough);
-        
-            var expected = [];
-        
-            expect(res.toString() === expected.toString(), 'horroy and result should be the same');           
-        });
-
-        it('does not mutate horroy called', function () {
-            var hor = new Horroy(12, 5, 8, 130, 44);
-        
-            function isBigEnough(value) {
-            return value >= 10;
-            };
-        
-            var res = hor.filter(isBigEnough);
-        
-            var expected = [12, 5, 8, 130, 44];
-        
-            expect(hor.toString() === expected.toString(), 'horroy and result should be the same');           
         });
     });
 
@@ -178,143 +89,13 @@ suite('horroy', function(){
             var hor = new Horroy(1,2,3);
 
             var found = hor.map(function(x) { return x + 10; });
+
+            console.log(found)
+            console.log(found.toString())
         
             var expected = [11, 12, 13];
         
-            expect(found.toString() === expected.toString(), 'found value ' + found + ' does not match expected ' + expected);     
+            expect(found === expected, 'found value ' + found + ' does not match expected ' + expected);     
         });
     });
-
-    describe('.pop function', function(){
-        it('all arguments', function () {
-            var hor = new Horroy(1,2,3,4);
-
-            var found = hor.pop();
-        
-            var expected = 4;
-        
-            expect(found.toString() === expected.toString(), 'found value ' + found + ' does not match expected ' + expected);     
-        });
-    });
-
-    describe('.push function', function(){
-        it('all arguments', function () {
-            var hor = new Horroy(1,2,3);
-
-            var found = hor.push(4);
-        
-            var expected = 4;
-        
-            expect(found.toString() === expected.toString(), 'found value ' + found + ' does not match expected ' + expected);     
-        });
-    });
-
-    describe('.reduce function', function(){
-        it('all arguments', function () {
-            var hor = new Horroy(
-                { product: 'T-Shirt', price: 12 },
-                { product: 'Slips', price: 7 },
-                { product: 'Shorts', price: 22 },
-                { product: 'Sockets', price: 3 }
-            );
-
-            var found = hor.reduce(function (accumulator, product) {
-                return accumulator + product.price;
-            }, 0);
-        
-            var expected = 44;
-        
-            expect(found.toString() === expected.toString(), 'found value ' + found + ' does not match expected ' + expected);     
-        });
-    });
-
-    describe('.reverse function', function(){
-        it('all arguments', function () {
-            var hor = new Horroy(1,2,3);
-
-            var found = hor.reverse();
-        
-            var expected = [3,2,1];
-        
-            expect(found.toString() === expected.toString(), 'found value ' + found + ' does not match expected ' + expected);     
-        });
-    });
-
-    describe('.shift function', function(){
-        it('all arguments', function () {
-            var hor = new Horroy(1,2,3,4);
-
-            var found = hor.shift(a);
-        
-            var expected = 1;
-        
-            expect(found.toString() === expected.toString(), 'found value ' + found + ' does not match expected ' + expected);     
-        });
-    });
-
-    describe('.slice function', function(){
-        it('all arguments', function () {
-            var hor = new Horroy(5, 12, 8, 130, 44);
-
-            var found = hor.slice(2,4);
-        
-            var expected = [8, 130];
-        
-            expect(found.toString() === expected.toString(), 'found value ' + found + ' does not match expected ' + expected);     
-        });
-    });
-
-    describe('.some function', function(){
-        it('all arguments', function () {
-            var hor = new Horroy(1,3,5);
-
-            var even = function(element) {
-                return element % 2 === 0;
-                };
-            
-            var found = hor.some(even);
-        
-            var expected = false;
-        
-            expect(found.toString() === expected.toString(), 'found value ' + found + ' does not match expected ' + expected);     
-        });
-    });
-
-    describe('.splice function', function(){
-        it('all arguments', function () {
-            var hor = new Horroy(1,2,2,4,5);
-            
-            var found = hor.splice(2, 3, 3, 3, 3);
-        
-            var expected = [2,4,5];
-        
-            expect(found.toString() === expected.toString(), 'found value ' + found + ' does not match expected ' + expected);     
-        });
-    });
-
-    describe('.unshift function', function(){
-        it('all arguments', function () {
-            var hor = new Horroy(1);
-            
-            var found = hor.unshift(2);
-        
-            var expected = 2;
-        
-            expect(found.toString() === expected.toString(), 'found value ' + found + ' does not match expected ' + expected);     
-        });
-    });
-
-    describe('.toString function', function(){
-        it('all arguments', function () {
-            var hor = new Horroy(1,2,3);
-            
-            var found = hor.toString();
-        
-            var expected = '1,2,3';
-        
-            expect(found.toString() === expected.toString(), 'found value ' + found + ' does not match expected ' + expected);     
-        });
-    });
-
-
 });
