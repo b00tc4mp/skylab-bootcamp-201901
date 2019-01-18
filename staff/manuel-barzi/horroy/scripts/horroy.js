707242a51990835363ad96e110df611240e74229
manuelbarzi <manuelbarzi@gmail.com>
2019-01-18 10:35:54 +0100

    update horroy

--- staff/manuel-barzi/horroy/scripts/horroy.js
+++ staff/manuel-barzi/horroy/scripts/horroy.js
@@ -2,16 +2,28 @@ function Horroy() {
     this.length = arguments.length;
 
     if (arguments.length)
-        for(var i = 0; i < arguments.length; i++)
+        for (var i = 0; i < arguments.length; i++)
             // this.push(arguments[i]); // WARN should avoid (if possible) calling member methods in a constructor (push)
             this[i] = arguments[i];
 }
 
-Horroy.prototype.push = function(value) {
+Horroy.from = function (value) {
+    var horr = new Horroy
+
+    horr.length = value.length
+
+    if (typeof value === 'string')
+        for (var i = 0; i < value.length; i++)
+            horr[i] = value[i]
+
+    return horr
+};
+
+Horroy.prototype.push = function (value) {
     this[this.length++] = value;
 };
 
-Horroy.prototype.forEach = function(callback) {
-    for(var i = 0; i < this.length; i++)
+Horroy.prototype.forEach = function (callback) {
+    for (var i = 0; i < this.length; i++)
         callback(this[i]);
 };
