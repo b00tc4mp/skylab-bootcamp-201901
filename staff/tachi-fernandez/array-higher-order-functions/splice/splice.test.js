f2047f5cf1c55c631607a3807acb23387c103607
Tachi <jrfluna8@gmail.com>
2019-01-18 12:18:11 +0100

    splyce

---
+++ staff/tachi-fernandez/array-higher-order-functions/splice/splice.test.js
@@ -0,0 +1,27 @@
+suite("splice" , function(){
+
+    describe("Argurments", function(){
+
+
+        it("three arguments" , function(){
+
+        var array = [1,3,4,5,6]
+
+        splice(array,1,2)
+
+        console.log(array)
+
+        })
+
+
+
+    })
+
+
+
+
+
+
+
+
+});
