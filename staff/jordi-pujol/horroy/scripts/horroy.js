e163dc79309f3edf5b32bc7a2a3bab781c3deeec
jordi <jordi.pujolg@gmail.com>
2019-01-18 17:38:02 +0100

    frist change

---
+++ staff/jordi-pujol/horroy/scripts/horroy.js
@@ -0,0 +1,13 @@
+function Horroy(){
+
+    if ((arguments.length === 1) && (Number.isInteger(arguments)) thro TypeError ('it has to be integer')
+    // if (arguments.length)
+
+    this.length = this.arguments
+
+    if (arguments.length){
+        for (var i =0; i< arguments.length; i++){
+            this[i] = arguments[i]
+        }
+    }
+}
