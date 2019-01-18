3c2d2b7a0ecf4c91ca1f03e9ac162997cc15f4d6
jordi <jordi.pujolg@gmail.com>
2019-01-18 16:21:03 +0100

    splice

---
+++ staff/jordi-pujol/array-functions/splice.test.js
@@ -0,0 +1,11 @@
+suite('splice')
+
+test('should work when we introduce all the parameters', function(){
+
+
+})
+
+test('should work when we do not introduce the deletecount', function(){
+
+    
+})
