aaf687f3be6db948c8074eaffa22865498773107
Tachi <jrfluna8@gmail.com>
2019-01-18 09:33:19 +0100

    changes

---
+++ staff/tachi-fernandez/e-commerce-protos/cart.test.js
@@ -0,0 +1,31 @@
+suite("cart", function () {
+
+    describe("cart text", function () {
+
+        it("If product is not an arguments" , function(){
+
+            var Error;
+
+            try{
+                Cart.prototype.add()
+            }catch{(Error)
+                Error = err
+            }
+                if(!(Error)) Error("should be an Error")               
+                if(!(Error)) Error("should be an Error")   
+        });
+
+
+
+
+
+        
+
+
+
+
+
+    });
+
+
+});
