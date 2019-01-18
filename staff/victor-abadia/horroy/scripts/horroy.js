ef35dc64531e368343c6482374a9bb6edf2d7f2d
Víctor Abadía <abadia.vic@gmail.com>
2019-01-18 11:33:49 +0100

    add more code

--- staff/victor-abadia/horroy/scripts/horroy.js
+++ staff/victor-abadia/horroy/scripts/horroy.js
@@ -2,25 +2,40 @@ function Horroy() {
     this.length = arguments.length;
 
     if (arguments.length)
-        for(var i = 0; i < arguments.length; i++)
+        for (var i = 0; i < arguments.length; i++)
             // this.push(arguments[i]); // WARN should avoid (if possible) calling member methods in a constructor (push)
             this[i] = arguments[i];
 }
 
-Horroy.prototype.push = function(value) {
+Horroy.prototype.push = function (value) {
     if (!value) throw Error('undefined element to push')
+
     this[this.length++] = value;
 };
 
-Horroy.prototype.forEach = function(callback) {
+Horroy.prototype.forEach = function (callback) {
     if (typeof callback !== 'function') throw new TypeError('callback is not a function');
-    for(var i = 0; i < this.length; i++)
+
+    for (var i = 0; i < this.length; i++)
         callback(this[i]);
 };
 
-Horroy.prototype.pop = function() {
+Horroy.prototype.pop = function () {
     var result = this[this.length - 1];
     delete this[this.length - 1];
     this.length--;
     return result;
+};
+
+Horroy.from = function (value) {
+    var horr = new Horroy
+    horr.length = value.length
+
+    if (typeof value === String) {
+
+        for (var i = 0; i < value.length; i++)
+            horr[i] = value[i]
+        return horr
+    }
+
 };
