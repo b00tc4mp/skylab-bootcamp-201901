f0bd9882a6c8e0c8b5622fabc1694e7f34c18ff0
Àlex Barba <alexander.barba@gmail.com>
2019-01-19 17:35:23 +0100

    last version

--- staff/alex-barba/horroy/scripts/horroy.spec.js
+++ staff/alex-barba/horroy/scripts/horroy.spec.js
@@ -67,7 +67,17 @@ describe('Horroy', function() {
     });
 
     describe('.copyWithin', function() {
-        it('passing 1 argument', function() {
+        it('should create a new Horroy with same values when passing 0 arguments', function() {
+            var test = new Horroy(1,2,3,4,5);
+
+            var horr = test.copyWithin();
+
+            var expected = new Horroy(1,2,3,4,5);
+
+            expect(horr.toString()).toBe(expected.toString()); 
+        });
+
+        it('should create a new Horroy passing only target argument', function() {
             var test = new Horroy(1,2,3,4,5);
 
             var horr = test.copyWithin(3);
@@ -77,7 +87,7 @@ describe('Horroy', function() {
             expect(horr.toString()).toBe(expected.toString()); 
         });
 
-        it('passing target value equal than horroy length', function() {
+        it('should create a new Horroy passing target argument equal to the horroy length', function() {
             var test = new Horroy(1,2,3,4,5);
 
             var horr = test.copyWithin(5);
@@ -87,7 +97,7 @@ describe('Horroy', function() {
             expect(horr.toString()).toBe(expected.toString()); 
         });
 
-        it('passing target and start arguments', function() {
+        it('should create a new Horroy passing target and start arguments', function() {
             var test = new Horroy(1,2,3,4,5);
 
             var horr = test.copyWithin(1,2);
@@ -96,5 +106,206 @@ describe('Horroy', function() {
 
             expect(horr.toString()).toBe(expected.toString()); 
         });
+
+        it('should create a new Horroy passing > 4 arguments', function() {
+            var test = new Horroy(1,2,3,4,5);
+
+            var horr = test.copyWithin(1,3,4,5,6);
+
+            var expected = new Horroy(1,4,3,4,5);
+
+            expect(horr.toString()).toBe(expected.toString()); 
+        });
+
+        it('should create a new Horroy with same values when passing one string argument ', function() {
+            var test = new Horroy(1,2,3,4,5);
+
+            var horr = test.copyWithin('alex');
+
+            var expected = new Horroy(1,2,3,4,5);
+
+            expect(horr.toString()).toBe(expected.toString()); 
+        });
     });
+
+    describe('.every', function() {
+        it('should return true testing an Horroy', function() {
+            var test = new Horroy(2,4,6,8);
+
+            var even = function(element) {
+                return element % 2 === 0;
+                };
+
+            var horr = test.every(even);
+
+            var expected = true;
+
+            expect(horr).toBe(expected); 
+        });
+
+        it('should return false testing an Horroy', function() {
+            var test = new Horroy(2,4,6,8,9,10);
+
+            var even = function(element) {
+                return element % 2 === 0;
+                };
+
+            var horr = test.every(even);
+
+            var expected = false;
+
+            expect(horr).toBe(expected); 
+        });
+
+        it('should return true testing every object of a Horroy', function() {
+            var test = new Horroy({a:1, b:2, c:3, d:4}, {a:1, x:2, y:3, z:4}, {a:1, x:2, y:3, z:4});
+
+            var even = function(element) {
+                return element.a === 1;
+                };
+
+            var horr = test.every(even);
+
+            var expected = true;
+
+            expect(horr).toBe(expected); 
+        });
+
+        it('should return TypeError with a string as a function', function() {
+            var test = new Horroy({a:1, b:2, c:3, d:4}, {a:1, x:2, y:3, z:4}, {a:1, x:2, y:3, z:4});
+
+            var even = 'alex';
+
+            expect(function(){test.every('alex')}).toThrow(); 
+        });
+    });
+
+    describe('.findIndex', function() {
+        it('should return the index', function() {
+            var test = new Horroy(2,4,6,8);
+
+            var even = function(element) {
+                return element % 2 === 0;
+                };
+
+            var horr = test.findIndex(even);
+
+            var expected = 0;
+
+            expect(horr).toBe(expected); 
+        });
+
+        it('should return -1', function() {
+            var test = new Horroy(1,3,5);
+
+            var even = function(element) {
+                return element % 2 === 0;
+                };
+
+            var horr = test.findIndex(even);
+
+            var expected = -1;
+
+            expect(horr).toBe(expected); 
+        });
+
+        it('should return TypeError with a string as a function', function() {
+            var test = new Horroy({a:1, b:2, c:3, d:4}, {a:1, x:2, y:3, z:4}, {a:1, x:2, y:3, z:4});
+
+            var even = 'alex';
+
+            expect(function(){test.findIndex('alex')}).toThrow(); 
+        });
+    });
+
+    describe('.includes', function() {
+        it('should return true passing only element as an argument', function() {
+            var test = new Horroy(2,4,6,8);
+
+            var horr = test.includes(6);
+
+            var expected = true;
+
+            expect(horr).toBe(expected); 
+        });
+
+        it('should return false passing only element as an argument', function() {
+            var test = new Horroy(1,3,5);
+
+            var horr = test.includes(2);
+
+            var expected = false;
+
+            expect(horr).toBe(expected); 
+        });
+
+        it('should return true passing the 2 arguments', function() {
+            var test = new Horroy(2,4,6,8);
+
+            var horr = test.includes(4,1);
+
+            var expected = true;
+
+            expect(horr).toBe(expected); 
+        });
+
+        it('should return false passing the 2 arguments', function() {
+            var test = new Horroy(2,3,5);
+
+            var horr = test.includes(2,1);
+
+            var expected = false;
+
+            expect(horr).toBe(expected); 
+        });
+    });
+
+    describe('.lastIndexOf', function(){
+        it('should return true passing the 2 arguments', function () {
+            var hor = new Horroy(5,1,5,5);
+
+            var found = hor.lastIndexOf(5,2);
+
+            var expected = 2;
+        
+            expect(found).toBe(expected);    
+        });
+
+        it('should return true passing only value argument', function () {
+            var hor = new Horroy(5,1,5,5);
+
+            var found = hor.lastIndexOf(5);
+
+            var expected = 3;
+        
+            expect(found).toBe(expected);    
+        });
+    });
+
+    describe('.reduceRight', function(){
+        it('should return sum passing 1 argument', function () {
+            var hor = new Horroy(1,2,3,4);
+
+            var found = hor.reduceRight(function(acc, value) {
+                return acc + value 
+            });
+
+            var expected = 10;
+        
+            expect(found.toString()).toBe(expected.toString());    
+        });
+
+        it('should return sum passing the 2 arguments', function () {
+            var hor = new Horroy(1,2,3,4);
+
+            var found = hor.reduceRight(function(acc, value) {
+                return acc + value 
+            }, 5);
+
+            var expected = 15;
+        
+            expect(found.toString()).toBe(expected.toString());    
+        });
+    });
+
 });
