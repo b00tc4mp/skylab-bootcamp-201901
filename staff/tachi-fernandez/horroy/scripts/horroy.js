b1c3f6397a5218cad6f334bee6dad63ff3f65243
Tachi <jrfluna8@gmail.com>
2019-01-19 19:12:20 +0100

    horroy

---
+++ staff/tachi-fernandez/horroy/scripts/horroy.js
@@ -0,0 +1,46 @@
+function Horroy() {
+    this.length = arguments.length;
+
+    if (arguments.length)
+        for (var i = 0; i < arguments.length; i++)
+            // this.push(arguments[i]); // WARN should avoid (if possible) calling member methods in a constructor (push)
+            this[i] = arguments[i];
+}
+
+Horroy.from = function (value) {
+    var horr = new Horroy
+
+    horr.length = value.length
+
+    if (typeof value === 'string')
+        for (var i = 0; i < value.length; i++)
+            horr[i] = value[i]
+
+    return horr
+};
+
+Horroy.prototype.push = function (value) {
+    this[this.length++] = value;
+};
+
+Horroy.prototype.forEach = function (callback) {
+    for (var i = 0; i < this.length; i++)
+        callback(this[i]);
+};
+
+Horroy.prototype.toString = function() {
+    var string = '';
+
+    for(var i = 0; i < this.length - 1; i++)
+        string += this[i] + ',';
+    
+    string += this[this.length - 1];
+
+    return string;
+};
+
+Horroy.prototype.pop = function(){
+    return this[this.length-1]
+    
+};
+            
