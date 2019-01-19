7c7334518849ed1f508f3d9fc18ec81e5ed1939d
jordi <jordi.pujolg@gmail.com>
2019-01-19 19:10:20 +0100

    splice & reduce

--- staff/jordi-pujol/array-functions/splice.js
+++ staff/jordi-pujol/array-functions/splice.js
@@ -15,24 +15,112 @@
 
 function splice(arr, start, countDel, item) {
 
-    splice(arr, "1", "2", "3", "4")
 
-    if (!(arr instanceof Array)) throw TypeError (arr + ' should be an array')
-    if (typeof arguments[1] !== "number") return []
+    if (!(arr instanceof Array)) throw TypeError(arr + ' should be an array')
+    if (typeof arguments[1] !== "number") return [];
 
-    
+    else if (typeof arguments[1] === "number") {
 
+        if (typeof arguments[2] === "number") {
 
+            if (arguments.length === 3) {
 
+                var ret = []
+                for (var i = start; i < (start + countDel); i++) {
+                    ret[i - start] = arr[i]
+                }
+                for (var i = (start + countDel); i < arr.length; i++) {
+                    arr[i - countDel] = arr[i]
+                }
+                arr.length -= start
+                return ret
 
+            }
 
+            else {
 
+                var countA = 3;
 
-    if (typeof start !== "number"){
-        for (var i = 1; i< arguments.length; i++){
-            arguments[i] = arguments[i+1]
+                var ret = []
+
+                for (var i = start; i < (start + countDel); i++) {
+                    ret[i - start] = arr[i]
+                }
+
+                if (arguments.length - 3 < countDel) {
+
+                    for (var i = (start); i < (start + countDel + (arguments.length - 3)); i++) {
+
+                        if (arguments[countA]) {
+                            arr[i] = arguments[countA]
+                            countA++
+                        }
+                        else {
+                            arr[i] = arr[i + countDel - countA + 3]
+                        }
+                    }
+                    arr.length -= start
+
+                }
+
+                else if (arguments.length - 3 > countDel) {
+
+                    // for (var i = start; i < (start + countDel); i++){
+
+                    //     arr[i] = arguments[countA]
+                    //     countA ++
+                    // }
+                    // var len = arr.length
+                    // for (var k = (start + countDel); k < len; k ++){
+                    //     arr[arr.length] = arr[k]
+                    //     arr[k] = arguments[countA]
+                    //     countA ++
+                    // }
+
+                    let leng = arr.length
+
+                    for (var i = 0; i < (arguments.length - 3 - countDel); i++) {
+
+                        let count = 0
+                        for (var k = (start + countDel); k < (leng +1); k++) {
+
+                            arr[arr.length - count] = arr[arr.length - count - 1]
+                            count ++
+                        }
+                    }
+                    for (var j = 0; j < (arguments.length -3); j++){
+
+                        arr[start+j] = arguments[3+j]
+                    }
+                }
+                return ret
+            }
+        }
+
+        else if (typeof arguments[2] === "number") {
+
+        }
+        if (arguments.length < 3) {
+            var ret = []
+            for (var i = start; i < arr.length; i++) {
+                ret[i - start] = arr[i]
+            }
+            arr.length -= start
+            return ret
         }
     }
+
+
+
+
+
+
+
+    // if (typeof start !== "number"){
+    //     for (var i = 1; i< arguments.length; i++){
+    //         arguments[i] = arguments[i+1]
+    //     }
+    // }
 
 
 }
--- staff/jordi-pujol/array-functions/splice.test.js
+++ staff/jordi-pujol/array-functions/splice.test.js
@@ -1,11 +1,65 @@
 suite('splice')
 
+test('should work when we introduce all the parameters & there are more elements to delete than to add', function(){
+
+    var a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    var res = splice(a, 2, 4, "house", "casa")
+
+    var exp = [2, 3, 4, 5];
+
+    if(res.toString() !== exp.toString()) throw Error ('the result should be the expected (deleted elements)')
+    if(a.toString() !== [0, 1, "house", "casa", 6, 7, 8, 9].toString()) throw Error ('the result should be the expected (deleted elements)')
+})
+
 test('should work when we introduce all the parameters', function(){
+   
+    var a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+    var res = splice(a, 2, 2, "house", "casa", "tomato", "tomaquet")
 
+    var exp = [2, 3];
+
+    if(res.toString() !== exp.toString()) throw Error ('the result should be the expected (deleted elements)')
+    if(a.toString() !== [0, 1, "house", "casa", "tomato", "tomaquet", 4, 5, 6, 7, 8, 9].toString()) throw Error ('the result should be the expected (deleted elements)')
 })
 
 test('should work when we do not introduce the deletecount', function(){
 
     
+})
+
+test('should work when we do not introduce any element to add nor deletecount', function(){
+
+    var a = [0, 1, 2, 3]
+
+    var res = splice(a, 2)
+
+    var exp = [2, 3];
+
+    if(res.toString() !== exp.toString()) throw Error ('the result should be the expected (deleted elements)')
+    
+})
+
+test('should work when we do not introduce any element but start and deletcount', function(){
+
+    var a = [0, 1, 2, 3, 4, 5, 6]
+
+    var res = splice(a, 2, 2)
+
+    var exp = [2, 3];
+
+    if(res.toString() !== exp.toString()) throw Error ('the result should be the expected (deleted elements)')
+
+})
+
+test('should return empty array if no start nor deletcount introduced', function(){
+
+    var a = [0, 1, 2, 3]
+
+    var res = splice(a, "home", "casa")
+
+    var exp = [];
+
+    if(res.toString() !== exp.toString()) throw Error ('the result should be the expected')
 })
