411dbee773a3640692824920f094923781069c21
Serena <serena@example.com>
2019-01-20 22:14:50 +0100

    Add function

--- staff/serena-mejias/horroy/scripts/horroy.spec.js
+++ staff/serena-mejias/horroy/scripts/horroy.spec.js
@@ -152,4 +152,62 @@ describe("Horroy", function() {
       expect(result).toEqual(expected);
     })
   })
+  
+  describe("pop", function() {
+    beforeEach(function(){
+      horroy = new Horroy('Fire', 'Wind', 'Rain');
+    })
+
+    it("should add new elements to the horroy and return the new lenght", function(){
+      var result = horroy.pop();
+      var expected = 'Rain';
+
+      expect(result).toEqual(expected);
+    })
+  })
+  
+  /*describe("pop", function() {
+    beforeEach(function(){
+      horroy = new Horroy(1, 2, 3, 4);
+    })
+
+    it("should add new elements to the horroy and return the new lenghtexecutes a reducer function (that you provide) on each member of the array resulting in a single output value", function(){
+      var reducer = function(accumulator, currentValue) { accumulator + currentValue};
+      var result = horroy.reduce(reducer);
+      var expected = 10;
+
+      expect(result).toEqual(expected);
+    })
+  })*/
+
+  describe("slice", function() {
+    beforeEach(function(){
+      horroy = new Horroy(1, 2, 3, 4);
+    })
+    
+    it("returns a shallow copy of a portion of an array into a new array object selected from begin to end", function(){
+      var result = horroy.slice(1,3);
+      var expected = {0:2, 1:3};
+
+      expect(JSON.stringify(result)).toBe(JSON.stringify(expected));
+    })
+  })
+
+  describe("some", function() {
+    beforeEach(function(){
+      horroy = new Horroy(1, 2, 3, 4);
+    })
+    
+    it("tests whether at least one element in the array passes the test implemented by the provided function", function(){
+      var result = horroy.some(function(value){return value === 2});
+      var expected = true;
+
+      expect(result).toEqual(expected);
+    })
+    
+    it("the argument must be a function", function() {
+      expect(function(){horroy.some(2)} ).toThrowError('the argument must be a function');
+    });
+  })
+
 });
