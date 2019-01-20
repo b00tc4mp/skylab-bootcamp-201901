1509089311b9f28b2929d62d5a5ca157fa06e750
Ury18 <ury_98@hotmail.com>
2019-01-20 18:43:18 +0100

    update

--- staff/oriol-escolar/array-higher-order-functions/splice.js
+++ staff/oriol-escolar/array-higher-order-functions/splice.js
@@ -1,22 +1,58 @@
 function splice(parameters) {
 
+    var array = arguments[0];
     var start = arguments[1];
-    var deletecount = arguments[2];
-    var deleteDestiny = arguments[1] + arguments[2];
-    var newArray;
-    var deleted = [];
-    newArray.length = arguments.length - deletecount;
+    var deleteCount = arguments[2];
+    var deleteDestiny = start + deleteCount;
+    var eliminated = [];
+    var elementsToAdd = arguments.length -3;
 
-    for (i = 0; i < deleteDestiny; i++) 
-    {
+    start = start !== undefined ? (start >= 0 ? (start > array.length ? array.length : start) : array.length + start) : 0;
+    deleteDestiny = deleteCount !== undefined ? (deleteCount >= array.length ? array.length : deleteDestiny) : array.length
 
-        newArray[i]= arguments[0][i+start]
+
+
+    for (i = start; i < deleteDestiny; i++) {
+
+        eliminated[eliminated.length] = array[i]
 
     }
 
-    return newArray;
 
+
+    if (arguments.length > 3) {
+
+        for(i=0;i<elementsToAdd;i++){
+
+            array[array.length] = null;
+
+        }
+
+        
+        
+        for(i=array.length-1; i>0;i--){
+
+
+            array[i] = array[i-elementsToAdd]
+
+        }
+       
+
+
+        for(i=0;i<elementsToAdd;i++)
+        
+        {
+            array[i] = arguments[i+3];
+
+        }
+
+        
+    }
+    return eliminated;
 
 }
 
 
+var arr = [1, 2, 3, 4, 5];
+
+splice(arr, 1, 4, 'pepino', 'patata');
