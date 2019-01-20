ba18b1126da813e9d6cf162c2b248c214d80433e
jordi <jordi.pujolg@gmail.com>
2019-01-20 14:15:18 +0100

    last functions

--- staff/jordi-pujol/array-functions/unshift.test.js
+++ staff/jordi-pujol/array-functions/unshift.test.js
@@ -5,7 +5,7 @@ test('should have the correct length', function(){
     var a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     
     var res = unshift(a, 6, 7, 8, 9, "casa")
-    console.log(a)
+
     var expected = 15
 
     if (res !== expected) throw Error ('the result length should be the expected one')
