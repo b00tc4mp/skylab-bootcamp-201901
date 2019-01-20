736e783db9dd8762316cb0856b43436efd815a0f
Carlos <carloscl1986@gmail.com>
2019-01-20 19:55:09 +0100

    2nd v

--- staff/carlos-calvo/feature-horroy/horroy.spec.js
+++ staff/carlos-calvo/feature-horroy/horroy.spec.js
@@ -65,6 +65,7 @@ describe("Horroy-isHorroy", function() {
 describe("Horroy - Concat", function() {
     beforeEach(function(){
         this.hor = new Horroy(1,2,3)
+        var x = false;
     })
     
     it("returns true if correctly concat", function() {
@@ -75,13 +76,101 @@ describe("Horroy - Concat", function() {
 
     it("Not an array, throw an error", function() {
         expect(function(){
-            this.hor3 = this.hor.concat(false)
-        }).toThrow(new TypeError('Not an horroy'))
+            this.hor.concat(x)
+        }).toThrow(new Error('Not an horroy'))
         //expect(function (){hor.isHorroy(1,2,3)}).toThrow(new Error('too many arguments'))
     });
 
 });
 
+
+
+describe("Horroy - Every", function() {
+
+    it("returns true as every item match condition provided in function", function() {
+        var hor = new Horroy(4,5,6)
+        var result
+        result = hor.every(function(item){
+            return item > 0
+        })
+        var expected = true
+        expect(result).toEqual(expected)
+    });
+
+    it("Throws Error when parameter not a function", function() {
+        var hor = new Horroy(4,-5,6)
+        var result = true
+        debugger
+        expect(function(){hor.every(result)}).toThrow();
+    });
+    
+    it("returns false as a item in the array do not match condition", function() {
+        var hor = new Horroy(4,-5,6)
+        var result
+        result = hor.every(function(item){
+            return item > 0
+        })
+        var expected = false
+        expect(result).toEqual(expected)
+    });
+});
+
+
+describe("Horroy - Fill", function() {
+
+    it("returns result expected when filling", function() {
+        var hor = new Horroy(1,2,3,4)
+        hor.fill(0, 2, 4)
+        var expectedresult = new Horroy(1,2,0,0)
+        expect(JSON.stringify(hor) == JSON.stringify(expectedresult)).toEqual(true)
+    });
+
+    it("returns result expected when filling with 2 parameters", function() {
+        var hor = new Horroy(1,2,3,4)
+        hor.fill(5,1)
+        var expectedresult = new Horroy(1, 5, 5, 5)
+        expect(JSON.stringify(hor) == JSON.stringify(expectedresult)).toEqual(true)
+    });
+
+    it("returns result expected when filling with 1 parameters", function() {
+        var hor = new Horroy(1,2,3,4)
+        hor.fill(5)
+        var expectedresult = new Horroy(5, 5, 5, 5)
+        expect(JSON.stringify(hor) == JSON.stringify(expectedresult)).toEqual(true)
+    });
+
+    it("returns error when parameter not a number", function() {
+        var hor = new Horroy(1,2,3,4)
+        expect(function(){hor.fill(true,true,false)}).toThrow()
+    });
+});
+
+
+describe("Horroy - toString", function() {
+
+    it("returns result expected", function() {
+        var hor = new Horroy(1,2,3,4)
+        expectedresult = '1,2,3,4'
+        expect(hor.toString()).toEqual(expectedresult)
+    });
+
+});
+
+describe("Horroy - Shift", function() {
+
+    it("returns result expected", function() {
+        var hor = new Horroy(1,2,3,4)
+        var expectedresult = 1
+        expect(hor.shift()).toEqual(expectedresult)
+    });
+
+    it("returns Error if length is zero", function() {
+        var hor = new Horroy()
+
+        expect(function(){hor.shift()}).toThrow()
+    });
+
+});
 
 
 
