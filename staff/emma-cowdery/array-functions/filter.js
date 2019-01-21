f478d905a4327c699d708cb8dd942643dd88cf50
Emma Cowdery <e2maaa@gmail.com>
2019-01-21 09:36:57 +0100

    functions

---
+++ staff/emma-cowdery/array-functions/filter.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @param {Array} array 
+ * @param {Function} callback 
+ * 
+ * The filter function creates a new array with all the elements of the old one that pass the test implemented by the callback  function.
+ */
+
+function filter(array, callback) {
+    newArray = [];
+    var count = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (callback(array[i])) {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+    return newArray;
+};
+
+filter([1, 2, 3, 4, 5, 6], function(element) {
+    return element > 4;
+});
