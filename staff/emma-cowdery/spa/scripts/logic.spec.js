416f1767268a772f781aff1ef3b849ad8357aad2
Emma Cowdery <e2maaa@gmail.com>
2019-01-21 16:47:26 +0100

    spa

---
+++ staff/emma-cowdery/spa/scripts/logic.spec.js
@@ -0,0 +1,10 @@
+describe('Logic SPEC', function() {
+
+    describe('Login', function() {
+
+        it('email should be string', function() {
+            email = "";
+            expect(email).toBe('string');
+        });
+    });
+});
