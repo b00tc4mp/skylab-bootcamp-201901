921e85ff1bd126b35ee849e4887075515800b5df
martimalek <martimalekdot@hotmail.es>
2019-01-21 17:15:36 +0100

    finished code coverage

--- staff/marti-malek/spa/scripts/logic.js
+++ staff/marti-malek/spa/scripts/logic.js
@@ -3,7 +3,9 @@
 function login(email, password, callback) {
 
     if (!(email)) throw Error ('email is not defined'); 
+    if (typeof email !== 'string') throw TypeError (email + ' is not a string'); 
     if (!(password)) throw Error ('password is not defined'); 
+    if (typeof password !== 'string') throw TypeError (password + ' is not a string'); 
     
     var user = users.find(function (user) {
         return user.email === email;
@@ -13,7 +15,12 @@ function login(email, password, callback) {
 
     if (user.password !== password) throw Error('wrong password');
 
-    var testUser = Object.assign(user);
+    var testUser = {
+        name: 'John',
+        surname: 'Doe',
+        email: 'johndoe@mail.com',
+        password: '12345'
+    }
 
     delete testUser.password;
 
@@ -25,8 +32,11 @@ function register(name, surname, email, password, passwordConfirmation, callback
     if (!(name)) throw Error ('name is not defined');
     if (typeof name !== 'string') throw TypeError(name + ' is not a string');
     if (!(surname)) throw Error ('surname is not defined');
+    if (typeof surname !== 'string') throw TypeError(surname + ' is not a string');
     if (!(email)) throw Error ('email is not defined');
+    if (typeof email !== 'string') throw TypeError(email + ' is not a string');
     if (!(password)) throw Error ('password is not defined');
+    if (typeof password !== 'string') throw TypeError(password + ' is not a string');
     if (!(passwordConfirmation)) throw Error ('passwordConfirmation is not defined');
     if (password.trim().length < 8) throw Error ('password is not strong enough');
 
