f673333b772504fc4979d2f7bf18bd222b75effd
Marc Sarroca Diaz <msarroca@Marcs-MacBook-Pro.local>
2019-01-21 08:53:08 +0100

    horroy-feature

--- staff/marc-sarroca/horroy/scripts/horroy.spec.js
+++ staff/marc-sarroca/horroy/scripts/horroy.spec.js
@@ -36,4 +36,58 @@ describe("Horroy", function() {
       expect(count).toEqual(res);
     });
   });
+
+  describe("toString", function() {
+    it("should return a new string ", function() {
+      var horroy = new Horroy(1, 2, 3);
+      var horroyString = horroy.toString();
+      expect(typeof horroyString).toBe("string");
+    });
+  });
+  describe("Pop", function() {
+    it("should return last item", function() {
+      var horroy = new Horroy(1, 2, 3);
+      var horroyPop = horroy.pop();
+      expect(horroyPop).toBe(3);
+    });
+  });
+
+  describe("indexof", function() {
+    it("should return the position of the value", function() {
+      var horroy = new Horroy(1, 2, 3);
+      var horroyIndexOf = horroy.indexof(2);
+      expect(horroyIndexOf).toBe(1);
+    });
+    it("should return -1 if value not exists", function() {
+      var horroy = new Horroy(1, 2, 3);
+      var horroyIndexOf = horroy.indexof(4);
+      expect(horroyIndexOf).toBe(-1);
+    });
+  });
+
+  describe("join", function() {
+    it("should return a string with new separated characters", function() {
+      var horroy = new Horroy(1, 2, 3);
+      var horroyJoin = horroy.join(",");
+      expect(horroyJoin).toBe("1,2,3");
+    });
+  });
+
+  describe("reverse", function() {
+    it("shuold return a reverse array", function() {
+      var horroy = new Horroy(1, 2, 3);
+      horroy.reverse();
+      expect(horroy[0]).toBe(3);
+      expect(horroy[1]).toBe(2);
+      expect(horroy[2]).toBe(1);
+    });
+  });
+
+  describe("reverse", function() {
+    it("should return a copy of array", function() {
+      var horroy = new Horroy(1, 2, 3, 4, 5);
+      var horroySplice = horroy.slice(1, 2);
+      expect(horroySplice).toEqual([2]);
+    });
+  });
 });
