2f5e25f446db236b83b77fd77fd788079534fbb8
Pedro <pe->
2019-01-21 15:22:22 +0100

    feature update

---
+++ staff/pedro-garriganogues/array-functions/README.md
@@ -0,0 +1,26 @@
+# Learning javascript
+
+#Getting Started:
+
+Trying to recreate some of js methods effects without using the methoda itself.
+
+It's should be FULL of bugs and mistakes, but these were learning exercises and mistakes are expected when learning.
+
+#Built With:
+
+VisualStudioCode,
+Javascript
+
+#Versioning:
+
+V1.0.0
+No updates pending
+
+#Authors:
+
+Pedro Garriga-Nogues
+[github](https://github.com/PedroGarrigaNogues)
+
+#Acknowledgments:
+
+Skylab Coders.
--- staff/pedro-garriganogues/array-functions/unshift.js
+++ staff/pedro-garriganogues/array-functions/unshift.js
@@ -1,40 +1,35 @@
 /**
  * Abstraction of indexOf.
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * @param {Array} arr
- * @param {resInterno} 
- * @param {respuesta} 
- * 
+ * @param {resInterno}
+ * @param {respuesta}
+ *
  * @throws {TypeError} - If array is not an array
  */
 var arr = [1, 2, 3];
-var kickme = '4,5';
+var result = [];
 
+function unshifter(data) {
+  var internalResult = [];
 
-var expected = [4, 1, 2, 3]
-
-function unshifter(array, data) {
-    var result = [];
-
-    if (!(array instanceof Array))
-        throw new TypeError(ele + ' is not an array');
-
-    for (var i = 0; i < data.length; i++) {
-        result.push(data)
-        console.log('data')
+  for (var i = 0; i < data.length; i++) {
+    internalResult = data[i];
+    result = result + internalResult;
+    internalResult = "";
+  }
+  if (i === data.length) {
+    for (var y = 0; y < this.length; y++) {
+      internalResult = this[y];
+      result = result + internalResult;
+      internalResult = "";
     }
+  }
 
-    for (var y = 0; y < array.length; y++) {
-        console.log('array')
-        result.push(array)
-    }
+  return result.length;
+}
 
-    return result
-
-};
-
-unshifter(arr, kickme)
-
+unshifter(4);
