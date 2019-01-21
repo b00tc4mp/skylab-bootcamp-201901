37211552d7fff9fdac530893395b01139c6f5c35
Marc Sarroca Diaz <msarroca@Marcs-MacBook-Pro.local>
2019-01-21 15:08:13 +0100

    first

---
+++ staff/marc-sarroca/spa/scripts/logic.js
@@ -0,0 +1,45 @@
+//#region (business) logic
+
+function login(email, password, callback) {
+  // TODO validate fields!
+
+  var user = users.find(function(user) {
+    return user.email === email;
+  });
+
+  if (!user) throw Error("user " + email + " not found");
+
+  if (user.password !== password) throw Error("wrong password");
+
+  callback(user);
+}
+
+function register(
+  name,
+  surname,
+  email,
+  password,
+  passwordConfirmation,
+  callback
+) {
+  // TODO validate fields!
+
+  var user = users.find(function(user) {
+    return user.email === email;
+  });
+
+  if (user) throw Error("user " + email + " already exists");
+
+  if (password !== passwordConfirmation) throw Error("passwords do not match");
+
+  users.push({
+    name: name,
+    surname: surname,
+    email: email,
+    password: password
+  });
+
+  callback();
+}
+
+//#endregion
