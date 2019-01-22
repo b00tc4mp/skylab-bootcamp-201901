73828bb5a46e0bcc47bc0c32aca4827c922308da
Tachi <jrfluna8@gmail.com>
2019-01-22 10:46:39 +0100

    spa

--- staff/tachi-fernandez/spa/scripts/logic.js
+++ staff/tachi-fernandez/spa/scripts/logic.js
@@ -1,7 +1,17 @@
+'use strict';
+
 //#region (business) logic
 
 function login(email, password, callback) {
     // TODO validate fields!
+    if (typeof email !== 'string') throw TypeError(email + ' is not a string');
+
+    if (!email.trim().length) throw Error('email cannot be empty');
+
+    if (typeof password !== 'string') throw TypeError(password + ' is not a string');
+
+    if (!password.trim().length) throw Error('password cannot be empty');
+
     
     var user = users.find(function (user) {
         return user.email === email;
@@ -11,15 +21,37 @@ function login(email, password, callback) {
 
     if (user.password !== password) throw Error('wrong password');
 
-    loggedInUser = {
-        name = user.name,
-        surname = user.surname,
-        email = user.email
-    }
+
+    var loggedInUser = {
+        name: user.name,
+        surname: user.surname,
+        email: user.email
+    };
+
     callback(loggedInUser);
 }
 
 function register(name, surname, email, password, passwordConfirmation, callback) {
+    if (typeof name !== 'string') throw TypeError(name + ' is not a string');
+
+    if (!name.trim().length) throw Error('name cannot be empty');
+
+    if (typeof surname !== 'string') throw TypeError(surname + ' is not a string');
+
+    if (!surname.trim().length) throw Error('surname cannot be empty');
+
+    if (typeof email !== 'string') throw TypeError(email + ' is not a string');
+
+    if (!email.trim().length) throw Error('email cannot be empty');
+
+    if (typeof password !== 'string') throw TypeError(password + ' is not a string');
+
+    if (!password.trim().length) throw Error('password cannot be empty');
+
+    if (typeof passwordConfirmation !== 'string') throw TypeError(passwordConfirmation + ' is not a string');
+
+    if (!passwordConfirmation.trim().length) throw Error('password confirmation cannot be empty');
+
     // TODO validate fields!
 
     var user = users.find(function (user) {
