7438bfa88ba830900e646fb9c26017f811a0ed08
Àlex Barba <alexander.barba@gmail.com>
2019-01-22 17:25:42 +0100

    components

---
+++ staff/alex-barba/spa/scripts/main.js
@@ -0,0 +1,71 @@
+var loginPanel = new LoginPanel
+var welcomePanel = new WelcomePanel
+var registerPanel = new RegisterPanel
+
+document.body.appendChild(loginPanel.element);
+document.body.appendChild(welcomePanel.element);
+document.body.appendChild(registerPanel.element);
+
+loginPanel.onLogin = function(email, password) {
+    try {
+        login(email, password, function(user) {
+            loginPanel.hide();
+
+            welcomePanel.user = user;
+            welcomePanel.show();
+        });
+    } catch(err) {
+        loginPanel.error = err.message;
+    }
+};
+
+loginPanel.onRegister = function() {
+    loginPanel.hide();
+    registerPanel.show();
+}
+
+registerPanel.onRegister = function (name, surname, email, password, passwordConf) {
+    try {
+        register(name, surname, email, password, passwordConf, function () {
+            registerPanel.hide();
+            loginPanel.show();
+            registerPanel.clear();
+        });
+    } catch (err) {
+        registerPanel.error = err.message;
+    }
+}; 
+
+welcomePanel.onLogout = function() {
+    welcomePanel.hide();
+    loginPanel.clear();
+    loginPanel.show();
+};
+
+registerPanel.onLogin = function() {
+    registerPanel.hide();
+    loginPanel.show();
+};
+
+
+// var loginPanel2 = new LoginPanel
+
+// document.body.appendChild(loginPanel2.element);
+
+// loginPanel2.onLogin = function(email, password) {
+//     console.log('llama a otra logica', email, password);
+// };
+
+
+// var mainPanel = new Panel
+
+// var registerPanel = new RegisterPanel
+// var welcomePanel = new WelcomePanel
+
+// mainPanel.add(loginPanel)
+// mainPanel.add(registerPanel)
+// mainPanel.add(welcomePanel)
+
+// loginPanel.onLogin = function() {}
+
+// ...
