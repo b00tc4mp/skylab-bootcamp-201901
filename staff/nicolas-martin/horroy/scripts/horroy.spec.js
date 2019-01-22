114094f3f7af31d95af6b280c0e764c21963be18
nico <nico@somoslaclave.com>
2019-01-22 14:49:01 +0100

    only from

--- staff/nicolas-martin/horroy/scripts/horroy.spec.js
+++ staff/nicolas-martin/horroy/scripts/horroy.spec.js
@@ -10,10 +10,41 @@ describe('Horroy', function() {
     describe('from', function() {
         it('should create a Horroy from string', function() {
             var string = 'hola mundo';
-
             var horr = Horroy.from(string);
+            expect(horr.toString()).toBe(string.split('').toString());
+        });
 
-            expect(horr.toString()).toBe(string.split('').toString()); // h,o,l,a, ,m,u,n,d,o
-        })
+        it('should create a Horroy from arguments empty', function() {
+            var horr = new Horroy;
+            expect(horr.length).toBe(0);
+            expect(horr).toBeDefined();
+        });
+    });
+
+    describe('ishorroy', function() {
+        it('should returns true if is a Horroy', function() {   
+            var horr = new Horroy;
+            debugger;
+            var isHorroy2 = Horroy.isHorroy(horr);
+            expect(isHorroy2).toBe(true);
+        });
+
+        it('should returns false if is not a Horroy', function() {   
+            var arr = new Array;
+            var isHorroy2 = Horroy.isHorroy(arr);
+            // expect(isHorroy2).toBe(false);
+            expect(isHorroy2).toBeFalsy();
+        });
+    });
+
+    describe('of', function() {
+        it('should returns a Horroy from a number', function() {   
+            //var horr = new Horroy;
+            var horr = Horroy.of(7);
+
+            expect(horr)
+            expect(horr.length).toBe(1);
+            expect(horr[0]).toBe(7);
+        });
     });
 });
