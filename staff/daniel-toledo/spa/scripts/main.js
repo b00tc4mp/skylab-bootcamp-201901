92966b734c69b7b12bcf1dd3664bb10032414a44
danieltolmon <daniel.toledomonfort@gmail.com>
2019-01-22 17:40:03 +0100

    spa in components, creating html with javascript

---
+++ staff/daniel-toledo/spa/scripts/main.js
@@ -0,0 +1,72 @@
+var loginPanel = new LoginPanel
+var welcomePanel = new WelcomePanel
+var registerPanel = new RegisterPanel
+
+document.body.appendChild(loginPanel.element);
+document.body.appendChild(welcomePanel.element);
+document.body.appendChild(registerPanel.element);
+
+loginPanel.onLogin = function(email, password) {
+    try {
+        login(email, password, function(user) {
+            loginPanel.hide();
+
+            welcomePanel.user = user;
+            welcomePanel.show();
+        });
+    } catch(err) {
+        loginPanel.error = err.message;
+    }
+};
+
+loginPanel.linkRegister= function(){
+    loginPanel.hide();
+    registerPanel.show();
+}
+
+welcomePanel.onLogout = function() {
+    welcomePanel.hide();
+    loginPanel.clear();
+    loginPanel.show();
+};
+
+registerPanel.linkLogin = function(){
+    loginPanel.show();
+    registerPanel.hide();
+}
+
+registerPanel.onRegister=function(name, surname, email, password, passwordConfirmation) {
+    try{
+        register(name, surname, email, password, passwordConfirmation, function(){
+            registerPanel.hide();
+            registerPanel.clear();
+            loginPanel.show();
+
+        });
+    } catch(err){
+        registerPanel.error=err.message;
+    }
+};
+
+
+// var loginPanel2 = new LoginPanel
+
+// document.body.appendChild(loginPanel2.element);
+
+// loginPanel2.onLogin = function(email, password) {
+//     console.log('llama a otra logica', email, password);
+// };
+
+
+// var mainPanel = new Panel
+
+// var registerPanel = new RegisterPanel
+// var welcomePanel = new WelcomePanel
+
+// mainPanel.add(loginPanel)
+// mainPanel.add(registerPanel)
+// mainPanel.add(welcomePanel)
+
+// loginPanel.onLogin = function() {}
+
+// ...
