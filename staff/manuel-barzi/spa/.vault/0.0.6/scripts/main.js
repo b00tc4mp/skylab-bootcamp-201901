8711d9bb733ccb83b59defe96862f1d38daa24a2
manuelbarzi <manuelbarzi@gmail.com>
2019-01-23 13:21:38 +0100

    update spa, stuff

--- staff/manuel-barzi/spa/.vault/0.0.6/scripts/main.js
+++ staff/manuel-barzi/spa/.vault/0.0.6/scripts/main.js
@@ -1,24 +1,19 @@
-'use strict';
-
 var loginPanel = new LoginPanel
 var registerPanel = new RegisterPanel
-var homePanel = new HomePanel
-var searchPanel = new SearchPanel
+var welcomePanel = new WelcomePanel
 
 document.body.appendChild(loginPanel.element);
 document.body.appendChild(registerPanel.element)
-document.body.appendChild(homePanel.element);
-
-homePanel.element.appendChild(searchPanel.element);
+document.body.appendChild(welcomePanel.element);
 
 loginPanel.onLogin = function(email, password) {
     try {
-        logic.login(email, password, function(user) {
+        login(email, password, function(user) {
             loginPanel.hide();
             loginPanel.clear();
 
-            homePanel.user = user;
-            homePanel.show();
+            welcomePanel.user = user;
+            welcomePanel.show();
         });
     } catch(err) {
         loginPanel.error = err.message;
@@ -34,7 +29,7 @@ loginPanel.onGoToRegister = function() {
 
 registerPanel.onRegister = function(name, surname, email, password, passwordConfirmation) {
     try {
-        logic.register(name, surname, email, password, passwordConfirmation, function() {
+        register(name, surname, email, password, passwordConfirmation, function() {
             registerPanel.hide();
             registerPanel.clear();
 
@@ -52,27 +47,31 @@ registerPanel.onGoToLogin = function() {
     loginPanel.show();
 };
 
-homePanel.onLogout = function() {
-    homePanel.hide();
-
-    searchPanel.clear();
-
+welcomePanel.onLogout = function() {
+    welcomePanel.hide();
     loginPanel.clear();
     loginPanel.show();
 };
 
-searchPanel.onSearch = function(query) {
-    try {
-        logic.search(query, function(error, results) {
-            if (error) searchPanel.error = error;
-            else searchPanel.results = results.map(function(result) {
-                return {
-                    text: result.title,
-                    image: result.imageUrl
-                }
-            });
-        });
-    } catch(err) {
-        searchPanel.error = err.message;
-    } 
-};
+
+// var loginPanel2 = new LoginPanel
+
+// document.body.appendChild(loginPanel2.element);
+
+// loginPanel2.onLogin = function(email, password) {
+//     console.log('llama a otra logica', email, password);
+// };
+
+
+// var mainPanel = new Panel
+
+// var registerPanel = new RegisterPanel
+// var welcomePanel = new WelcomePanel
+
+// mainPanel.add(loginPanel)
+// mainPanel.add(registerPanel)
+// mainPanel.add(welcomePanel)
+
+// loginPanel.onLogin = function() {}
+
+// ...
