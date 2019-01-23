ddf02060a34fdd0588d4dcacd2aa2603b7d646fe
Emma Cowdery <e2maaa@gmail.com>
2019-01-23 09:10:07 +0100

    mas horror

--- staff/emma-cowdery/horroy/scripts/horroy.js
+++ staff/emma-cowdery/horroy/scripts/horroy.js
@@ -1,53 +1,57 @@
 function Horroy() {
-    this.length = arguments.length;
+    if (arguments.length === 1 && typeof arguments[0] === 'number') {
+        if (!(Number.isInteger(arguments[0]))) throw RangeError('Invalid horroy length')
 
-    if (arguments.length)
+        this.length = arguments[0]
+    } else {
+        this.length = arguments.length;
         for (var i = 0; i < arguments.length; i++)
-            // this.push(arguments[i]); // WARN should avoid (if possible) calling member methods in a constructor (push)
+
             this[i] = arguments[i];
-};
+    }
 
-Horroy.prototype.from = function (value) {
-    var horr = new Horroy
 
-    horr.length = value.length
+    Horroy.prototype.from = function (value) {
+        var horr = new Horroy
 
-    if (typeof value === 'string')
-        for (var i = 0; i < value.length; i++)
-            horr[i] = value[i]
+        horr.length = value.length
 
-    return horr
-};
+        if (typeof value === 'string')
+            for (var i = 0; i < value.length; i++)
+                horr[i] = value[i]
 
-Horroy.prototype.push = function (value) {
-    this[this.length++] = value;
-};
+        return horr
+    };
 
-Horroy.prototype.pop = function () {
-    if (!(this instanceof Horroy)) throw TypeError(this + ' is not an horroy');  
-    if (arguments.length > 1) throw Error('too many arguments');
-    var popped = this[this.length-1];
-    this.length = this.length - 1;
-    return popped;
-}
+    Horroy.prototype.push = function (value) {
+        this[this.length++] = value;
+    };
 
-Horroy.prototype.forEach = function (callback) {
-    for (var i = 0; i < this.length; i++)
-        callback(this[i]);
-};
+    Horroy.prototype.pop = function () {
+        if (!(this instanceof Horroy)) throw TypeError(this + ' is not an horroy');
+        if (arguments.length > 1) throw Error('too many arguments');
+        var popped = this[this.length - 1];
+        this.length = this.length - 1;
+        return popped;
+    }
 
-Horroy.prototype.toString = function() {
-    var string = '';
+    Horroy.prototype.forEach = function (callback) {
+        for (var i = 0; i < this.length; i++)
+            callback(this[i]);
+    };
 
-    for(var i = 0; i < this.length - 1; i++)
-        string += this[i] + ',';
-    
-    string += this[this.length - 1];
+    Horroy.prototype.toString = function () {
+        var string = '';
 
-    return string;
-};
+        for (var i = 0; i < this.length - 1; i++)
+            string += this[i] + ',';
 
-Horroy.prototype.slice = function(a, b) {
+        string += this[this.length - 1];
+
+        return string;
+    };
+
+    Horroy.prototype.slice = function (a, b) {
         var newHorroy = new Horroy;
         if (a === undefined) {
             a = 0;
@@ -69,124 +73,120 @@ Horroy.prototype.slice = function(a, b) {
             return newHorroy;
         } else {
             for (var i = a; i < (b + 1); i++) {
-            newHorroy[i - a] = this[i];
+                newHorroy[i - a] = this[i];
             };
             return newHorroy;
         };
-        
+
     };
-    
-};
 
-Horroy.prototype.fill = function(value, start, end) {
-    if (arguments.length > 4) throw Error('too many arguments');
+    Horroy.prototype.fill = function (value, start, end) {
+        if (arguments.length > 4) throw Error('too many arguments');
 
-    if (!(this instanceof Horroy))
-        throw new TypeError(this + ' is not an horroy');
+        if (!(this instanceof Horroy))
+            throw new TypeError(this + ' is not an horroy');
 
-    start = start === undefined ? 0 : (start < 0 ? this.length + start : start);
-    end = end === undefined ? horroy.length : (end < 0 ? this.length + end : end);
+        start = start === undefined ? 0 : (start < 0 ? this.length + start : start);
+        end = end === undefined ? horroy.length : (end < 0 ? this.length + end : end);
 
-    for (var i = start; i < end; i++)
-        this[i] = value;
+        for (var i = start; i < end; i++)
+            this[i] = value;
 
-    return this;
-};
+        return this;
+    };
 
-Horroy.prototype.filter = function(callback) {
-    if (!(this instanceof Horroy)) throw TypeError(this + ' is not an horroy');
-    var newHorroy = new Horroy;
-    var count = 0;
-    for (var i = 0; i < this.length; i++) {
-        if (callback(this[i])) {
-            newHorroy[count] = this[i];
-            count++;
+    Horroy.prototype.filter = function (callback) {
+        if (!(this instanceof Horroy)) throw TypeError(this + ' is not an horroy');
+        var newHorroy = new Horroy;
+        var count = 0;
+        for (var i = 0; i < this.length; i++) {
+            if (callback(this[i])) {
+                newHorroy[count] = this[i];
+                count++;
+            }
         }
+        return newHorroy;
     }
-    return newHorroy;
-}
 
-Horroy.prototype.find = function (callback) {
-    if (!(this instanceof Horroy)) throw TypeError(this + ' is not an horroy');
-    for (var i = 0; i < this.length; i++) {
-        if (callback(this[i])) {
-            return this[i];
+    Horroy.prototype.find = function (callback) {
+        if (!(this instanceof Horroy)) throw TypeError(this + ' is not an horroy');
+        for (var i = 0; i < this.length; i++) {
+            if (callback(this[i])) {
+                return this[i];
+            };
         };
-    };
-}
-
-Horroy.prototype.findIndex = function (callback) {
-    if (!(this instanceof Horroy)) throw TypeError(this + ' is not an horroy');
-    for (var i = 0; i < this.length; i++) {
-        if (callback(this[i])) {
-            return i;
-        };
-    };
-    return -1;
-}
-
-Horroy.prototype.join = function (separator) {
-    var result = '';
-    if (!(this instanceof Horroy)) throw TypeError(this + 'is not an horroy');
-    separator = (separator === undefined) ? ',': separator;
-    for (var i = 0; i < this.length; i++) {
-        var element = this[i];
-        result += (i !== this.length - 1) ? element + separator : element; 
     }
-    return result;
-}
 
-Horroy.prototype.reduce = function (callback, counter) {
-    if (!(this instanceof Horroy)) throw TypeError(this + ' is not an horroy');
-    if (!(callback instanceof Function)) throw TypeError(callback + ' is not an function');
-    var i = 0;
-    if (counter === undefined) {
-        counter = this[0];
-        i = 1;
-    };
-    for (; i < this.length; i++) {
-        var item = this[i];
-        counter = callback(counter, item);
-    };
-    return counter;
-}
-
-Horroy.prototype.reverse = function () {
-    if (!(this instanceof Horroy)) throw TypeError(this + ' is not an horroy');
-    var reversed = [];
-    counter = this.length - 1
-    for (var i = 0; i < this.length; i++) {
-        reversed[counter] = this[i];
-        counter--;
-    };
-    return reversed;
-}
-
-Horroy.prototype.shift = function () {
-    var firstElement = "";
-    if (this === undefined) {
-        return undefined;
-    };
-    firstElement = this[0];
-    counter = 0;
-    for (var i = 1; i < this.length; i++) {
-        this[counter] = this[i];
-        counter++;
-    };
-    this.length = this.length -1;
-    return firstElement;
-}
-
-Horroy.prototype.some = function (callback) {
-    for (var i = 0; i < this.length; i++) {
-        if (callback(this[i])) {
-            return true;
+    Horroy.prototype.findIndex = function (callback) {
+        if (!(this instanceof Horroy)) throw TypeError(this + ' is not an horroy');
+        for (var i = 0; i < this.length; i++) {
+            if (callback(this[i])) {
+                return i;
+            };
         };
-    };
-    return false;
+        return -1;
+    }
 
-}
+    Horroy.prototype.join = function (separator) {
+        var result = '';
+        if (!(this instanceof Horroy)) throw TypeError(this + 'is not an horroy');
+        separator = (separator === undefined) ? ',' : separator;
+        for (var i = 0; i < this.length; i++) {
+            var element = this[i];
+            result += (i !== this.length - 1) ? element + separator : element;
+        }
+        return result;
+    }
 
-Horroy.prototype.unshift ()
+    Horroy.prototype.reduce = function (callback, counter) {
+        if (!(this instanceof Horroy)) throw TypeError(this + ' is not an horroy');
+        if (!(callback instanceof Function)) throw TypeError(callback + ' is not an function');
+        var i = 0;
+        if (counter === undefined) {
+            counter = this[0];
+            i = 1;
+        };
+        for (; i < this.length; i++) {
+            var item = this[i];
+            counter = callback(counter, item);
+        };
+        return counter;
+    }
 
+    Horroy.prototype.reverse = function () {
+        if (!(this instanceof Horroy)) throw TypeError(this + ' is not an horroy');
+        var reversed = [];
+        counter = this.length - 1
+        for (var i = 0; i < this.length; i++) {
+            reversed[counter] = this[i];
+            counter--;
+        };
+        return reversed;
+    }
 
+    Horroy.prototype.shift = function () {
+        var firstElement = "";
+        if (this === undefined) {
+            return undefined;
+        };
+        firstElement = this[0];
+        counter = 0;
+        for (var i = 1; i < this.length; i++) {
+            this[counter] = this[i];
+            counter++;
+        };
+        this.length = this.length - 1;
+        return firstElement;
+    }
+
+    Horroy.prototype.some = function (callback) {
+        for (var i = 0; i < this.length; i++) {
+            if (callback(this[i])) {
+                return true;
+            };
+        };
+        return false;
+
+    }
+
+};
