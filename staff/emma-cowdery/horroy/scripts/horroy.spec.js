b3af4e887af5756693de5c8117ce2da7ce3853d2
Emma Cowdery <e2maaa@gmail.com>
2019-01-23 09:10:07 +0100

    mas horror

--- staff/emma-cowdery/horroy/scripts/horroy.spec.js
+++ staff/emma-cowdery/horroy/scripts/horroy.spec.js
@@ -1,5 +1,10 @@
 describe('Horroy', function() {
-    describe('FROM', function() {
+    var horroy;
+
+    beforeEach(function() {
+        horroy = new Horroy(1, 2, 3, 4);
+    });
+    /*describe('FROM', function() {
         it('should create a Horroy from string', function() {
             var string = 'hola mundo';
 
@@ -15,19 +20,69 @@ describe('Horroy', function() {
             
             expect(horr.toString()).toBe(string.split("").toString());
         })
-    });
+    });*/
     describe('PUSH', function() {
         it('should add value to end of horroy', function() {
-            var horroy = new Horroy;
+            horroy.push(5);
             
-            horroy = [1, 2, 3, 4];
+            //var exp = [1, 2, 3, 4, 5];
             
-            var result = horroy.push(5);
-            
-            var expected = [1, 2, 3, 4, 5];
-            
-            expect(result.toBe.expected);
+            //expect(horroy).toBe.(exp);
+            //expect(horroy[4]).toBe.(5);
+            expect(horroy.length).toEqual(5);
         })
+    })
+
+    describe('POP', function() {
+        it('all arguments are correct', function() {
+            horroy.pop()
+            
+            var exp = 4;
+
+            expect(horroy.length).toBe(4);
+            expect(horroy.pop.toString).toBe(exp.toString);
+
+        })
+    })
+
+    describe('FOREACH', function() {
+        it('should execute callback for each horroy element', function() {
+            horroy.forEach(function(elements) {
+                return elements++;
+            })
+
+            var exp = [2, 3, 4, 5];
+
+            expect(horroy).toBe(exp);
+            expect(horroy.length).toBe(4);
+        })
+    })
+
+    describe('TOSTRING', function() {
+        it('should transform horroy to string', function() {
+            horroy.toString();
+
+            var exp = '1,2,3,4';
+
+            expect(horroy).toBe(exp);
+            expect(horroy instanceof String);
+        })
+    })
+
+    describe('SLICE', function() {
+        it('should create new horroy', function() {
+            var newHorroy = new Horroy();
+            newHorroy = horroy.slice(1);
+
+            var exp = [3, 4];
+
+            expect(horroy.slice(1)).toBe(exp);
+            expect(horroy).toBe([1, 2, 3, 4]);
+        })
+    })
+
+    describe('fill', function() {
+        it()
     })
 
 });
