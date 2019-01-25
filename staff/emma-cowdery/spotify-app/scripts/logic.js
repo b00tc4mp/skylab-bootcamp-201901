7307398baed5c6186220f01f1c041b60f3eb49e6
Emma Cowdery <e2maaa@gmail.com>
2019-01-25 18:07:19 +0100

    kdfk

---
+++ staff/emma-cowdery/spotify-app/scripts/logic.js
@@ -0,0 +1,12 @@
+const lohic = {
+    searchArtist(query, callback) {
+        if (typeof query !== 'string') throw TypeError(`${query} is not a string`)
+
+        if (!query.trim().length) throw Error('query is empty')
+
+        if (typeof callback !== 'function') throw TypeError(`${callback} is not a function`)
+
+        spotifyApi.searchArtist(query, callback)
+
+    }
+}
