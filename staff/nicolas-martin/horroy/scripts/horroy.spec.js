073d6d0959b5ca4aa25a89d29733fa9081c221e6
nico <nico@somoslaclave.com>
2019-01-26 11:41:25 +0100

    horroy indexof, map and improved push

--- staff/nicolas-martin/horroy/scripts/horroy.spec.js
+++ staff/nicolas-martin/horroy/scripts/horroy.spec.js
@@ -1,5 +1,4 @@
 describe('Horroy', function() {
-
     describe('from', function() {
         it('should create a Horroy from string', function() {
             var string = 'hola mundo';
@@ -60,16 +59,103 @@ describe('Horroy', function() {
             expect(horr.length).toBe(1);
             expect(horr[0]).toEqual([]);
         });
+    });
 
-        it('should return a Horroy from four arguments', function() {   
-            var horr = Horroy.of('1', '2', '3', '4');  
+    describe('concat', function(){
+        it('should returns a new Horroy joining 2 arrays', function() {   
+            var arr1 = [1, 2, 3];
+            var arr2 = [4, 5, 6];
+            var horr = new Horroy;
+            var resultHorr = horr.concat(arr1, arr2);
 
-            expect(horr);
-            expect(horr.length).toBe(4);
-            expect(horr[0]).toBe('1');
-            expect(horr[1]).toBe('2');
-            expect(horr[2]).toBe('3');
-            expect(horr[3]).toBe('4');
+            expect(resultHorr);
+            expect(resultHorr.length).toBe(6);
+            expect(resultHorr[3]).toEqual(4);
+        });
+
+        it('should returns the same array when there is no arguments', function() {   
+            var horr = new Horroy;
+            var resultHorr = horr.concat();
+
+            expect(resultHorr);
+            expect(resultHorr.length).toBe(0);
+            expect(resultHorr[1]).toBeUndefined();
+        });
+    });
+
+    describe('every', function () {
+        it('should returns true is all element are even', function () {
+            var horr = new Horroy(2, 4, 6, 8, 10);
+            var result = horr.every(function(value) {
+                return value % 2 === 0
+            });
+
+            expect(result).toBeTruthy();
+        });
+
+        it('should returns false is all element are odd', function () {
+            var horr = new Horroy(2, 4, 6, 8, 9);
+            var result = horr.every(function(value) {
+                return value % 2 === 0
+            });
+
+            expect(result).toBeFalsy();
+        })
+    });
+
+    describe('indexOf', function () {
+        it('should throw an error when arguments are bigger than 2', function () {
+            var horr = new Horroy;
+            expect(horr.indexOf('f', 3, 45, 5)).toThrowError('too many arguments', Error);
+            expect(horr.indexOf('f', 3, 45, 5)).toThrow('too many arguments');
+        });
+
+        it('should succeed and returns the position 2 for the letter "c"', function () {
+            var horr = new Horroy('a', 'b', 'c', 'd');
+            var indexOf = horr.indexOf('c');
+
+            expect(indexOf).toEqual(2);
+        });
+
+        it('should returns -1 when the search element is not found', function () {
+            var horr = new Horroy('a', 'b', 'c', 'd');
+            var indexOf = horr.indexOf('g');
+
+            expect(indexOf).toEqual(-1);
+        });
+    });
+
+    describe('map', function () {
+        it('succeed returnig a new array where every member is multiplicated by 2', function () {
+            var horr = new Horroy(1, 2, 3, 4, 5);
+            var resultHorr = horr.map(function (item) {
+                return item * 2;
+            });
+
+            expectedResult = [2, 4, 6, 8, 10];
+
+            expect(resultHorr.length).toEqual(5);
+            expect(resultHorr.toString()).toBe(expectedResult.toString());
+        });
+
+        it('succeed returnig an empty array from an empty array', function () {
+            var horr = new Horroy;
+            var resultHorr = horr.map(function (item) {
+                return item * 2;
+            });
+
+            expect(resultHorr.length).toEqual(0);
+        });
+    });
+
+    describe('toString', function () {
+        it('succeed returnig a new string from an array', function () {
+            var horr = new Horroy(1, 2, 3, 4, 5);
+            var resultHorr = horr.toString();
+            expectedResult = '1,2,3,4,5';
+
+            expect(horr.length).toEqual(5);
+            expect(resultHorr).toEqual(expectedResult);
         });
     });
 });
