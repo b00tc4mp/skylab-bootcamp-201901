3cc6902622fed2efcc195bb37efe857169a147dd
Pedro <pe->
2019-01-29 11:08:18 +0100

    update

--- staff/pedro-garriganogues/spotify-app/scripts/main.jsx
+++ staff/pedro-garriganogues/spotify-app/scripts/main.jsx
@@ -1,27 +1,59 @@
-spotifyApi.token = 'BQCnSDHCsm8GsQi_5Lc0XxKxjyDBHtSio2F-ss14hsU0XAf9KBdxQSjmcfc3qRWr7cwj7PcGVhMdsQNzPvdo3KnCAFO-511UHl77w6cK3aUOrOflnTB1d25RN5exwyTz9REo-Vj10eYn_NM'
+// spotifyApi.token = 'BQCnSDHCsm8GsQi_5Lc0XxKxjyDBHtSio2F-ss14hsU0XAf9KBdxQSjmcfc3qRWr7cwj7PcGVhMdsQNzPvdo3KnCAFO-511UHl77w6cK3aUOrOflnTB1d25RN5exwyTz9REo-Vj10eYn_NM'
 
-const searchPanel = new SearchPanel
-const artistsPanel = new ArtistsPanel
+// const searchPanel = new SearchPanel
+// const artistsPanel = new ArtistsPanel
 
-const $root = $('#root')
+// const $root = $('#root')
 
-artistsPanel.hide()
+// artistsPanel.hide()
 
-$root.append(searchPanel.$container)
-$root.append(artistsPanel.$container)
+// $root.append(searchPanel.$container)
+// $root.append(artistsPanel.$container)
 
-searchPanel.onSearch = function(query) {
-    try {
-        logic.searchArtists(query, function(error, artists) {
-            if (error) searchPanel.error = error.message
-            else {
-                artistsPanel.artists = artists
+class App extends React.Component {
+	state = { email: "", surname: "" }
 
-                artistsPanel.show()
-            }
-        })
-    } catch(err) {
+	handleEmailInput = event => this.setState({ email: event.target.value })
+	handlePasswordInput = event => this.setState({ password: event.target.value })
 
-    }
+	searchPanel = query => {
+		try {
+			logic.searchArtists(query, function(error, artists) {
+				if (error) searchPanel.error = error.message
+				else {
+					artistsPanel.artists = artists
+
+					artistsPanel.show()
+				}
+			})
+		} catch (err) {}
+	}
+
+	render() {
+		const {
+			state: { selectedLanguage, loginFeedback },
+			handleLanguageSelected,
+			handleLogin
+		} = this
+
+		const title = <h1>{i18n[selectedLanguage].title}</h1>
+
+		return (
+			<main className="app">
+				<LanguageSelector
+					selectedLanguage={selectedLanguage}
+					languages={[en]}
+					onLanguageSelected={handleLanguageSelected}
+				/>
+				{title}
+				<Login
+					title={i18n[selectedLanguage].loginTitle}
+					onLogin={handleLogin}
+					feedback={loginFeedback}
+				/>
+			</main>
+		)
+	}
 }
 
+ReactDOM.render(<App />, document.getElementById("root"))
