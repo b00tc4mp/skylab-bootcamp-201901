68d1beca244764a7cb6892bdaa8fe7997c6c0dee
Víctor Abadía <abadia.vic@gmail.com>
2019-01-29 12:32:58 +0100

    add new code

--- staff/victor-abadia/spotify-app/scripts/logic.spec.js
+++ staff/victor-abadia/spotify-app/scripts/logic.spec.js
@@ -1,8 +1,16 @@
-spotifyApi.token = 'BQCnSDHCsm8GsQi_5Lc0XxKxjyDBHtSio2F-ss14hsU0XAf9KBdxQSjmcfc3qRWr7cwj7PcGVhMdsQNzPvdo3KnCAFO-511UHl77w6cK3aUOrOflnTB1d25RN5exwyTz9REo-Vj10eYn_NM'
+/**
+ * logic.spec.js for Spotify App
+ * 
+ * Testing logic.js with Jasmine
+ * 
+ */
 
 describe('logic', function () {
+
+    /* searchArtists function */
+
     describe('search artists', function () {
-        it('should succeed on mathing query', function (done) {
+        it('should succeed on matching query', function (done) {
             const query = 'madonna'
 
             logic.searchArtists(query, function (error, artists) {
@@ -22,6 +30,78 @@ describe('logic', function () {
             const query = ''
 
             expect(() => logic.searchArtists(query, function (error, artists) { })).toThrowError('query is empty')
+        })
+    })
+
+    /* retrieveAlbums function */
+    
+    describe('retrieve albums', function () {
+        it('should succeed on matching query', function (done) {
+            const query = '2ye2Wgw4gimLv2eAKyk1NB'
+
+            logic.retrieveAlbums(query, function (error, albums) {
+                expect(error).toBeUndefined()
+
+                expect(albums).toBeDefined()
+                expect(albums instanceof Array).toBeTruthy()
+                expect(albums.length).toBeGreaterThan(0)
+
+                done()
+            })
+        })
+
+        it('should fail on empty query', function () {
+            const query = ''
+
+            expect(() => logic.retrieveAlbums(query, function (error, albums) { })).toThrowError('artist is empty')
+        })
+    })
+
+    /* retrieveTracks function */
+    
+    describe('retrieve tracks', function () {
+        it('should succeed on matching query', function (done) {
+            const query = '4bcUiX49wpmDRhrC8TvDWV'
+
+            logic.retrieveTracks(query, function (error, tracks) {
+                expect(error).toBeUndefined()
+
+                expect(tracks).toBeDefined()
+                expect(tracks instanceof Array).toBeTruthy()
+                expect(tracks.length).toBeGreaterThan(0)
+
+                done()
+            })
+        })
+
+        it('should fail on empty query', function () {
+            const query = ''
+
+            expect(() => logic.retrieveTracks(query, function (error, tracks) { })).toThrowError('album is empty')
+        })
+    })
+
+    /* retrieveTrack function */
+    
+    describe('retrieve track', function () {
+        it('should succeed on matching query', function (done) {
+            const query = '0QV3swr7L8MZr72lL2izV1'
+
+            logic.retrieveTrack(query, function (error, track) {
+                expect(error).toBeUndefined()
+
+                expect(track).toBeDefined()
+                expect(track instanceof Object).toBeTruthy()
+                
+
+                done()
+            })
+        })
+
+        it('should fail on empty query', function () {
+            const query = ''
+
+            expect(() => logic.retrieveTrack(query, function (error, tracks) { })).toThrowError('track is empty')
         })
     })
 })
