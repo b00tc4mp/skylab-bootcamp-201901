1a53d1ba8f22c32c4e18f4bf738e285dfd8674c1
Tachi <jrfluna8@gmail.com>
2019-01-30 17:50:54 +0100

    node

---
+++ staff/tachi-fernandez/spotify-app-react-scripts/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './components/HelloWorld'
+import Feedback from '.components/Feedback'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <HelloWorld name = "Tachi" />
+        <Feedback message="normal feedback" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
