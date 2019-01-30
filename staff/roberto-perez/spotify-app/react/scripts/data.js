e8be034dedfb8d3f27fd9182bdacb07aafa40f84
robertz <robertz.perez1985@gmail.com>
2019-01-30 14:48:03 +0100

    Add favorite event in track

--- staff/roberto-perez/spotify-app/react/scripts/data.js
+++ staff/roberto-perez/spotify-app/react/scripts/data.js
@@ -3,12 +3,12 @@
 //#region data
 
 var users = [
-    { name: 'Manuel', surname: 'Barzi', email: 'manuelbarzi@gmail.com', password: 'p' },
-    { name: 'John', surname: 'Doe', email: 'johndoe@mail.com', password: '12345' },
-    { name: 'Jane', surname: 'Doe', email: 'janedoe@mail.com', password: '54321' },
-    { name: 'Cama', surname: 'Ron', email: 'camaron@mail.com', password: 'delaisla' },
-    { name: 'Rain', surname: 'Bow', email: 'rainbow@mail.com', password: 'arcoiris' },
-    { name: 'Hul', surname: 'Io', email: 'hulio@mail.com', password: 'vivaerbetih' }
+    { name: 'Manuel', surname: 'Barzi', email: 'manuelbarzi@gmail.com', password: 'p', favorites: [] },
+    { name: 'John', surname: 'Doe', email: 'johndoe@mail.com', password: '12345', favorites: [] },
+    { name: 'Jane', surname: 'Doe', email: 'janedoe@mail.com', password: '54321', favorites: [] },
+    { name: 'Cama', surname: 'Ron', email: 'camaron@mail.com', password: 'delaisla', favorites: [] },
+    { name: 'Rain', surname: 'Bow', email: 'rainbow@mail.com', password: 'arcoiris', favorites: [] },
+    { name: 'Hul', surname: 'Io', email: 'hulio@mail.com', password: 'vivaerbetih', favorites: [] }
 ];
 
 //#endregion
