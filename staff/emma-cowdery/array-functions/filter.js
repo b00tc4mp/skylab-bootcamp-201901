896cb0f3ad0064ee3df76b482ca6c29d37337663
Emma Cowdery <e2maaa@gmail.com>
2019-01-31 09:31:02 +0100

    doneee

--- staff/emma-cowdery/array-functions/filter.js
+++ staff/emma-cowdery/array-functions/filter.js
@@ -13,7 +13,6 @@ function filter(array, callback) {
     for (var i = 0; i < array.length; i++) {
         if (callback(array[i])) {
             newArray[newArray.length] = array[i];
-            newArray.length++;
         }
     }
     return newArray;
