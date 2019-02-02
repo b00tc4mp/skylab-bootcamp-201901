742d0bb98bd187fbbcaf64fae416ec53c00ce941
Ury18 <ury_98@hotmail.com>
2019-02-02 14:42:04 +0100

    Poekmon api v1

--- staff/groups/funciton/funciton-pokedex/src/apipokemon/index.spec.js
+++ staff/groups/funciton/funciton-pokedex/src/apipokemon/index.spec.js
@@ -56,5 +56,62 @@ describe('pokemon API', () => {
                 })
                 .catch(error => expect(error).toBeUndefined())
         })
+        it('It should fail on a wrong query parameter - boolean', () =>{
+            expect(() => pokemonApi.searchPokemonsByType(false).toThrowError())
+        })
+        it('It should fail on a wrong query parameter - object', () =>{
+            const query = {}
+            expect(() => pokemonApi.searchPokemonsByType(query).toThrowError())
+        })
+        it('It should fail on a wrong query parameter - number', () =>{
+            const query = 12
+            expect(() => pokemonApi.searchPokemonsByType(query).toThrowError())
+        })
+        it('It should fail on a wrong query parameter - Array', () =>{
+            const query = [12, 22]
+            expect(() => pokemonApi.searchPokemonsByType(query).toThrowError())
+        })
+        it('It should fail on a wrong query parameter - Undefined', () =>{
+            const query = undefined
+            expect(() => pokemonApi.searchPokemonsByType(query).toThrowError())
+        })
+        it('It should fail on a wrong query parameter - null', () =>{
+            const query = null
+            expect(() => pokemonApi.searchPokemonsByType(query).toThrowError())
+        })
+    })
+    describe('search for pokemon by ability', () => {
+        it('should succeed on retrieving pokemon ability', () =>{
+            let query = 'limber';
+            return pokemonApi.searchPokemonsByAbility(query)
+                .then((result) => {
+                    expect(result.name).toBe('limber')
+                    expect(result.pokemon.length).toBe(12)
+                })
+                .catch(error => expect(error).toBeUndefined())
+        })
+        it('It should fail on a wrong query parameter - boolean', () =>{
+            expect(() => pokemonApi.searchPokemonsByAbility(false).toThrowError())
+        })
+        it('It should fail on a wrong query parameter - object', () =>{
+            const query = {}
+            expect(() => pokemonApi.searchPokemonsByAbility(query).toThrowError())
+        })
+        it('It should fail on a wrong query parameter - number', () =>{
+            const query = 12
+            expect(() => pokemonApi.searchPokemonsByAbility(query).toThrowError())
+        })
+        it('It should fail on a wrong query parameter - Array', () =>{
+            const query = [12, 22]
+            expect(() => pokemonApi.searchPokemonsByAbility(query).toThrowError())
+        })
+        it('It should fail on a wrong query parameter - Undefined', () =>{
+            const query = undefined
+            expect(() => pokemonApi.searchPokemonsByAbility(query).toThrowError())
+        })
+        it('It should fail on a wrong query parameter - null', () =>{
+            const query = null
+            expect(() => pokemonApi.searchPokemonsByAbility(query).toThrowError())
+        })
     })
 })
