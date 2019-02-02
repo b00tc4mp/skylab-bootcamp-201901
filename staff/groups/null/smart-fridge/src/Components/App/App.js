94ac37f7086d32661eadd2ecdf032ecf22106d6f
martimalek <martimalekdot@hotmail.es>
2019-02-02 15:01:55 +0100

    updated app

--- staff/groups/null/smart-fridge/src/Components/App/App.js
+++ staff/groups/null/smart-fridge/src/Components/App/App.js
@@ -1,27 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+/* import Home from '../Home' */
+/* import Welcome from '../Welcome' */
+import { BrowserRouter, Route } from 'react-router-dom'
 
 class App extends Component {
+
+  state = { user: null }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+    const { state: { user } }
+
+    return <BrowserRouter>
+            <main className="app">
+                {/*!user && <Welcome /> */}
+                {/*user && <Home /> */}
+            </main>
+        </BrowserRouter>
   }
 }
 
