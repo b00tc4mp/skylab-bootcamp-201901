a2a1142e945c95b5ae3b9b65108e90580b3b60b0
Marc Sarroca Diaz <msarroca@Marcs-MacBook-Pro.local>
2019-02-02 19:23:54 +0100

    search-v1

--- staff/groups/funciton/funciton-pokedex/src/apipokemon/index.js
+++ staff/groups/funciton/funciton-pokedex/src/apipokemon/index.js
@@ -2,74 +2,86 @@
 const path = require('path');
 /**
  * Pokemon API Client
- * 
+ *
  * @version 1.0.0
  */
 const pokemonApi = {
 
-    apiURL : 'https://pokeapi.co/api/v2/',
+  apiURL: 'https://pokeapi.co/api/v2/',
 
-    /**
-     * Searches a pokemon 
-     * 
-     * @param {string} name - The text to match on artists search.
-     * 
-     * the query to the API returns large amount of raw data
-     */
-    searchPokemonByName(query){
-        const searchCriteria = 'pokemon/'
-        if (typeof query !== 'string') throw TypeError(`${query} is not a string`)
-        if (!query.trim().length) throw Error('query is empty')
+  /**
+   * Searches a pokemon
+   *
+   * @param {string} name - The text to match on artists search.
+   *
+   * the query to the API returns large amount of raw data
+   */
 
-        return fetch(`${this.apiURL}${searchCriteria}${query}`, {
+  searchAllPokemons() {
+    const searchCriteria = "pokemon/?limit=151";
+    return fetch(`${this.apiURL}${searchCriteria}`, {})
+      .then(res => {
+        if (res == null) throw Error("This pokemon does not exist");
+        return res.json();
+      })
+      .then(res => res.results);
+  },
 
-        })
-        .then(res => {
-            if(res == null) throw Error ('This pokemon does not exist')
-            return res.json()})
-        .then(res => {
+  searchPokemonByName(query) {
+    const searchCriteria = 'pokemon/'
+    if (typeof query !== 'string') throw TypeError(`${query} is not a string`)
+    if (!query.trim().length) throw Error('query is empty')
 
-            const name = res.forms[0].name
-            const image = res.sprites.front_default
-            const types = res.types.map(type => type.type.name)
-            const result = {name, image, types}
-            
-            return result
-        })
-    },
+    return fetch(`${this.apiURL}${searchCriteria}${query}`, {
 
-    searchPokemonsByType(query){
-        const searchCriteria = 'type/';
+    })
+      .then(res => {
+        if (res == null) throw Error('This pokemon does not exist')
+        return res.json()
+      })
+      .then(res => {
 
-        if (typeof query !== 'string') throw TypeError(`${query} is not a string`)
-        if (!query.trim().length) throw Error('query is empty')
+        const name = res.forms[0].name
+        const image = res.sprites.front_default
+        const types = res.types.map(type => type.type.name)
+        const result = { name, image, types }
 
-        return fetch(`${this.apiURL}${searchCriteria}${query}`, {
+        return result
+      })
+  },
 
-        })
-        .then(res => {
-            if(res == null) throw Error ('This type of pokemon does not exist')
-            return res.json()
-        })
-        .then(res => res.pokemon)
+  searchPokemonsByType(query) {
+    const searchCriteria = 'type/';
 
-    },
-    searchPokemonsByAbility(query){
-        const searchCriteria = 'ability/';
+    if (typeof query !== 'string') throw TypeError(`${query} is not a string`)
+    if (!query.trim().length) throw Error('query is empty')
 
-        if (typeof query !== 'string') throw TypeError(`${query} is not a string`)
-        if (!query.trim().length) throw Error('query is empty')
+    return fetch(`${this.apiURL}${searchCriteria}${query}`, {
 
-        return fetch(`${this.apiURL}${searchCriteria}${query}`, {
+    })
+      .then(res => {
+        if (res == null) throw Error('This type of pokemon does not exist')
+        return res.json()
+      })
+      .then(res => res.pokemon)
 
-        })
-        .then(res => {
-            if(res == null) throw Error ('This type of pokemon does not exist')
-            return res.json()
-        })
-        .then(res => res)
+  },
+  searchPokemonsByAbility(query) {
+    const searchCriteria = 'ability/';
 
-    }
+    if (typeof query !== 'string') throw TypeError(`${query} is not a string`)
+    if (!query.trim().length) throw Error('query is empty')
+
+    return fetch(`${this.apiURL}${searchCriteria}${query}`, {
+
+    })
+      .then(res => {
+        if (res == null) throw Error('This type of pokemon does not exist')
+        return res.json()
+      })
+      .then(res => res)
+
+  }
 
 }
 
