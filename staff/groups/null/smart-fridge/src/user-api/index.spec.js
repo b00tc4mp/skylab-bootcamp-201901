953e9e08d51cf391708baf0b00339e1d5f2da39e
martimalek <martimalekdot@hotmail.es>
2019-02-02 20:32:21 +0100

    user api

---
+++ staff/groups/null/smart-fridge/src/user-api/index.spec.js
@@ -0,0 +1,167 @@
+'use strict'
+
+import userApi from '.'
+
+describe('user api', () => {
+
+    describe('register', () => {
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const username = `manuelbarzi-${Math.random()}`
+        const password = '123'
+        const gender = 'female'
+        const birthDate = '1985/10/15'
+        const height = 161
+        const weight = 60
+        const lifeStyle = 'sedentary'
+
+        it('should succeed on correct data', () =>
+            userApi.register(name, surname, username, password, password, gender, birthDate, height, weight, lifeStyle)
+                .then(id => expect(id).toBeDefined())
+        )
+
+        it('should fail on already existing user', () =>
+            userApi.register(name, surname, username, password, password, gender, birthDate, height, weight, lifeStyle)
+                .then(() => {
+                    throw Error('should not have passed by here')
+                })
+                .catch(error => {
+                    expect(error).toBeDefined()
+                    expect(error.message).toBe(`user with username \"${username}\" already exists`)
+                })
+        )
+
+        // TODO more unit test cases
+    })
+
+    describe('authenticate', () => {
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const username = `manuelbarzi-${Math.random()}`
+        const password = '123'
+        const gender = 'female'
+        const birthDate = '1985/10/15'
+        const height = 161
+        const weight = 60
+        const lifeStyle = 'sedentary'
+
+        let _id
+
+        beforeEach(() =>
+            userApi.register(name, surname, username, password, password, gender, birthDate, height, weight, lifeStyle)
+                .then(id => _id = id)
+        )
+
+        it('should succeed on correct data', () =>
+            userApi.authenticate(username, password)
+                .then(({ id, token }) => {
+                    expect(id).toBe(_id)
+                    expect(token).toBeDefined()
+                })
+        )
+
+        // TODO more unit test cases
+    })
+
+    describe('retrieve', () => {
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const username = `manuelbarzi-${Math.random()}`
+        const password = '123'
+        const gender = 'female'
+        const birthDate = '1985/10/15'
+        const height = 161
+        const weight = 60
+        const lifeStyle = 'sedentary'
+
+        let _id, _token
+
+        beforeEach(() =>
+            userApi.register(name, surname, username, password, password, gender, birthDate, height, weight, lifeStyle)
+                .then(id => _id = id)
+                .then(() => userApi.authenticate(username, password))
+                .then(({ token }) => _token = token)
+        )
+
+        it('should succeed on correct data', () =>
+            userApi.retrieve(_id, _token)
+                .then(user => {
+                    expect(user.id).toBe(_id)
+                    expect(user.name).toBe(name)
+                    expect(user.surname).toBe(surname)
+                    expect(user.username).toBe(username)
+                })
+        )
+
+        // TODO more unit test cases
+    })
+
+    describe('update', () => {
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const username = `manuelbarzi-${Math.random()}`
+        const password = '123'
+        const gender = 'female'
+        const birthDate = '1985/10/15'
+        const height = 161
+        const weight = 60
+        const lifeStyle = 'sedentary'
+
+        let _id, _token
+
+        beforeEach(() =>
+            userApi.register(name, surname, username, password, password, gender, birthDate, height, weight, lifeStyle)
+                .then(id => _id = id)
+                .then(() => userApi.authenticate(username, password))
+                .then(({ token }) => _token = token)
+        )
+
+        it('should succeed on correct data', () => {
+            const data = { name: 'Pepito', surname: 'Grillo', age: 32 }
+
+            return userApi.update(_id, _token, data)
+                .then(() => userApi.retrieve(_id, _token))
+                .then(user => {
+                    expect(user.id).toBe(_id)
+                    expect(user.name).toBe(data.name)
+                    expect(user.surname).toBe(data.surname)
+                    expect(user.age).toBe(data.age)
+                    expect(user.username).toBe(username)
+                })
+        })
+
+        // TODO more unit test cases
+    })
+
+    describe('remove', () => {
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const username = `manuelbarzi-${Math.random()}`
+        const password = '123'
+        const gender = 'female'
+        const birthDate = '1985/10/15'
+        const height = 161
+        const weight = 60
+        const lifeStyle = 'sedentary'
+
+        let _id, _token
+
+        beforeEach(() =>
+            userApi.register(name, surname, username, password, password, gender, birthDate, height, weight, lifeStyle)
+                .then(id => _id = id)
+                .then(() => userApi.authenticate(username, password))
+                .then(({ token }) => _token = token)
+        )
+
+        it('should succeed on correct data', () => {
+            return userApi.remove(_id, _token, username, password)
+                .then(() => userApi.retrieve(_id, _token))
+                .then(() => {
+                    throw Error('should not pass by here')
+                })
+                .catch(({message}) => expect(message).toBe(`user with id \"${_id}\" does not exist`))
+        })
+
+        // TODO more unit test cases
+    })
+})
