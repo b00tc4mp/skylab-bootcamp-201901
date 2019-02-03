a486df4375e21b7fede5edfeae9e8f891afb08b8
Àlex Barba <alexander.barba@gmail.com>
2019-02-03 20:47:30 +0100

    new layout landing, login and form

--- staff/groups/wtf/marvel-app/src/components/Landing/index.js
+++ staff/groups/wtf/marvel-app/src/components/Landing/index.js
@@ -15,15 +15,20 @@ class Landing extends Component {
 
         const {handleOnLogin, handleOnRegsitration} = this
 
-        return <section className="container ">
+        return <section className="container">
             <div className="columns is-mobile is-centered has-text-centered">
-                <div className="column is-half-widescreen is-half-tablet is-three-quarters-mobile is-centered">
-                    <h1 className="title is-1 white">Welcome to Marvel Studios!</h1>
-                    <div className="is-grouped btn_grp">
-                            <p className="control"><a href="#" onClick={handleOnLogin} className="button is-danger is-outlined is-small is-rounded">Log in</a></p>
-                            <p className="control"><a href="#" onClick={handleOnRegsitration} className="button is-dark is-small is-rounded">Sign Up</a></p>
-                    </div>             
-                </div>   
+                <div className="modal is-active is-clipped">
+                    <div className="modal-background"></div>
+                    <div className="modal-content column is-half-widescreen is-three-fifths-tablet is-three-quarters-mobile is-centered">
+                        <h1 className="title is-1 white">Welcome to Marvel App</h1>
+                        <h5 className="subtitle is-5 white">Your free Marvel repository</h5>
+                        <div className="is-grouped btn_grp">
+                                <p className="control"><button onClick={handleOnLogin} className="button is-danger is-outlined is-small is-rounded">Log in</button></p>
+                                <p>&nbsp;</p>
+                                <p className="control"><button onClick={handleOnRegsitration} className="button is-inverted is-outlined is-danger is-small is-rounded">Sign Up</button></p>
+                        </div>   
+                    </div>
+                </div>          
             </div>
         </section>
     }
