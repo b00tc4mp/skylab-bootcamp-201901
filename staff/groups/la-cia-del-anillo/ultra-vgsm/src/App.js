eef8627f3cdec350dec0cef38751d321d2e05443
Luis <quinwacca@gmail.com>
2019-02-03 23:21:25 +0100

    LOGIC: retrieve game by gameId, and retrieve Images by gameId

--- staff/groups/la-cia-del-anillo/ultra-vgsm/src/App.js
+++ staff/groups/la-cia-del-anillo/ultra-vgsm/src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a>         
         </header>
       </div>
     );
