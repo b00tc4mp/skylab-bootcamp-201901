62b5ced451996e3bb3967fd689162e34dddf056e
nico <nico@somoslaclave.com>
2019-02-03 12:15:18 +0100

    omdb api with mininum tests

--- staff/groups/undefined/undefined/src/ombd-api/index.js
+++ staff/groups/undefined/undefined/src/ombd-api/index.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 // http://www.omdbapi.com/?apikey=ef8a2f56&s=madonna
 
 /**
@@ -9,7 +8,7 @@
  * @version 1.0.0
  */
 const omdbApi = {
-    api: 'ef8a2f56',
+    key: 'NO-KEY',
     url: `http://www.omdbapi.com/?apikey=ef8a2f56`,
 
      /**
@@ -24,6 +23,28 @@ const omdbApi = {
      * //http://www.omdbapi.com/?apikey=ef8a2f56&s=titanic
      */
 
+    // **** search for query with 10 results
+    // {  
+    //     "Search": [
+    //         {
+    //             "Title": "Titanic",
+    //             "Year": "1997",
+    //             "imdbID": "tt0120338",
+    //             "Type": "movie",
+    //             "Poster": "https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+    //         },
+    //         // +9 items
+    //     ],
+    //     "totalResults": "174",
+    //     "Response": "True"
+    // }
+
+    // ***** search for query with no results
+    // {
+    //     "Response": "False",
+    //     "Error": "Movie not found!"
+    // }
+
     searchItems(query) { 
         if (typeof query !== 'string') throw TypeError(`${query} is not a string`)
         if (!query.trim().length) throw Error('query is empty')
@@ -31,7 +52,7 @@ const omdbApi = {
         return fetch(`${this.url}&s=${query}`)
             .then(response => response.json())
             .then(response => {
-                if (!response.Response) throw Error(response.Reponse)
+                if (!response.Response) throw Error(response.Error)
                 console.log(response.Search)
                 return response.Search
         })
@@ -47,47 +68,63 @@ const omdbApi = {
      * @throws {Error} - On empty parameters value.
      * 
      * // http://www.omdbapi.com/?apikey=ef8a2f56&i=tt1632708
+     * 
+     * 
+     
+     /* retrieve item with imdbID = tt0081400
+     * 
+     * {    Title: "Raise the Titanic", Year: "1980", Rated: "PG", Released: "01 Aug 1980", Runtime: "115 min", …}
+            Actors: "Jason Robards, Richard Jordan, David Selby, Anne Archer"
+            Awards: "3 nominations."
+            BoxOffice: "N/A"
+            Country: "UK, USA"
+            DVD: "21 Jan 2014"
+            Director: "Jerry Jameson"
+            Genre: "Action, Drama, Thriller, Adventure"
+            Language: "English"
+            Metascore: "N/A"
+            Plot: "To obtain a supply of a rare mineral, a ship raising operation is conducted for the only known source, the R.M.S. Titanic."
+            Poster: "https://m.media-amazon.com/images/M/MV5BM2MyZWYzOTQtMTYzNC00OWIyLWE2NWItMzMwODA0OGQ2ZTRkXkEyXkFqcGdeQXVyMjI4MjA5MzA@._V1_SX300.jpg"
+            Production: "Associated Film Distribution"
+            Rated: "PG"
+            Ratings: (2) [{…}, {…}]
+            Released: "01 Aug 1980"
+            Response: "True"
+            Runtime: "115 min"
+            Title: "Raise the Titanic"
+            Type: "movie"
+            Website: "N/A"
+            Writer: "Adam Kennedy (screenplay), Eric Hughes (adaptation), Clive Cussler (novel)"
+            Year: "1980"
+            imdbID: "tt0081400"
+            imdbRating: "4.8"
+            imdbVotes: "3,460"
+
+        }
+
+        retrieve item with non exists ID = asdcasdc 
+        {
+            "Response": "False",
+            "Error": "Movie not found!"
+        }
      */
+
+    // DUDA - no debería lanzar un error este método??
+    // por qué devuelve una Promise, se maneja desde la lógica?
+    // DUDA: la validación de datos de entrada se debe hacer tanto en la lógica como 
+    // en esta API? sólo en un lado? xq? puede llegar un itemId que NO existe?
+
     retrieveItem(itemId) {
         if (typeof itemId !== 'string') throw TypeError(`${itemId} is not a string`)
         if (!itemId.trim().length) throw Error('itemId is empty')
 
         return fetch(`${this.url}&i=${itemId}`)
             .then(item => item.json())
-            .then(item => console.log(item))
-        
+            .then(response => {
+                if (!response.Response) throw Error(response.Error)
+                return response.Search
+            })
     }
-
-    // {
-    //     Actors: "Jason Robards, Richard Jordan, David Selby, Anne Archer",
-    //     Awards: "3 nominations.",
-    //     BoxOffice: "N/A",
-    //     Country: "UK, USA",
-    //     DVD: "21 Jan 2014",
-    //     Director: "Jerry Jameson",
-    //     Genre: "Action, Drama, Thriller, Adventure",
-    //     Language: "English",
-    //     Metascore: "N/A",
-    //     Plot: "To obtain a supply of a rare mineral, a ship raising operation is conducted for the only known source, the R.M.S. Titanic.",
-    //     Poster: "https://m.media-amazon.com/images/M/MV5BM2MyZWYzOTQtMTYzNC00OWIyLWE2NWItMzMwODA0OGQ2ZTRkXkEyXkFqcGdeQXVyMjI4MjA5MzA@._V1_SX300.jpg",
-    //     Production: "Associated Film Distribution",
-    //     Rated: "PG",
-    //     Ratings:    [
-    //                     {Source: "Internet Movie Database", Value: "4.8/10"}.
-    //                     {Source: "Rotten Tomatoes", Value: "43%"}
-    //                 ],
-    //     Released: "01 Aug 1980",
-    //     Response: "True",
-    //     Runtime: "115 min",
-    //     Title: "Raise the Titanic",
-    //     Type: "movie",
-    //     Website: "N/A",
-    //     Writer: "Adam Kennedy (screenplay), Eric Hughes (adaptation), Clive Cussler (novel)",
-    //     Year: "1980",
-    //     imdbID: "tt0081400",
-    //     imdbRating: "4.8",
-    //     imdbVotes: "3,460",
-    // }
 }
 
 omdbApi.searchItems('titanic')
--- staff/groups/undefined/undefined/src/ombd-api/index.spec.js
+++ staff/groups/undefined/undefined/src/ombd-api/index.spec.js
@@ -2,18 +2,63 @@
 
 import omdbApi from '.'
 
-//const { env: { OMDB_API } } = process
+const { env: { OMDB_API } } = process
 
 omdbApi.api = 'ef8a2f56'
 
 describe('searchItems', () => {
-    const query = 'titanic'
+    
     it ('should succeed on matching query', () => {
+        const query = 'titanic'
         return omdbApi.searchItems(query)
             .then(items => {
                 expect(items).toBeDefined()
-                //expect(items instanceof 'Array').toBeTruthy()
-                //expect(items.length).toBe(10)
+                expect(items instanceof 'Array').toBeTruthy()
+                expect(items.length).toBe(10)
             }) 
+    })
+
+    it ('should fail on empty query', () => {
+        const query = ''
+        expect(() => omdbApi.searchItems(query).toThrowError('query is empty'))
+    })
+})
+
+
+// DUDA: cómo manejar estas mayúcuslas?
+// tenemos que convertir las keys del objeto a toLowerCase()
+
+describe('retrieveItem', () => {
+    
+    // expected object - test some fields
+    const expectedResult = {
+        Title: "Raise the Titanic",
+        Actors: "Jason Robards, Richard Jordan, David Selby, Anne Archer",
+        Runtime: "115 min",
+        imdbID: "tt0081400",
+        imdbRating: "4.8"
+    }
+    const itemId = expectedResult.imdbID
+
+    it('should retrieve a movie or serie item object with valid id', () => { //DUDA: xq hace un return aquí?
+        return omdbApi.retrieveItem(itemId)
+                .then(({ Title, Actors, Runtime, imdbID, imdbRating }) => {
+                    expect(Title).toBe(expectedResult.Title)
+                    expect(Actors).toBe(expectedResult.Actors)
+                    expect(Runtime).toBe(expectedResult.Runtime)
+                    expect(imdbID).toBe(expectedResult.imdbID)
+                    expect(imdbRating).toBe(expectedResult.imdbRating)
+                })
+    })
+
+    // DUDA: qué es lo que hace el try catch aquí? el expect o el it?
+    it('should fail on empty itemID', () => {
+        const itemId = ''
+        expect(() => omdbApi.retrieveItem(itemId).toThrowError('itemId is empty'))
+    })
+
+    it('should fail on wrong itemID parameter', () => {
+        const itemId = 'wwwwwwwww'
+        expect(() => omdbApi.retrieveItem(itemId).toThrowError('Movie not found!'))
     })
 })
