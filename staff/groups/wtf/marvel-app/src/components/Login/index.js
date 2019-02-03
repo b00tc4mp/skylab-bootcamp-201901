1c799697146c42774a9fa9c38b6b2a912ef91fc6
Àlex Barba <alexander.barba@gmail.com>
2019-02-03 20:47:30 +0100

    new layout landing, login and form

--- staff/groups/wtf/marvel-app/src/components/Login/index.js
+++ staff/groups/wtf/marvel-app/src/components/Login/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import React, {Component} from 'react'
+import { withRouter, Route } from 'react-router-dom'
 // import Feedback from '../Feedback'
 
 class Login extends Component {
@@ -18,50 +19,45 @@ class Login extends Component {
         onLogin(email, password)
     }
 
-    handleOnRegister = () => {
-        const { props: {onToRegister} } = this
-
-        onToRegister()
-    }
+    handleOnRegistration = () => this.props.history.push('/register')
     
     render() {
 
-        const { handleEmailInput, handlePasswordInput, handleFormSubmit, handleOnRegister, props: {feedback} } = this
+        const { handleEmailInput, handlePasswordInput, handleFormSubmit, handleOnRegistration, props: {feedback} } = this
         
         return <section className="login container" >
         <div className="columns is-mobile is-centered">
-            <form className="login__form column is-half-widescreen is-half-tablet is-three-quarters-mobile is-centered" onSubmit={handleFormSubmit}>
-                <h4 className="subtitle is-4">Log in</h4>
-                <div className="field">
-                    <p className="control has-icons-left has-icons-right">
-                        <input className="input is-small is-rounded" type="email" name="email" placeholder="Email" required onChange={handleEmailInput}/>
-                        <span className="icon is-small is-left">
-                            <i className="fas fa-envelope"></i>
-                        </span>
-                        <span className="icon is-small is-right">
-                            <i className="fas fa-check"></i>
-                        </span>
-                    </p>
+            <div className="modal is-active is-clipped">
+                <div className="modal-background"></div>
+                <div className="modal-content column is-half-widescreen is-three-fifths-tablet is-three-quarters-mobile is-centered">
+                    <form className="login__form" onSubmit={handleFormSubmit}>
+                        <h4 className="subtitle is-4 white">Log in</h4>
+                        <div className="field">
+                            <p className="control has-icons-left has-icons-right">
+                                <input className="input is-small is-rounded" type="email" name="email" placeholder="Email" required onChange={handleEmailInput}/>
+                                <span className="icon is-small is-left">
+                                    <i className="fas fa-envelope"></i>
+                                </span>
+                                <span className="icon is-small is-right">
+                                    <i className="fas fa-check"></i>
+                                </span>
+                            </p>
+                        </div>
+                        <div className="field">
+                            <p className="control has-icons-left">
+                                <input className="input is-small is-rounded" type="password" name="password" placeholder="Password" required onChange={handlePasswordInput} />
+                                <span className="icon is-small is-left">
+                                    <i className="fas fa-lock"></i>
+                                </span>
+                            </p>
+                        </div>
+                        <div className="field is-grouped btn_grp">
+                            <p className="control"><button className="button is-outlined is-danger is-small is-rounded" type="submit">Log in</button></p>
+                            <p className="control"><a href="#" onClick={handleOnRegistration} className="button is-inverted is-outlined is-danger is-small is-rounded">Sign Up</a></p>
+                        </div>
+                    </form>
                 </div>
-                <div className="field">
-                    <p className="control has-icons-left">
-                        <input className="input is-small is-rounded" type="password" name="password" placeholder="Password" required onChange={handlePasswordInput} />
-                        <span className="icon is-small is-left">
-                            <i className="fas fa-lock"></i>
-                        </span>
-                    </p>
-                </div>
-                <div className="field is-grouped btn_grp">
-                    <p className="control">
-                        <button className="button is-success is-small is-rounded" type="submit">
-                        Log in
-                        </button>
-                    </p>
-                    <p className="control"
-                        ><a href="#" onClick={handleOnRegister} className="button is-outlined is-small is-rounded" >Sign Up</a>
-                    </p>
-                </div>
-            </form>
+            </div> 
         </div>
         {/* {feedback && <Feedback message={feedback} />} */}
     </section> 
@@ -69,4 +65,4 @@ class Login extends Component {
 
 }
 
-export default Login
+export default withRouter(Login)
--- staff/groups/wtf/marvel-app/src/components/Register/index.js
+++ staff/groups/wtf/marvel-app/src/components/Register/index.js
@@ -1,9 +1,10 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
+import { withRouter, Route } from 'react-router-dom'
 // import Feedback from '../Feedback'
 
-class Register extends React.Component {
+class Register extends Component {
     state = {name:'', surname:'', email:'', password:'', passwordConfirmation:''}
 
     handleNameInput = event => this.setState({name: event.target.value})
@@ -16,7 +17,7 @@ class Register extends React.Component {
 
     handlePasswordConfirmationInput = event => this.setState({passwordConfirmation: event.target.value})
 
-    handleOnRegistration = event => {
+    handleFormSubmit = event => {
         event.preventDefault()
 
         const { state: { name, surname, email, password, passwordConfirmation}, props: {onRegistration} } = this
@@ -24,85 +25,78 @@ class Register extends React.Component {
         onRegistration(name, surname, email, password, passwordConfirmation)
     }
 
-    handleOnLogin = () => {
-        const { props: {onToLogin} } = this
-
-        onToLogin()
-    }
+    handleOnLogin = () => this.props.history.push('/login')
 
     render() {
-         const { handleOnLogin, handleOnRegistration, handleNameInput, handleSurnameInput, handleEmailInput, handlePasswordInput, handlePasswordConfirmationInput, props: {feedback}  } = this
+         const { handleOnLogin, handleFormSubmit, handleNameInput, handleSurnameInput, handleEmailInput, handlePasswordInput, handlePasswordConfirmationInput, props: {feedback}  } = this
     
-        return <section className="register container margin-top">
-        <div className="columns is-mobile is-centered">
-            <form onSubmit={handleOnRegistration} className="register__form column is-half-widescreen is-half-tablet is-three-quarters-mobile is-centered">
-                <h4 className="subtitle is-4">Sign Up</h4>
-                <div className="field">
-                    <p className="control has-icons-left has-icons-right">
-                        <input className="input is-small is-rounded" type="text" name="name" placeholder="Name" required onChange={handleNameInput} />
-                        <span className="icon is-small is-left">
-                            <i className="far fa-user"></i>
-                        </span>
-                        <span className="icon is-small is-right">
-                            <i className="fas fa-check"></i>
-                        </span>
-                    </p>
-                </div>
-                <div className="field">
-                <p className="control has-icons-left has-icons-right">
-                    <input className="input is-small is-rounded" type="text" name="surname" placeholder="Surame" required onChange={handleSurnameInput} />
-                    <span className="icon is-small is-left">
-                        <i className="far fa-user"></i>
-                    </span>
-                    <span className="icon is-small is-right">
-                        <i className="fas fa-check"></i>
-                    </span>
-                </p>
-                </div>
-                <div className="field">
-                    <p className="control has-icons-left has-icons-right">
-                        <input className="input is-small is-rounded" type="email" name="email" placeholder="Email" required onChange={handleEmailInput}/>
-                        <span className="icon is-small is-left">
-                            <i className="fas fa-envelope"></i>
-                        </span>
-                        <span className="icon is-small is-right">
-                            <i className="fas fa-check"></i>
-                        </span>
-                    </p>
-                </div>
-                <div className="field">
-                    <p className="control has-icons-left">
-                        <input className="input is-small is-rounded" type="password" name="password"placeholder="Password" required onChange={handlePasswordInput} />
-                        <span className="icon is-small is-left">
-                            <i className="fas fa-lock"></i>
-                        </span>
-                    </p>
-                </div>
-                <div className="field">
-                    <p className="control has-icons-left">
-                        <input className="input is-small is-rounded" type="password" name="password-confirmation"placeholder="Confirm password" required onChange={handlePasswordConfirmationInput}/>
-                        <span className="icon is-small is-left">
-                            <i className="fas fa-lock"></i>
-                        </span>
-                    </p>
-                </div>
-                <div className="field is-grouped btn_grp">
-                    <p className="control">
-                        <button className="button is-success is-small is-rounded" type="submit">
-                        Sign Up
-                        </button>
-                    </p>
-                    <p className="control">
-                        <a href="#" onClick={handleOnLogin} className="button is-outlined is-small is-rounded">
-                        Log in
-                        </a>
-                    </p>
-                </div>
-            </form>
+        return <section className="register container">
+        <div className="columns is-mobile">
+            <div className="modal is-active is-clipped ">
+                <div className="modal-background"></div>
+                <div className="modal-content column is-half-widescreen is-three-fifths-tablet is-three-quarters-mobile is-centered">
+                    <form className="register__form " onSubmit={handleFormSubmit}>
+                        <h4 className="subtitle is-4 white">Sign Up</h4>
+                        <div className="field">
+                            <p className="control has-icons-left has-icons-right">
+                                <input className="input is-small is-rounded" type="text" name="name" placeholder="Name" required onChange={handleNameInput} />
+                                <span className="icon is-small is-left">
+                                    <i className="far fa-user"></i>
+                                </span>
+                                <span className="icon is-small is-right">
+                                    <i className="fas fa-check"></i>
+                                </span>
+                            </p>
+                        </div>
+                        <div className="field">
+                        <p className="control has-icons-left has-icons-right">
+                            <input className="input is-small is-rounded" type="text" name="surname" placeholder="Surame" required onChange={handleSurnameInput} />
+                            <span className="icon is-small is-left">
+                                <i className="far fa-user"></i>
+                            </span>
+                            <span className="icon is-small is-right">
+                                <i className="fas fa-check"></i>
+                            </span>
+                        </p>
+                        </div>
+                        <div className="field">
+                            <p className="control has-icons-left has-icons-right">
+                                <input className="input is-small is-rounded" type="email" name="email" placeholder="Email" required onChange={handleEmailInput}/>
+                                <span className="icon is-small is-left">
+                                    <i className="fas fa-envelope"></i>
+                                </span>
+                                <span className="icon is-small is-right">
+                                    <i className="fas fa-check"></i>
+                                </span>
+                            </p>
+                        </div>
+                        <div className="field">
+                            <p className="control has-icons-left">
+                                <input className="input is-small is-rounded" type="password" name="password"placeholder="Password" required onChange={handlePasswordInput} />
+                                <span className="icon is-small is-left">
+                                    <i className="fas fa-lock"></i>
+                                </span>
+                            </p>
+                        </div>
+                        <div className="field">
+                            <p className="control has-icons-left">
+                                <input className="input is-small is-rounded" type="password" name="password-confirmation"placeholder="Confirm password" required onChange={handlePasswordConfirmationInput}/>
+                                <span className="icon is-small is-left">
+                                    <i className="fas fa-lock"></i>
+                                </span>
+                            </p>
+                        </div>
+                        <div className="field is-grouped btn_grp">
+                            <p className="control"><button className="button is-outlined is-danger is-small is-rounded" type="submit">Sign Up</button></p>
+                            <p className="control"><a href="#" onClick={handleOnLogin} className="button is-inverted is-outlined is-danger is-small is-rounded">Log in</a></p>
+                        </div>
+                    </form>
+                </div>    
+            </div>
         </div>
         {/* {feedback && <Feedback message={feedback} />} */}
     </section>
     }
 }
 
-export default Register
+export default withRouter(Register)
