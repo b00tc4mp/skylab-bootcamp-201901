2888583175696c610721ec7fcd189aa8a96d567f
robertz <robertz.perez1985@gmail.com>
2019-02-04 09:39:30 +0100

    Add search game by name method

---
+++ staff/groups/la-cia-del-anillo/ultra-vgsm/src/games.js
@@ -0,0 +1,497 @@
+const games = {
+    "code": 200,
+    "status": "Success",
+    "data": {
+        "count": 20,
+        "games": [
+            {
+                "id": 49433,
+                "game_title": "Zelda II - The Adventure of Link",
+                "release_date": "2013-09-12",
+                "platform": 38,
+                "developers": [
+                    6037
+                ]
+            },
+            {
+                "id": 2383,
+                "game_title": "The Legend of Zelda: Collector's Edition",
+                "release_date": "2003-11-14",
+                "platform": 2,
+                "developers": [
+                    6037
+                ]
+            },
+            {
+                "id": 2758,
+                "game_title": "Zelda II: The Adventure of Link",
+                "release_date": "1988-09-26",
+                "platform": 7,
+                "developers": [
+                    6037
+                ]
+            },
+            {
+                "id": 61737,
+                "game_title": "Zelda II: The Adventure of Link - Gold Cart",
+                "release_date": "1988-09-26",
+                "platform": 7,
+                "developers": [
+                    6055
+                ]
+            },
+            {
+                "id": 175,
+                "game_title": "The Legend of Zelda: Majora's Mask",
+                "release_date": "2000-10-26",
+                "platform": 3,
+                "developers": [
+                    6043
+                ]
+            },
+            {
+                "id": 161,
+                "game_title": "The Legend of Zelda: Ocarina of Time",
+                "release_date": "1998-11-23",
+                "platform": 3,
+                "developers": [
+                    6043
+                ]
+            },
+            {
+                "id": 1318,
+                "game_title": "The Legend of Zelda: A Link to the Past",
+                "release_date": "1992-04-13",
+                "platform": 6,
+                "developers": [
+                    6043
+                ]
+            },
+            {
+                "id": 18165,
+                "game_title": "The Legend of Zelda: A Link Between Worlds",
+                "release_date": "2013-11-22",
+                "platform": 4912,
+                "developers": [
+                    6037
+                ]
+            },
+            {
+                "id": 19053,
+                "game_title": "Zelda no Densetsu: The Hyrule Fantasy",
+                "release_date": "1986-02-21",
+                "platform": 4936,
+                "developers": [
+                    6037
+                ]
+            },
+            {
+                "id": 49620,
+                "game_title": "Zelda: Mystery of Solarus DX",
+                "release_date": "2017-12-16",
+                "platform": 1,
+                "developers": null
+            },
+            {
+                "id": 43567,
+                "game_title": "Zelda: Mystery of The Solarus XD",
+                "release_date": null,
+                "platform": 1,
+                "developers": null
+            },
+            {
+                "id": 37684,
+                "game_title": "Zelda: Picross",
+                "release_date": "2010-08-12",
+                "platform": 1,
+                "developers": [
+                    9392
+                ]
+            },
+            {
+                "id": 43557,
+                "game_title": "Zelda: Return of the Hylian",
+                "release_date": "2017-08-13",
+                "platform": 1,
+                "developers": [
+                    7963
+                ]
+            },
+            {
+                "id": 14753,
+                "game_title": "Zelda: The Wand of Gamelon",
+                "release_date": "1993-10-10",
+                "platform": 4917,
+                "developers": [
+                    511
+                ]
+            },
+            {
+                "id": 14755,
+                "game_title": "Zelda's Adventure",
+                "release_date": "1994-06-05",
+                "platform": 4917,
+                "developers": [
+                    9406
+                ]
+            },
+            {
+                "id": 26377,
+                "game_title": "BS Zelda no Densetsu: Inishie no Sekiban",
+                "release_date": "1995-08-09",
+                "platform": 6,
+                "developers": [
+                    6037
+                ]
+            },
+            {
+                "id": 50758,
+                "game_title": "BS The Legend of Zelda: Ancient Stone Tablets",
+                "release_date": "1997-03-30",
+                "platform": 6,
+                "developers": [
+                    6037
+                ]
+            },
+            {
+                "id": 26364,
+                "game_title": "BS Zelda no Densetsu",
+                "release_date": "1995-08-09",
+                "platform": 6,
+                "developers": [
+                    6037
+                ]
+            },
+            {
+                "id": 198,
+                "game_title": "Classic NES Series: Zelda II: The Adventure of Link",
+                "release_date": "2004-10-25",
+                "platform": 5,
+                "developers": [
+                    6043
+                ]
+            },
+            {
+                "id": 55790,
+                "game_title": "Famicom Mini Series Vol. 25: Zelda II: The Adventure of Link",
+                "release_date": "2004-08-10",
+                "platform": 5,
+                "developers": [
+                    6037
+                ]
+            }
+        ]
+    },
+    "include": {
+        "boxart": {
+            "base_url": {
+                "original": "https://cdn.thegamesdb.net/images/original/",
+                "small": "https://cdn.thegamesdb.net/images/small/",
+                "thumb": "https://cdn.thegamesdb.net/images/thumb/",
+                "cropped_center_thumb": "https://cdn.thegamesdb.net/images/cropped_center_thumb/",
+                "medium": "https://cdn.thegamesdb.net/images/medium/",
+                "large": "https://cdn.thegamesdb.net/images/large/"
+            },
+            "data": {
+                "161": [
+                    {
+                        "id": 894,
+                        "type": "boxart",
+                        "side": "back",
+                        "filename": "boxart/back/161-1.jpg",
+                        "resolution": "1000x697"
+                    },
+                    {
+                        "id": 83199,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/161-1.jpg",
+                        "resolution": "1050x768"
+                    }
+                ],
+                "175": [
+                    {
+                        "id": 7469,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/175-1.jpg",
+                        "resolution": "2100x1532"
+                    },
+                    {
+                        "id": 7470,
+                        "type": "boxart",
+                        "side": "back",
+                        "filename": "boxart/back/175-1.jpg",
+                        "resolution": "2100x1532"
+                    }
+                ],
+                "198": [
+                    {
+                        "id": 3232,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/198-1.jpg",
+                        "resolution": "1536x1370"
+                    },
+                    {
+                        "id": 3233,
+                        "type": "boxart",
+                        "side": "back",
+                        "filename": "boxart/back/198-2.jpg",
+                        "resolution": "1530x1370"
+                    }
+                ],
+                "1318": [
+                    {
+                        "id": 3278,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/1318-1.jpg",
+                        "resolution": "2100x1540"
+                    },
+                    {
+                        "id": 3279,
+                        "type": "boxart",
+                        "side": "back",
+                        "filename": "boxart/back/1318-1.jpg",
+                        "resolution": "2100x1532"
+                    }
+                ],
+                "2383": [
+                    {
+                        "id": 4969,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/2383-1.jpg",
+                        "resolution": "640x909"
+                    },
+                    {
+                        "id": 81281,
+                        "type": "boxart",
+                        "side": "back",
+                        "filename": "boxart/back/2383-1.jpg",
+                        "resolution": "640x896"
+                    }
+                ],
+                "2758": [
+                    {
+                        "id": 57825,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/2758-1.jpg",
+                        "resolution": "1536x2100"
+                    },
+                    {
+                        "id": 57826,
+                        "type": "boxart",
+                        "side": "back",
+                        "filename": "boxart/back/2758-1.jpg",
+                        "resolution": "1551x2100"
+                    }
+                ],
+                "14753": [
+                    {
+                        "id": 42492,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/14753-1.jpg",
+                        "resolution": "640x639"
+                    },
+                    {
+                        "id": 42493,
+                        "type": "boxart",
+                        "side": "back",
+                        "filename": "boxart/back/14753-1.jpg",
+                        "resolution": "640x548"
+                    }
+                ],
+                "14755": [
+                    {
+                        "id": 42496,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/14755-1.jpg",
+                        "resolution": "640x627"
+                    },
+                    {
+                        "id": 42497,
+                        "type": "boxart",
+                        "side": "back",
+                        "filename": "boxart/back/14755-1.jpg",
+                        "resolution": "640x554"
+                    }
+                ],
+                "18165": [
+                    {
+                        "id": 51038,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/18165-1.jpg",
+                        "resolution": "1802x1601"
+                    }
+                ],
+                "19053": [
+                    {
+                        "id": 84060,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/19053-1.jpg",
+                        "resolution": "860x970"
+                    }
+                ],
+                "26364": [
+                    {
+                        "id": 87289,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/26364-1.png",
+                        "resolution": "238x438"
+                    }
+                ],
+                "26377": [
+                    {
+                        "id": 82666,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/26377-1.jpg",
+                        "resolution": "300x217"
+                    }
+                ],
+                "37684": [
+                    {
+                        "id": 106172,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/37684-1.png",
+                        "resolution": "320x240"
+                    }
+                ],
+                "43557": [
+                    {
+                        "id": 126845,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/43557-1.png",
+                        "resolution": "1195x800"
+                    }
+                ],
+                "43567": [
+                    {
+                        "id": 116256,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/43567-1.png",
+                        "resolution": "1000x700"
+                    }
+                ],
+                "49433": [
+                    {
+                        "id": 126436,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/49433-1.png",
+                        "resolution": "140x134"
+                    }
+                ],
+                "49620": [
+                    {
+                        "id": 126837,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/49620-1.png",
+                        "resolution": "1195x800"
+                    }
+                ],
+                "50758": [
+                    {
+                        "id": 129290,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/50758-1.jpg",
+                        "resolution": "691x503"
+                    }
+                ],
+                "55790": [
+                    {
+                        "id": 138117,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/55790-1.jpg",
+                        "resolution": "500x653"
+                    }
+                ],
+                "61737": [
+                    {
+                        "id": 205011,
+                        "type": "boxart",
+                        "side": "front",
+                        "filename": "boxart/front/61737-1.jpg",
+                        "resolution": null
+                    }
+                ]
+            }
+        },
+        "platform": {
+            "1": {
+                "id": 1,
+                "name": "PC",
+                "alias": "pc"
+            },
+            "2": {
+                "id": 2,
+                "name": "Nintendo GameCube",
+                "alias": "nintendo-gamecube"
+            },
+            "3": {
+                "id": 3,
+                "name": "Nintendo 64",
+                "alias": "nintendo-64"
+            },
+            "5": {
+                "id": 5,
+                "name": "Nintendo Game Boy Advance",
+                "alias": "nintendo-gameboy-advance"
+            },
+            "6": {
+                "id": 6,
+                "name": "Super Nintendo (SNES)",
+                "alias": "super-nintendo-snes"
+            },
+            "7": {
+                "id": 7,
+                "name": "Nintendo Entertainment System (NES)",
+                "alias": "nintendo-entertainment-system-nes"
+            },
+            "38": {
+                "id": 38,
+                "name": "Nintendo Wii U",
+                "alias": "nintendo-wii-u"
+            },
+            "4912": {
+                "id": 4912,
+                "name": "Nintendo 3DS",
+                "alias": "nintendo-3ds"
+            },
+            "4917": {
+                "id": 4917,
+                "name": "Philips CD-i",
+                "alias": "philips-cd-i"
+            },
+            "4936": {
+                "id": 4936,
+                "name": "Famicom Disk System",
+                "alias": "fds"
+            }
+        }
+    },
+    "pages": {
+        "previous": null,
+        "current": "https://api.thegamesdb.net/Games/ByGameName?apikey=ec70893446ed73812756c3fc599bb3da6c263807fb2500265d4b675a6855aab8&name=Zelda&include=boxart%2Cplatform&page=1",
+        "next": "https://api.thegamesdb.net/Games/ByGameName?apikey=ec70893446ed73812756c3fc599bb3da6c263807fb2500265d4b675a6855aab8&name=Zelda&include=boxart%2Cplatform&page=2"
+    },
+    "remaining_monthly_allowance": 1497,
+    "extra_allowance": 0,
+    "allowance_refresh_timer": 2474819
+}
+
+export default games
