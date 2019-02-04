5a09f0d5f54c2e95ca14dd1516dc814a2f64dce8
Ury18 <ury_98@hotmail.com>
2019-02-05 00:13:44 +0100

    redesign v1

--- staff/groups/funciton/funciton-pokedex/src/App.js
+++ staff/groups/funciton/funciton-pokedex/src/App.js
@@ -47,3 +47,16 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+function bindEvent(e, eventName, callback) {
+  if(e.addEventListener) // new browsers
+      e.addEventListener(eventName, callback, false);
+  else if(e.attachEvent) // IE
+      e.attachEvent('on'+ eventName, callback);
+};
+
+bindEvent(document.body, 'scroll', function(e) {
+  document.body.scrollLeft = 0;
+});
--- staff/groups/funciton/funciton-pokedex/src/components/ItemResult/index.js
+++ staff/groups/funciton/funciton-pokedex/src/components/ItemResult/index.js
@@ -16,14 +16,13 @@ class ItemResult extends React.Component {
         let pokemonId = getPokemonId(stringPokemonId)
         const source = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`
 
-        return <div className="card pokemonCard">
+        return <div onClick={this.retrieveDataFromItem} className="card pokemonCard">
             <img src={source} className="card-img-top"/>
 
             {/* <img className="pokemonCard__pokeball" src="https://cdn4.iconfinder.com/data/icons/pokemon-go/512/Pokemon_Go-01-128.png"/> */}
-            <div className="card-body">
+            
                 <p className="card-text">{pokemonName.toUpperCase()}</p>
                 <button onClick={this.retrieveDataFromItem} className="btn btn-danger pokemonCard__details">More</button>
-            </div>
         </div>
     }
 }
