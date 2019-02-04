492935fd7268a779eb3fe38187bdd63cc936c47c
Carlos <carloscl1986@gmail.com>
2019-02-04 20:07:32 +0100

    1v modal login

--- staff/groups/funciton/funciton-pokedex/src/App.js
+++ staff/groups/funciton/funciton-pokedex/src/App.js
@@ -3,11 +3,14 @@ import "./App.css";
 import PokemonSearch from "./components/PokemonSearch";
 import DetailedPokemonPanel from "./components/DetailPanel"
 import logic from "./logic";
+import Home from './components/Home'
+import LoginPanel from "./components/LoginPanelComponent";
 
 class App extends Component {
   state = {
     pokemonVisible: null,
-    searchText: null
+    searchText: null,
+    loginPanelVisible : false
   }
   handlePokemonDetail = (name) => {
     logic.retrievePokemon(name)
@@ -16,29 +19,38 @@ class App extends Component {
   }
 
   onBackButtonDetailedPokemon = () => {
-
     this.setState({ pokemonVisible: null, searchText:this.state.searchText })
 
   }
 
   setSearchTextApp = (query) => {
-
     this.setState({ searchText:query })
 
   }
 
+  onLoginRequested = (username, password) => {
+    try {
+        logic.loginUser(username, password)
+    } catch (error) {
+      //Funciton de errorPanel
+    }
+  }
+  toogleShowLogin = () => {
+    const bool = !this.state.loginPanelVisible
+    this.setState({loginPanelVisible : bool})
+  }
+
   render() {
-
     const {
-      state: { pokemonVisible }
+      state: { pokemonVisible, loginPanelVisible }
     } = this
-
-
 
     return (
       <div className="App">
-        {!pokemonVisible && <PokemonSearch onPokemonDetail={this.handlePokemonDetail} setSearchTextApp={this.setSearchTextApp} searchText={this.state.searchText} />}
-        {pokemonVisible && <DetailedPokemonPanel pokemonToShow={pokemonVisible} onBackButton={this.onBackButtonDetailedPokemon} />}
+        <Home onHandleShowLogin = {this.toogleShowLogin}/>
+        <LoginPanel onLogin={this.onLoginRequested} show={loginPanelVisible} />
+        {/* {!pokemonVisible && <PokemonSearch onPokemonDetail={this.handlePokemonDetail} setSearchTextApp={this.setSearchTextApp} searchText={this.state.searchText} />}
+        {pokemonVisible && <DetailedPokemonPanel pokemonToShow={pokemonVisible} onBackButton={this.onBackButtonDetailedPokemon} />} */}
       </div>
     );
   }
