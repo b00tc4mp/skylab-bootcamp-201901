6eda8b6241e14c0535d7af8a536bf90c97d36840
Marc Sarroca Diaz <msarroca@Marcs-MacBook-Pro.local>
2019-02-04 10:26:13 +0100

    search-style

--- staff/groups/funciton/funciton-pokedex/src/components/PokemonSearch/index.js
+++ staff/groups/funciton/funciton-pokedex/src/components/PokemonSearch/index.js
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from "react";
 import pokemonApi from "../../apipokemon";
+import './index.sass'
+import titleImage from "../../img/title.png"
 
 class PokemonSearch extends Component {
   state = { pokemons: [], searchText: "", loading: true };
@@ -17,30 +19,35 @@ class PokemonSearch extends Component {
   };
 
   renderList = () => (
-    <ul>
+
+    <ul className='pokemon__ul'>
       {
         this.state.pokemons
           .filter(pokemon => pokemon.name.includes(this.state.searchText))
-          .map(pokemon => <li>{pokemon.name}</li>)
+          .map(pokemon => <li className='pokemon__list'>{pokemon.name}</li>)
       }
     </ul>
+
   )
 
   render() {
     return (
       <Fragment>
-        <h2>Search Pokemon</h2>
-        <input
-          onChange={this.handleChange}
-          type="text"
-          placeholder="Search you Pokemon"
-        />
-        {
-          this.state.loading && <h1>LOADING</h1>
-        }
-        {
-          this.state.searchText !== "" && this.renderList()
-        }
+        <img src={titleImage} alt="poke_title"></img>
+        <h2 className='title__search'>Search Pokemon</h2>
+        <div>
+          <input className="input__searchPokemon"
+            onChange={this.handleChange}
+            type="text"
+            placeholder="Search you Pokemon"
+          />
+          {
+            this.state.loading && <h1>LOADING</h1>
+          }
+          {
+            this.state.searchText !== "" && this.renderList()
+          }
+        </div>
       </Fragment>
     );
   }
