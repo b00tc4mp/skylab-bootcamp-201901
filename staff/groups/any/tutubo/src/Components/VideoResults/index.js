fcaa94994f3217076e77c9c4c470ee40e3cd7928
Scostta <sergio.cs04@gmail.com>
2019-02-04 10:48:46 +0100

    update

---
+++ staff/groups/any/tutubo/src/Components/VideoResults/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class VideoRasults extends Component {
+
+    componentDidMount(){
+        console.log('monuterds')
+    }
+
+    componentWillReceiveProps(props){
+        console.log(props)
+    }
+
+    render(){
+        return <p>LOQUESEA</p>
+    }
+}
+
+export default VideoRasults
