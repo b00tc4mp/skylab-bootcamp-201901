f9f79b70567aaf08162e470d97dd497f405481dc
Carlos <carloscl1986@gmail.com>
2019-02-04 15:00:41 +0100

    Integration v1

--- staff/groups/funciton/funciton-pokedex/src/components/ItemResult/index.js
+++ staff/groups/funciton/funciton-pokedex/src/components/ItemResult/index.js
@@ -6,19 +6,23 @@ import {getPokemonId} from '../../utils'
 class ItemResult extends React.Component {
 
 
+    retrieveDataFromItem = (query) => {
+        this.props.onGoToDetails(this.props.pokemonName)
+    }
+
     render(){
 
-        const {props: {stringPokemonId, pokemonName, goToDetails}} = this
+        const {props: {stringPokemonId, pokemonName}} = this
         let pokemonId = getPokemonId(stringPokemonId)
         const source = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`
 
         return <div className="card pokemonCard">
             <img src={source} className="card-img-top"/>
 
-            <img className="pokemonCard__pokeball" src="https://cdn4.iconfinder.com/data/icons/pokemon-go/512/Pokemon_Go-01-128.png"/>
+            {/* <img className="pokemonCard__pokeball" src="https://cdn4.iconfinder.com/data/icons/pokemon-go/512/Pokemon_Go-01-128.png"/> */}
             <div className="card-body">
                 <p className="card-text">{pokemonName}</p>
-                <button onClick={goToDetails} className="btn btn-danger pokemonCard__details">More</button>
+                <button onClick={this.retrieveDataFromItem} className="btn btn-danger pokemonCard__details">More</button>
             </div>
         </div>
     }
