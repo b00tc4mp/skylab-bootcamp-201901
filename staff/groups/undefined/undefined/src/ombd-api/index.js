18f28e970ddf271ab1df9f87c989ac0301395ece
nico <nico@somoslaclave.com>
2019-02-04 10:19:33 +0100

    test working and omdb-api working

--- staff/groups/undefined/undefined/src/ombd-api/index.js
+++ staff/groups/undefined/undefined/src/ombd-api/index.js
@@ -1,7 +1,5 @@
 'use strict'
 
-// http://www.omdbapi.com/?apikey=ef8a2f56&s=madonna
-
 /**
  * OMDB API client.
  * 
@@ -12,7 +10,7 @@ const omdbApi = {
     url: `http://www.omdbapi.com/?apikey=ef8a2f56`,
 
      /**
-     * Search movie items *** 
+     * Search movie items 
      * 
      * @param {string} query - The text to match on movies & series search.
      * @retuns {Promise} - Resolves with array of movies, otherwise rejects with an error.
@@ -21,30 +19,9 @@ const omdbApi = {
      * @throws {Array} - Array of objects related with every item
      *                   Empty Array if there is no results
      * 
-     * //http://www.omdbapi.com/?apikey=ef8a2f56&s=titanic
-     /*/
-
-    // **** search for query with 10 results
-    // {  
-    //     "Search": [
-    //         {
-    //             "Title": "Titanic",
-    //             "Year": "1997",
-    //             "imdbID": "tt0120338",
-    //             "Type": "movie",
-    //             "Poster": "https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-    //         },
-    //         // +9 items
-    //     ],
-    //     "totalResults": "174",
-    //     "Response": "True"
-    // }
-
-    // ***** search for query with no results
-    // {
-    //     "Response": "False",
-    //     "Error": "Movie not found!"
-    // }
+     * http://www.omdbapi.com/?apikey=ef8a2f56&s=titanic
+     * 
+     */
 
     searchItems(query) { 
         if (typeof query !== 'string') throw TypeError(`${query} is not a string`)
@@ -69,50 +46,7 @@ const omdbApi = {
      * 
      * // http://www.omdbapi.com/?apikey=ef8a2f56&i=tt1632708
      * 
-     * 
-     
-     /* retrieve item with imdbID = tt0081400
-     * 
-     * {    Title: "Raise the Titanic", Year: "1980", Rated: "PG", Released: "01 Aug 1980", Runtime: "115 min", …}
-            Actors: "Jason Robards, Richard Jordan, David Selby, Anne Archer"
-            Awards: "3 nominations."
-            BoxOffice: "N/A"
-            Country: "UK, USA"
-            DVD: "21 Jan 2014"
-            Director: "Jerry Jameson"
-            Genre: "Action, Drama, Thriller, Adventure"
-            Language: "English"
-            Metascore: "N/A"
-            Plot: "To obtain a supply of a rare mineral, a ship raising operation is conducted for the only known source, the R.M.S. Titanic."
-            Poster: "https://m.media-amazon.com/images/M/MV5BM2MyZWYzOTQtMTYzNC00OWIyLWE2NWItMzMwODA0OGQ2ZTRkXkEyXkFqcGdeQXVyMjI4MjA5MzA@._V1_SX300.jpg"
-            Production: "Associated Film Distribution"
-            Rated: "PG"
-            Ratings: (2) [{…}, {…}]
-            Released: "01 Aug 1980"
-            Response: "True"
-            Runtime: "115 min"
-            Title: "Raise the Titanic"
-            Type: "movie"
-            Website: "N/A"
-            Writer: "Adam Kennedy (screenplay), Eric Hughes (adaptation), Clive Cussler (novel)"
-            Year: "1980"
-            imdbID: "tt0081400"
-            imdbRating: "4.8"
-            imdbVotes: "3,460"
-
-        }
-
-        retrieve item with non exists ID = asdcasdc 
-        {
-            "Response": "False",
-            "Error": "Movie not found!"
-        }
-     */
-
-    // DUDA - no debería lanzar un error este método??
-    // por qué devuelve una Promise, se maneja desde la lógica?
-    // DUDA: la validación de datos de entrada se debe hacer tanto en la lógica como 
-    // en esta API? sólo en un lado? xq? puede llegar un itemId que NO existe?
+     **/
 
     retrieveItem(itemId) {
         if (typeof itemId !== 'string') throw TypeError(`${itemId} is not a string`)
@@ -121,7 +55,7 @@ const omdbApi = {
         return fetch(`${this.url}&i=${itemId}`)
             .then(item => item.json())
             .then(response => {                
-                if (response.Response === 'False') throw Error('Incorrect IMDb ID.')                
+                if (response.Response === 'False') throw Error(response.Error)                
                 return response
             })
     }
--- staff/groups/undefined/undefined/src/ombd-api/index.spec.js
+++ staff/groups/undefined/undefined/src/ombd-api/index.spec.js
@@ -5,6 +5,7 @@ omdbApi.api = 'ef8a2f56'
 
 describe('ombd-api' , () => {
     describe('searchItems', () => {
+
         it ('should succeed on matching query', () => {
             let query = 'titanic'
             return omdbApi.searchItems(query)
@@ -17,15 +18,7 @@ describe('ombd-api' , () => {
 
         it ('should throw an error on empty query', () => {
             let query = ''
-
-            //esta es la forma correcta pero no veo por qué le pasamos la función 
-            //al expect
             expect(() => omdbApi.searchItems(query)).toThrowError('query is empty')
-            
-            //DUDA: esta construcción de aquí parece que no está llegando bien
-            //he probado con cambiarle el mensaje a ver si peta el test
-            //y sigue pasando exactamente lo mismo
-            //expect(() => omdbApi.searchItems(query).toThrowError('query is empty'))
         })
 
         it ('should returns an empty array when no results found', () => {
@@ -45,23 +38,18 @@ describe('ombd-api' , () => {
 
     })
 
-
-    // DUDA: cómo manejar estas mayúcuslas?
-    // tenemos que convertir las keys del objeto a toLowerCase()
-
     describe('retrieveItem', () => {
         
-        // expected object - test some fields
         const expectedResult = {
-            Title: "Raise the Titanic",
-            Actors: "Jason Robards, Richard Jordan, David Selby, Anne Archer",
-            Runtime: "115 min",
-            imdbID: "tt0081400",
-            imdbRating: "4.8"
+            Title: 'Raise the Titanic',
+            Actors: 'Jason Robards, Richard Jordan, David Selby, Anne Archer',
+            Runtime: '115 min',
+            imdbID: 'tt0081400',
+            imdbRating: '4.8'
         }
         var itemId = expectedResult.imdbID
 
-        it('should retrieve a movie or serie item object with valid id', () => { //DUDA: xq hace un return aquí?
+        it('should retrieve a movie or serie item object with valid id', () => {
             return omdbApi.retrieveItem(itemId)
                     .then(({ Title, Actors, Runtime, imdbID, imdbRating }) => {
                         expect(Title).toBe(expectedResult.Title)
@@ -72,7 +60,6 @@ describe('ombd-api' , () => {
                     })
         })
 
-        // DUDA: qué es lo que hace el try catch aquí? el expect o el it?
         it('should fail using a space as query for itemID', () => {
             var emptyId = ' '
             expect(() => omdbApi.retrieveItem(emptyId)).toThrowError('itemId is empty')
@@ -85,9 +72,12 @@ describe('ombd-api' , () => {
 
         it('should fail on wrong itemID parameter', () => {
             var newId = '123qdsad'
-            //expect(() => omdbApi.retrieveItem(newId)).toThrowError('Incorrect IMDb ID.')
 
-            //expect(() => omdbApi.retrieveItem(newId)).toThrowError('Incorrect IMDb ID.')
+            return omdbApi.retrieveItem(newId)
+                .then(() => {
+                    throw Error('should not have passed by here')
+                })
+                .catch(({message}) =>  expect(message).toBe('Incorrect IMDb ID.'))
         })
     })
 })
