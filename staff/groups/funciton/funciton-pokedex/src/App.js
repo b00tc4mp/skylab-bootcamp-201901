a9c4d3062aed5e11cd04c7c0fe8331f8da93235d
Carlos <carloscl1986@gmail.com>
2019-02-05 10:56:40 +0100

    retoque de logic, Feedback component, App.js contempla pasar a pokemonsearch despues de login

--- staff/groups/funciton/funciton-pokedex/src/App.js
+++ staff/groups/funciton/funciton-pokedex/src/App.js
@@ -1,17 +1,20 @@
-import React, { Component } from "react";
-import "./App.css";
-import PokemonSearch from "./components/PokemonSearch";
-import DetailedPokemonPanel from "./components/DetailPanel"
-import logic from "./logic";
+import React, { Component } from "react"
+import "./App.css"
+import PokemonSearch from "./components/PokemonSearch"
+import logic from "./logic"
 import Home from './components/Home'
-import LoginPanel from "./components/LoginPanelComponent";
+import LoginPanel from "./components/LoginPanelComponent"
 
 class App extends Component {
   state = {
-    pokemonVisible: null,
+
     searchText: null,
-    loginPanelVisible : false
+    loginPanelVisible : false,
+    user: null,
+    loginFeedback: ''
+
   }
+
   handlePokemonDetail = (name) => {
     logic.retrievePokemon(name)
       .then((pokemonVisible) => this.setState({ pokemonVisible }))
@@ -28,13 +31,22 @@ class App extends Component {
 
   }
 
-  onLoginRequested = (username, password) => {
+  onLoginRequested = (user, password) => {
     try {
-        logic.loginUser(username, password)
-    } catch (error) {
-      //Funciton de errorPanel
+        logic.loginUser(user, password)
+          .then(({user}) => {
+            this.setState({user})
+          })
+          .catch(({ message }) => this.setState({ loginFeedback: message }))
+    } catch ({message}) {
+        this.setState({ loginFeedback: message })
     }
   }
+
+
+
+
+
   toogleShowLogin = () => {
     const bool = !this.state.loginPanelVisible
     this.setState({loginPanelVisible : bool})
@@ -42,15 +54,14 @@ class App extends Component {
 
   render() {
     const {
-      state: { pokemonVisible, loginPanelVisible }
+      state: { user, loginPanelVisible, loginFeedback }
     } = this
 
     return (
       <div className="App">
-        <Home onHandleShowLogin = {this.toogleShowLogin}/>
-        <LoginPanel onLogin={this.onLoginRequested} show={loginPanelVisible} />
-        {/* {!pokemonVisible && <PokemonSearch onPokemonDetail={this.handlePokemonDetail} setSearchTextApp={this.setSearchTextApp} searchText={this.state.searchText} />}
-        {pokemonVisible && <DetailedPokemonPanel pokemonToShow={pokemonVisible} onBackButton={this.onBackButtonDetailedPokemon} />} */}
+        {!user && <Home onHandleShowLogin = {this.toogleShowLogin}/>}
+        {!user && <LoginPanel onLogin={this.onLoginRequested} show={loginPanelVisible} message={loginFeedback} />}
+        {user && <PokemonSearch onPokemonDetail={this.handlePokemonDetail} setSearchTextApp={this.setSearchTextApp} searchText={this.state.searchText} />}
       </div>
     );
   }
