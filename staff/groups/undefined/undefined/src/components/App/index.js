031577e3117faccc3b4273c3e6e6f2999b25b5b5
Tachi <jrfluna8@gmail.com>
2019-02-05 11:26:42 +0100

    create component login

--- staff/groups/undefined/undefined/src/components/App/index.js
+++ staff/groups/undefined/undefined/src/components/App/index.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 import Home from '../Home'
+import Login from '../Login'
+import logic from '../../logic'
 
 
 class App extends Component {
 
+state = { Login : false}
+
+
+onGoToLogin = event => {
+  event.preventDefault()
+  this.setState({Login: true})
+
+}
+
+handleLogin = (email,password) => {
+  try{
+    logic.loginUser(email,password)
+    .then(user =>{
+      this.setState({user})
+      console.log(user)
+    }).catch( ({message}) => {
+    this.setState({message})
+    })
+
+  }catch({message}){
+    this.setState({message})
+  }
+
+}
+
   render() {
+    const {props : {onGoToLogin , handleLogin}} = this
     return (
       <div className="App">
+        <button onClick={onGoToLogin}>Login</button>
         <Home /> 
+        <Login onClick={onGoToLogin} onLogin={() => handleLogin} />
       </div>
     )
   }
