583013001b13391151c3985ab234bc7ee90d4157
Jordi <jordi.pujolg@gmail.com>
2019-02-05 19:57:10 +0100

    changing to route

--- staff/groups/undefined/undefined/src/components/Results/index.js
+++ staff/groups/undefined/undefined/src/components/Results/index.js
@@ -6,32 +6,69 @@ import logic from '../../logic'
 
 class Results extends Component  {
     
-    state = { videoSelected: null }
+    state = { videoSelected: null, results: null, query: null }
 
-    handleVideoClick = id => {
-        console.log(this.props)
-        logic.retrieveVideo(id)
-            .then(details => {
-                console.log('DDD', details)
-                this.setState({ videoSelected: details}) })
-            .catch()
-        console.log('Desde Results', id)
+    // handleVideoClick = id => {
+    //     console.log(this.props)
+    //     logic.retrieveVideo(id)
+    //         .then(details => {
+    //             console.log('DDD', details)
+    //             this.setState({ videoSelected: details}) })
+    //         .catch()
+    //     console.log('Desde Results', id)
+    // }
+
+    // handleVideoClose = () => {
+    //     console.log('llega aquí!')
+    //     this.setState({ videoSelected: null })
+    // }
+
+
+    componentDidMount () {
+        const {props:{query}, handleSearch} = this
+
+        handleSearch(query) 
     }
 
-    handleVideoClose = () => {
-        console.log('llega aquí!')
-        this.setState({ videoSelected: null })
+    handleSearch = query =>{
+        try {
+            logic.searchVideos(query)
+                .then(results => {
+                    this.setState({ results, searchFeedback: null })                
+                })
+                .catch( ({message}) => {
+                    this.setState({ results: null, searchFeedback: message })
+                }) 
+        } catch ({message}) {
+            this.setState({ results: null, searchFeedback: message })
+        }
+    }
+
+    static getDerivedStateFromProps(props, state) {
+
+        if (props.query !== state.query)
+            return {
+                query: props.query
+            }
+
+        return null
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+
+        const { props: { query } } = this
+
+        if (query !== prevProps.query)
+            this.handleSearch(query)
     }
 
     render() {
         const {
-            props : {results}, 
-            state : {videoSelected}, 
+            state : {videoSelected, results},   
                     handleVideoClick,
                     handleVideoClose
             } = this
-
-        console.log(results)
+            
         return (
             <section className="results">
                 <h1>Hola</h1>
