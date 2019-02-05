26e7920109bae6270552dfa81d8c73eff21550de
nico <nico@somoslaclave.com>
2019-02-05 11:26:44 +0100

    Feedback component

---
+++ staff/groups/undefined/undefined/src/components/Feedback/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Feedback({ message, level }) {
+    return <section className={`feedback ${level ? `feedback--${level}` : ''}`}>{message}</section>
+}
+
+export default Feedback
