5fbb9683143bf79896f6ad15919fbc4c1f468115
lluna <lluna@example.com>
2019-02-05 19:19:58 +0100

    results

--- staff/groups/null/smart-fridge/src/Components/Results/index.js
+++ staff/groups/null/smart-fridge/src/Components/Results/index.js
@@ -1,49 +1,99 @@
 import React, { Component } from 'react'
 // import search from '../'
 import './index.sass'
+import logic from '../../logic'
 
 
-class Result extends Component{
+class Results extends Component{
+
+    move=() => {
+        var elem = document.getElementById("progressBar");
+        var text = document.getElementById("progressText") 
+        var width = 0;
+        var id = setInterval(frame, 20);
+        function frame() {
+          if (width >= 33) {
+            clearInterval(id);
+          } else {
+            width++; 
+            elem.style.width = width + '%';
+            text.style.marginLeft = width + '%';
+            text.innerHTML = width * 1  + '%';
+          }
+        }
+    }
     
+    follow=()=> {
+        const elem = document.getElementById("slider")
+        const follower = document.getElementById("range-follow")
+        follower.style.width = (elem.value * (elem.offsetWidth - 16)) / elem.max + 'px' 
+    }
+
     render() {
 
+        const user= JSON.parse(sessionStorage.getItem('user'))
+        const maxCalories= logic.caloriesCounter(user.gender, user.height, user.weight, user.birthDate, user.lifeStyle)
+        console.log(maxCalories)
         const { recipes } = this.props
-
-        
 
         return <section className="results">
             <h3><u>Recipes:</u></h3>
             <div className="card_columns">  
 
             {
+
+
+
+
+
                 recipes.map(recipe => (
                     <div className= "card">
                          <div className="card-body">       
-                            <h5 className="card-title">{recipe.label}</h5>                 
-                            <img className="card-img-top" alt="recipe" src={recipe.image}/>
-                            <div class="bar">
-                            <div small className="bar_calories">Total calories: {Math.round((recipe.totalNutrients.ENERC_KCAL.quantity)/recipes.yield)}</div>
+                            <h5 className="card-title">{recipe.recipe.label}</h5>                 
+                            <img className="card-img-top" alt="recipe" src={recipe.recipe.image}/>
+                            <div onClick={this.move.bind(this)}>
+                                <div class="progressBarContainer">
+                                    <div id="progressBar" class="progressBar">
+                                        <p class="progressText" id="progressText">60%</p>
+                                    </div>
+                                </div>
+
+                                <div class="progressBarContainer">
+                                    <div id="progressBar" class="progressBar">
+                                        <p class="progressText" id="progressText">60%</p>
+                                    </div>
+                                </div>
+
+                                
                             </div>
+                            <label>Total Calories</label>
+                                <div id="range-follow" class="range-follow">
+                                    <input class="slider" id="slider" onmousemove="follow()" type="range" min="0" max="100" value={Math.round((recipe.recipe.totalNutrients.ENERC_KCAL.quantity)/recipe.recipe.yield)} />
+                                </div>
                             <p></p>
-                            <div class="bar">
-                            <div className="bar_carbs">Total Carbs: {Math.round((recipe.totalNutrients.CHOCDF.quantity)/recipes.yield)}</div>
-                            </div>
+                            <label>Total carbohydrates:</label>
+                                <div id="range-follow" class="range-follow">
+                                    <input class="slider" id="slider" onmousemove="follow()" type="range" min="0" max="100" value={Math.round((recipe.recipe.totalNutrients.CHOCDF.quantity)/recipe.recipe.yield)} />
+                                </div>
                             <p></p>
-                            <div class="bar">
-                            <div className="bar_protein">Total Protein: {Math.round((recipe.totalNutrients.PROCNT.quantity)/recipes.yield)}</div> 
-                            </div>
-                            <p></p>
-                            <div class="bar">
-                            <div className="bar_fats">Total Fats: {Math.round((recipe.totalNutrients.FAT.quantity)/recipes.yield)}</div>                              
-                            </div>
-                        </div>
+                            <label>Total protein:</label>
+                                <div id="range-follow" class="range-follow">
+                                    <input class="slider" id="slider" onmousemove="follow()" type="range" min="0" max="100" value={Math.round((recipe.recipe.totalNutrients.PROCNT.quantity)/recipe.recipe.yield)}/>
+                                </div>
+                            <label>Total fats:</label>
+                                <div id="range-follow" class="range-follow">
+                                    <input class="slider" id="slider" onmousemove="follow()" type="range" min="0" max="100" value= {Math.round((recipe.recipe.totalNutrients.FAT.quantity)/recipe.recipe.yield)}/>
+                                </div>
                     </div>
-                    ))
-            }  
+                    </div>
+                ))
+            }
+               
             </div>
         </section>
         }
+        
                     
     }                
 
-export default Result
+export default Results
