e126dfce1b7a2ae74fbb3499b62d9d5f729d023e
Jordi <jordi.pujolg@gmail.com>
2019-02-05 11:26:39 +0100

    feature Register

--- staff/groups/undefined/undefined/src/components/App/index.js
+++ staff/groups/undefined/undefined/src/components/App/index.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import Home from '../Home'
+import Register from '../Register'
+import logic from '../../logic';
 
 
 class App extends Component {
 
+  state ={ user: null, registered: null}
+
+
+  handleGoToRegister = () =>{
+    this.setState({ registered: true})
+  }
+
+  handleRegister = (name, surname, email, password, passwordConfirmation) => {
+
+    try {
+        logic.Register(name, surname, email, password, passwordConfirmation)
+        .then( () => {
+          this.setState({registered: null})
+        })
+        .catch(error =>{
+          console.log(error)
+        })
+    }catch (error){ 
+
+    }
+  }
+
   render() {
+
+    const {handleGoToRegister, handleRegister, state: {registered, user}} = this
+
     return (
       <div className="App">
-        <Home /> 
+        {!user && <button onClick={handleGoToRegister}>Register</button>}
+        {user && <Home />}
+        {registered && <Register onRegister={handleRegister}/>}
       </div>
     )
   }
