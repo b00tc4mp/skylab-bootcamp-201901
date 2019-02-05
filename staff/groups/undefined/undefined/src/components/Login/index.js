71e723cee6a42e7001c5b0ea2b8cec7d6e6c76f9
Tachi <jrfluna8@gmail.com>
2019-02-05 11:26:42 +0100

    create component login

---
+++ staff/groups/undefined/undefined/src/components/Login/index.js
@@ -0,0 +1,39 @@
+import React,{Component} from 'react'
+
+
+
+class Login extends Component {
+    state = {email: '' , password: '' }
+
+    handleEmailInput = event => this.setState({email : event.target.value})
+    handlePasswordInput = event => this.setState({password : event.target.value})
+
+    handleFromSubmit = event => {
+        event.preventDefault()
+        const {state :{email , password} , props:{onLogin}} = this
+        onLogin(email, password)
+    }
+
+
+    render() {
+        const {handleEmailInput,handlePasswordInput,handleFromSubmit} = this
+        return (
+            <section>
+                <h1>Login</h1>
+                <form onSubmit={handleFromSubmit}>
+                    <label>E-mail:</label>
+                    <input type="text" name="email" onChange={handleEmailInput}></input>
+                    <label>Password:</label>
+                    <input type="password" name="password" onChange={handlePasswordInput}></input>
+                    <button>Login</button>
+                </form>
+                <button>Register</button> 
+
+            </section>
+        )
+
+        
+    }
+}
+
+export default Login
