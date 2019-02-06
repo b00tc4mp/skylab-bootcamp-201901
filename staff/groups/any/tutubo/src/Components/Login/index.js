9c29f31f3d7a4257305aa662603aa2ca277b1b7e
Emma Cowdery <e2maaa@gmail.com>
2019-02-06 10:33:35 +0100

    styles

--- staff/groups/any/tutubo/src/Components/Login/index.js
+++ staff/groups/any/tutubo/src/Components/Login/index.js
@@ -22,14 +22,22 @@ class Login extends Component {
     render() {
         const { handleEmailInput, handlePasswordInput, handleFromSubmit, props: { onGoToRegister, feedback } } = this
 
-        return <section>
-            <h2>Login</h2>
-            <form onSubmit={handleFromSubmit}>
-                <input className="red" name='email' type='text' placeholder='email' onChange={handleEmailInput} />
-                <input name='password' type='password' placeholder='password' onChange={handlePasswordInput} />
-                <button>Login</button>
-            </form>
-            <button onClick={onGoToRegister}>Register</button>
+        return <section className="login">
+            <div className="loginBox">
+                <div className="content">
+                    <div className="text">
+                        <i class="fab fa-youtube fa-5x"></i>
+                        <h3 className="title">Sign in</h3>
+                        <p>to continue to TuTubo</p>
+                    </div>
+                    <form className="form" onSubmit={handleFromSubmit}>
+                        <input className="input" name='email' type='text' placeholder='email' onChange={handleEmailInput} />
+                        <input className="input" name='password' type='password' placeholder='password' onChange={handlePasswordInput} />
+                        <button className="loginButton">Login</button>
+                    </form>
+                    <a className="createAccount" onClick={onGoToRegister}>Create account</a>
+                </div>
+            </div>
             {feedback && <Feedback message = {feedback}/>}
         </section> 
     }
