a0fd84188bd4747783b799f4cd797e9e9bd5d619
robertz <robertz.perez1985@gmail.com>
2019-02-06 11:47:09 +0100

    Add page not found

--- staff/groups/la-cia-del-anillo/ultra-vgsm/src/components/NotFound/index.js
+++ staff/groups/la-cia-del-anillo/ultra-vgsm/src/components/NotFound/index.js
@@ -6,7 +6,12 @@ class NotFound extends Component {
     render() {
 
         return (
-            'asd'
+            <section className="notfound">
+                <div className="notfound__content">
+                    <h2 className="notfound__title">Ups! Lo sentimos, pero esta página no está disponible</h2>
+                    <img src="./images/ups.svg" className="notfound__image" alt="" />
+                </div>
+            </section>
         );
     }
 }
