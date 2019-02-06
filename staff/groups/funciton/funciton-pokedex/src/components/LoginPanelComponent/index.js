5f058dabafb10c8ac86182cad47627367d78383a
Carlos <carloscl1986@gmail.com>
2019-02-06 17:22:47 +0100

    1a versión con redirect

--- staff/groups/funciton/funciton-pokedex/src/components/LoginPanelComponent/index.js
+++ staff/groups/funciton/funciton-pokedex/src/components/LoginPanelComponent/index.js
@@ -1,12 +1,14 @@
 import React, { Component, Fragment } from "react";
 import './index.sass'
 import Feedback from '../../components/Feedback'
+import Home from '../Home'
+import logic from "../../logic";
 
 
 
 class LoginPanel extends Component {
 
-    state = { username: '', password: '' }
+    state = { username: '', password: '', loginFeedback: '' }
 
     handleEmailInput = event => this.setState({ username: event.target.value })
 
@@ -14,24 +16,40 @@ class LoginPanel extends Component {
 
     handleFormSubmit = event => {
         event.preventDefault()
-        const { state: { username, password }, props: { onLogin } } = this
-        onLogin(username, password)
+        const { state: { username, password }, login  } = this
+        login(username, password)
     }
 
-    onGoBack = () => {
-        this.setState({ username: '', password: '' })
-        this.props.goBackHomeLogin()
-    }
+    goBack = () => this.props.history.push("/")
 
+
+    login = (user, password) => {
+        try {
+          logic.loginUser(user, password)
+            .then(({ user }) => {
+              this.setState({ user }, () => this.props.history.push("/welcome"))
+            })
+            .catch(({ message }) => this.showLoginFeedback(message))
+        } catch ({ message }) {
+          this.showLoginFeedback(message)
+        }
+      }
+
+    hideLoginFeedback = () => this.setState({ loginFeedback: '' })
+    
+    showLoginFeedback = message => {
+        this.setState({ loginFeedback: message })
+        setTimeout(this.hideLoginFeedback, 2000)
+    }
 
 
 
     render() {
-        const { handleEmailInput, handlePasswordInput, handleFormSubmit, onGoBack } = this
-        const showHideClassName = this.props.show ? 'containerLogin' : 'containerLoginNone'
+        const { handleEmailInput, handlePasswordInput, handleFormSubmit, goBack } = this
 
         return <Fragment>
-            <div className={showHideClassName}>
+            <Home/>
+            <div className="containerLogin">
                 <form onSubmit={handleFormSubmit}>
                     <div>
                         <label htmlFor="exampleInputEmail1">Email address</label>
@@ -41,8 +59,8 @@ class LoginPanel extends Component {
                         <input type="password" value={this.state.password} className="form-control" id="exampleInputPassword1" placeholder="Password" onChange={handlePasswordInput} />
                         <br></br>
                         <button type="submit" className="btn btn-danger">Submit</button>
-                        <button onClick={onGoBack} type="button" className="goBack__button btn btn-light">Go Back</button>
-                        <Feedback message={this.props.message} level="warn" />
+                        <button onClick={goBack} type="button" className="goBack__button btn btn-light">Go Back</button>
+                        <Feedback message={this.state.loginFeedback} level="warn" />
                     </div>
                 </form>
             </div>
