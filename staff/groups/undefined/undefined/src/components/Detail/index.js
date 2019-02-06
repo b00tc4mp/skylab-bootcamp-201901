db4fc756b944fa1538808ce3360d3e2bd50547a2
nico <nico@somoslaclave.com>
2019-02-06 18:13:39 +0100

    Detail finished with bulma css

--- staff/groups/undefined/undefined/src/components/Detail/index.js
+++ staff/groups/undefined/undefined/src/components/Detail/index.js
@@ -28,26 +28,57 @@ class Detail extends Component {
 
     onClose = () => {
         this.setState({ videoSelected: null })
-
         this.props.history.push(`/videos/${this.props.match.params.query}`)
     }
 
 
     printDetails = () => {
         if (this.state.videoSelected) {
-            const {state: {videoSelected: {Title, Year, Rated, Runtime, Plot, Genre, Actors, Poster}}} = this
+            const {state: {videoSelected: {Title, Runtime, Plot, Genre, Actors, Poster, Released}}} = this
             return (
-                <section className="detail">
-                <button onClick={this.onClose}>X</button>
-                <h3>{Title}</h3>
-                <p className="detail__year">{Year}</p>
-                <p className="detail__rated">{Rated}</p>
-                <p className="detail__runtime">{Runtime}</p>
-                <p className="detail__plot">{Plot}</p>
-                <p className="detail__genre">{Genre}</p>
-                <p className="detail__actors">{Actors}</p>
-                <img src={Poster} className="detail__poster" alt={Title} />
-            </section>
+                <section className="detail modal is-active">
+
+                    <div class="modal-background"></div>
+                    <div class="modal-content">
+                        <div class="box">
+                            <article class="media">
+
+                                <div class="media-left">
+                                    <figure class="image">
+                                        <img src={Poster} alt={Title} />
+                                    </figure>
+                                </div>
+
+                                <div class="media-content">
+                                    <div class="content">
+                                        <h4 class="title is-3">{Title}</h4>
+                                        <p>{Plot}</p>
+                                    </div>
+
+                                    <div class="info">
+
+                                        <div class="info-item">
+                                            <span class="genres">Genres: <span>{Genre}</span></span>
+                                        </div>
+
+                                        <div class="info-item">
+                                            <span class="released">Released: <span>{Released}</span></span>
+                                        </div>
+
+                                        <div class="info-item">
+                                            <span class="runtime">Runtime: <span>{Runtime}</span></span>
+                                        </div>
+
+                                        <div class="info-item">
+                                            <span class="runtime">Actors: <span>{Actors}</span></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                    <button onClick={this.onClose} class="modal-close is-large" aria-label="close"></button>
+                </section>
             )
         }
 
