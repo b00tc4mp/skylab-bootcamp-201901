a4c5e43a539cc4c252a9f871a967519915a50530
Waldo Geraldo Faldo <gcelma@MacBook-Air-de-Waldo.local>
2019-02-06 17:56:28 +0100

    initial commit

--- staff/groups/high-order-boys/ticket-app/src/components/Header/index.js
+++ staff/groups/high-order-boys/ticket-app/src/components/Header/index.js
@@ -8,28 +8,18 @@ class Header extends Component {
     render() {
         const { props: { user, onLogout } } = this
 
-        return <section className="hero is-small is-primary is-bold">
+        return <header className="hero is-small is-black">
             <div className="hero-body">
-                <div className="container">
-                    <h1 className="title">
-                        Ticket app title
-                    </h1>
-                    <h2 className="subtitle">
-                        Mini ticket app title
-                    </h2>
+                <div className="container is-pulled-left">
+                    <h1 className="title">HANGOUT<i class="fas fa-circle first"></i><i class="fas fa-circle second"></i><i class="fas fa-circle third"></i></h1>
                 </div>
-                {!!user && <div className="navbar-end">
-                    <Link className="navbar-item" to="/home/user" name="userInfo">
-                        {user.name}
-                    </Link>
-
-                    <a className="navbar-item" onClick={onLogout} name="logOut">
-                        Logout
-                    </a>
+     {!!user && <div className="navbar-end header-icons">
+                    <Link className="navbar-item" to="/home/user" name="userInfo">{user.name}</Link>
+                    <a className="navbar-item" onClick={onLogout} name="logOut">Logout</a>
                 </div>
                 }
             </div>
-        </section >
+        </header >
 
     }
 }
