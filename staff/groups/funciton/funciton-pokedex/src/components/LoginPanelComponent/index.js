425f4aae884a006eac7a80d4b61f4064a3c88af3
Marc Sarroca Diaz <msarroca@Marcs-MacBook-Pro.local>
2019-02-06 09:09:16 +0100

    final register 2

--- staff/groups/funciton/funciton-pokedex/src/components/LoginPanelComponent/index.js
+++ staff/groups/funciton/funciton-pokedex/src/components/LoginPanelComponent/index.js
@@ -1,44 +1,52 @@
-import React from 'react'
+import React, { Component, Fragment } from "react";
 import './index.sass'
 import Feedback from '../../components/Feedback'
 
 
 
-class LoginPanel extends React.Component {
+class LoginPanel extends Component {
 
-    state = { email: '', password: '' }
+    state = { username: '', password: '' }
 
-    handleEmailInput = event => this.setState({ email: event.target.value })
+    handleEmailInput = event => this.setState({ username: event.target.value })
 
     handlePasswordInput = event => this.setState({ password: event.target.value })
 
     handleFormSubmit = event => {
         event.preventDefault()
-        const { state: { email, password }, props: { onLogin } } = this
-        onLogin(email, password)
+        const { state: { username, password }, props: { onLogin } } = this
+        onLogin(username, password)
     }
 
-    //OnLogin tiene que ser el nombre de la propiedad que llegue desde el ente superior.
+    onGoBack = () => {
+        this.setState({ username: '', password: '' })
+        this.props.goBackHomeLogin()
+    }
 
-    render(){
-        const { handleEmailInput, handlePasswordInput, handleFormSubmit } = this
+
+
+
+    render() {
+        const { handleEmailInput, handlePasswordInput, handleFormSubmit, onGoBack } = this
         const showHideClassName = this.props.show ? 'containerLogin' : 'containerLoginNone'
-            
-        return <div className={showHideClassName}>
-        <form onSubmit={handleFormSubmit}>
-            <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email address</label>
-                <input type="name" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange={handleEmailInput}/>
-                <small id="emailHelp" className="form-text text-muted">Your pokemon trainer's email!</small>
+
+        return <Fragment>
+            <div className={showHideClassName}>
+                <form onSubmit={handleFormSubmit}>
+                    <div>
+                        <label htmlFor="exampleInputEmail1">Email address</label>
+                        <input type="name" value={this.state.username} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Username" onChange={handleEmailInput} />
+                        <small id="emailHelp" className="form-text text-muted">Your pokemon trainer's email!</small>
+                        <label htmlFor="exampleInputPassword1">Password</label>
+                        <input type="password" value={this.state.password} className="form-control" id="exampleInputPassword1" placeholder="Password" onChange={handlePasswordInput} />
+                        <br></br>
+                        <button type="submit" className="btn btn-danger">Submit</button>
+                        <button onClick={onGoBack} type="button" className="goBack__button btn btn-light">Go Back</button>
+                        <Feedback message={this.props.message} level="warn" />
+                    </div>
+                </form>
             </div>
-            <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" onChange={handlePasswordInput}/>
-            </div>
-            <button type="submit" className="btn btn-danger">Submit</button>
-            <Feedback message={this.props.message} level="warn" />
-        </form>
-        </div>
+        </Fragment>
     }
 }
 
