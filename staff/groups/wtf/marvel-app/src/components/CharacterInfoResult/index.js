08533cfd78b914d8b8d9abc4435444b479bc69af
Àlex Barba <alexander.barba@gmail.com>
2019-02-07 13:49:56 +0100

    bulma

--- staff/groups/wtf/marvel-app/src/components/CharacterInfoResult/index.js
+++ staff/groups/wtf/marvel-app/src/components/CharacterInfoResult/index.js
@@ -1,89 +1,71 @@
-"use strict";
+"use strict"
 
-import React, { Component } from "react";
-import logic from "../Logic";
-import PaintCharacter from "../PaintCharacter";
+import React, { Component } from "react"
+import logic from "../Logic"
+import PaintCharacter from "../PaintCharacter"
 
 class CharacterInfoResult extends Component {
-  state = { character: null, feedback: null, isFav: null };
+  state = { character: null, feedback: null, isFav: null }
 
   componentWillMount() {
-    const {
-      props: { id }
-    } = this;
+    const { props: { id } } = this
 
-    this.handleCharacterSelected(id);
-    // debugger
-    // return logic.retrieveFavourites()
-    //     .then((favs)=>{
-    //         favs.includes(this.state.character.id)? this.setState({isFav: true}) : this.setState({isFav: false})
-    // })
+    this.handleCharacterSelected(id)
   }
 
   componentWillReceiveProps(nextProps) {
-    const { id } = nextProps;
+    const { id } = nextProps
 
-    this.handleCharacterSelected(id);
+    this.handleCharacterSelected(id)
   }
 
   handleCharacterSelected = id => {
     try {
       logic
         .retrieveCharacter(id)
-        .then(character => {
-          this.setState({ character: character.results[0], feedback: null });
+        .then(character => {this.setState({ character: character.results[0], feedback: null })
         })
         .then(() => {
           return logic.retrieveFavourites().then(favs => {
-            let result = favs.some(
-              obj => obj.id === this.state.character.id
-            );
+            let result = favs.some( obj => obj.id === this.state.character.id)
             if (result) {
-              this.setState({ isFav: true });
+              this.setState({ isFav: true })
             } else {
-              this.setState({ isFav: false });
+              this.setState({ isFav: false })
             }
-          });
+          })
         })
         .catch(({ message }) =>
           this.setState({ feedback: message, character: null })
-        );
+        )
     } catch ({ message }) {
-      this.setState({ feedback: message, character: null });
+      this.setState({ feedback: message, character: null })
     }
-  };
+  }
 
   handleFavourite = data => {
     try {
       logic.updateFavourites(data).then(favs => {
-        let result = favs.some(obj => obj.id === this.state.character.id);
+        let result = favs.some(obj => obj.id === this.state.character.id)
         if (result) {
-          this.setState({ isFav: true });
+          this.setState({ isFav: true })
         } else {
-          this.setState({ isFav: false });
+          this.setState({ isFav: false })
         }
-      });
+      })
     } catch ({ message }) {
-      this.setState({ feedback: message, character: null });
+      this.setState({ feedback: message, character: null })
     }
-  };
+  }
 
   render() {
     const {
-      state: { character, feedback, isFav },
-      props: { onComicSelected }
-    } = this;
+      state: { character, feedback, isFav }, props: { onComicSelected }} = this
 
     return (
-      <PaintCharacter
-        results={character}
-        feedback={feedback}
-        onItemClick={onComicSelected}
-        handleFavourite={this.handleFavourite}
-        isFav={isFav}
-      />
-    );
+      <PaintCharacter results={character} feedback={feedback} onItemClick={onComicSelected} handleFavourite={this.handleFavourite} isFav={isFav}/>
+    )
   }
 }
 
-export default CharacterInfoResult;
+export default CharacterInfoResult
---
+++ staff/groups/wtf/marvel-app/src/components/Top Characters/index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+import React, {Component} from 'react'
+
+class TopCharacter extends Component {
+
+    state = {results: [{"id": "1009662", "name" : "Thing", "src": "http://i.annihil.us/u/prod/marvel/i/mg/9/00/527bb4d36b5c2/detail.jpg"}, {"id": "1009158", "name" : "Black Cat", "src": "http://i.annihil.us/u/prod/marvel/i/mg/e/03/526952357d91c/detail.jpg"}, {"id": "1009368", "name":"Iron Man", "src":"http://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55/detail.jpg"} ]}
+
+
+    render(){
+
+        const {state:{results}, props:{onCharacterSelected}} = this
+
+        return <section className="container has-text-centered margin-top">
+        <h2 className="title is-4 white">Top Characters</h2>
+        <div className="columns is-mobile is-multiline is-centered">
+            {results && results.map(({id, src}) => 
+            
+                <div key={id} onClick={() => onCharacterSelected(id)} data-id={id} className="column cursor card is-one-fourth-widescreen is-three-quarters-mobile has-text-centered">
+                    <div className="pointer card-image">
+                        <figure className="image is-centered">
+                            <img className="hover" src={src}/>
+                        </figure>
+                    </div>
+                </div>
+            )} 
+        </div> 
+    </section>
+    }
+
+
+}
+
+export default TopCharacter
