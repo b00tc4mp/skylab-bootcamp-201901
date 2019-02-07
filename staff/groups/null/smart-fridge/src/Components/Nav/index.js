2f547ed2bf028f93e9069130cadb26f4f683421f
danieltolmon <daniel.toledomonfort@gmail.com>
2019-02-07 18:26:50 +0100

    logo

--- staff/groups/null/smart-fridge/src/Components/Nav/index.js
+++ staff/groups/null/smart-fridge/src/Components/Nav/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './index.sass'
+import logo from '../images/logo.png'
 
 
 class Nav extends React.Component {
@@ -22,8 +23,8 @@ class Nav extends React.Component {
 
 
     render() {
-        return <nav className="navbar">
-                <img src='./logo.png' alt='logo' className="navbar-brand" ></img>
+        return <nav className="navbar container">
+                <img src={logo}  alt='logo' className="navbar-brand logo" ></img>
                 {this.props.results && <button className="btn btn-outline-light inline inputs pb-2 pt-2" onClick={this.handleOnInputs}>Back to Inputs</button>}
                 <button className="navbar-toggler btn btn-dark active p-3" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     {JSON.parse(sessionStorage.getItem('user')).name}
