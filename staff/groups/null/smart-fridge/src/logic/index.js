d283fe37e936af027ccce8fb0f864d44cc432d02
lluna <lluna@example.com>
2019-02-07 22:28:59 +0100

    testing

--- staff/groups/null/smart-fridge/src/logic/index.js
+++ staff/groups/null/smart-fridge/src/logic/index.js
@@ -379,11 +379,20 @@ const logic = {
  * @param {array} recipes 
  * 
  * @throws {Error} wheh recipeUri is not found in recipes
+ * @throws {TypeError} - On different argument types
  * 
  * @return {Object} - Recipe found
  */
     detail(recipeUri, recipes){
+
+        if (typeof recipeUri !== 'string') throw TypeError(recipeUri + ' is not a string')
+
+        if (!recipeUri.trim().length) throw Error(recipeUri + ' cannot be empty')
+
+        if (recipes.constructor !== Array) throw TypeError(recipes + ' is not an array')
+
         let recipe=null
+        
         recipes.find( element => {
             if(element.recipe.uri===recipeUri){
                 recipe=element.recipe
@@ -393,7 +402,26 @@ const logic = {
         else throw Error('Details are not found')
     },
 
+  /**
+   * 
+   * @param {array} ingredientsRecipe 
+   * @param {array} ingredientsQuery 
+   * 
+   * @throws {Error} wheh recipeUri is not found in recipes
+   * @throws {TypeError} - On different arguments types
+   * 
+   * @return {object} - fridge and shopping creates
+   */
     generateLists(ingredientsRecipe, ingredientsQuery){
+
+        if (ingredientsRecipe.constructor !== Array) throw TypeError(ingredientsRecipe + ' is not an array')
+
+        if (ingredientsRecipe.length <1) throw Error(ingredientsRecipe + ' cannot be empty')
+
+        if (ingredientsQuery.constructor !== Array) throw TypeError(ingredientsQuery + ' is not an array')
+
+        if (ingredientsQuery.length <1) throw Error(ingredientsQuery + ' cannot be empty')
+    
         let fridge =[]
         let shopping=[]
 
