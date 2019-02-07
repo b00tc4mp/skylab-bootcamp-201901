0683418e8ee0c234a0a3d3f5f459640e45a2e26a
Oriol Bover Vila <boversauros@Oriols-MacBook-Pro.local>
2019-02-07 10:09:54 +0100

    delete localstorage + update minor changes

--- staff/groups/high-order-boys/ticket-app/src/components/Header/index.js
+++ staff/groups/high-order-boys/ticket-app/src/components/Header/index.js
@@ -25,7 +25,7 @@ class Header extends Component {
         return <header className="hero is-small is-black">
             <div className="hero-body">
                 <div className="container is-pulled-left">
-                    <h1 className="title">HANGOUT<i class="fas fa-circle first"></i><i class="fas fa-circle second"></i><i class="fas fa-circle third"></i></h1>
+                    <Link to="/home" className="title"><h1 className="title">HANGOUT<i class="fas fa-circle first"></i><i class="fas fa-circle second"></i><i class="fas fa-circle third"></i></h1></Link>
                 </div>
      {!!user && <div className="navbar-end header-icons">
                     <Link className="navbar-item" to="/home/user" name="userInfo">{user.name}</Link>
