903b74232bcbf8e2dbaf439751d8ba1550f87781
Oriol Bover Vila <boversauros@Oriols-MacBook-Pro.local>
2019-02-07 16:47:05 +0100

    logic functionality features

--- staff/groups/high-order-boys/ticket-app/src/components/Register/index.js
+++ staff/groups/high-order-boys/ticket-app/src/components/Register/index.js
@@ -1,59 +1,86 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import logic from '../../logic/'
 import Feedback from '../Feedback/'
 
 class Register extends Component {
 
-    state = { name: '', surname: '', email: '', password: '', passwordConfirmation: '', bDate: ''}
+    state = { name: '', surname: '', email: '', password: '', passwordConfirmation: '', bDate: '', user: null}
 
     handleInput = event => this.setState({ [event.target.name]: event.target.value })
+
+    componentDidMount()
+    {
+        const {props: user} = this
+        if(user)        
+            this.setState({ 
+                user: user,
+                name: user.name,
+                surname: user.surname,
+                email: user.email,
+                bDate: user.bDate ? user.bDate: ''
+            })
+    }
+
+    componentWillReceiveProps(props) {
+        const {user} = props
+        if(user)
+            this.setState({ 
+                user: user,
+                name: user.name,
+                surname: user.surname,
+                email: user.email,
+                bDate: user.bDate ? user.bDate: ''
+            })
+    }
 
     onSubmitClick = event => {
         event.preventDefault()
 
-        const { state: { name, surname, email ,password, passwordConfirmation }, props:{onRegister} } = this
+        const { state: { name, surname, email ,password, passwordConfirmation, bDate }, props:{onRegister} } = this
 
-        onRegister(name, surname, email ,password, passwordConfirmation)
+        onRegister(name, surname, email ,password, passwordConfirmation, bDate)
+
+        this.setState({password: '', passwordConfirmation: ''})
     }
+
     render() {
 
-        const { handleInput, props: {registerFeedback, user} } = this
+        const { handleInput, props: {registerFeedback}, state: {user} } = this
         return <section>
 
 
             <form onSubmit={this.onSubmitClick}>
                 <div>
-
                     <label>name: </label>
-                    <input type="text" name="name" placeholder="Name" onChange={handleInput} required />
+                    <input type="text" name="name" placeholder="Name" onChange={handleInput}  value={this.state.name} required />
                 </div>
                 <div >
                     <label >surname: </label>
-                    <input type="text" name="surname" placeholder="Surname" onChange={handleInput} required />
+                    <input type="text" name="surname" placeholder="Surname" onChange={handleInput} value={this.state.surname} required />
                 </div>
                 <div>
                     <label >Email: </label>
-                    <input type="email" name="email" placeholder="email" onChange={handleInput} required />
+                    <input type="email" name="email" placeholder="email" onChange={handleInput} value={this.state.email} required />
                 </div>
                 {user && <div>
                     <label >Birth date: </label>
-                    <input type="date" name="bDate" placeholder="birth date" onChange={handleInput} required />
+                    <input type="date" name="bDate" placeholder="birth date" onChange={handleInput} value={this.state.bDate}/>
                 </div>}
                 <div>
                     <label>Password: </label>
-                    <input type="password" name="password" placeholder="password" onChange={handleInput} required />
+                    <input type="password" name="password" placeholder="password" onChange={handleInput} required={user ? false: true} />
                 </div>
-                <div>
+                {this.state.password.length > 0 && <div>
                     <label>Confirm Password: </label>
-                    <input type="password" name="passwordConfirmation" placeholder="password confirmation" onChange={handleInput} required />
-                </div>
+                    <input type="password" name="passwordConfirmation" placeholder="password confirmation" onChange={handleInput} required={user && this.state.password.length ==  0 ? false: true} />
+                </div>}
                 <button type="submit"><strong>{user ? 'Save changes': 'Register' }</strong></button>
             </form>
             <div>
-                <button> <Link to="/login"> To Login</Link></button>
+                {!user &&  <button> <Link to="/login"> To Login</Link></button>}
+                {user && <p>Let the password empty if you don't want to change it</p>}
             </div>
-
+                
             {registerFeedback && < Feedback message={registerFeedback} />}
         </section>
     }
--- staff/groups/high-order-boys/ticket-app/src/components/User/index.js
+++ staff/groups/high-order-boys/ticket-app/src/components/User/index.js
@@ -1,56 +1,73 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Route, withRouter } from 'react-router-dom'
 import Register from '../Register'
+import Favourites from '../Favourites'
 import logic from '../../logic'
 
 
 
 class User extends Component {
-    state = {user: null, registerFeedback: null}
+    state = { user: null, registerFeedback: null, eventsFavourites: null }
 
-    componentDidMount(){
+    componentDidMount() {
+        this.handleUser()
+    }
+
+    componentWillMount() {
+        this.props.history.listen(() => this.handleUser())
+    }
+
+    
+    handleUser = () =>{
         logic.getUserApiToken() && logic.retrieveUser()
-            .then(user => this.setState({user}))
+            .then(user => this.setState({ user }))
+            .then(() => this.handleFavourites())
     }
 
-    componentWillReceiveProps(props){
-        
-        this.setState({user: props.user})
-    }
+    handleRegister = (name, surname, email, password, passwordConfirmation, bDate) => {
 
-    handleRegister = (name, surname, email, password, passwordConfirmation) => {
-        try {
-        //   logic.registerUser(name, surname, email, password, passwordConfirmation)
-        //     .then(() => this.props.history.push('/login'))
-        //     .catch(({ message }) => this.setState({ registerFeedback: message }))
-        } catch ({ message }) {
-          this.setState({ registerFeedback: message })
+        const updateInfo = {
+            name: name,
+            surname: surname,
+            username: email,
         }
-      }
+
+        if (password.lenght > 0) updateInfo.password = password
+        if (passwordConfirmation.lenght > 0) updateInfo.passwordConfirmation = passwordConfirmation
+        if (bDate.length > 0) updateInfo.bDate = bDate
+
+        try {
+            logic.updateUser(updateInfo)
+                .then(() => this.props.history.push('/home/user'))
+                .then(() => this.setState({ registerFeedback: 'user updated correctly' }))
+                .then(() => logic.retrieveUser())
+                .then(user => this.setState({ user }))
+                .catch(({ message }) => this.setState({ registerFeedback: message }))
+        } catch ({ message }) {
+            this.setState({ registerFeedback: message })
+        }
+    }
+
+    handleFavourites() {
+        const { state: { user } } = this
+
+        if(user && user.favourites.length > 0)
+            logic.getFavourites(user.favourites)
+            .then(res => this.setState({eventsFavourites: res}))
+    }
 
     render() {
-        const { state: { user }, handleRegister, registerFeedback} = this
+        const { location: { pathname } } = this.props
+        const isFavourites = (pathname.includes("home/user/favourites"))
+        const { state: { user, eventsFavourites }, handleRegister, registerFeedback } = this
 
         return <section>
-            <Link to="/home" className="button">Search Moar</Link>
-            <Link to="/home/favourites" className="button">Favourites</Link>
-            < Register user={user} onRegister={handleRegister} registerFeedback={registerFeedback}/>
-            <div>
-                <label>Name:</label>
-                {user && <p>{user.name}</p>}
-            </div>
-            <div>
-                <label>Surname:</label>
-                {user && <p>{user.surname}</p>}
-            </div>
-            <div>
-                <label>Email:</label>
-                {user && <p>{user.email}</p>}
-            </div>
-
-
+            {!isFavourites && <Link to="/home/user/favourites" className="button">Favourites</Link>}
+            {!isFavourites && <Register user={user} onRegister={handleRegister} registerFeedback={registerFeedback} />}
+            <Route exact path="/home/user/favourites" render={(props) => < Favourites events={eventsFavourites} {...props} />}  />
+            
         </section >
     }
 }
 
-export default User
+export default withRouter(User)
