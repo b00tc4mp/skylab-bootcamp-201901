c3d799b7748890e1d8455f15b7c802407c220c51
Carlos <carloscl1986@gmail.com>
2019-02-07 17:42:19 +0100

    3v

--- staff/groups/funciton/funciton-pokedex/src/components/mainPanel/index.js
+++ staff/groups/funciton/funciton-pokedex/src/components/mainPanel/index.js
@@ -12,12 +12,12 @@ class MainPanel extends Component  {
 render(){
     return <section className='main'>
     <nav className='main__image'>
-        <button onClick = {this.props.logout} >Logout</button>
-        <img src = 'https://media.giphy.com/media/26hisC8akEsnLmmSQ/giphy.gif'></img>
+        <button className="btn btn-dark" onClick = {this.props.logout} >Logout</button>
+        <img  src = 'https://media.giphy.com/media/26hisC8akEsnLmmSQ/giphy.gif'></img>
         {/* <img  src = 'https://i.gifer.com/W0w7.gif'></img> */}
         </nav>
-        <button onClick = {this.props.enableFavorites} >Favorites</button>
-        <button onClick = {this.props.enableSearch} >Search</button>
+        <button className="btn btn-dark" onClick = {this.props.enableFavorites} >Favorites</button>
+        <button className="btn btn-dark" onClick = {this.props.enableSearch} >Search</button>
     </section>
 }
 
