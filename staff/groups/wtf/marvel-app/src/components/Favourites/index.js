03d0ce9c80cab978a8d61c904cd15b83357e99df
Serena <serena@example.com>
2019-02-07 13:50:53 +0100

    add changes favourite panel

--- staff/groups/wtf/marvel-app/src/components/Favourites/index.js
+++ staff/groups/wtf/marvel-app/src/components/Favourites/index.js
@@ -1,37 +1,51 @@
-import React, { Component } from "react";
-import logic from "../Logic";
+import React, { Component } from 'react';
+import logic from '../Logic';
+import './index.sass';
 
 class Favourite extends Component {
-  state = { userFavourites: [] };
-
+  state = { userFavourites: undefined };
   componentDidMount() {
-    return logic.retrieveFavourites()
-      .then(userFavourites => this.setState({userFavourites}));
+    return logic
+      .retrieveFavourites()
+      .then(userFavourites => this.setState({ userFavourites }));
   }
 
   handleItemChosen = id => {
-    const {props: { onItem } } = this;
+    const {
+      props: { onItem }
+    } = this;
 
     onItem(id);
   };
 
   render() {
-    const { state: { userFavourites }, handleItemChosen, handleToHome} = this
-
+    const { userFavourites } = this.state;
+    const { handleItemChosen } = this.props;
     return (
-      <section className="favourites container margin-top">
+      <section className="favourites container margin-top has-text-centered">
         <nav className="panel list-group track">
-          {
-            userFavourites.map(({id, name}) =>  {
-
-            return (<a onClick={() => handleItemChosen(id)} data-id={id}className="panel-block">
+          {userFavourites === undefined && <span>
+                                              <i className=" favourite__loading fas fa-spinner fa-5x" />
+                                           </span>}
+          {userFavourites && userFavourites.length === 0 && (
+            <h3 className="favourites__title white title is-4">
+              No favourite characters added
+            </h3> 
+          )}
+          {userFavourites && userFavourites.length > 0 &&
+            userFavourites.map(({ id, name }) => (
+              <a
+                key={id}
+                onClick={() => handleItemChosen(id)}
+                data-id={id}
+                className="favourite__item white title is-6 column is-three-fifths is-offset-one-fifth"
+              >
                 <span className="panel-icon">
-                  <i class="fas fa-bolt"></i>
+                  <i className="fas fa-bolt" />
                 </span>
                 {name}
-              </a>)
-          })  
-          }
+              </a>
+            ))}
         </nav>
       </section>
     );
