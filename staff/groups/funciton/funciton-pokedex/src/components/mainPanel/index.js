8937aa90b182677e2a3d40aeb18f54efe87b2247
Carlos <carloscl1986@gmail.com>
2019-02-07 17:01:08 +0100

    2v

--- staff/groups/funciton/funciton-pokedex/src/components/mainPanel/index.js
+++ staff/groups/funciton/funciton-pokedex/src/components/mainPanel/index.js
@@ -11,21 +11,13 @@ class MainPanel extends Component  {
 
 render(){
     return <section className='main'>
-    <div className='main__image'>
-
-    <button onClick = {this.props.logout} >Logout</button>
-
-
-    <img  src = 'https://media.giphy.com/media/26hisC8akEsnLmmSQ/giphy.gif'></img>
-    {/* <img  src = 'https://i.gifer.com/W0w7.gif'></img> */}
-
-    </div>
-
-    <button onClick = {this.props.enableFavorites} >Favorites</button>
-    <button onClick = {this.props.enableSearch} >Search</button>
-    
-    
-    
+    <nav className='main__image'>
+        <button onClick = {this.props.logout} >Logout</button>
+        <img src = 'https://media.giphy.com/media/26hisC8akEsnLmmSQ/giphy.gif'></img>
+        {/* <img  src = 'https://i.gifer.com/W0w7.gif'></img> */}
+        </nav>
+        <button onClick = {this.props.enableFavorites} >Favorites</button>
+        <button onClick = {this.props.enableSearch} >Search</button>
     </section>
 }
 
