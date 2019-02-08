98e53836f38b7c7d3598a6d14081d558c65d0bfc
manuelbarzi <manuelbarzi@manuelbarzis-MacBook-Pro.local>
2019-02-08 14:58:35 +0100

    update spotify-app-react-scripts

--- staff/manuel-barzi/spotify-app-react-scripts/src/components/Results/index.jsx
+++ staff/manuel-barzi/spotify-app-react-scripts/src/components/Results/index.jsx
@@ -9,7 +9,7 @@ function Results({ title, results, onItemClick, onToggleFavorite, feedback }) {
         <h3>{title}</h3>
         {feedback && <Feedback message={feedback} level="warn" />}
         <ul>
-            {results && results.map(({ id, title }) => <li className="results__item" key={id}><a href="#" onClick={() => onItemClick(id)}>{title}</a> <i class="far fa-heart" onClick={() => onToggleFavorite(id)}></i></li>)}
+            {results && results.map(({ id, title, isFavorite }) => <li className="results__item" key={id}><a href="#" onClick={() => onItemClick(id)}>{title}</a> <i className={`${isFavorite? 'fas' : 'far'} fa-heart`} onClick={() => onToggleFavorite(id)}></i></li>)}
         </ul>
     </section>
 }
