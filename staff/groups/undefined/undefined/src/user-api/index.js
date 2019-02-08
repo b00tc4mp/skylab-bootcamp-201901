671a9cee6b17f10d210e41c7c98c9360fa28ea42
Tachi <jrfluna8@gmail.com>
2019-02-08 09:00:32 +0100

    document logic an user-api

--- staff/groups/undefined/undefined/src/user-api/index.js
+++ staff/groups/undefined/undefined/src/user-api/index.js
@@ -11,10 +11,10 @@ const userApi = {
 
     /**
      * 
-     * @param {string} name the user's name.
-     * @param {string} surname the user's surname.
-     * @param {string} username the user's username.
-     * @param {string} password the user's password.
+     * @param {string} - name the user's name.
+     * @param {string}  - surname the user's surname.
+     * @param {string} - username the user's username.
+     * @param {string} - password the user's password.
      * 
      * @retuns {Promise} - Resolves with array of movies, otherwise rejects with an error.
      * 
@@ -52,6 +52,18 @@ const userApi = {
             })
     },
 
+     /**
+     * 
+     * @param {string} - username the user's username.
+     * @param {string} - password the user's password.
+     * 
+     * @retuns {Promise} - Response an object with id and token user
+     * 
+     * @throws {TypeError} - On wrong parameters type.
+     * @throws {Error} - On empty parameters value.
+     */
+
+
     authenticate(username, password) {
         if (typeof username !== 'string') throw TypeError(`${username} is not a string`)
         if (!username.trim().length) throw Error('username is empty')
@@ -76,6 +88,18 @@ const userApi = {
             })
     },
 
+        /**
+     * 
+     * @param {string} - user id
+     * @param {string} - user token 
+     * 
+     * @retuns {Promise} - Response an object with  the user's parameters
+     * 
+     * @throws {TypeError} - On wrong parameters type.
+     * @throws {Error} - On empty parameters value.
+     */
+
+
     retrieve(id, token) {
         if (typeof id !== 'string') throw TypeError(`${id} is not a string`)
         if (!id.trim().length) throw Error('id is empty')
@@ -97,6 +121,20 @@ const userApi = {
             })
     },
 
+        /**
+     * 
+     * @param {string} - user id
+     * @param {string} - user token 
+     * @param {object} - data what do you want to modify
+     *
+     * 
+     * @retuns {Promise} - Response an object with the modified data
+     * 
+     * @throws {TypeError} - On wrong parameters type.
+     * @throws {Error} - On empty parameters value.
+     */
+
+
     update(id, token, data) {
         if (typeof id !== 'string') throw TypeError(`${id} is not a string`)
         if (!id.trim().length) throw Error('id is empty')
@@ -104,7 +142,6 @@ const userApi = {
         if (typeof token !== 'string') throw TypeError(`${token} is not a string`)
         if (!token.trim().length) throw Error('token is empty')
 
-        //if (data.constructor !== Object) throw TypeError(`${data} is not an object`)
 
         return fetch(`${this.url}/user/${id}`, {
             method: 'PUT',
@@ -123,6 +160,20 @@ const userApi = {
                 throw Error(response.error)
             })
     },
+
+    /**
+     * 
+     * @param {string} - user id
+     * @param {string}  - token id
+     * @param {string} - username the user's username.
+     * @param {string} - password the user's password.
+     * 
+     * @retuns {Promise} - Delete user
+     * 
+     * @throws {TypeError} - On wrong parameters type.
+     * @throws {Error} - On empty parameters value.
+     */
+
 
     remove(id, token, username, password) {
         if (typeof id !== 'string') throw TypeError(`${id} is not a string`)
