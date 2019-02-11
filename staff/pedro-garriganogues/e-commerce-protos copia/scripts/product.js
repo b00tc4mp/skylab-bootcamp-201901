3cf29e17b0a03d5acb16fc82418ea00a8c934f3c
Pedro <pe->
2019-02-11 18:00:50 +0100

    deleted unnecessary files

--- staff/pedro-garriganogues/e-commerce-protos copia/scripts/product.js
+++
@@ -1,7 +0,0 @@
-function Product(brand, model, size, color, price) {
-    this.brand = brand;
-    this.model = model;
-    this.size = size;
-    this.color = color;
-    this.price = price;
-}
--- staff/pedro-garriganogues/e-commerce-protos copia/scripts/tourism.js
+++
@@ -1,6 +0,0 @@
-function Tourism(brand, model, size, color, price, power, fuel, year) {
-    Vehicle.call(this, brand, model, size, color, price, power, fuel, year);
-}
-
-Tourism.prototype = Object.create(Vehicle.prototype);
-Tourism.prototype.constructor = Tourism;
