afdd7ab8a3ebb33ec551a3e8829ab0f8df9a8105
robertz <robertz.perez1985@gmail.com>
2019-02-12 17:37:29 +0100

    add new learnyounode solutions

--- staff/roberto-perez/learnyounode/11-http-file-server/index.js
+++ staff/roberto-perez/learnyounode/11-http-file-server/index.js
@@ -1,7 +1,14 @@
-var http = require('http');
-var fs = require('fs');
+var http = require("http");
+var fs = require("fs");
 
-var server = http.createServer((req, res) => {
-    res.write('Hello World!');
-    res.end();
-}).listen(80);
+const {
+  argv: [, , port, file]
+} = process;
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, {'content-type': 'text/html'});
+    const readStrem = fs.createReadStream(file);
+    readStrem.pipe(res);
+  })
+  .listen(8000);
