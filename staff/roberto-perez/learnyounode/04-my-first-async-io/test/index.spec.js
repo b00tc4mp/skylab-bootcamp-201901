04bb15f908816718d124a98162ad4d1c3731ccd4
robertz <robertz.perez1985@gmail.com>
2019-02-13 11:45:29 +0100

    Correct modules and testing

--- staff/roberto-perez/learnyounode/04-my-first-async-io/test/index.spec.js
+++ staff/roberto-perez/learnyounode/04-my-first-async-io/test/index.spec.js
@@ -1,13 +1,14 @@
 const { expect } = require("chai");
+const path = require("path");
 const readFile = require("./index");
 
 describe("My firts async IO", () => {
   it("Should succeed on correct file path", done => {
-    const file = __dirname + "/helloworld.txt";
-    readFile(file, (err, data) => {
-      done();
+    const filePath = path.join(__dirname, 'helloworld.txt');
+    readFile(filePath, (err, data) => {
       expect(err).to.equal(null);
       expect(data).to.equal(3);
+      done();
     });
   });
 });
