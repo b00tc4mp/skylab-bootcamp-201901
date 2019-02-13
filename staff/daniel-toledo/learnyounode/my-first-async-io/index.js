eb95099c0b56f4e50202bcbc9d359dad1b110868
danieltolmon <daniel.toledomonfort@gmail.com>
2019-02-13 11:48:08 +0100

    some testing

--- staff/daniel-toledo/learnyounode/my-first-async-io/index.js
+++ staff/daniel-toledo/learnyounode/my-first-async-io/index.js
@@ -1,7 +1,8 @@
-const {argv} = process
-
 const fs=require('fs')
-const buffer=fs.readFile(argv[2], 'utf8', (error, data)=> {
+
+const {argv:[,,path]} = process
+
+fs.readFile(path, 'utf8', (error, data)=> {
     if (error) throw Error('Asyncron error')
     else console.log(data.split('\n').length-1)
 })
