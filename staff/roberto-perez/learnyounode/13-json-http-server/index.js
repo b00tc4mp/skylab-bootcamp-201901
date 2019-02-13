34541a0c1830b4c5de872e126a7acb6030b8498b
robertz <robertz.perez1985@gmail.com>
2019-02-13 11:44:46 +0100

    Correct modules and testing

--- staff/roberto-perez/learnyounode/13-json-http-server/index.js
+++ staff/roberto-perez/learnyounode/13-json-http-server/index.js
@@ -1,33 +1,32 @@
-const http = require('http')
+const http = require("http");
+const url = require("url");
 
-const { argv: [, , port] } = process
+const {
+  argv: [, , port]
+} = process;
 
-http.createServer((req, res) => {
-    if (req.url.startsWith('/api/')) {
-        const [path, query] = req.url.split('?')
-        const [, timestamp] = query.split('=')
+http
+  .createServer((req, res) => {
+    if (req.url.startsWith("/api/")) {
+      const parseUrl = url.parse(req.url, true);
+      const timestamp = parseUrl.query.iso;
+      const date = new Date(timestamp);
+      let response;
 
-        if (path === '/api/unixtime') {
-            const response = {
-                unixtime: new Date(timestamp).getTime()
-            }
+      if (parseUrl.pathname === "/api/unixtime") {
+        response = {
+          unixtime: date.getTime()
+        };
+      } else if (parseUrl.pathname === "/api/parsetime") {
+        response = {
+          hour: date.getHours(),
+          minute: date.getMinutes(),
+          second: date.getSeconds()
+        };
+      }
 
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-
-            res.end(JSON.stringify(response))
-        } else {
-
-            var day = new Date(timestamp);
-
-            const response = {
-                hour: day.getHours(),
-                minute: day.getMinutes(),
-                second: day.getSeconds()
-            }
-
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-
-            res.end(JSON.stringify(response))
-        }
-    } 
-}).listen(port)
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(response));
+    }
+  })
+  .listen(port);
