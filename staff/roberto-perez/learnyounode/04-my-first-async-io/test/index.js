ee228d3aa3b9997b9e8a7093c09e32f348beafe5
robertz <robertz.perez1985@gmail.com>
2019-02-13 11:45:29 +0100

    Correct modules and testing

--- staff/roberto-perez/learnyounode/04-my-first-async-io/test/index.js
+++ staff/roberto-perez/learnyounode/04-my-first-async-io/test/index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 module.exports = (file, callback) => {
   fs.readFile(file, 'utf8', (error, data) => {
-    if(error) throw Error(error);
+    if(error) return callback(error);
     return callback(null, data.split('\n').length - 1);
   });
 };
