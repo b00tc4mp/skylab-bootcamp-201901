a5a2c2bd947453709ba0bc8a064e5fe0a1710b86
Pedro <pe->
2019-02-15 19:51:47 +0100

    robert issues

--- staff/pedro-garriganogues/spotify-web-app/src/logic/index.js
+++ staff/pedro-garriganogues/spotify-web-app/src/logic/index.js
@@ -98,61 +98,6 @@ class Logic {
             }))
     }
 
-    /**
-   * Search artists.
-   * 
-   * @param {string} query 
-   * @param {function} callback 
-   */
-    searchArtists(query, callback) {
-        if (typeof query !== 'string') throw TypeError(`${query} is not a string`)
-
-        if (!query.trim().length) throw Error('query is empty')
-
-        if (typeof callback !== 'function') throw TypeError(`${callback} is not a function`)
-
-        spotifyApi.searchArtists(query, callback)
-    }
-
-    //     /**
-    //      * Retrieves albums from artist.
-    //      * 
-    //        * @param {string} artistId 
-    //      * @param {function} callback 
-    //      */
-    //     retrieveAlbums(artistId, callback) {
-    //         if (typeof artistId !== 'string') throw TypeError(`${artistId} is not a string`)
-
-    //         if (!artistId.trim().length) throw Error('artistId is empty')
-
-    //         if (typeof callback !== 'function') throw TypeError(`${callback} is not a function`)
-
-    //         spotifyApi.retrieveAlbums(artistId, callback)
-    //     }
-
-    //     // TODO retrieveTracks(albumId, callback) // endpoint /v1/albums/${albumId}/tracks
-
-    //     // TODO retrieveTrack(id, callback) // endpoint 	/v1/tracks/{id}
-
-    //     /**
-    //   * Retrieves songs from album.
-    //   * 
-    //     * @param {string} albumId 
-    //   * @param {function} callback 
-    //   */
-    //     retrieveSongs(albumId, callback) {
-
-    //         if (typeof albumId !== 'string') throw TypeError(`${albumId} is not a string`)
-
-    //         if (!albumId.trim().length) throw Error('albumId is empty')
-
-    //         if (typeof callback !== 'function') throw TypeError(`${callback} is not a function`)
-
-
-    //         spotifyApi.retrieveSongs(albumId, callback)
-    //     }
-
-    // TODO updateUser and removeUser
 }
 
 module.exports = Logic
