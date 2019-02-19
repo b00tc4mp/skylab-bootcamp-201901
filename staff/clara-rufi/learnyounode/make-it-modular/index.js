6dcd62b820ee177c7fda4a29e82ac747320ea01b
lluna <lluna@example.com>
2019-02-19 09:16:45 +0100

    .

--- staff/clara-rufi/learnyounode/make-it-modular/index.js
+++
@@ -1,9 +0,0 @@
-const { argv: [, , dir, ext] } = process
-
-const filterByExtension = require('./filter-by-extension')
-
-filterByExtension(dir, ext, (error, files) => {
-    if (error) throw error
-
-    files.forEach(file => console.log(file))
-})
