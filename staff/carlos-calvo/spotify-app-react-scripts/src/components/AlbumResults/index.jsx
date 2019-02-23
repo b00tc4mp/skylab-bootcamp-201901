7286c5ce4f3b4d1323f58ae0a57f457274e6278f
carlosclatg <carloscl1986@gmail.com>
2019-02-23 21:04:30 +0100

    insert comments, but no retrieve comments to an artist

---
+++ staff/carlos-calvo/spotify-app-react-scripts/src/components/AlbumResults/index.jsx
@@ -0,0 +1,53 @@
+'use strict'
+
+import React, { Component } from 'react'
+import Results from '../Results'
+import logic from '../../logic'
+
+class AlbumResults extends Component {
+    state = { artist: null, albums: null, feedback: null }
+
+    componentDidMount() {
+        const { props: { artistId } } = this
+
+        this.handleArtistSelected(artistId)
+    }
+
+    componentWillMount() {
+        const { props: { artistId } } = this
+
+        this.handleArtistSelected(artistId)
+    }
+
+    componentWillReceiveProps(props) {
+        const { artistId } = props
+
+        this.handleArtistSelected(artistId)
+    }
+
+    handleArtistSelected = artistId => {
+        try {
+            Promise.all([
+                logic.retrieveArtist(artistId),
+                logic.retrieveAlbums(artistId)
+            ])
+                .then(([artist, albums]) => {
+                    this.setState({
+                        artist,
+                        albums: albums.map(({ id, name: title }) => ({ id, title }))
+                    })
+                })
+                .catch(({ message }) => this.setState({ feedback: message }))
+        } catch ({ message }) {
+            this.setState({ feedback: message })
+        }
+    }
+
+    render() {
+        const { state: { artist, albums, feedback }, props: { onAlbumSelected } } = this
+
+        return artist ? <Results title={`${artist.name} (Albums)`} results={albums} feedback={feedback} onItemClick={onAlbumSelected} /> : null
+    }
+}
+
+export default AlbumResults
