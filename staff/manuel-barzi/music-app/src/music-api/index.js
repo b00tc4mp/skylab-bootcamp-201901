5387180acf70d104ba608caab84232ac0d273a3e
manuelbarzi <manuelbarzi@manuelbarzis-MacBook-Pro.local>
2019-02-25 13:48:19 +0100

    move spotify-api/app to music-api/app

---
+++ staff/manuel-barzi/music-app/src/music-api/index.js
@@ -0,0 +1,138 @@
+'use strict'
+
+const musicApi = {
+    url: 'http://localhost:8000/api',
+
+    register(name, surname, email, password, passwordConfirm) {
+        if (typeof name !== 'string') throw TypeError(`${name} is not a string`)
+        if (!name.trim().length) throw Error('name is empty')
+
+        if (typeof surname !== 'string') throw TypeError(`${surname} is not a string`)
+        if (!surname.trim().length) throw Error('surname is empty')
+
+        if (typeof email !== 'string') throw TypeError(`${email} is not a string`)
+        if (!email.trim().length) throw Error('email is empty')
+
+        if (typeof password !== 'string') throw TypeError(`${password} is not a string`)
+        if (!password.trim().length) throw Error('password is empty')
+
+        if (typeof passwordConfirm !== 'string') throw TypeError(`${passwordConfirm} is not a string`)
+        if (!passwordConfirm.trim().length) throw Error('password confirm is empty')
+
+        debugger
+
+        return fetch(`${this.url}/user`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ name, surname, email, password, passwordConfirm })
+        })
+            .then(response => response.json())
+            .then(({ id, error }) => {
+                if (error) throw Error(error)
+
+                return id
+            })
+    },
+
+    authenticate(email, password) {
+        if (typeof email !== 'string') throw TypeError(`${email} is not a string`)
+        if (!email.trim().length) throw Error('email is empty')
+
+        if (typeof password !== 'string') throw TypeError(`${password} is not a string`)
+        if (!password.trim().length) throw Error('password is empty')
+
+        return fetch(`${this.url}/user/auth`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        })
+            .then(response => response.json())
+            .then(response => response)
+    },
+
+    retrieve(id, token) {
+        if (typeof id !== 'string') throw TypeError(`${id} is not a string`)
+        if (!id.trim().length) throw Error('id is empty')
+
+        if (typeof token !== 'string') throw TypeError(`${token} is not a string`)
+        if (!token.trim().length) throw Error('token is empty')
+
+        return fetch(`${this.url}/user/${id}`, {
+            headers: {
+                authorization: `Bearer ${token}`
+            }
+        })
+            .then(response => response.json())
+            .then(response => {
+                const { status } = response
+
+                if (status === 'OK') return response.data
+
+                throw Error(response.error)
+            })
+    },
+
+    update(id, token, data) {
+        if (typeof id !== 'string') throw TypeError(`${id} is not a string`)
+        if (!id.trim().length) throw Error('id is empty')
+
+        if (typeof token !== 'string') throw TypeError(`${token} is not a string`)
+        if (!token.trim().length) throw Error('token is empty')
+
+        if (data.constructor !== Object) throw TypeError(`${data} is not an object`)
+
+        return fetch(`${this.url}/user/${id}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${token}`,
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(response => {
+                const { status } = response
+
+                if (status === 'OK') return
+
+                throw Error(response.error)
+            })
+    },
+
+    remove(id, token, email, password) {
+        if (typeof id !== 'string') throw TypeError(`${id} is not a string`)
+        if (!id.trim().length) throw Error('id is empty')
+
+        if (typeof token !== 'string') throw TypeError(`${token} is not a string`)
+        if (!token.trim().length) throw Error('token is empty')
+
+        if (typeof email !== 'string') throw TypeError(`${email} is not a string`)
+        if (!email.trim().length) throw Error('email is empty')
+
+        if (typeof password !== 'string') throw TypeError(`${password} is not a string`)
+        if (!password.trim().length) throw Error('password is empty')
+
+        return fetch(`${this.url}/user/${id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `Bearer ${token}`,
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        })
+            .then(response => response.json())
+            .then(response => {
+                const { status } = response
+
+                if (status === 'OK') return
+
+                throw Error(response.error)
+            })
+    }
+}
+
+export default musicApi
