3628231e84fe56014cd8c6dfaceee8a7b80426c9
manuelbarzi <manuelbarzi@manuelbarzis-MacBook-Pro.local>
2019-02-25 18:06:18 +0100

    update music-apx

--- staff/manuel-barzi/music-app/src/music-api/index.js
+++ staff/manuel-barzi/music-app/src/music-api/index.js
@@ -3,7 +3,7 @@
 const musicApi = {
     url: 'http://localhost:8000/api',
 
-    register(name, surname, email, password, passwordConfirm) {
+    registerUser(name, surname, email, password, passwordConfirm) {
         if (typeof name !== 'string') throw TypeError(`${name} is not a string`)
         if (!name.trim().length) throw Error('name is empty')
 
@@ -36,7 +36,7 @@ const musicApi = {
             })
     },
 
-    authenticate(email, password) {
+    authenticateUser(email, password) {
         if (typeof email !== 'string') throw TypeError(`${email} is not a string`)
         if (!email.trim().length) throw Error('email is empty')
 
@@ -54,7 +54,7 @@ const musicApi = {
             .then(response => response)
     },
 
-    retrieve(id, token) {
+    retrieveUser(id, token) {
         if (typeof id !== 'string') throw TypeError(`${id} is not a string`)
         if (!id.trim().length) throw Error('id is empty')
 
@@ -68,15 +68,13 @@ const musicApi = {
         })
             .then(response => response.json())
             .then(response => {
-                const { status } = response
+                if (response.error) throw Error(response.error)
 
-                if (status === 'OK') return response.data
-
-                throw Error(response.error)
+                return response
             })
     },
 
-    update(id, token, data) {
+    updateUser(id, token, data) {
         if (typeof id !== 'string') throw TypeError(`${id} is not a string`)
         if (!id.trim().length) throw Error('id is empty')
 
@@ -95,15 +93,13 @@ const musicApi = {
         })
             .then(response => response.json())
             .then(response => {
-                const { status } = response
+                if (response.error) throw Error(response.error)
 
-                if (status === 'OK') return
-
-                throw Error(response.error)
+                return response
             })
     },
 
-    remove(id, token, email, password) {
+    removeUser(id, token, email, password) {
         if (typeof id !== 'string') throw TypeError(`${id} is not a string`)
         if (!id.trim().length) throw Error('id is empty')
 
@@ -126,12 +122,164 @@ const musicApi = {
         })
             .then(response => response.json())
             .then(response => {
-                const { status } = response
+                if (response.error) throw Error(response.error)
 
-                if (status === 'OK') return
-
-                throw Error(response.error)
+                return response
             })
+    },
+
+    /**
+         * Searches artists.
+         * 
+         * @param {string} query - The text to match on artists search.
+         * @retuns {Promise} - Resolves with artists, otherwise rejects with error.
+         * 
+         * @throws {TypeError} - On wrong parameters type.
+         * @throws {Error} - On empty parameters value.
+         */
+    searchArtists(query) {
+        if (typeof query !== 'string') throw TypeError(`${query} is not a string`)
+
+        if (!query.trim().length) throw Error('query is empty')
+
+        return fetch(`${this.url}/artists?q=${query}`)
+            .then(response => response.json())
+            .then(response => {
+                if (response.error) throw Error(response.error)
+
+                return response
+            })
+    },
+
+    /**
+     * Retrieves an artist.
+     * 
+     * @param {string} artistId - The artist to retrieve.
+     * @returns {Promise} - Resolves with albums, otherwise rejects with error.
+     * 
+     * @throws {TypeError} - On wrong parameters type.
+     * @throws {Error} - On empty parameters value.
+     */
+    retrieveArtist(artistId) {
+        if (typeof artistId !== 'string') throw TypeError(`${artistId} is not a string`)
+
+        if (!artistId.trim().length) throw Error('artistId is empty')
+
+        return fetch(`${this.url}/artists/${artistId}`, {
+            headers: {
+                authorization: `Bearer ${this.token}`
+            }
+        })
+            .then(response => response.json())
+    },
+
+    addCommentToArtist(userId, token, artistId, text) {
+        // TODO validate arguments
+
+        return fetch(`${this.url}/artist/${artistId}/comment`, {
+            method: 'POST',
+            headers: {
+                authorization: `Bearer ${token}`,
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ userId, text })
+        })
+            .then(response => response.json())
+            .then(response => {
+                if (response.error) throw Error(response.error)
+
+                return response
+            })
+    },
+
+    /**
+     * Retrieves albums from artist.
+     * 
+     * @param {string} artistId - The artist to retrieve albums from.
+     * @returns {Promise} - Resolves with albums, otherwise rejects with error.
+     * 
+     * @throws {TypeError} - On wrong parameters type.
+     * @throws {Error} - On empty parameters value.
+     */
+    retrieveAlbums(artistId) {
+        if (typeof artistId !== 'string') throw TypeError(`${artistId} is not a string`)
+
+        if (!artistId.trim().length) throw Error('artistId is empty')
+
+        return fetch(`${this.url}/artists/${artistId}/albums`, {
+            headers: {
+                authorization: `Bearer ${this.token}`
+            }
+        })
+            .then(response => response.json())
+            .then(({ items }) => items)
+    },
+
+    /**
+     * Retrieves an album.
+     * 
+     * @param {string} albumId - The album to retrieve.
+     * @preturns {Promise} - Resolves with tracks, otherwise rejects with error.
+     * 
+     * @throws {TypeError} - On wrong parameters type.
+     * @throws {Error} - On empty parameters value.
+     */
+    retrieveAlbum(albumId) {
+        if (typeof albumId !== 'string') throw TypeError(`${albumId} is not a string`)
+
+        if (!albumId.trim().length) throw Error('albumId is empty')
+
+        return fetch(`${this.url}/albums/${albumId}`, {
+            headers: {
+                authorization: `Bearer ${this.token}`
+            }
+        })
+            .then(response => response.json())
+    },
+
+    /**
+     * Retrieves tracks from album.
+     * 
+     * @param {string} albumId - The album to retrieve tracks from.
+     * @preturns {Promise} - Resolves with tracks, otherwise rejects with error.
+     * 
+     * @throws {TypeError} - On wrong parameters type.
+     * @throws {Error} - On empty parameters value.
+     */
+    retrieveTracks(albumId) {
+        if (typeof albumId !== 'string') throw TypeError(`${albumId} is not a string`)
+
+        if (!albumId.trim().length) throw Error('albumId is empty')
+
+        return fetch(`${this.url}/albums/${albumId}/tracks`, {
+            headers: {
+                authorization: `Bearer ${this.token}`
+            }
+        })
+            .then(response => response.json())
+            .then(({ items }) => items)
+    },
+
+    /**
+     * Retrieves track.
+     * 
+     * @param {string} trackId - The id of the track to be retrieved.
+     * @returns {Promise} Resolves with track, otherwise rejects with error.
+     * 
+     * @throws {TypeError} - On wrong parameters type.
+     * @throws {Error} - On empty parameters value.
+     */
+    retrieveTrack(trackId) {
+        if (typeof trackId !== 'string') throw TypeError(`${trackId} is not a string`)
+
+        if (!trackId.trim().length) throw Error('trackId is empty')
+
+        return fetch(`${this.url}/tracks/${trackId}`, {
+            headers: {
+                authorization: `Bearer ${this.token}`
+            }
+        })
+            .then(response => response.json())
     }
 }
 
