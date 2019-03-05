8d1525bd0f925af14cbe28cca285931e918512dc
Pedro <pe->
2019-03-05 17:05:08 +0100

    glorious navbar added

--- staff/pedro-garriganogues/pfinal/client/src/components/Navbar/index.js
+++ staff/pedro-garriganogues/pfinal/client/src/components/Navbar/index.js
@@ -8,11 +8,8 @@ import Products from '../Products'
 
 
 class Navbar extends Component {
-    state = {
-        user: {},
-        categories: []
 
-    }
+
 
     // retrieveUser() {
     //     if (this.props.loggedIn)
@@ -44,18 +41,41 @@ class Navbar extends Component {
 
     render() {
         return (
-            <nav className="navbar">
-                <div className="container">
 
-                    <Link to="/" >HOME</Link>
-                    <Link to="/about" >About</Link>
-                    <Link to="/contact" >Contact</Link>
-                    <Link to="/products" >Products</Link>
-                    <Link to="/" >Carro</Link>
 
-                    {< SignedIn />}
-                    {<SignedOut />}
-                </div>
+            <nav>
+                <body>
+                    <div class="navegacion">
+                        <nav>
+                            <ul>
+                                <li><Link to="/" >HOME</Link> <span class="icon icon-up-dir"></span></li>
+                                <li>  <Link to="/about" >About</Link><span class="icon icon-up-dir"></span></li>
+                                <li>  <Link to="/contact" >Contact</Link><span class="icon icon-up-dir"></span></li>
+                                <li>  <Link to="/products" >Products</Link><span class="icon icon-up-dir"></span></li>
+                                <li>
+                                    <p>User</p>
+                                    <div class="submenu">
+                                        <div class="submenu-items">
+                                            {logic.__userApiToken__ && < SignedIn />}
+                                        </div>
+                                        <div class="submenu-items">
+                                            {!logic.__userApiToken__ && <SignedOut />}
+                                        </div>
+                                        {/* <div class="submenu-items">
+                                            <p>Submenu #3</p>
+                                            <ul>
+                                                <li><a href="#">Item #1</a></li>
+                                                <li><a href="#">Item #2</a></li>
+                                                <li><a href="#">Item #3</a></li>
+                                            </ul>
+                                        </div> */}
+                                    </div>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </body>
+
             </nav>
 
         )
