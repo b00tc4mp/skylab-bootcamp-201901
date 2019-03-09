a19c613d59d264112788141cefaba2783b7e40a4
Marc Sarroca Diaz <msarroca@Marcs-MacBook-Pro.local>
2019-03-09 17:28:49 +0100

    remove create react app stuff

--- staff/marc-sarroca/insta-food/insta-food-app/src/index.js
+++ staff/marc-sarroca/insta-food/insta-food-app/src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "../src/components/App";
-import * as serviceWorker from "./serviceWorker";
 import { HashRouter } from "react-router-dom";
 import logic from "./logic";
 
@@ -34,8 +32,3 @@ ReactDOM.render(
   </HashRouter>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
