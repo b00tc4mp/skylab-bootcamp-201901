39b707e7a33027d0179a5fb42caa6b733cad3c78
martimalek <martimalekdot@hotmail.es>
2019-03-10 23:02:38 +0100

    basic css toolbar

--- staff/marti-malek/final-project/osi-app/src/components/Toolbar/index.js
+++ staff/marti-malek/final-project/osi-app/src/components/Toolbar/index.js
@@ -5,10 +5,10 @@ function Toolbar() {
 
     return <section className="toolbar">
         <div className="toolbar__logo"></div>
-        <div className="toolbar__file"><i class="far fa-file"><i class="fas fa-plus"></i></i></div>
-        <div className="toolbar__folder"><i class="fas fa-folder-plus"></i></div>
-        <div className="toolbar__calc"><i class="fas fa-calculator"></i></div>
-        <div className="toolbar__finder"><i class="fas fa-search"></i></div>
+        <div className="toolbar__file"><i class="icon fas fa-file"><i class="sub-icon fas fa-plus"></i></i></div>
+        <div className="toolbar__folder"><i class="icon fas fa-folder-plus"></i></div>
+        <div className="toolbar__calc"><i class="icon fas fa-calculator"></i></div>
+        <div className="toolbar__finder"><i class="icon fas fa-search"></i></div>
     </section>
 }
 
