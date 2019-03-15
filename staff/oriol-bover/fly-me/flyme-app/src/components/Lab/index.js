26ed1333dcb46a899da6cafe7f5d2917c40f7540
Oriol Bover Vila <boversauros@Oriols-MacBook-Pro.local>
2019-03-15 15:32:43 +0100

    adding dnd program creation

--- staff/oriol-bover/fly-me/flyme-app/src/components/Lab/index.js
+++ staff/oriol-bover/fly-me/flyme-app/src/components/Lab/index.js
@@ -1,17 +1,119 @@
-import React, { useState, Fragment } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import initialData from './initial-data'
 import './index.sass'
 
 export default function Lab() {
     const [state, setState] = useState(initialData)
+    const [name, setName] = useState('')
+    const [feedback, setFeedback] = useState(null)
 
     function onDragEnd(result) {
-        console.log(result)
+        const { destination, source, draggableId } = result
+
+        if (!destination) return
+
+        if (destination.droppableId === source.droppableId && destination.index === source.index) return
+
+        const start = state.columns[source.droppableId]
+        const finish = state.columns[destination.droppableId]
+
+        if (start === finish) {
+            const newTaskIds = Array.from(start.taskIds)
+
+            newTaskIds.splice(source.index, 1)
+            newTaskIds.splice(destination.index, 0, draggableId)
+
+            const newColumn = {
+                ...start,
+                taskIds: newTaskIds
+            }
+
+            const newState = {
+                ...state,
+                columns: {
+                    ...state.columns,
+                    [newColumn.id]: newColumn
+                }
+            }
+
+            setState(newState)
+
+            return
+        }
+
+        const startTaskIds = Array.from(start.taskIds)
+        // startTaskIds.splice(source.index, 1)
+
+        const newStart = {
+            ...start,
+            taskIds: startTaskIds
+        }
+
+        const finishTaskIds = Array.from(finish.taskIds)
+        const newId = Object.keys(initialData.tasks).length + 1
+        const newTaskId = `task-${newId}`
+
+        initialData.tasks[newTaskId] = Object.assign({}, initialData.tasks[draggableId])
+        initialData.tasks[newTaskId].id = newTaskId
+
+        finishTaskIds.splice(destination.index, 0, newTaskId)
+
+        const newFinish = {
+            ...finish,
+            taskIds: finishTaskIds
+        }
+
+        const newState = {
+            ...state,
+            columns: {
+                ...state.columns,
+                [newStart.id]: newStart,
+                [newFinish.id]: newFinish
+            }
+        }
+
+        setState(newState)
+    }
+
+    function saveProgram() {
+        const ids = state.columns['column-2'].taskIds
+        const orders = []
+
+        if (ids.length === 0) {
+            setFeedback('Program is empty')
+            return
+        }
+
+        ids.forEach(id => {
+            orders.push(state.tasks[id])
+        })
+
+        console.log('orders', orders)
+    }
+
+    function deleteCommand(commandId) {
+        const idDelete = state.columns['column-2'].taskIds.indexOf(commandId)
+        if (idDelete > -1) state.columns['column-2'].taskIds.splice(idDelete, 1)
+
+        const newState = {
+            ...state,
+            columns: {
+                ...state.columns,
+                ['column-2']: state.columns['column-2'],
+            }
+        }
+
+        setState(newState)
     }
 
     return (<DragDropContext on onDragEnd={onDragEnd} >
         <section className="section">
+            <div className="columns">
+                <div className="control column">
+                    <input type="text" className="input" placeholder="Program name" onChange={e => setName(e.target.value)} />
+                </div>
+            </div>
             <div className="columns">
                 {state.columnOrder.map(columnId => {
                     const column = state.columns[columnId]
@@ -20,13 +122,15 @@ export default function Lab() {
                     return (<Fragment key={column.id}>
                         <div className="column t-container">
                             <h3 className="t-title">{column.title}</h3>
-                            <Droppable droppableId={column.id} >
+                            <Droppable droppableId={column.id} isDropDisabled={column.id === 'column-1'} >
                                 {(provided) => (
                                     <div className="t-tasks" ref={provided.innerRef} {...provided.droppableProps}>
                                         {tasks.map((task, index) =>
                                             <Draggable draggableId={task.id} index={index}>
                                                 {(provided) => (
-                                                    <div className="t-task" key={task.id} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>{task.content}</div>
+                                                    <div className="t-task" key={task.id} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                                        {task.content} {column.id == 'column-2' ? <button className="delete" onClick={() => deleteCommand(task.id)} ></button> : ''}
+                                                    </div>
                                                 )}
                                             </Draggable>)}
                                         {provided.placeholder}
@@ -37,6 +141,8 @@ export default function Lab() {
                     </Fragment>)
                 })}
             </div>
+            {feedback && <p>{feedback}</p>}
+            <button className="button" onClick={() => saveProgram()} >Save</button>
         </section>
     </DragDropContext>)
 }
