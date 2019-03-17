fae012bd9338e40dc33a8b6dc0a7955d3f28cd3d
martimalek <martimalekdot@hotmail.es>
2019-03-17 10:48:38 +0100

    clock

--- staff/marti-malek/final-project/osi-app/src/components/Clock/index.js
+++ staff/marti-malek/final-project/osi-app/src/components/Clock/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import './index.sass'
 
-function Clock() {
+function Clock({ clock, checkTime }) {
+    clock = () => {
+        let time = new Date()
+
+        let hours = time.getHours()
+
+        let minutes = time.getMinutes()
+
+        let seconds = time.getSeconds()
+
+        checkTime = time => {
+            if (time < 10) {
+                time = '0' + time
+            }
+            return time;
+        }
+
+        document.querySelectorAll('.clock')[0].innerHTML = checkTime(hours) + ":" + checkTime(minutes) + ":" + checkTime(seconds);
+
+    }
+    setInterval(clock, 1000);
 
     return <section className="clock">
-        
+
     </section>
 }
 
