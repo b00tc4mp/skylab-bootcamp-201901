c043f364e2426ada881f461778c169f5c31b278f
manuelbarzi <manuelbarzi@manuelbarzis-MacBook-Pro.local>
2019-03-19 20:38:54 +0100

    add fs cp, mv, rm

--- stuff/react/react-get-derived-state-from-props-and-more/scripts/components/feedback.jsx
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(() => {
-    function Feedback({ message, level }) {
-        return <section className={`feedback ${level ? `feedback--${level}` : ''}`}>{message}</section>
-    }
-    
-    modules.export('feedback', Feedback)
-})()
