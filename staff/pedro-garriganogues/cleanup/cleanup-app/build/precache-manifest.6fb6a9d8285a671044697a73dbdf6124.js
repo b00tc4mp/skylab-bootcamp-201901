0ae8fe24b11d9a2e6bb599bf6eda79d47b4d356e
Pedro <pe->
2019-03-20 15:48:39 +0100

    surge testing

---
+++ staff/pedro-garriganogues/cleanup/cleanup-app/build/precache-manifest.6fb6a9d8285a671044697a73dbdf6124.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f5f0dd5e8893b9a494ab",
+    "url": "/static/js/main.f5f0dd5e.chunk.js"
+  },
+  {
+    "revision": "ce886124fbfb550c4aeb",
+    "url": "/static/js/1.ce886124.chunk.js"
+  },
+  {
+    "revision": "f5f0dd5e8893b9a494ab",
+    "url": "/static/css/main.0202d3a8.chunk.css"
+  },
+  {
+    "revision": "cf7782ddf985dd55e702357d1c02e581",
+    "url": "/index.html"
+  }
+];
