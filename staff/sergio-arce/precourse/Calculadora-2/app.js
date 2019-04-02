c55be18a94344823d41344a763c33e607e21f57e
SERGIO MICHAEL ARCE CHIJO <sergio.arce@MacBook-Pro-de-SERGIO.local>
2019-04-02 16:57:14 +0200

    add ;

--- staff/sergio-arce/precourse/Calculadora-2/app.js
+++ staff/sergio-arce/precourse/Calculadora-2/app.js
@@ -1,16 +1,17 @@
-const idScreen = document.getElementById("screen")
-var operation = ''
-var number1 = ''
-var number2 =''
+
+const idScreen = document.getElementById("screen");
+var operation = '';
+var number1 = '';
+var number2 ='';
 
 
 
 function insert(num) {
   
   if ((num === '-' && idScreen.value === "0") || (num === "/" && idScreen.value === "0") || (num === "*" && idScreen.value === "0") || (num === "+" && idScreen.value === "0")) {
-    return idScreen.value = "0"
+    return idScreen.value = "0";
   }
-  idScreen.value === "0" ? idScreen.value = num : idScreen.value += num
+  idScreen.value === "0" ? idScreen.value = num : idScreen.value += num;
 }
 
 function equal() {
@@ -25,10 +26,3 @@ function square() {
   var x = idScreen.value;
   idScreen.value = idScreen.value = Math.pow(x, 2);
 }
-
-
-// function decimal() {
-//   if(idScreen.value.split('').filter(point => point == '.').length == 2){
-//     idScreen.value.slice(0, idScreen.value.length -1)
-//   }
-// }
