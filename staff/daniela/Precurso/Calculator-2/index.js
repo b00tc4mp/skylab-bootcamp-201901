23364347004e18225c54cde93438c69ddaab5fbc
Daniela <daniela@MacBook-Pro-6.local>
2019-04-02 17:40:25 +0200

    add ;

--- staff/daniela/Precurso/Calculator-2/index.js
+++ staff/daniela/Precurso/Calculator-2/index.js
@@ -6,8 +6,8 @@
     const mathResults = document.querySelector(".instructions");
     //console.log(sendButton);
     sendButton.addEventListener("click", function(){
-        const firstNum = Number.parseInt(firstNumber.value)
-        const secondNum = Number.parseInt(secondNumber.value) 
+        const firstNum = Number.parseInt(firstNumber.value);
+        const secondNum = Number.parseInt(secondNumber.value); 
        // console.log(mathResults); 
         const result = calculator(firstNum, secondNum);
         mathResults.innerHTML = result;
@@ -24,7 +24,7 @@ const calculator = function (firstNum, secondNum) {
     let raizCuadrada;
 
     if (isNaN(firstNum) && isNaN(secondNum)){
-        return "You are making a mistake. Try again"
+        return "You are making a mistake. Try again";
     }
 
     if (typeof firstNum === 'number' && typeof secondNum === 'number' && !isNaN(firstNum) && !isNaN(secondNum)){
@@ -34,36 +34,36 @@ const calculator = function (firstNum, secondNum) {
         division = firstNum / secondNum;
 
         if (!Number.isInteger(suma)){
-            suma = suma.toFixed(3)
+            suma = suma.toFixed(3);
         }
-        results.push(firstNum + ' + ' + secondNum + ' = ' + suma)
+        results.push(firstNum + ' + ' + secondNum + ' = ' + suma);
 
         if (!Number.isInteger(resta)){
-            resta = resta.toFixed(3)
+            resta = resta.toFixed(3);
         }  
-        results.push(firstNum + ' - ' + secondNum + ' = ' + resta)
+        results.push(firstNum + ' - ' + secondNum + ' = ' + resta);
         
         if (!Number.isInteger(multiplicacion)){
-           multiplicacion = multiplicacion.toFixed(3)
+           multiplicacion = multiplicacion.toFixed(3);
         } 
-        results.push(firstNum + ' * ' + secondNum + ' = ' + multiplicacion)
+        results.push(firstNum + ' * ' + secondNum + ' = ' + multiplicacion);
 
         if (!Number.isInteger(division)){
-           division = division.toFixed(3)
+           division = division.toFixed(3);
         }
         results.push(firstNum + ' / ' + secondNum + ' = ' + division)
-        return ('Los resultados son: <br> ' + results.join("<br>"))
+        return ('Los resultados son: <br> ' + results.join("<br>"));
         
     }
   
     if (!isNaN(firstNum)){
         raizCuadrada = Math.sqrt(firstNum);
-        return 'La raiz cuadrada de ese número es ' + raizCuadrada.toFixed(3)
+        return 'La raiz cuadrada de ese número es ' + raizCuadrada.toFixed(3);
     }
   
     if (!isNaN(secondNum)){
         raizCuadrada = Math.sqrt(secondNum);
-        return 'La raiz cuadrada de ese número es ' + raizCuadrada.toFixed(3)
+        return 'La raiz cuadrada de ese número es ' + raizCuadrada.toFixed(3);
     } 
 }
 
