422f93fd2d08ec57f25c1845b3b765d2839a2032
mlila <marialilapetri@gmail.com>
2019-04-03 11:23:19 +0200

    precourse

--- staff/lila-petri/calculator-2/index.js
+++
@@ -1,9 +0,0 @@
-
-function calculate(){
-    var calc = document.getElementById("display").value;
-    var result=eval(calc);
-    document.getElementById('display').value = String(result);
-
-}
-
-
