2c8ddc4b1e0ae16ce41443d78e8fdacd8a3e2297
Miguel Angel Zapatero <m.zapes@gmail.com>
2019-04-09 13:23:53 +0200

    modify function functionality with other types values

--- staff/miguel-angel-zapatero/array-functions/concat.js
+++ staff/miguel-angel-zapatero/array-functions/concat.js
@@ -1,3 +1,25 @@
-function (arr1, arr2, callback) {
-    
+/**
+ * Iterate diferents arrays to concatenate the values into a new array.
+ * 
+ * @param  {arguments} args The array (or arrays) to concatenate
+ * 
+ * @returns {Array}
+ */
+
+function concat() {
+    var args = Array.from(arguments);
+    var result = [];
+    var i = 0;
+    for(item of args) {
+        if (!isArray(item)) {
+            result[i] = item;
+            i++;
+        } else {
+            for(var j = 0; j < item.length; j++) {
+                result[i] = item[j];
+                i++;
+            }
+        }
+    }
+    return result;
 }
