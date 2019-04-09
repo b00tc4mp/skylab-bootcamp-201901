0f8e811d60d63db457a515c2d1860fa2e4952b15
Miguel Angel Zapatero <m.zapes@gmail.com>
2019-04-09 19:15:59 +0200

    add reduceRight function demos

--- staff/miguel-angel-zapatero/array-functions/reduce-right.demo.js
+++ staff/miguel-angel-zapatero/array-functions/reduce-right.demo.js
@@ -1,11 +1,22 @@
 console.log('DEMO', 'reduceRight');
 
-var a = [[0, 1], [2, 3], [4, 5]];
+var a = [2, 5, 10];
+var b = [[0, 1], [2, 3], [4, 5]];
 
 console.log('case 1');
-console.log(reduceRight(a, function(x, y){
-    return concat(x, y)
+console.log(reduceRight(a, function(acc, elem) {
+    return acc + elem
 }));
+// 17
+
+console.log('case 1');
+console.log(reduceRight(a, function(acc, elem) {
+    return acc * elem
+}, 5));
+// 500
+
+console.log('case 2');
+console.log(reduceRight(b, concat));
 // [4, 5, 2, 3, 0, 1]
 
 console.log('\n');
