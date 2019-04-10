efa6d0a5495d5e07d1993c4970cddace2c53c97f
Edgar Frías <edfriasji@gmail.com>
2019-04-10 16:01:06 +0200

    add changes to forEach

--- staff/edgar-frias-jimenez/array-functions/for-each.js
+++ staff/edgar-frias-jimenez/array-functions/for-each.js
@@ -5,10 +5,28 @@
  * @param {Function} callback The expression to evaluate.
  */
 
+// function forEach(array, callback) {
+// 	if (!(array instanceof Array)) throw TypeError(array + ' is not an array');
+// 	if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
+
+// 	for (var i = 0; i < array.length; i++)
+// 		callback(array[i], i);
+// }
+
+// forEach without forEach && without for loop
 function forEach(array, callback) {
 	if (!(array instanceof Array)) throw TypeError(array + ' is not an array');
 	if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
 
-	for (var i = 0; i < array.length; i++)
-		callback(array[i], i);
+	if (array.length > 0) {
+		function forEach(index) {
+			callback(array[index], index);
+
+			if (++index < array.length) {
+				forEach(index)
+			}
+		}
+
+		forEach(0);
+	}
 }
