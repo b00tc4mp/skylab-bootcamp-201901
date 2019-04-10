43cd9237f3b1f1494300369084adda8b79a0515a
Pablo Rey <pabloforpresident@gmail.com>
2019-04-10 13:25:51 +0200

    recursive challenge in for-each

--- staff/pablo-rey/array-functions/for-each.js
+++ staff/pablo-rey/array-functions/for-each.js
@@ -10,9 +10,26 @@ function forEach(array, callbackfn, _this) {
 	if (!(array instanceof Array)) throw new TypeError("array param is not an array");
 	if (!(callbackfn instanceof Function)) throw new TypeError("undefined is not a function");
 	
-	for (var k = 0; k < array.length; k++){
-		callbackfn.call(_this, array[k], k);
-	}
+	simFor(array.length, function (index) {
+		callbackfn.call(_this, array[index], index);
+	})
 
 	return undefined;
 }
+
+function simFor (cap, callbackfn, next) {
+	var i = next || 0;
+	callbackfn(i);
+	if (i < cap-1) simFor(cap, callbackfn, i + 1)
+}
+
+// function forEach(array, callbackfn, _this) {
+// 	if (!(array instanceof Array)) throw new TypeError("array param is not an array");
+// 	if (!(callbackfn instanceof Function)) throw new TypeError("undefined is not a function");
+	
+// 	for (var k = 0; k < array.length; k++){
+// 		callbackfn.call(_this, array[k], k);
+// 	}
+
+// 	return undefined;
+// }
