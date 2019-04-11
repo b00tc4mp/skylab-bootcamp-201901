e2ac747d00a2e5134809022885841e18a29ddeec
Sergio Arce <arcesech30@gmail.com>
2019-04-11 18:31:23 +0200

    some, reverse ....

---
+++ staff/sergio-arce/array-functions/reverse.test.js
@@ -0,0 +1,71 @@
+'use strict';
+
+describe('reverse', function(){
+    it('should return...', function(){
+        var array = [1, 2, 3, 4];
+
+        var result = reverse(array);
+        var exp = [4, 3, 2, 1];
+
+
+
+        expect(result, exp); 
+
+    });
+
+    // it('shoul ..', function(){
+    //     var array = [1, 2, 3, 4];
+
+
+    //     expect(result,);
+
+    // });
+
+
+
+    // it('shoul...........', function(){
+
+
+        
+    //     try {
+    //         reverse()
+            
+    //         throw Error('should not reach this point');
+            
+    //     } catch (error) {
+            
+    //         expect(error.message, ".....");
+            
+    //     }
+    // });
+        
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log('DEMO', 'reverse');
+
+// var a = [1, 2, 3];
+
+// console.log('case 1');
+
+// console.log(reverse(a));
+
+// console.log('case 2');
+
+// var array1 = ['one', 'two', 'three'];
+
+// console.log(reverse(array1));
--- staff/sergio-arce/array-functions/some.test.js
+++ staff/sergio-arce/array-functions/some.test.js
@@ -45,3 +45,5 @@ describe('some', function(){
 });
 
 
+
+
