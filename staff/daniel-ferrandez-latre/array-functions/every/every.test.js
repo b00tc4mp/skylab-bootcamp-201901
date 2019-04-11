c74fc18d1feaa152d8f2245636c8c6050b7084ee
DaniLatreCodes <danilatre.skycode@gmail.com>
2019-04-11 16:55:45 +0200

    Every test fixed

--- staff/daniel-ferrandez-latre/array-functions/every/every.test.js
+++ staff/daniel-ferrandez-latre/array-functions/every/every.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-suite('every', function () {
-    test('should return true on all items matching condition', function () {
+describe('every', function () {
+    it('should return true on all items matching condition', function () {
         var array = [1, 2, 3];
 
         var result = every(array, function (v) { return v > 0; });
@@ -10,7 +10,7 @@ suite('every', function () {
     });
 
 
-    test('should return false on any of the items not matching the condition', function () {
+    it('should return false on any of the items not matching the condition', function () {
         var array = [1, 2, 3];
 
         var result = every(array, function (v) { return v < 0; });
@@ -18,22 +18,22 @@ suite('every', function () {
         expect(result, false);
     });
 
-    test('should break on undefined array', function () {
+    it('should break on undefined array', function () {
         try {
             every();
 
             throw Error('should not reach this point');
         } catch (error) {
+            console.log(error.message)
             expect(error.message, 'undefined is not an array');
         }
     });
 
-    test('should break on undefined callback', function () {
+    it('should break on undefined callback', function () {
         var array = [1, 2, 3];
 
         try {
             every(array);
-
             throw Error('should not reach this point');
         } catch (error) {
             expect(error.message, 'undefined is not a function');
--- staff/daniel-ferrandez-latre/array-functions/expect-1.0.2.js
+++ staff/daniel-ferrandez-latre/array-functions/expect-1.0.2.js
@@ -1,11 +1,13 @@
 'use strict';
 
+console.log('%c expect', 'font-weight: bold; font-size: 50px; color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+
 /**
  * Checks wether a condition is satisfied, otherwise throws an error.
  * 
  * @param {*} actual The actual value to check agains the expected value.
  * @param {*} expected The expected value.
- * @param {boolean} [deep] Indicates wether the interal check comparison must be deep or not (in case of arrays or objects).
+ * @param {boolean} [deep] Indicates wether the comparison must be deep or not (in case of arrays or objects).
  */
 function expect(actual, expected, deep) {
     if (!deep) {
