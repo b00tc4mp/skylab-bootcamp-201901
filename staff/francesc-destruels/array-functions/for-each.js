f243c6b69c5d5abec561dd9046eb84a0715151df
Fran Destruels <frand.ross@icloud.com>
2019-04-11 10:23:29 +0200

    grammar

--- staff/francesc-destruels/array-functions/for-each.js
+++ staff/francesc-destruels/array-functions/for-each.js
@@ -5,14 +5,19 @@
  * 
  * @param {Array} array The array to iterate.
  * @param {Function} callback The expression to evaluate.
+*/
 
+/*
 function forEach(array, callback) {
 	if (!(array instanceof Array)) throw TypeError(array + ' is not an array');
 	if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
 
 	for (var i = 0; i < array.length; i++)
 		callback(array[i], i);
-} */
+} 
+
+*/
+
 function forEach(array, callback){
 	if (!(array instanceof Array)) throw TypeError(array + ' is not an array');
 	if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
