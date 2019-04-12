b5e04b7b682f671a8d60db73e659563a1dacfa55
Miguel Angel Sevillano Hidalgo <mash5550@gmail.com>
2019-04-12 18:50:22 +0200

    array-update

--- staff/miguel-angel-sevillano/array-functions/sort.js
+++ staff/miguel-angel-sevillano/array-functions/sort.js
@@ -1,48 +1,6 @@
-// Split the array into halves and merge them recursively 
-function mergeSort (arr) {
-    if (arr.length === 1) {
-      // return once we hit an array with a single item
-      return arr
-    }
-  
-    const middle = Math.floor(arr.length / 2) // get the middle item of the array rounded down
-    const left = arr.slice(0, middle) // items on the left side
-    const right = arr.slice(middle) // items on the right side
-  
-    return merge(
-      mergeSort(left),
-      mergeSort(right)
-    )
-  }
-  
-  // compare the arrays item by item and return the concatenated result
-  function merge (left, right) {
-    let result = []
-    let indexLeft = 0
-    let indexRight = 0
-  
-    while (indexLeft < left.length && indexRight < right.length) {
-      if (left[indexLeft] < right[indexRight]) {
-        result.push(left[indexLeft])
-        indexLeft++
-      } else {
-        result.push(right[indexRight])
-        indexRight++
-      }
-    }
-  
-    return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
-  }
-  
-  const list = [2, 5, 1, 3, 7, 2, 3, 8, 6, 3]
-  console.log(mergeSort(list)) // [ 1, 2, 2, 3, 3, 3, 5, 6, 7, 8 ]
 
 
-
-
-
-
-  var a =[6,4,7,5]
+  var a =[6,4,7,5,6,7]
 
 
   function sort(array){
@@ -51,13 +9,12 @@ function mergeSort (arr) {
     var n=0;
     var acc = array.length 
     var e =array.length -1;
-    var final =[];
     var next = false;
 
     for(let a = 0;a<array.length;a++){
 
         for(var i = 0 ;i<acc;i++){
-            debugger
+           
              if (array[i]>array[i+1] &&  i<e){
                  newar[n]=array[i+1]
                  n++
@@ -70,14 +27,15 @@ function mergeSort (arr) {
         
         }
         acc --;
-        final = newar;
-        array = newar;
+        for(var b=0;b<newar.length;b++){
+              array[b] = newar[b]
+        }
         next = true;
         n=0;
         
     }
 
-        return final
+        return newar  
   }
 
 
