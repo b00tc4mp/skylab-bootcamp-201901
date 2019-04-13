ba000cd4badd102ced8fe8bf544ba194a919b970
mlila <marialilapetri@gmail.com>
2019-04-14 00:38:56 +0200

    hooray

--- staff/lila-petri/hooray/hooray.js
+++ staff/lila-petri/hooray/hooray.js
@@ -1,47 +1,305 @@
-'use strict';
+"use strict";
 
 /**
- * 
+ *
  */
 function Hooray() {
-    
-    if(typeof arguments[0]==='number' && arguments.length===1){
-        this.length = arguments[0];
-    }else{
-        for (var i = 0; i < arguments.length; i++) this[i] = arguments[i];
-        this.length = arguments.length;
-    }
+  var first = arguments[0];
+
+  if (arguments.length === 1 && typeof first === "number")
+    if (parseInt(first) !== first) throw RangeError("Invalid hooray");
+    else return (this.length = first);
+
+  for (var i = 0; i < arguments.length; i++) this[i] = arguments[i];
+  this.length = arguments.length;
 }
 
 /**
-* Adds a value at the end of an hooray, incrementing its length by 1.
-* 
-* @param {*} value The value to push in the hooray.
-* 
-* @returns {number} The length of the hooray after adding the new value.
-*/
+ * Adds a value at the end of an hooray, incrementing its length by 1.
+ *
+ * @param {*} value The value to push in the hooray.
+ *
+ * @returns {number} The length of the hooray after adding the new value.
+ */
 Hooray.prototype.push = function(value) {
-   if (arguments.length > 0)
-       for(var i = 0; i < arguments.length; i++)
-           this[this.length++] = arguments[i];
+  if (arguments.length > 0)
+    for (var i = 0; i < arguments.length; i++)
+      this[this.length++] = arguments[i];
 
-   return this.length;
-}
+  return this.length;
+};
 
 /**
  * Iterates the current hooray and evaluates an expression on each of its values.
- * 
+ *
  * @param {Function} callback The expression to evaluate.
  */
 Hooray.prototype.forEach = function(callback) {
-	if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
+  if (typeof callback !== "function")
+    throw new TypeError(callback + " is not a function");
 
-    var self = this;
+  var self = this;
 
-	this.length && (function forEach(index) {
-		callback(self[index], index);
+  this.length &&
+    (function forEach(index) {
+      callback(self[index], index);
 
-		if (++index < self.length)
-			forEach(index);
-	})(0);
+      if (++index < self.length) forEach(index);
+    })(0);
+};
+/**
+ * This method cconcetenats two hoorays
+ * @param {Hooray} hooray First hooray, used as a base
+ * @param {Hooray} hooray2 Second hooray which will be added in the end of the first one
+ */
+Hooray.prototype.concat = function(hooray) {
+  if (!(hooray instanceof Hooray))
+    throw TypeError(hooray + " is not an hooray");
+
+  var newhorray = new Hooray();
+
+  for (var j = 0; j < this.length; j++) {
+    newhorray.push(this[j]);
+  }
+  var keys = Object.keys(arguments[0]);
+  var len = keys.length;
+  for (var i = 0; i < len - 1; i++) {
+    newhorray.push(arguments[0][i]);
+  }
+  return newhorray;
+};
+
+/**
+ * Iterates an hooray and evaluates an expression on each of its values, returning true if all of them match it. Otherwise returns false.
+ *
+ * @param {hooray} hooray The hooray to iterate.
+ * @param {Function} callback The expression to evalute.
+ *
+ * @returns {boolean} True if all values match the expression, otherwise false.
+ */
+Hooray.prototype.every = function(callback) {
+  if (typeof callback !== "function")
+    throw new TypeError(callback + " is not a function");
+  for (var i = 0; i < arguments.length; i++)
+    if (!callback(this[i])) return false;
+
+  return true;
+};
+/**
+ * Iterates an hooray and get a new hooray form the original hooray
+ * @param {Hooray} hooray
+ * @param {Function} callback The expression to evaluate.
+ *
+ *@return a new hooray with only the elements which meet the condition
+ */
+Hooray.prototype.filter = function(callback) {
+  if (typeof callback !== "function")
+    throw new TypeError(callback + " is not a function");
+  var hooray = new Hooray();
+  for (var i = 0; i < this.length; i++) {
+    if (callback(this[i])) {
+      hooray.push(this[i]);
+    }
+  }
+  return hooray;
+};
+
+/** Given an hooray and a value, the function returns the index where is the value inside the hooray
+ * If the value is not on the array the function returns -1
+ *
+ * @param {Array} array
+ * @param {Element} searchElement
+ */
+
+Hooray.prototype.indexOf = function(searchElement) {
+  if (searchElement === undefined) {
+    return -1;
+  }
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] === searchElement) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+/**
+ * This method verify if the param is an hooray
+ * @param {Hooray} obj The hooray to check
+ * @returns {boolean} True if the param is an array, otherwise false.
+ */
+
+Hooray.prototype.isArray = function() {
+  return this instanceof Hooray;
+};
+
+Hooray.isHooray = function(obj) {
+  return obj.constructor === Hooray;
+};
+/**
+ * Join all the elements of the hooray
+ * @param {Hooray} hooray
+ * @param {*} separator
+ */
+Hooray.prototype.join = function(separator) {
+  var value = "";
+  if (this.length !== 0) {
+    var i = 0;
+    do {
+      if (separator === undefined) {
+        value += this[i] + ",";
+      } else {
+        value += this[i] + separator;
+      }
+      i++;
+    } while (i < this.length - 1);
+    value += this[i];
+  }
+  return value;
+};
+/**
+ * Ceate a new hooray with the results of the function called
+ * @param {Hooray} horray to evaluate
+ * @param {Function} callback
+ *
+ * @returns {Horray} the new hooray
+ */
+Hooray.prototype.map = function(callback) {
+  if (typeof callback !== "function")
+    throw TypeError(callback + " is not a function");
+  var newarray = new Hooray();
+  for (var i = 0; i < this.length; i++) {
+    newarray.push(callback(this[i], i, this));
+  }
+
+  return newarray;
+};
+/**
+ * Retunr the first element of the hooray and move the rest of the elements on the hooray to the correct position
+ * @param {Hooray} array
+ *
+ * @returns {Element} first element of the array
+ */
+
+Hooray.prototype.shift = function() {
+  if (this.length > 0) {
+    var values = Object.values(this);
+    var el = values[0];
+    var keys = Object.keys(this);
+    var key = keys[0];
+    delete this[key];
+    for (var i = 0; i < this.length; i++) {
+      this[i] = this[i - 1];
+    }
+    return el;
+  }
+};
+/**
+ * Iterates an array and check if at least one element of the array meets the condition implemented by the function
+ * @param {Hooray} hooray  to check
+ * @param {Function} callback The expression to evaluate.
+ */
+
+Hooray.prototype.some = function(callback) {
+  if (typeof callback !== "function")
+    throw TypeError(callback + " is not a function");
+  var flag = false;
+  for (var i = 0; i < this.length; i++) {
+    if (callback(this[i])) {
+      flag = true;
+    }
+  }
+  return flag;
+};
+
+/**
+ * Returns the last index at which a given element can be found in the hooray, or -1 if it is not present.
+ * The array is searched backwards, starting at fromIndex.
+ *
+ * @param {horray} horrat       The hooray to iterate.
+ * @param {*} searchElement     The element to search in the array
+ * @param {[number]} fromIndex  The index at which to start searching backwards.
+ *
+ * @returns {number} - The last index of the element in the array; -1 if not found.
+ *
+ */
+
+Hooray.prototype.lastIndexOf = function(searchElement, fromIndex) {
+  var indexStart = 0;
+  if (fromIndex === undefined || fromIndex >= this.length) {
+    indexStart = this.length - 1;
+  } else {
+    if (fromIndex < 0) {
+      indexStart = this.length + fromIndex;
+    } else {
+      indexStart = fromIndex;
+    }
+  }
+
+  for (var i = indexStart; i >= 0; i--) {
+    if (this[i] === searchElement) {
+      return i;
+    }
+  }
+
+  return -1;
+};
+/**
+ * The reduce method applies a function to accumulate every value of the hooray
+ * @param {Hooray} horrat The array with values to accumulate
+ * @param {Function} callback The function which makes the accumulation
+ * @returns {Element} Retunrs the accumulated value
+ */
+
+Hooray.prototype.reduce = function(callback) {
+  if (typeof callback !== "function")
+    throw TypeError(callback + " is not a function");
+  var accumulated = this[0];
+  for (var i = 1; i < this.length; i++) {
+    accumulated = callback(accumulated, this[i]);
+  }
+  return accumulated;
+};
+/**
+ * The reduce method applies a function to accumulate every value of the array, going through the horray from the end 
+ * @param {Horray} horray The array with values to accumulate
+ * @param {Function} callback The function which makes the accumulation 
+ * @returns {Element} Retunrs the accumulated value
+ */
+Hooray.prototype.reduceRight= function(callback){
+    if(typeof callback !== 'function') throw TypeError (callback+' is not a function');
+
+    var accumulated=this[this.length-1] 
+    for (var i = this.length-2; i >-1; i--){
+        accumulated=(callback(accumulated, this[i]))
+    }
+    return accumulated;
 }
+
+/**
+ * Revert the order of the elements of an array
+ * @param {Hooray} horray Array to revert
+ * 
+ * @returns the horray with all elements reverted
+ */
+// Hooray.prototype.reverse= function(){
+//     var hooray2=new Hooray;
+//     for (var i = this.length-1 ; i >= 0; i--){
+//         hooray2.push(this[i])
+        
+//     }
+//     return this=hooray2;
+
+// }
+// function reverse(array){
+//     if(!(array instanceof Array)) throw TypeError(array + ' is not an array');
+//     count=0;
+//     array2=[];
+//     for (var i = array.length-1 ; i >= 0; i--){
+//         array2[count]=array[i];
+//         count++
+//     }
+//    return array=array2;
+// }
+
+
---
+++ staff/lila-petri/hooray/hooray.spec.js
@@ -0,0 +1,533 @@
+"use strict";
+
+describe("hooray", function() {
+  describe("constructor", function() {
+    true && it("should construct an empty hooray when no arguments", function() {
+        var hooray = new Hooray();
+
+        expect(hooray.length).toBe(0);
+        expect(Object.keys(hooray).length).toBe(1);
+      });
+
+    true && it("should construct a non-empty hooray when existing arguments", function() {
+        var hooray = new Hooray(1, 2, 3);
+
+        expect(hooray.length).toBe(3);
+        expect(JSON.stringify(hooray)).toBe(
+          JSON.stringify({ 0: 1, 1: 2, 2: 3, length: 3 })
+        );
+        expect(Object.keys(hooray).length).toBe(4);
+      });
+
+    true && it("should construct an empty hooray with length equal to when only one numeric argument", function() {
+        var hooray = new Hooray(1);
+
+        expect(hooray.length).toBe(1);
+        expect(Object.keys(hooray).length).toBe(1);
+      });
+
+    true && it("should construct a non-empty hooray with only one non-numeric argument", function() {
+        var hooray = new Hooray("1");
+
+        expect(hooray.length).toBe(1);
+        expect(JSON.stringify(hooray)).toBe(
+          JSON.stringify({ 0: "1", length: 1 })
+        );
+        expect(Object.keys(hooray).length).toBe(2);
+      });
+  });
+
+  describe("push", function() {
+    true && it("should add a value at the end of an hooray", function() {
+        var hooray = new Hooray(1, 2, 3);
+
+        var length = hooray.push(4);
+
+        expect(hooray.length).toBe(4);
+        expect(length).toBe(hooray.length);
+        expect(JSON.stringify(hooray)).toBe(
+          JSON.stringify({ 0: 1, 1: 2, 2: 3, 3: 4, length: 4 })
+        );
+        expect(hooray).toEqual(
+          jasmine.objectContaining({ 0: 1, 1: 2, 2: 3, 3: 4, length: 4 })
+        );
+      });
+
+    true && it("should add multiple values at the end of an hooray in order", function() {
+        var hooray = new Hooray(1, 2, 3);
+
+        var length = hooray.push(4, 5);
+
+        expect(hooray.length).toBe(5);
+        expect(length).toBe(hooray.length);
+        expect(hooray).toEqual(
+          jasmine.objectContaining({ 0: 1, 1: 2, 2: 3, 3: 4, 4: 5, length: 5 })
+        );
+      });
+
+    true && it("should not add a non-provided value at the end of an hooray", function() {
+        var hooray = new Hooray(1, 2, 3);
+
+        var length = hooray.push();
+
+        expect(hooray.length).toBe(3);
+        expect(length).toBe(hooray.length);
+        expect(hooray).toEqual(
+          jasmine.objectContaining({ 0: 1, 1: 2, 2: 3, length: 3 })
+        );
+      });
+  });
+
+  describe("forEach", function() {
+    true && it("should itearate an hooray without altering it", function() {
+        var hooray = new Hooray(1, 2, 3);
+
+        var result = new Hooray();
+
+        hooray.forEach(function(v, i) {
+          result.push(v);
+        });
+
+        expect(result).toEqual(jasmine.objectContaining(hooray));
+        var expected = { 0: 1, 1: 2, 2: 3, length: 3 };
+
+        expect(hooray).toEqual(
+          jasmine.objectContaining({ 0: 1, 1: 2, 2: 3, length: 3 })
+        );
+      });
+
+    true && it("should do nothing if hooray has not content", function() {
+        var hooray = new Hooray();
+
+        var result = new Hooray();
+
+        hooray.forEach(function(v, i) {
+          result.push(v);
+        });
+
+        expect(result.length).toBe(0);
+      });
+
+    true && it("should break on undefined callback", function() {
+        var hooray = new Hooray(1, 2, 3);
+
+        try {
+          hooray.forEach();
+
+          throw Error("should not reach this point");
+        } catch (error) {
+          expect(error.message).toBe("undefined is not a function");
+        }
+      });
+  });
+  describe("every", function() {
+    true && it("should return true on all items matching condition", function() {
+        var hooray = new Hooray(1, 2, 3);
+
+        var result = new Hooray();
+
+        result = hooray.every(function(v) {
+          return v > 0;
+        });
+
+        expect(result).toBe(true);
+      });
+
+    true && it("should return false on any of the items not matching the condition", function() {
+        var hooray = new Hooray(1, 2, 3);
+
+        var result = hooray.every(function(v) {
+          return v < 0;
+        });
+
+        expect(result).toBe(false);
+      });
+
+    true && it("should fail on undefined callback", function() {
+        var hooray = new Hooray(1, 2, 3);
+
+        try {
+          hooray.every();
+
+          throw Error("should not reach this point");
+        } catch (error) {
+          expect(error.message).toBe("undefined is not a function");
+        }
+      });
+  });
+
+  describe("concat", function() {
+    true && it("Should create a new hooray from two hooray ", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var hooray2 = new Hooray(4, 5, 6);
+        var result = new Hooray();
+        result = hooray.concat(hooray2);
+
+        expect(result).toEqual(
+          jasmine.objectContaining({
+            0: 1,
+            1: 2,
+            2: 3,
+            3: 4,
+            4: 5,
+            5: 6,
+            length: 6
+          })
+        );
+        expect(hooray).toEqual(
+          jasmine.objectContaining({ 0: 1, 1: 2, 2: 3, length: 3 })
+        );
+      });
+    true && it("Should create a new hooray from two hooray even if the second one is empty hooray", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var hooray2 = new Hooray();
+        var result = new Hooray();
+        result = hooray.concat(hooray2);
+
+        expect(result).toEqual(
+          jasmine.objectContaining({ 0: 1, 1: 2, 2: 3, length: 3 })
+        );
+        expect(hooray).toEqual(
+          jasmine.objectContaining({ 0: 1, 1: 2, 2: 3, length: 3 })
+        );
+      });
+
+    true && it("Should fail with undefined arguments", function() {
+        var hooray = new Hooray(1, 2, 3);
+        try {
+          hooray.concat();
+          throw Error("should not reach this point");
+        } catch (error) {
+          expect(error.message).toBe("undefined is not an hooray");
+        }
+      });
+  });
+
+  describe("filter", function() {
+    true && it("Should create a new hooray form an specific condition", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var result = new Hooray();
+        var expected = new Hooray(1);
+
+        result = hooray.filter(function(v) {
+          return v < 2;
+        });
+        expect(result).toEqual(jasmine.objectContaining(expected));
+      });
+
+    true && it("Should fail on undefined arguments", function() {
+        var hooray = new Hooray(1, 2, 3);
+        try {
+          hooray.filter();
+          throw Error("should not reach this point");
+        } catch (error) {
+          expect(error.message).toBe("undefined is not a function");
+        }
+      });
+
+    true && it("Should fail if the argument is not a function", function() {
+        var hooray = new Hooray(1, 2, 3);
+        try {
+          hooray.filter("string");
+          throw Error("should not reach this point");
+        } catch (error) {
+          expect(error.message).toBe("string is not a function");
+        }
+      });
+
+    true && it("Should fail if the second argument is null", function() {
+        var hooray = new Hooray(1, 2, 3);
+        try {
+          hooray.filter(null);
+          throw Error("should not reach this point");
+        } catch (error) {
+          expect(error.message).toBe("null is not a function");
+        }
+      });
+  });
+  describe("indexOf", function() {
+    true && it("Should return the index of the element given", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var searchElement = 1;
+        var expected = 0;
+        var result = new Hooray();
+
+        result = hooray.indexOf(searchElement);
+
+        expect(expected).toBe(result);
+      });
+    true && it("Should return -1 if the element does not exist on the array", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var searchElement = 6;
+        var expected = -1;
+        var result = new Hooray();
+        result = hooray.indexOf(searchElement);
+
+        expect(expected).toBe(result);
+      });
+
+    true && it("Should return -1 if the searchElement is not send", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var expected = -1;
+        var result = new Hooray();
+        result = hooray.indexOf();
+
+        expect(expected).toBe(result);
+      });
+
+    true && it("Should return -1 if the searchElement is a null", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var searchElement = null;
+        var expected = -1;
+        var result = new Hooray();
+        result = hooray.indexOf();
+
+        expect(expected).toBe(result);
+      });
+  });
+  describe("isArray", function() {
+    true && it("Should return true if the argument is an hooray", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var result = new Hooray();
+        result = hooray.isArray();
+        expect(result).toBe(true);
+      });
+
+    true && it("Should return false if the argument is an string", function() {
+        var result = new Hooray();
+        result = Hooray.isHooray("String");
+        expect(result).toBe(false);
+      });
+
+    true && it("Should return false if the argument is a number", function() {
+        var result = new Hooray();
+        result = Hooray.isHooray(7);
+        expect(result).toBe(false);
+      });
+  });
+  describe("join", function() {
+    true && it("Should return all elements of the hooray as a string separated by the separator", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var separator = "*_";
+        var result = new Hooray();
+        var expected = "1*_2*_3";
+        result = hooray.join(separator);
+
+        expect(expected).toBe(result);
+      });
+
+    true && it("Should return all elements of the hooray as a string separated by commas", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var result = new Hooray();
+        var expected = "1,2,3";
+        result = hooray.join();
+
+        expect(expected).toBe(result);
+      });
+    !true && it("Should return an empty string in case of empty hooray", function() {
+        var array = [];
+        var result;
+        var expected = "";
+        result = join(array);
+
+        expect(expected, result);
+      });
+  });
+  describe("map", function() {
+    true && it("Should return an hooray where every element has been modified by the function", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var expected = new Hooray(2, 4, 6);
+        var result = new Hooray();
+        result = hooray.map(function(v) {
+          return v * 2;
+        });
+
+        expect(expected).toEqual(jasmine.objectContaining(result));
+      });
+
+    true && it("Should fail if the second argument is not a function", function() {
+        var hooray = new Hooray(1, 2, 3);
+        try {
+          hooray.map("string");
+          throw Error("should not reach this point");
+        } catch (error) {
+          expect(error.message).toBe("string is not a function");
+        }
+      });
+  });
+  describe("shift", function() {
+    true && it("Should return the first element form the hooray", function() {
+        var hooray = new Hooray("a", "b", "c");
+        var result = new Hooray();
+        var expected = "a";
+        var expectedHorray = new Hooray("b", "c");
+        result = hooray.shift();
+        expect(expected).toBe(result);
+        // expect(expectedHorray).toEqual(jasmine.objectContaining(hooray));
+      });
+
+    !true && it("Should return undefine in case of empty array", function() {
+        var array = [];
+        var result;
+        var expected = undefined;
+
+        result = shift(array);
+
+        expect(expected, result);
+      });
+
+    !true && it("should fail on undefined array", function() {
+        try {
+          shift();
+
+          throw Error("should not reach this point");
+        } catch (error) {
+          expect(error.message, "undefined is not an array");
+        }
+      });
+  });
+  describe("some", function() {
+    true && it("Should return false if no element in the array meets the condition", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var result = new Hooray();
+        result = hooray.some(function(e) {
+          return e > 10;
+        });
+
+        expect(result).toBeFalsy();
+      });
+
+    true && it("Should return true if some element in the array meets the condition", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var result = new Hooray();
+        result = hooray.some(function(e) {
+          return e > 2;
+        });
+
+        expect(result).toBeTruthy();
+      });
+
+    true && it("Should fail if the  argument is not a function", function() {
+        var hooray = new Hooray(1, 2, 3);
+        try {
+          hooray.some();
+          throw Error("should not reach this point");
+        } catch (error) {
+          expect(error.message).toBe("undefined is not a function");
+        }
+      });
+  });
+  describe("lastIndexOf", function() {
+    true && it("Should return the last index of the element given", function() {
+        var hooray = new Hooray(1, 2, 3, 4, 5, 3);
+        var searchElement = 3;
+        var expected = 5;
+        var result = new Hooray();
+        result = hooray.lastIndexOf(searchElement);
+
+        expect(expected).toEqual(result);
+      });
+    true && it("Should return -1 if the element does not exist on the array", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var searchElement = 6;
+        var expected = -1;
+        var result = new Hooray();
+        result = hooray.lastIndexOf(searchElement);
+
+        expect(expected).toEqual(result);
+      });
+
+    true && it("Should return -1 if the searchElement is not send", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var expected = -1;
+        var result = new Hooray();
+        result = hooray.lastIndexOf();
+
+        expect(expected).toEqual(result);
+      });
+
+    true && it("Should return -1 if the searchElement is a null", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var searchElement = null;
+        var expected = -1;
+        var result = new Hooray();
+        result = hooray.lastIndexOf(searchElement);
+
+        expect(expected).toEqual(result);
+      });
+    true && it("Should return the searchElement  found from a specific index", function() {
+        var hooray = new Hooray(2, 5, 9, 2);
+        var searchElement = 2;
+        var fromIndex = 3;
+        var expected = 3;
+        var result = new Hooray();
+        result = hooray.lastIndexOf(searchElement, fromIndex);
+
+        expect(expected).toEqual(result);
+      });
+    });
+
+  describe("reduce", function() {
+    true && it("Should return an accumulated value ", function() {
+        var hooray = new Hooray(1, 2, 3);
+        var result = new Hooray();
+        var expected = 6;
+
+        result = hooray.reduce(function(a, v) {
+          return a + v;
+        });
+
+        expect(expected).toEqual(result);
+      });
+    true && it("Should fail if the second argument is not a function", function() {
+        var hooray = new Hooray(1, 2, 3);
+        try {
+          hooray.reduce("string");
+          throw Error("should not reach this point");
+        } catch (error) {
+          expect(error.message).toBe("string is not a function");
+        }
+      });
+  });
+  describe('reduceRight', function(){
+   true && it('Should return an accumulated value ', function(){
+        var hooray=new Hooray('a', 'b', 'c');    
+        var expected='cba';
+        var result=new Hooray;
+        result=hooray.reduceRight(function(a,v) {return a+v;});
+
+        expect(expected).toEqual(result);
+
+    });
+
+    true && it('Should fail if the second argument is not a function', function(){
+       var horray=new Hooray(1,2,3);
+        try{
+            
+            horray.reduceRight('string');
+            throw Error ('should not reach this point');
+
+        }catch(error){
+            
+            expect(error.message).toBe('string is not a function');
+
+        }
+
+    });
+
+});
+
+// describe('reverse', function(){
+//     !true && it('Should return an array with all its elements reversed.', function(){
+//         var hooray= new Hooray(1,2,3);
+//         var expected=new Horray(3,2,1);
+//         var result=new Hooray;
+//         result=hooray.reverse();
+
+//         expect(expected).toEqual(jasmine.objectContaining(result));
+
+//     });
+
+// });
+
+});
