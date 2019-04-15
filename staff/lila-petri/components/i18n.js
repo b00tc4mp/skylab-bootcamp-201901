c67aff9e5d5b269ab19d57744e4f4165c500c8a8
mlila <marialilapetri@gmail.com>
2019-04-15 19:40:24 +0200

    components

---
+++ staff/lila-petri/components/i18n.js
@@ -0,0 +1,72 @@
+var i18n = {
+    signUp: {
+        en: {
+            title: 'Sign up',
+            title_login: 'Sign in',
+            name: 'Name',
+            surname: 'Surname',
+            email: 'E-mail',
+            password: 'Password'
+        },
+        es: {
+            title: 'Registro',
+            title_login:'Acceder',
+            name: 'Nombre',
+            surname: 'Apellido',
+            email: 'E-milio',
+            password: 'Contraseña'
+        },
+        ca: {
+            title: 'Registre',
+            title_login:'Accedir',
+            name: 'Nom',
+            surname: 'Cognom',
+            email: 'E-mil·li',
+            password: 'Contrasenya'
+        },
+        ga: {
+            title: 'Rexistro',
+            title_login:'Enviar',
+            name: 'Nome',
+            surname: 'Apelido',
+            email: 'E-miliño',
+            password: 'Contrasinal'
+        }
+    },
+    signIn: {
+        en: {
+            title: 'Sign in',
+            name: 'Name',
+            surname: 'Surname',
+            email: 'E-mail',
+            password: 'Password'
+        },
+        es: {
+            title:'Acceder',
+            name: 'Nombre',
+            surname: 'Apellido',
+            email: 'E-milio',
+            password: 'Contraseña'
+        },
+        ca: {
+            title:'Accedir',
+            name: 'Nom',
+            surname: 'Cognom',
+            email: 'E-mil·li',
+            password: 'Contrasenya'
+        },
+        ga: {
+            title:'Acceso',
+            name: 'Nome',
+            surname: 'Apelido',
+            email: 'E-miliño',
+            password: 'Contrasinal'
+        }
+    },
+    admin: {
+        en: 'Administrator',
+        es: 'Administrador',
+        ca: 'Gestor',
+        ga: 'Administrador'
+    }
+};
---
+++ staff/lila-petri/components/logic.js
@@ -0,0 +1,25 @@
+// business logic
+
+function register(name, surname, email, password) {
+    console.log('register', name, surname, email, password);
+}
+
+function registerAdmin(name, surname, email, password) {
+    console.log('register admin', name, surname, email, password);
+}
+
+function registerSuperAdmin(name, surname, email, password) {
+    console.log('register super admin', name, surname, email, password);
+}
+
+function login(email, password) {
+    console.log('login', email, password);
+}
+
+function loginAdmin(email, password) {
+    console.log('login admin', email, password);
+}
+
+function loginSuperAdmin(email, password) {
+    console.log('login super admin', email, password);
+}
