9cf4ef29afb3c516c4af4d69124f6e23c5424560
Miguel Angel Zapatero <m.zapes@gmail.com>
2019-04-15 23:07:32 +0200

    add sign-in component

---
+++ staff/miguel-angel-zapatero/components/i18n.js
@@ -0,0 +1,30 @@
+var i18n = {
+    signIn: {
+        en: {
+            title: 'Sign In',
+            email: 'E-mail',
+            password: 'Password'
+        },
+        es: {
+            title: 'Inicio de sesión',
+            email: 'E-milio',
+            password: 'Contraseña'
+        },
+        ca: {
+            title: 'Inici de sesió',
+            email: 'E-mil·li',
+            password: 'Contrasenya'
+        },
+        ga: {
+            title: 'Inicio de sesión',
+            email: 'E-miliño',
+            password: 'Contrasinal'
+        }
+    },
+    admin: {
+        en: 'Administrator',
+        es: 'Administrador',
+        ca: 'Gestor',
+        ga: 'Administrador'
+    }
+};
---
+++ staff/miguel-angel-zapatero/components/logic.js
@@ -0,0 +1,13 @@
+// business logic
+
+function login(email, password) {
+    console.log('register', email, password);
+}
+
+function loginAdmin(email, password) {
+    console.log('register admin', email, password);
+}
+
+function loginSuperAdmin(email, password) {
+    console.log('register super admin', email, password);
+}
