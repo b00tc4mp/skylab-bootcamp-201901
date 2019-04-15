16ee5a2c194faf4df6f804c3b2978175ef80f982
alicia-alcaide <alicia.alcaide@gmail.com>
2019-04-15 23:01:43 +0200

    feature/components v1.0, CSS pending

---
+++ staff/alicia-alcaide/components/i18n.js
@@ -0,0 +1,60 @@
+var i18n = {
+    signUp: {
+        en: {
+            title: 'Sign up',
+            name: 'Name',
+            surname: 'Surname',
+            email: 'E-mail',
+            password: 'Password'
+        },
+        es: {
+            title: 'Registro',
+            name: 'Nombre',
+            surname: 'Apellido',
+            email: 'E-milio',
+            password: 'Contraseña'
+        },
+        ca: {
+            title: 'Registre',
+            name: 'Nom',
+            surname: 'Cognom',
+            email: 'E-mil·li',
+            password: 'Contrasenya'
+        },
+        ga: {
+            title: 'Rexistro',
+            name: 'Nome',
+            surname: 'Apelido',
+            email: 'E-miliño',
+            password: 'Contrasinal'
+        }
+    },
+    signIn: {
+        en: {
+            title: 'Sign in',
+            email: 'E-mail',
+            password: 'Password'
+        },
+        es: {
+            title: 'Inicio de sesión',
+            email: 'E-milio',
+            password: 'Contraseña'
+        },
+        ca: {
+            title: 'Inicia sessió',
+            email: 'E-mil·li',
+            password: 'Contrasenya'
+        },
+        ga: {
+            title: 'Inicio da sesion',
+            email: 'E-miliño',
+            password: 'Contrasinal'
+        }
+    },
+    admin: {
+        en: 'Administrator',
+        es: 'Administrador',
+        ca: 'Gestor',
+        ga: 'Administrador'
+    }
+};
