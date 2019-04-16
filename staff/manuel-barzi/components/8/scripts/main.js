4702926a3af610c0f67e3456e8b4a064265c62ab
manuelbarzi <manuelbarzi@manuelbarzis-MacBook-Pro.local>
2019-04-16 16:56:36 +0200

    update components challenge

--- staff/manuel-barzi/components/8/scripts/main.js
+++ staff/manuel-barzi/components/8/scripts/main.js
@@ -1,32 +1,58 @@
 'use strict';
 
-var defaultLanguage = 'en';
+var languageSelected = 'en';
 
 var select = document.getElementsByTagName('select')[0];
 var languageSelector = new LanguageSelector(select, function (language) {
-    signUp.language = language;
-    signIn.language = language;
+    languageSelected = language;
+
+    landing.language = language;
+    register.language = language;
+    login.language = language;
+});
+
+var sections = document.getElementsByTagName('section');
+
+var landing = new Landing(sections[0], i18n.landing, function() {
+    landing.visible = false;
+    register.visible = true;
+}, function() {
+    landing.visible = false;
+    login.visible = true;
 });
 
 var forms = document.getElementsByTagName('form');
 
-var signUp = new SignUp(forms[0], function(name, surname, email, password) {
+var register = new Register(forms[0], function (name, surname, email, password) {
     logic.register(name, surname, email, password);
 
-    signUp.visible = false;
-    signUpOk.visible = true;
-}, i18n.signUp, defaultLanguage);
+    register.visible = false;
+    registerOk.visible = true;
+}, i18n.register, languageSelected);
+register.visible = false;
 
-var signIn = new SignIn(forms[1], logic.login, i18n.signIn, defaultLanguage);
-signIn.visible = false;
+var login = new Login(forms[1], function (email, password) {
+    try {
+        logic.login(email, password);
 
-var sections = document.getElementsByTagName('section');
-
-var signUpOk = new SignUpOk(sections[0], function() {
-    signUpOk.visible = false;
-    signIn.visible = true;
+        login.visible = false;
+        home.visible = true;
+    } catch (error) {
+        login.error = i18n.errors[languageSelected][error.code];
+    }
+}, i18n.login, languageSelected, function() {
+    this.__feedback__.visible = false;
 });
-signUpOk.visible = false;
+login.visible = false;
 
+var registerOk = new RegisterOk(sections[1], function () {
+    registerOk.visible = false;
+    login.visible = true;
+});
+registerOk.visible = false;
+
+var main = document.getElementsByTagName('main')[0];
+var home = new Home(main);
+home.visible = false;
 
 
