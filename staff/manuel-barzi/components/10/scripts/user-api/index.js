a88104d70aba256387b65bc865a6751d1e3ebcc2
manuelbarzi <manuelbarzi@manuelbarzis-MacBook-Pro.local>
2019-04-19 13:55:21 +0200

    update components challenge

--- staff/manuel-barzi/components/10/scripts/user-api/index.js
+++ staff/manuel-barzi/components/10/scripts/user-api/index.js
@@ -1,5 +1,31 @@
 'use strict'
 
 const userApi = {
-    // TODO
+    __url__: 'https://skylabcoders.herokuapp.com/api',
+
+    __call__(path, method, body, callback) {
+        // TODO validate inputs
+
+        const xhr = new XMLHttpRequest
+
+        xhr.open(method, `${this.__url__}/${path}`)
+
+        xhr.addEventListener('load', function () {
+            callback(JSON.parse(this.responseText))
+        })
+
+        if (method === 'GET') {
+            if (body) throw Error('cannot send body in GET request')
+            else xhr.send()
+        } else {
+            if (body) {
+                xhr.setRequestHeader('content-type', 'application/json')
+                xhr.send(JSON.stringify(body))
+            } else xhr.send()
+        }
+    },
+
+    create(name, surname, username, password, callback) {
+        this.__call__('/user', 'POST', { name, surname, username, password }, callback)
+    }
 }
--- staff/manuel-barzi/components/10/scripts/user-api/index.spec.js
+++ staff/manuel-barzi/components/10/scripts/user-api/index.spec.js
@@ -1,5 +1,47 @@
 'use strict'
 
 describe('user api', () => {
-    // TODO start from here
+    const name = 'Manuel'
+    const surname = 'Barzi'
+    let username
+    const password = '123'
+
+    beforeEach(() => username = `manuelbarzi-${Math.random()}@gmail.com`)
+
+    describe('register', () => {
+        it('should succeed on correct user data', done => {
+            userApi.create(name, surname, username, password, function (response) {
+                expect(response).toBeDefined()
+
+                const { status, data: { id } } = response
+
+                expect(status).toBe('OK')
+                expect(typeof id).toBe('string')
+                expect(id.length).toBeGreaterThan(0)
+
+                done()
+            })
+        })
+
+        describe('on already existing user', () => {
+            beforeEach(done => userApi.create(name, surname, username, password, done))
+
+            it('should fail on retrying to register', done => {
+                userApi.create(name, surname, username, password, function (response) {
+                    expect(response).toBeDefined()
+
+                    const { status, error } = response
+
+                    expect(status).toBe('KO')
+                    expect(error).toBe(`user with username \"${username}\" already exists`)
+
+                    done()
+                })
+            })
+        })
+    })
+
+    describe('update', () => {
+        // blah blah blah
+    })
 })
