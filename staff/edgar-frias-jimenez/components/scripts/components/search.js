3d373976223e282f4f6a271e010f7f6f0d3461f7
Edgar Frías <edfriasji@gmail.com>
2019-04-19 10:29:08 +0200

    erase detail when a new search

--- staff/edgar-frias-jimenez/components/scripts/components/search.js
+++ staff/edgar-frias-jimenez/components/scripts/components/search.js
@@ -10,6 +10,9 @@ class Search extends Component {
         this.container.addEventListener('submit', function(event) {
             event.preventDefault();
 
+            const detail = document.getElementsByClassName('item-section')[0];
+            detail.children[0].innerHTML = '';
+
             const query = this.query.value;
 
             callback(query);
