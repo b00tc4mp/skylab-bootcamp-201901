1bfd21cdd10959cde990993b5622d5cba9e2e1bd
Pablo Rey <pabloreysobral@gmail.com>
2019-04-27 16:26:32 +0200

    Scaffolding react and common files

---
+++ staff/groups/nozama/nozama-app/src/components/App/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Container, Button, Form, FormGroup, Label, Input } from "reactstrap";
+
+function App() {
+  function somethingForTest(e) {
+    e.preventDefault();
+    console.log("Holitas");
+  }
+
+  return (
+    <Container>
+      <Form onSubmit={somethingForTest}>
+        <FormGroup>
+          <Label for="name">username</Label>
+          <Input type="text" name="username" id="name" placeholder="Username" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="email">Email</Label>
+          <Input type="email" name="email" id="email" placeholder="email" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+        </FormGroup>
+      </Form>
+      <Button>Submit</Button>
+    </Container>
+  );
+}
+
+export default App;
