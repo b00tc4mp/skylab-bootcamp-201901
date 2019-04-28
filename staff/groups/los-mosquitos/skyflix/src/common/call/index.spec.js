849efbfc858f7c4c99ccc3e62ea3887917b0411a
Miguel Angel Zapatero <m.zapes@gmail.com>
2019-04-28 23:27:53 +0200

    add common tools tests/specs

--- staff/groups/los-mosquitos/skyflix/src/common/call/index.spec.js
+++ staff/groups/los-mosquitos/skyflix/src/common/call/index.spec.js
@@ -1,3 +1,8 @@
-xdescribe('call', () => {
-    // TODO
+import call from '.'
+
+describe('call', () => {
+    it('should succed on correct URL', () => {
+        let result = call('https://www.google.es')
+        expect(result instanceof Promise).toBeTruthy()
+    })
 })
--- staff/groups/los-mosquitos/skyflix/src/common/normalize/index.spec.js
+++ staff/groups/los-mosquitos/skyflix/src/common/normalize/index.spec.js
@@ -1,3 +1,28 @@
-xdescribe('normalize', () => {
-    // TODO
+import normalize from '.'
+
+describe('normalize', () => {
+    describe('undefinedOrNull', () => {
+        it('should succed on not-null and not-undefined value', () => {
+            let value = Math.random()
+            
+            const result = normalize.undefinedOrNull(value)
+            expect(result).toBeDefined()
+            expect(result).toBe(value)
+        })
+
+        it('should return null on string null value', () => {
+            let value = 'null'
+            
+            const result = normalize.undefinedOrNull(value)
+            expect(result).toBeDefined()
+            expect(result).toBe(null)
+        })
+
+        it('should return undefined on string undefined value', () => {
+            let value = 'undefined'
+            
+            const result = normalize.undefinedOrNull(value)
+            expect(result).toBeUndefined()
+        })
+    })
 })
