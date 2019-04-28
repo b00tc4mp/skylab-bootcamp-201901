a30df5cad30175deef1a614698a1bbc7f4b22f44
Miguel Angel Zapatero <m.zapes@gmail.com>
2019-04-28 20:35:38 +0200

    add landing and language selector

--- staff/groups/los-mosquitos/skyflix/src/App.js
+++ staff/groups/los-mosquitos/skyflix/src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import i18n from './common/i18n'
+import LanguageSelector from './components/LanguageSelector'
+
+import Landing from './components/Landing'
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+	state = {lang: i18n.language, visible: 'landing'}
 
-      </header>
-    </div>
-  );
+	handleLanguageChange = lang => this.setState({ lang: i18n.language = lang }) // NOTE setter runs first, getter runs after (i18n)
+
+	handleRegisterNav = () => this.setState({ visible: 'register' })
+
+    handleLoginNav = () => this.setState({ visible: 'login' })
+
+	render() {
+		const {
+			state: { lang, visible },
+			handleLanguageChange,
+			handleLoginNav,
+			handleRegisterNav
+		} = this
+
+		return <>
+			<LanguageSelector lang={lang} onLanguageChange={handleLanguageChange}/>
+			{visible === 'landing' && <Landing lang={lang} onLogin={handleRegisterNav} onRegister={handleLoginNav}/>}
+		</>
+	}
 }
 
 export default App;
