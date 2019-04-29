984a73dbf3917554fb33e18b9803cb07655421ff
Markuson <el.marcuson@gmail.com>
2019-04-29 19:03:23 +0200

    starting with components

---
+++ staff/groups/batmom-y-robin/wtunes-react-app/src/components/App.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react'
+import logic from '../logic'
+//import CitySelector from './CitySelector'
+import Landing from './Landing'
+//import Register from './Register'
+//import Login from './Login'
+//import Home from './Home'
+import { Route, withRouter, Redirect, Switch } from 'react-router-dom'
+
+class App extends Component {
+    state = { city: null, visible: null, error: null, name: null }
+
+    //handleCityChange = lang => this.setState({ city: selectedCity})
+
+    handleRegisterNavigation = () => this.props.history.push('/register')
+
+    handleLoginNavigation = () => this.props.history.push('/login')
+
+    // handleLogin = (username, password) => {
+    //     try {
+    //         logic.loginUser(username, password)
+    //             .then(() =>
+    //                 logic.retrieveUser()
+    //             )
+    //             .then(({ name }) => {
+    //                 this.setState({ name, error: null }, () => this.props.history.push('/home'))
+    //             })
+    //             .catch(error =>
+    //                 this.setState({ error: error.message })
+    //             )
+    //     } catch ({ message }) {
+    //         this.setState({ error: message })
+    //     }
+    // }
+
+    componentDidMount() {
+        // logic.isUserLoggedIn &&
+        //     logic.retrieveUser()
+        //         .then(user =>
+        //             this.setState({ name: user.name })
+        //         )
+        //         .catch(error =>
+        //             this.setState({ error: error.message })
+        //         )
+    }
+
+    // handleRegister = (name, surname, username, password) => {
+    //     try {
+    //         logic.registerUser(name, surname, username, password)
+    //             .then(() =>
+    //                 this.setState({ visible: 'register-ok', error: null })
+    //             )
+    //             .catch(error =>
+    //                 this.setState({ error: error.message })
+    //             )
+    //     } catch ({ message }) {
+    //         this.setState({ error: message })
+    //     }
+    // }
+
+    // handleLogout = () => {
+    //     logic.logoutUser()
+
+    //     this.props.history.push('/')
+    // }
+
+    // componentDidUpdate(prevProps) {
+    //     if (this.props.location !== prevProps.location) this.setState({ visible: null })
+    // }
+
+    render() {
+        const {
+            state: {  city, visible, error, name },
+            // handleCityChange,
+            handleRegisterNavigation,
+            handleLoginNavigation,
+            // handleLogin,
+            // handleRegister,
+            // handleLogout
+        } = this
+
+        return <>
+                <Switch>
+                    <Route exact path="/" render={() => logic.isUserLoggedIn ? <Redirect to="/home" /> : <Landing onRegister={handleRegisterNavigation} onLogin={handleLoginNavigation} />} />
+
+                    {/* <Route path="/register" render={() => logic.isUserLoggedIn ? <Redirect to="/home" /> :
+                        visible !== 'register-ok' ?
+                        <Register lang={lang} onRegister={handleRegister} error={error} /> :
+                        <RegisterOk lang={lang} onLogin={handleLoginNavigation} />
+                    } />
+
+                    <Route path="/login" render={() => logic.isUserLoggedIn ? <Redirect to="/home" /> : <Login lang={lang} onLogin={handleLogin} error={error} />} />
+
+                    <Route path="/home" render={() => logic.isUserLoggedIn ? <Home lang={lang} name={name} onLogout={handleLogout} /> : <Redirect to="/" />} />
+
+                <Redirect to="/" /> */}
+                </Switch>
+        </>
+    }
+}
+
+export default withRouter(App)
