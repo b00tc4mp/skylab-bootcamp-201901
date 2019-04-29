1052bafd8eec5699157a80a7007dcc1a6eeea9a9
Sergio Arce <arcesech30@gmail.com>
2019-04-29 09:05:02 +0200

    Added RecipeApi spec, components Register and Login

--- staff/groups/pinche-wey/food-lab/src/components/Register/index.js
+++ staff/groups/pinche-wey/food-lab/src/components/Register/index.js
@@ -1,34 +1,58 @@
-import React from 'react'
-import literals from './literals'
-import './index.sass'
+import React, { Component } from "react";
 
-function Register({ lang, onRegister, error }) {
-    const { title, name, surname, email, password } = literals[lang]
 
-    function handleSubmit(e) {
-        e.preventDefault()
+function Register ({onRegister, error}) {
+  // ahora register no tiene estado
+  // xq sacamos los campos/inputs desde el valor del input cuando se hace el submit
 
-        const {
-            name: { value: name },
-            surname: { value: surname },
-            username: { value: username },
-            password: { value: password }
-        } = e.target
+  function handleSubmit (event) {
+    event.preventDefault();
 
-        onRegister(name, surname, username, password)
-    }
+    // const name = event.target.name.value
+    // const surname = event.target.surname.value
+    // const age = event.target.age.value
+    // const email = event.target.email.value
+    // const password = event.target.password.value
+    // const passwordConfirm = event.target.passwordConfirm.value
 
-    return <section className="register">
-        <h2>{title}</h2>
-        <form onSubmit={handleSubmit}>
-            <input type="text" name="name" placeholder={name} />
-            <input type="text" name="surname" placeholder={surname} />
-            <input type="text" name="username" placeholder={email} />
-            <input type="password" name="password" placeholder={password} />
-            <button>{title}</button>
-            <span>{error}</span>
+
+    const {
+        name: { value: name },
+        surname: { value: surname },
+        email: { value: email },
+        confirmEmail: { value: confirmEmail},
+        password: { value: password },
+        confirmPassword: { value: confirmPassword},
+        confirmAge: { checked: confirmAge },
+        confirmConditions: { checked: confirmConditions }
+    } = event.target
+
+    // para sacar el valor booleano del checkbox => event.target.confirmAge.checkbox
+
+    onRegister(name, surname, email, confirmEmail, password, confirmPassword, confirmAge, confirmConditions)
+  };
+
+  
+    return (
+      <section className="register">
+        <h2>Register</h2>
+        <form onSubmit={ handleSubmit }>
+          <input type="text" name="name" placeholder="name" required />
+          <input type="text" name="surname" placeholder="surname" required />
+          <input type="text" name="email" placeholder="email" required />
+          <input type="text" name="confirmEmail" placeholder="email confirmation" required />
+          <input type="password" name="password" placeholder="password" required />
+          <input type="password" name="confirmPassword" placeholder="password confirmation" required />
+          <label>Confirm age: <input type="checkbox" name="confirmAge" required /></label>
+          <label>Confirm conditions: <input type="checkbox" name="confirmConditions" required /></label>
+
+          <button>Register</button>
+
+          <span>{error}</span>
         </form>
-    </section>
+      </section>
+    );
+
 }
 
-export default Register
+export default Register;
