9f7d21aff960b0d95027bb923078fc6614fb17d9
caliquerecuto <caliquerecuto@gmail.com>
2019-04-29 10:24:25 +0200

    logic start point

---
+++ staff/groups/miluca/drunk-n-drop/src/logic/index.js
@@ -0,0 +1,14 @@
+import normalize from '../common/normalize'
+import validate from '../common/validate'
+import userApi from '../data/user-api'
+import Cocktail from '../data/cocktail-api'
+import { LogicError } from '../common/errors'
+
+
+const logic = {
+
+
+
+}
+
+export default logic
---
+++ staff/groups/miluca/drunk-n-drop/src/logic/index.spec.js
@@ -0,0 +1,8 @@
+import logic from '.'
+import { LogicError, RequirementError, ValueError, FormatError } from '../common/errors'
+import userApi from '../data/user-api'
+import cocktail from '../data/cocktail-api';
+
+describe('logic', () => {
+
+})
