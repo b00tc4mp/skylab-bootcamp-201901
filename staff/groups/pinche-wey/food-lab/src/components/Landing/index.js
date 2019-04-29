1501539bbef60cb534242e7191af13fd9b3635c5
Sergio Arce <arcesech30@gmail.com>
2019-04-29 10:18:50 +0200

    modified  Landing

--- staff/groups/pinche-wey/food-lab/src/components/Landing/index.js
+++ staff/groups/pinche-wey/food-lab/src/components/Landing/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 import literals from './literals'
 import './index.sass'
 
-function Landing({ lang, onRegister, onLogin }) {
-    const { register, or, login } = literals[lang]
+function Landing({ onRegister, onLogin }) {
+   
+    return <section className="landing" 
+                    onClick={e => e.preventDefault()}>
 
-    return <section className="landing" onClick={e => e.preventDefault()}>
-        <a href="" onClick={() => onRegister()}>{register}</a> <span>{or}</span> <a href="" onClick={() => onLogin()}>{login}</a>.
+        <a href="" onClick={() => onRegister()}>Register</a> <span> or </span> <a href="" onClick={() => onLogin()}>Login</a>.
         </section>
 }
 
