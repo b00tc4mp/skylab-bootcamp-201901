5923ac5ce7498562e1d8275c67b37d3b6f4139ce
mlila <marialilapetri@gmail.com>
2019-04-29 10:29:46 +0200

    register logic

---
+++ staff/groups/batmom-y-robin/wtunes-react-app/src/logic/index.js
@@ -0,0 +1,85 @@
+import normalize from '../common/normalize'
+import validate from '../common/validate'
+import userApi from '../data/user-api'
+import { LogicError } from '../common/errors'
+
+const logic = {
+
+    set __userId__(id) {
+        sessionStorage.userId = id
+    },
+
+    get __userId__() {
+        return normalize.undefinedOrNull(sessionStorage.userId)
+    },
+
+    set __userToken__(token) {
+        sessionStorage.userToken = token
+    },
+
+    get __userToken__() {
+        return normalize.undefinedOrNull(sessionStorage.userToken)
+    },
+
+    get isUserLoggedIn() {
+        return !!(this.__userId__ && this.__userToken__)
+    },
+    registerUser(name, surname, email, password, city){
+        validate.arguments([
+            { name: 'name', value: name, type: 'string', notEmpty: true },
+            { name: 'surname', value: surname, type: 'string', notEmpty: true },
+            { name: 'email', value: email, type: 'string', notEmpty: true },
+            { name: 'password', value: password, type: 'string', notEmpty: true },
+            { name: 'city', value: city, type: 'string', notEmpty: true }
+        ])
+
+        validate.email(email)
+        let preferences=[{Thunderstorm:'Classical'}, {Drizzle :'Pop'}, {Rain :'Jazz'}, {Snow :'Christmas'}, {Clear :'Urbano latino'}, {Clouds :'Rock'}, {Default :'Tango'}]
+        const app='wtunes'
+        return userApi.create(email, password, {name, surname, preferences, city, app})
+            .then(response => {
+                if(response.status==='OK') return
+                else throw new LogicError(response.error)
+            })
+    },
+    loginUser(email, password) {
+        validate.arguments([
+            { name: 'email', value: email, type: 'string', notEmpty: true },
+            { name: 'password', value: password, type: 'string', notEmpty: true }
+        ])
+
+        validate.email(email)
+
+        return userApi.authenticate(email, password)
+            .then(response => {
+                if (response.status === 'OK') {
+                    const { data: { id, token } } = response
+
+                    this.__userId__ = id
+                    this.__userToken__ = token
+                } else throw new LogicError(response.error)
+            })
+    },
+    updateUserPreferences(preferences){
+        validate.arguments([
+            { name: 'preferences', value: preferences, type: 'object', notEmpty: true }
+        ])
+        return userApi.update(this.__userId__, this.__userToken__, {preferences})
+            .then(response =>{
+                if(response.status==='OK')return
+                else throw new LogicError(response.error)
+            })
+    },
+    updateUserCity(city){
+        validate.arguments([
+            { name: 'city', value: city, type: 'string', notEmpty: true }
+        ])
+        return userApi.update(this.__userId__, this.__userToken__, {city})
+            .then(response =>{
+                if(response.status==='OK')return
+                else throw new LogicError(response.error)
+            })
+    }
+}
+
+export default logic
