c9bbd40bd1768ff7a1241cc885588ee35b28631a
didactor91 <didactor91@gmail.com>
2019-04-29 19:32:47 +0200

    logic spec

---
+++ staff/groups/aldi/siempre-tarde/src/logic/index.spec.js
@@ -0,0 +1,192 @@
+import logic from '.'
+import { LogicError, RequirementError, ValueError, FormatError } from '../common/errors'
+import userApi from '../data/user-api'
+import iBusApi from '../data/ibus-api'
+
+describe('logic', () => {
+    describe('users', () => {
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        let email
+        const password = '123'
+        const password2 = '123'
+
+        beforeEach(() => {
+            email = `manuelbarzi-${Math.random()}@gmail.com`
+
+            logic.__userId__ = null
+            logic.__userToken__ = null
+        })
+
+        describe('register user', () => {
+            it('should succeed on correct user data', () =>
+                logic.registerUser(name, surname, email, password, password2)
+                    .then(response => expect(response).toBeUndefined())
+            )
+
+            describe('on already existing user', () => {
+                beforeEach(() => logic.registerUser(name, surname, email, password, password2))
+
+                it('should fail on retrying to register', () =>
+                    logic.registerUser(name, surname, email, password, password2)
+                        .then(() => { throw Error('should not reach this point') })
+                        .catch(error => {
+                            expect(error).toBeDefined()
+                            expect(error instanceof LogicError).toBeTruthy()
+
+                            expect(error.message).toBe(`user with username \"${email}\" already exists`)
+                        })
+                )
+            })
+
+            it('should fail on undefined name', () => {
+                const name = undefined
+
+                expect(() => logic.registerUser(name, surname, email, password, password2)).toThrowError(RequirementError, `name is not optional`)
+            })
+
+            it('should fail on empty name', () => {
+                const name = ''
+
+                expect(() => logic.registerUser(name, surname, email, password, password2)).toThrowError(ValueError, 'name is empty')
+            })
+
+            it('should fail on blank name', () => {
+                const name = ' \t    \n'
+
+                expect(() => logic.registerUser(name, surname, email, password, password2)).toThrowError(ValueError, 'name is empty')
+            })
+
+            it('should fail on undefined surname', () => {
+                const surname = undefined
+
+                expect(() => logic.registerUser(name, surname, email, password, password2)).toThrowError(RequirementError, `surname is not optional`)
+            })
+
+            it('should fail on empty surname', () => {
+                const surname = ''
+
+                expect(() => logic.registerUser(name, surname, email, password, password2)).toThrowError(ValueError, 'surname is empty')
+            })
+
+            it('should fail on blank surname', () => {
+                const surname = ' \t    \n'
+
+                expect(() => logic.registerUser(name, surname, email, password, password2)).toThrowError(ValueError, 'surname is empty')
+            })
+
+            it('should fail on undefined email', () => {
+                const email = undefined
+
+                expect(() => logic.registerUser(name, surname, email, password, password2)).toThrowError(RequirementError, `email is not optional`)
+            })
+
+            it('should fail on empty email', () => {
+                const email = ''
+
+                expect(() => logic.registerUser(name, surname, email, password, password2)).toThrowError(ValueError, 'email is empty')
+            })
+
+            it('should fail on blank email', () => {
+                const email = ' \t    \n'
+
+                expect(() => logic.registerUser(name, surname, email, password, password2)).toThrowError(ValueError, 'email is empty')
+            })
+
+            it('should fail on non-email email', () => {
+                const nonEmail = 'non-email'
+
+                expect(() => logic.registerUser(name, surname, nonEmail, password, password2)).toThrowError(FormatError, `${nonEmail} is not an e-mail`)
+            })
+
+            // TODO password fail cases
+        })
+
+        describe('login user', () => {
+            let id
+
+            beforeEach(() =>
+                userApi.create(email, password, { name, surname })
+                    .then(response => id = response.data.id)
+            )
+
+            it('should succeed on correct user credential', () =>
+                logic.loginUser(email, password)
+                    .then(() => {
+                        const { __userId__, __userToken__ } = logic
+
+                        expect(typeof __userId__).toBe('string')
+                        expect(__userId__.length).toBeGreaterThan(0)
+                        expect(__userId__).toBe(id)
+
+                        expect(typeof __userToken__).toBe('string')
+                        expect(__userToken__.length).toBeGreaterThan(0)
+
+                        const [, payloadB64,] = __userToken__.split('.')
+                        const payloadJson = atob(payloadB64)
+                        const payload = JSON.parse(payloadJson)
+
+                        expect(payload.id).toBe(id)
+
+                        expect(logic.isUserLoggedIn).toBeTruthy()
+                    })
+            )
+
+            it('should fail on non-existing user', () =>
+                logic.loginUser(email = 'unexisting-user@mail.com', password)
+                    .then(() => { throw Error('should not reach this point') })
+                    .catch(error => {
+                        expect(error).toBeDefined()
+                        expect(error instanceof LogicError).toBeTruthy()
+
+                        expect(error.message).toBe(`user with username \"${email}\" does not exist`)
+                    })
+            )
+        })
+
+        describe('retrieve user', () => {
+            let id, token
+
+            beforeEach(() =>
+                userApi.create(email, password, { name, surname })
+                    .then(response => {
+                        id = response.data.id
+
+                        return userApi.authenticate(email, password)
+                    })
+                    .then(response => {
+                        token = response.data.token
+
+                        logic.__userId__ = id
+                        logic.__userToken__ = token
+                    })
+            )
+
+            it('should succeed on correct user id and token', () =>
+                logic.retrieveUser()
+                    .then(user => {
+                        expect(user.id).toBeUndefined()
+                        expect(user.name).toBe(name)
+                        expect(user.surname).toBe(surname)
+                        expect(user.email).toBe(email)
+                        expect(user.password).toBeUndefined()
+                    })
+            )
+
+            it('should fail on incorrect user id', () => {
+                logic.__userId__ = '5cb9998f2e59ee0009eac02c'
+
+                return logic.retrieveUser()
+                    .then(() => { throw Error('should not reach this point') })
+                    .catch(error => {
+                        expect(error).toBeDefined()
+                        expect(error instanceof LogicError).toBeTruthy()
+
+                        expect(error.message).toBe(`token id \"${id}\" does not match user \"${logic.__userId__}\"`)
+                    })
+            })
+        })
+
+    })
+
+})
