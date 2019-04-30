a34fa04041856f23268e83b22dfbbc95a33ff7ae
Miguel Angel Zapatero <m.zapes@gmail.com>
2019-04-30 15:10:01 +0200

    add search component

---
+++ staff/groups/los-mosquitos/skyflix/src/components/Search/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import literals from './literals'
+
+function Search({ lang, onSearch }) {
+    const { search } = literals['en-US'] //cambiar por lang
+
+    return <form onSubmit={e => {
+        e.preventDefault()
+
+        const query = e.target.query.value
+
+        onSearch(query)
+    }}>
+        <input type="text" name="query" placeholder={placeholder}/>
+        <button>{search}</button>
+    </form>
+}
+
+export default Search
