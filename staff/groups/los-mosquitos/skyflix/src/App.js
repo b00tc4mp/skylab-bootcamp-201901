988d06fac80a299acba5814a6b0c15e79a2bbb9a
Llorenç Comellas <llorenscomellas@gmail.com>
2019-04-30 11:51:18 +0200

    update login

--- staff/groups/los-mosquitos/skyflix/src/App.js
+++ staff/groups/los-mosquitos/skyflix/src/App.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react'
 import Login from './components/Login'
 import i18n from './common/i18n'
+import logic from './logic';
 
 class App extends Component {
- 
-  state = { lang: i18n.language, visible: 'login' }
-  handleLogin = (username, password) => {
 
-    //TODO
+  state = { lang: i18n.language, visible: 'login', error: null }
+  handleLogin = (username, password) => {
+    try {
+      logic.loginUser(username, password)
+        .then(() =>
+          logic.retrieveUser()
+        )
+        .then(user => {
+          console.log(user.fullname)
+        })
+        .catch(error =>
+          this.setState({ error: error.message })
+        )
+    } catch ({ message }) {
+      this.setState({ error: message })
+    }
 
   }
 
   render() {
     const {
-      state: { lang, visible },
+      state: { lang, visible, error },
       handleLogin
     } = this
-    debugger
     return <>
-      {visible === 'login' && <Login lang={lang} onLogin={handleLogin} />}
+      {visible === 'login' && <Login lang={lang} onLogin={handleLogin} error={error} />}
     </>
   }
 
