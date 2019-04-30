1d3728a4d2f8345e7f2a1ff101cc874bf3ece369
Miguel Angel Zapatero <m.zapes@gmail.com>
2019-04-30 13:39:17 +0200

    add home nav menu

---
+++ staff/groups/los-mosquitos/skyflix/src/components/Nav/literals.js
@@ -0,0 +1,14 @@
+const literals = {
+    "en-Us": {
+        list: 'My List',
+        profile: 'Profile',
+        logout: 'LOGOUT',
+    },
+    "es-Es": {
+        list: 'Mi Lista',
+        profile: 'Perfil',
+        logout: 'Salir',
+    }
+}
+
+export default literals
