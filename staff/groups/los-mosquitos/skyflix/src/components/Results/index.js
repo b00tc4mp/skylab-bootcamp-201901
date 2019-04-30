1a11fbd1560324a9a4d556a6415a0a0c75deb4e1
Miguel Angel Zapatero <m.zapes@gmail.com>
2019-04-30 15:59:37 +0200

    add results component

---
+++ staff/groups/los-mosquitos/skyflix/src/components/Results/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons'
+
+function Results({ items, onItem, onList, list}) {
+    return <ul>
+        {
+            items.map(({ id, title, image }) => {
+                const isList = list.some(movie => movie.id === id)
+
+                return <li key={id} onClick={() => onItem(id)}>
+                    <h2>{title}</h2>
+                    {/* FontAwesome */}
+                    <img src={image} />
+                </li> 
+            })
+        }
+    </ul>
+}
+
+export default Results
+
+//<FontAwesomeIcon icon={isFav ? faHeartSolid : faHeartRegular} onClick={e => {
+//                         e.stopPropagation()
+//                         onFav(id)
+//                     }} />
