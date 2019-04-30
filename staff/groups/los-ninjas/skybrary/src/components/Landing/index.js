a4d7de1bfbf76e9a8eae0b83aa31dd0857d3faa5
Edgar Frías <edfriasji@gmail.com>
2019-04-30 10:23:37 +0200

    add bulma styles to the css file

--- staff/groups/los-ninjas/skybrary/src/components/Landing/index.js
+++ staff/groups/los-ninjas/skybrary/src/components/Landing/index.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import './index.sass'
 
 function Landing({ onClickRegister, onClickLogin }) {
 
-    return <section className="landing" onClick={e => e.preventDefault()}>
-        <a href="#" onClick={() => onClickRegister()}>Register</a> <span>or</span> 
-        <a href="#" onClick={() => onClickLogin()}>Login</a>.
-        </section>
+    return (
+        <Fragment>
+            <section className="section">
+                <section className="container landing" onClick={e => e.preventDefault()}>
+                    <a href="#" onClick={() => onClickRegister()}>Register</a>
+                    <span>or</span>
+                    <a href="#" onClick={() => onClickLogin()}>Login</a>.
+                </section>
+            </section>
+        </Fragment>
+    )
 }
 
 export default Landing
