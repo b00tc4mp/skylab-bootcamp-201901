9fb41b9adb1845b2e6917d626fb4ed3d44838959
Llorenç Comellas <llorenscomellas@gmail.com>
2019-04-30 20:01:25 +0200

    update nav, results,search

--- staff/groups/los-mosquitos/skyflix/src/components/Results/index.js
+++ staff/groups/los-mosquitos/skyflix/src/components/Results/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons'
-import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons'
+// import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons'
 
 function Results({ items, onItem, onList, list}) {
     return <ul>
         {
-            items.map(({ id, title, image }) => {
-                const isList = list.some(movie => movie.id === id)
-
+            items.map(({ id, title, poster_path: image }) => {
+                // const isList = list.some(movie => movie.id === id)
+                const imagePath =  `https://image.tmdb.org/t/p/w300/${image}`
                 return <li key={id} onClick={() => onItem(id)}>
                     <h2>{title}</h2>
                     {/* FontAwesome */}
-                    <img src={image} />
+                    <img src={imagePath} />
                 </li> 
             })
         }
