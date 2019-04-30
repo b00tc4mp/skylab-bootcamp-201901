48fbca1b4dff044625274b6ba396f2718322b696
Fran Destruels <frand.ross@icloud.com>
2019-04-30 18:42:04 +0200

    SmallCard

--- staff/groups/pinche-wey/food-lab/src/components/smallCard/index.js
+++ staff/groups/pinche-wey/food-lab/src/components/smallCard/index.js
@@ -1,7 +1,17 @@
-// import React from 'react'
+import React from 'react'
+import logic from '../../logic';
 
-// function SmallCard (){
-    
-// }
+function SmallCard({ toPaint, onItem }) {
+    return <div className="results">
+        {
+          toPaint && toPaint.map(({ idMeal: id, strMeal: title, strMealThumb: img }) => {
+                return <span key={id} onClick={() => onItem(id)}>
+                    <img src={img} />
+                    <h4>{title}</h4> 
+                </span>
+            })
+        }
+    </div>
+}
 
-// export default SmallCard
+export default SmallCard
