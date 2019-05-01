3c72aabf07fd40fcb74474bc7b63fd6e94317398
Llorenç Comellas <llorenscomellas@gmail.com>
2019-05-01 10:30:00 +0200

    resolve conflicts app

--- staff/groups/los-mosquitos/skyflix/src/App.js
+++ staff/groups/los-mosquitos/skyflix/src/App.js
@@ -1,100 +1,98 @@
-
 import React, { Component } from 'react'
 import logic from './logic'
 import i18n from './common/i18n'
 import LanguageSelector from './components/language-selector'
 import Landing from './components/Landing'
 import Login from './components/Login'
-import ChoosePlan from './components/ChoosePlan'
+import ChoosePlan from './components/choosePlan'
 import Register from './components/Register'
+import Home from './components/Home'
 import logo from './logo.svg'
 import './App.css'
 import { Route, withRouter, Switch, Redirect } from 'react-router-dom'
-import userApi from './data/user-api';
-
 
 class App extends Component {
 
-  state = { lang: i18n.language, plan: null, error: null }
+    state = { lang: i18n.language, plan: null, error: null }
 
-  handleLanguageChange = lang => this.setState({ lang: i18n.language = lang })
+    handleLanguageChange = lang => this.setState({ lang: i18n.language = lang })
 
-  handleRegister = (fullname, email, password, confirmPassword) => {
+    handleRegister = (fullname, email, password, confirmPassword) => {
 
-    try {
-      logic.registerUser(fullname, email, password, confirmPassword, this.state.plan)
-
-        .then(() => { this.setstate(this.props.history.push('/login')) })
-        .catch(error => {
-          this.setState({ error: error.message })
-        })
-
-    } catch (error) {
-      this.setState({ error: error.message })
-    }
-  }
-
-  handleLogin = (username, password) => {
-    try {
-
-      logic.loginUser(username, password)
-        .then(() =>
-          logic.retrieveUser()
-        )
-        .then(user => {
-          console.log(user.fullname)
-        })
-        .catch(error =>
-          this.setState({ error: error.message })
-        )
-    } catch ({ message }) {
-      this.setState({ error: message })
+        try {
+            logic.registerUser(fullname, email, password, confirmPassword, this.state.plan)
+                .then(() => this.props.history.push('/login'))
+                .catch(error => {
+                    this.setState({ error: error.message })
+                })
+        } catch (error) {
+            this.setState({ error: error.message })
+        }
     }
 
+    handleLogin = (username, password) => {
+        try {
+            logic.loginUser(username, password)
+                .then(() =>
+                    logic.retrieveUser()
+                )
+                .then(user => this.setState({ name: user.fullname }, () => this.props.history.push('/home')))
+                .catch(error =>
+                    this.setState({ error: error.message })
+                )
+        } catch ({ message }) {
+            this.setState({ error: message })
+        }
+    }
 
-  }
+    handleSelectedPlan = (plan) => this.setState({ plan }, () => this.props.history.push('/signup/form'))
 
+    handleRegisterNav = () => this.props.history.push('/signup')
 
-  handleSelectedPlan = (plan) => this.setState({ plan }, () => this.props.history.push('/signup/form'))
+    handleLoginNav = () => this.props.history.push('/login')
 
-  handleRegisterNav = () => this.props.history.push('/signup')
+    render() {
+        const {
+            state: { lang, error, name, plan },
+            handleLoginNav,
+            handleRegisterNav,
+            handleLogin,
+            handleRegister,
+            handleLanguageChange,
+            handleSelectedPlan
+        } = this
 
-  handleLoginNav = () => this.props.history.push('/login')
+        return <>
+            <LanguageSelector lang={lang} onLanguageChange={handleLanguageChange} />
 
-
-
-
-
-
-  render() {
-    const {
-      state: { lang, error, plan },
-      handleLanguageChange,
-      handleLoginNav,
-      handleRegisterNav,
-      handleLogin,
-      handleRegister,
-      handleSelectedPlan
-    } = this
-
-
-    return <>
-      <LanguageSelector lang={lang} onLanguageChange={handleLanguageChange} /> 
-
-      <Switch>
-        <Route exact path="/" render={() => <Landing lang={lang} onLogin={handleLoginNav} onRegister={handleRegisterNav} />} />
-        <Route path="/signup/form" render={() => plan ? <Register lang={lang} onRegister={handleRegister} plan={plan} error={error} /> : <Redirect to='/signup' />} />
-        <Route path="/signup" render={() => <ChoosePlan lang={lang} onSelectedPlan={handleSelectedPlan} />} />
-        <Route patch="/login" render={() => <Login lang={lang} onLogin={handleLogin} error={error} />} />
-
-      </Switch>
-
-
-    </>
-  }
-
+            <Switch>
+                <Route exact path="/" render={() => logic.isUserLoggedIn ? <Redirect to='/home'/> : <Landing lang={lang} onLogin={handleLoginNav} onRegister={handleRegisterNav} />} />
+                
+				<Route path="/signup/form" render={() => logic.isUserLoggedIn ? <Redirect to='/home'/> :
+					plan ? <Register lang={lang} onRegister={handleRegister} plan={plan} error={error} /> : <Redirect to='/signup' />} />
+                
+				<Route path="/signup" render={() => logic.isUserLoggedIn ? <Redirect to='/home'/> : <ChoosePlan lang={lang} onSelectedPlan={handleSelectedPlan} />} />
+                
+				<Route patch="/login" render={() => logic.isUserLoggedIn ? <Redirect to='/home'/> : <Login lang={lang} onLogin={handleLogin} error={error} />} />
+                
+				<Route patch='/home' render={() => logic.isUserLoggedIn ? <Home lang={lang} name={name} /> :  <Redirect to='/'/> } />
+            </Switch>
+        </>
+    }
 }
 
-
 export default withRouter(App);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
