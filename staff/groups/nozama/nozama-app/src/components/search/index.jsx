add87e6c478b2fde6d03fb0d81417bde136def88
Carlos7979 <carloscorredorwg@gmail.com>
2019-05-01 12:06:00 +0200

    add categories and changes on search and shopping cart

--- staff/groups/nozama/nozama-app/src/components/search/index.jsx
+++ staff/groups/nozama/nozama-app/src/components/search/index.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 
-import {Row, Container, Col, Button} from 'reactstrap'
+import {Row, Container, Col, Button, Input} from 'reactstrap'
 
 function Search(props) {
 
 
   return (
     <Container>
-      <Row>
-          <Col><Input placeholder="Search" /></Col>
-          <Col xs="3"><Button color="success"><img width="100%" src="https://cdn.pixabay.com/photo/2017/01/13/01/22/magnifying-glass-1976105_960_720.png" /></Button></Col>
-        </Row>
-      </Container>
-
+      <Row >
+        <Col >
+          <Input placeholder="Search" />
+        </Col>
+        <Col sm="2" xs="3">
+          <Button color="success">
+            <img width="100%" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Magnifying_glass_icon.svg/1024px-Magnifying_glass_icon.svg.png" />
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
