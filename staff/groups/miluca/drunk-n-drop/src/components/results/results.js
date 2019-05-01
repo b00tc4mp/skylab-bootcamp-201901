89c5e35470900c4a13f101059de46adf9f1ae92c
Miguel Angel Sevillano Hidalgo <mash5550@gmail.com>
2019-05-01 10:47:35 +0200

    update

--- staff/groups/miluca/drunk-n-drop/src/components/results/results.js
+++ staff/groups/miluca/drunk-n-drop/src/components/results/results.js
@@ -1,20 +1,28 @@
 import React from 'react'
 
 
-function Results(items) {
+function Results({items,onFavorites}) {
+
+    console.log(items)
 
 
-    return <ul>
+    return '' /* <ul>
         {
-           items.map(({ name, image, id }) => {
-                return <li key={id}>
-                    <h2>{name}</h2>
-                    <img src={image} />
-                    <span>{id}</span>
-                </li>
-            })
+    
+        items.map(({strDrink,strDrinkThumb,strIngredient1,idDrink})=>{
+
+            return <li key={idDrink} onClick={() => onFavorites(idDrink)}>
+                <h2>{strDrink}</h2>
+                <img src={strDrinkThumb}/>
+                <span>{strIngredient1}</span>
+                <span></span>
+            </li>
+        })
+
+       
         }
-     </ul>
+         </ul> */
+      
 }
 
 export default Results        
