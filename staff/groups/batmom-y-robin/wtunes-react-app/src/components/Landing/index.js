61da64ddf12f999286dacb916690cb97ea88c71c
mlila <marialilapetri@gmail.com>
2019-05-01 19:32:44 +0200

    header

--- staff/groups/batmom-y-robin/wtunes-react-app/src/components/Landing/index.js
+++ staff/groups/batmom-y-robin/wtunes-react-app/src/components/Landing/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import './index.sass' 
 
 function Landing({onRegister, onLogin}){
 
-    return <section onClick={e => e.preventDefault()} >
-        <a href="" onClick={() => onRegister()}>Register</a><span> or </span><a href="" onClick={() => onLogin()}>Login</a>
-    </section>
+    return <main className="mainlanding">
+        <div className="container">
+            <section className="columns is-multiline has-text-centered">
+                <div className="column is-8-desktop is-6-tablet is-10-mobile is-offset-one-quarter">   
+                    <p className="title is-2">Weatunes</p>
+                    <p className="subtitle">Welcome to our application</p>
+                </div>
+                <div className="column is-8-desktop is-6-tablet is-10-mobile is-offset-one-quarter">
+                    <p className="buttons" onClick={e => e.preventDefault()}>
+                        <a className="button is-fullwidth is-primary" href="" onClick={() => onRegister()}>Register</a>
+                        <a className="button is-fullwidth is-primary" href="" onClick={() => onLogin()}>Login</a>
+                    </p>
+                </div>
+            </section>
+
+            
+        </div>
+    </main>
 
 }
 
