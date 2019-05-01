f02158b0a6d7fc20afd80cfd0e39ebe68e72bd1c
Llorenç Comellas <llorenscomellas@gmail.com>
2019-05-01 09:53:48 +0200

    update results path_image

--- staff/groups/los-mosquitos/skyflix/src/components/Results/index.js
+++ staff/groups/los-mosquitos/skyflix/src/components/Results/index.js
@@ -3,17 +3,17 @@ import React from 'react'
 // import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons'
 // import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons'
 
-function Results({ items, onItem, onList, list}) {
+function Results({ items, onItem, onList, list }) {
     return <ul>
         {
-            items.map(({ id, title, poster_path: image }) => {
+            items.map(({ id, title, image }) => {
                 // const isList = list.some(movie => movie.id === id)
-                const imagePath =  `https://image.tmdb.org/t/p/w300/${image}`
+
                 return <li key={id} onClick={() => onItem(id)}>
                     <h2>{title}</h2>
                     {/* FontAwesome */}
-                    <img src={imagePath} />
-                </li> 
+                    <img src={image} />
+                </li>
             })
         }
     </ul>
