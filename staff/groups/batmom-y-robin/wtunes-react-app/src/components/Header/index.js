90e289ee9cff094b43c42e0bf52284c368ff95ab
Markuson <el.marcuson@gmail.com>
2019-05-01 21:16:25 +0200

    desliada

--- staff/groups/batmom-y-robin/wtunes-react-app/src/components/Header/index.js
+++ staff/groups/batmom-y-robin/wtunes-react-app/src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import logic from '../../logic'
 import images from './images'
+import './index.sass'
 
 
 class Header extends Component {
@@ -27,11 +28,17 @@ class Header extends Component {
     render(){
         const {
             hadleCityChange,
-            state: {currentWeather, currentCity, error}
+            state: {currentWeather, currentCity, images},
+            props: {onLogout, onProfile, onPreferences}
         } = this
-        return <>
-        <header>
-            <img  src={this.state.images}/>
+
+        return <div className="myHeader" style={{backgroundImage: `url(${images})`}}>
+        <h1>weatunes</h1>
+        <button onClick={onLogout}>Logout</button>
+        <button onClick={onProfile}>Profile</button>
+        <button onClick={onPreferences}>Preferences</button>
+
+            {/* <img className='img'  src={this.state.images}/> */}
         <select name="city" onChange={event => hadleCityChange(event.target.value)} defaultValue={currentCity}>
             <option value="Alaska">Alaska</option>
             <option value="Auckland">Auckland</option>
@@ -48,9 +55,7 @@ class Header extends Component {
         </select>
         <h2>{currentCity}</h2>
         <h2>{currentWeather}</h2>
-        <img src=""/>
-        </header>
-        </>
+        </div>
     }
 }
 
--- staff/groups/batmom-y-robin/wtunes-react-app/src/components/Results/index.js
+++ staff/groups/batmom-y-robin/wtunes-react-app/src/components/Results/index.js
@@ -1,25 +1,27 @@
 import React from 'react'
+import './index.sass'
 
 function Results({ error, items, onPlayer }) {
 
 
 
-    return <section className="results">
-        <h2>results</h2>
-        <ul>
-        {
-        items.map(({ trackName, artistName, previewUrl, artWork, genere }) =>{
+    return <div className="results">
+        <section className="container">
+            <div className='columns is-multiline is-mobile is-centered'>
+            {
+            items.map(({ trackName, artistName, previewUrl, artWork, genere }) =>{
 
-            return <li key={previewUrl} onClick={() => onPlayer(previewUrl)}>
-                    <h3>{trackName}</h3>
-                    <h4>{artistName}</h4>
-                    <p>{genere}</p>
-                    <img src={artWork} />
-                </li>
-            })
-        }
-        </ul>
-    </section>
+                return <div key={previewUrl} className="column is-2-desktop is-3-tablet is-5-mobile box"  onClick={() => onPlayer(previewUrl)}>
+                        <img src={artWork} />
+                        <h3>{trackName}</h3>
+                        <h4>{artistName}</h4>
+                        <p>{genere}</p>
+                    </div>
+                })
+            }
+            </div>
+        </section>
+    </div>
 }
 
 export default Results
