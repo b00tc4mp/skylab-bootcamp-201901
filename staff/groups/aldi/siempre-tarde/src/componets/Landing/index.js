49d1e274fff5b4b7f98105b601800d38b2c48cdb
didactor91 <didactor91@gmail.com>
2019-05-02 18:19:50 +0200

    bulma

--- staff/groups/aldi/siempre-tarde/src/componets/Landing/index.js
+++ staff/groups/aldi/siempre-tarde/src/componets/Landing/index.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import literals from './literals'
+import './index.sass'
 
-function Landing({lang, onRegister, onLogin}) {
-    const { welcome, register, or, login} = literals[lang]
+function Landing({ lang, onRegister, onLogin }) {
+    const { welcome, register, or, login } = literals[lang]
 
-    return <section>
-
-    <h1>{welcome}</h1>
-    <button onClick={()=>onRegister()}>{register}</button>
-    <p>{or}</p>
-    <button onClick={()=>onLogin()}>{login}</button>
-
+    return <div className='block'>
+    <section className='hero is-danger'>
+        <div className='hero-body'>
+            <h1 className='title'>{welcome}</h1>
+        </div>
     </section>
+        <div className='columns is-mobile'>
+            <div className='column is-4 is-offset-one-quarter'>
+                <div className='block'>
+                    <button className='button is-danger is-rounded is-outlined' onClick={() => onRegister()}>{register}</button>
+                </div>
+                <div className='block'>
+                    <p>{or}</p>
+                </div>
+                <div className='block'>
+                    <button className='button is-danger is-rounded is-outlined' onClick={() => onLogin()}>{login}</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 }
 export default Landing
