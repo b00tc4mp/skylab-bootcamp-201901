02c3784883559456da0af111832daf9a99e18e74
Edgar Frías <edfriasji@gmail.com>
2019-05-02 15:28:37 +0200

    fix layout issues

--- staff/groups/los-ninjas/skybrary/src/components/Results/index.js
+++ staff/groups/los-ninjas/skybrary/src/components/Results/index.js
@@ -8,8 +8,7 @@ function Results({ items, onItem }) {
             <ul className="columns is-centered is-multiline is-mobile">
                 {
                     items.map(({ key, isbn, cover_edition_key, title, author_name, cover_i, publish_date }) => {
-
-                        return (isbn && cover_edition_key) && <li className="library__book column is-3-desktop is-5-tablet is-10-mobile" key={cover_edition_key} onClick={() => onItem(isbn[0], key)}>
+                        return (isbn && cover_edition_key) && <li className="library__book column is-3-desktop is-4-tablet is-10-mobile" key={cover_edition_key} onClick={() => onItem(isbn[0], key)}>
                             <article className="card">
                                 <div className="card-image">
                                     <figure className="image is-3by4">
