bb163befee78a63558c740a533e9947fb3b67f3b
DaniLatreCodes <danilatre.skycode@gmail.com>
2019-05-02 16:38:45 +0200

    Added id URL Link for product to product-hor-slim

--- staff/groups/nozama/nozama-app/src/components/Products/product-hor-slim/index.jsx
+++ staff/groups/nozama/nozama-app/src/components/Products/product-hor-slim/index.jsx
@@ -5,6 +5,7 @@ import Image from '../Detail/Image'
 import Price from '../Detail/Price';
 import Currency from '../Detail/Currency'
 import ButtonAddToCart from '../../Buttons/AddToCart'
+import {Link, withRouter} from 'react-router-dom'
 
 
 function ProductHorizontalSlim(props) {
@@ -18,27 +19,29 @@ function ProductHorizontalSlim(props) {
    onAddToCart(product)
   }
   
-  const { imageSmall, productName, subtitle, originalPrice, displayCurrency } = product;
+  const { imageSmall, productName, subtitle, originalPrice, displayCurrency, productId } = product;
   return (
     <div style={{marginTop:"0.2rem", }}>
       <div className="container-fluid">
         <div className="row">
-          <div className="col-xs-5 col-sm-4" >
-            <Image image={imageSmall} onClick={handleClickImage}/>
-          </div>
+          <Link to={`/detailProduct/${productId}`}>
+            <div className="col-xs-5 col-sm-4" >
+              <img src={imageSmall}/>
+            </div>
+          </Link>
+
           <div className="col-xs-7 col-sm-4">
             <Title title={productName}/>
             <Subtitle subtitle={subtitle}/>
-              <div style={{display:"flex" }}>
+              <div style={{ display:"flex" }}>
                   <Price price={originalPrice}/>
                   <Currency currency={displayCurrency}/>
               </div>
           </div>
         </div>
       </div>
-
     </div>
   )
 }
 
-export default ProductHorizontalSlim
+export default withRouter(ProductHorizontalSlim)
