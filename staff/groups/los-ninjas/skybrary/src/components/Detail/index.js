7e2f5be65e69369e7458983d43fa67f30b689a66
Daniela <aguileramalave@gmail.com>
2019-05-02 10:49:30 +0200

    bulma detail finished

--- staff/groups/los-ninjas/skybrary/src/components/Detail/index.js
+++ staff/groups/los-ninjas/skybrary/src/components/Detail/index.js
@@ -11,10 +11,10 @@ function Detail({ item }) {
     return <section className='container'>
         <article className='columns card-detail'>
 
-            <div className='column is-4 is-offset-2 card-detail__image'>
+            <div className='column is-4 card-detail__image'>
                 {<img src={(item.cover && item.cover > 0) ? `https://covers.openlibrary.org/b/id/${item.cover}-L.jpg` : 'https://www.motorolasolutions.com/content/dam/msi/images/business/products/accessories/p_-_r/raf4220a/_images/static_files/b2b_product_raf4220a_lg_us-en.jpg'} />}
             </div>
-            <div className='column is-4 card-detail__info'>
+            <div className='column is-8 card-detail__info'>
                 {item.title && <span><p className="info__BoldTitle">Title: </p> <p>{item.title}</p></span>}
                 <span><p className="info__BoldTitle">Author: </p>{item.author_name.join(', ')}</span>
                 {item.numberOfPages && <span><p className="info__BoldTitle">Num of pages: </p>{item.numberOfPages} </span>}
