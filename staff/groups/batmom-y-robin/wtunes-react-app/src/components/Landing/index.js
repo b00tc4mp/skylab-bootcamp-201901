92f11646b0c603c35e2714268d34cbd926aa9b7c
mlila <marialilapetri@gmail.com>
2019-05-03 11:13:35 +0200

    finally

--- staff/groups/batmom-y-robin/wtunes-react-app/src/components/Landing/index.js
+++ staff/groups/batmom-y-robin/wtunes-react-app/src/components/Landing/index.js
@@ -5,12 +5,10 @@ function Landing({onRegister, onLogin}){
 
     return <main className="main-landing">
         <div className="container-landing">
-            <section className="is-multiline has-text-centered">
-                <div className="title-landing column is-10-desktop is-10-tablet is-10-mobile is-offset-one-quarter">   
+            <section className="columns is-mobile has-text-centered is-centered">
+                <div className="title-landing column is-12-desktop is-12-tablet is-10-mobile ">   
                     <p className="title is-2">Weatunes</p>
                     <p className="subtitle">Welcome to our application</p>
-                </div>
-                <div className="column is-10-desktop is-10-tablet is-10-mobile is-offset-one-quarter">
                     <p className="buttons" onClick={e => e.preventDefault()}>
                         <a className="button is-fullwidth is-primary" href="" onClick={() => onRegister()}>Sign Up</a>
                         <a className="button is-fullwidth is-primary" href="" onClick={() => onLogin()}>Sign In</a>
--- staff/groups/batmom-y-robin/wtunes-react-app/src/components/Login/index.js
+++ staff/groups/batmom-y-robin/wtunes-react-app/src/components/Login/index.js
@@ -13,18 +13,18 @@ function Login({ onLogin, error }) {
     }
 
     return <main className="mainlogin">
-    <section className="login column is-4-desktop is-4-tablet is-10-mobile is-one-fifth">
-        <div className="has-text-centered field">
-            <h2 className="title is-2">Weatunes</h2>
-            <p className="subtitle"> Sign in with your email address</p>
-        </div >
-        <form onSubmit={handleSubmit}>
-            <input className="input field" type="text" name="username" placeholder="email"/>
-            <input className="input field" type="password" name="password" placeholder="password" />
-            <button className="button is-fullwidth is-primary">Sign In</button>
-            <span className="help is-danger">{error}</span>
-        </form>
-    </section>
+        <section className="login column is-4-desktop is-4-tablet is-10-mobile is-one-fifth">
+            <div className="has-text-centered field">
+                <h2 className="title is-2">Weatunes</h2>
+                <p className="subtitle"> Sign in with your email address</p>
+            </div >
+            <form onSubmit={handleSubmit}>
+                <input className="input field" type="text" name="username" placeholder="email"/>
+                <input className="input field" type="password" name="password" placeholder="password" />
+                <button className="button is-fullwidth is-primary">Sign In</button>
+                <span className="help is-danger">{error}</span>
+            </form>
+        </section>
     </main> 
 }
 
