ccfe515eeccaf634f55dff14c0237ad0bd8039b4
Edgar Frías <edfriasji@gmail.com>
2019-05-06 18:53:29 +0200

    solve exercise 8

--- staff/edgar-frias-jimenez/learnyounode/8-http-collect/collect.js
+++ staff/edgar-frias-jimenez/learnyounode/8-http-collect/collect.js
@@ -1,21 +1,12 @@
 const http = require('http')
 
-const [, , url] = process.argv
+const { argv: [, , url] } = process
 
 http.get(url, response => {
   response.setEncoding('utf8')
-  // response.on('error', error => { throw error })
+  response.on('error', error => { throw error })
 
   let content = ''
   response.on('data', data => content += data)
   response.on('end', () => console.log(`${content.length}\n${content}`))
-}).on('error', error => { throw error })
-
-// const bl = require('bl')
-
-// http.get(url, res => {
-//   res.pipe(bl((error, content) => {
-//     if (error) throw error
-//     console.log(`${content.length}\n${content}`)
-//   }))
-// })
+})
