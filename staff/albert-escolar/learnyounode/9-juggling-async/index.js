bc964df9817dba1ceb62259b01a810e0a018570c
Albert <albert_escoli@hotmail.com>
2019-05-07 16:30:17 +0200

    update

---
+++ staff/albert-escolar/learnyounode/9-juggling-async/index.js
@@ -0,0 +1,28 @@
+const http = require('http')
+const bl = require('bl')
+
+const results = []
+const count = 0
+
+function printResults() {
+    for (let i = 0; i<3; i++)
+    console.log(results[i])
+}
+
+function httpGet(index){
+    http.get(process.argv[2+index], function (response){
+        response.pipe(bl(function(err,data){
+            if(err)
+            return console.error(err)
+
+            results[index] = data.toString()
+            count ++
+
+            if (count == 3)
+            printResults()
+        }))
+    })
+}
+
+for (let i= 0; i<3; i++)
+httpGet(i)
