711beb0ccf2b6f26bee928822ee0e9ed2a5c77de
Markuson <el.marcuson@gmail.com>
2019-05-08 15:41:41 +0200

    solve conflicts

--- staff/manuel-barzi/array-functions/2/for-each.js
+++ staff/manuel-barzi/array-functions/2/for-each.js
@@ -7,6 +7,7 @@
 function forEach(array, callback) {
 	if (!(array instanceof Array)) throw TypeError(array + ' is not an array');
 	if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
+	
 	for (var i = 0; i < array.length; i++)
 		callback(array[i], i);
 }
