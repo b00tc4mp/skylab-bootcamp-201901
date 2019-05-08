4e1483175c78bd94273a78181230877cf9a1f872
Markuson <el.marcuson@gmail.com>
2019-05-08 15:38:45 +0200

    solving conflicts

---
+++ staff/marc-uson/array-functions/some.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @param {Array} array 
+ * @param {any} value 
+ * @param {Function} callback 
+ */
+
+function some(array, callback){
+    if (!(array instanceof Array)) throw new TypeError(array + ' is not an array');
+    if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
+
+    for(var i = 0; i< array.length; i++) if (callback(array[i])) return true;
+
+    return false;
+    
+}
