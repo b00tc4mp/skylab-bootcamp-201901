db468a6c37f1d7eb86399e34a4e9c0790d7b607b
Pablo Rey <pabloreysobral@gmail.com>
2019-05-13 10:40:15 +0200

    Add folders and routes to split code

---
+++ staff/pablo-rey/web-app/routes/home/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const { checkLogin } = require('../../middlewares');
+var router = express.Router();
+
+router.use(checkLogin('/', false));
+
+router.use('/favorites', require('./favorites'));
+router.use('/search', require('./search'));
+router.use('/duck', require('./duck'));
+
+router.get('/', (req, res) => {
+  const {
+    session: { logic },
+  } = req;
+
+  logic
+    .retrieveUser()
+    .then(({ name }) => res.render('home', { name }))
+    .catch(({ message }) => res.render('home', { name, message }));
+});
+
+module.exports = router;
