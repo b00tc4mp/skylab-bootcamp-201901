d229346d020c7211c497df6fb1ed89bdeb129005
carmecass <carmecass@gmail.com>
2019-05-22 12:05:48 +0200

    firts mongodb

---
+++ staff/carme-casserras/rest-api-c/3/logic/index.js
@@ -0,0 +1,138 @@
+const validate = require('../common/validate')
+const duckApi = require('../data/duck-api')
+const { LogicError } = require('../common/errors')
+const userData = require('../data/user-data')
+
+const logic = {
+    registerUser(name, surname, email, password) {
+        validate.arguments([
+            { name: 'name', value: name, type: 'string', notEmpty: true },
+            { name: 'surname', value: surname, type: 'string', notEmpty: true },
+            { name: 'email', value: email, type: 'string', notEmpty: true },
+            { name: 'password', value: password, type: 'string', notEmpty: true }
+        ])
+
+        validate.email(email)
+
+        return (async () => {
+             const users = await userData.find(user => user.email === email)
+            
+            if (users.length) throw new LogicError(`user with email "${email}" already exists`)
+
+            return await userData.create({ email, password, name, surname })
+        })()
+    },
+
+    authenticateUser(email, password) {
+        validate.arguments([
+            { name: 'email', value: email, type: 'string', notEmpty: true },
+            { name: 'password', value: password, type: 'string', notEmpty: true }
+        ])
+
+        validate.email(email)
+
+        return (async () => {
+            const users = await userData.find(user => user.email === email)
+        
+            if (!users.length) throw new LogicError(`user with email "${email}" does not exist`)
+
+            const [user] = users
+
+            if (user.password !== password) throw new LogicError('wrong credentials')
+
+            return user.id
+        })()
+    },
+
+    retrieveUser(id) {
+        validate.arguments([
+            { name: 'id', value: id, type: 'string', notEmpty: true }
+        ])
+
+        return (async () => {
+
+        const user = await userData.retrieve(id)
+            
+        if (!user) throw new LogicError(`user with id "${id}" does not exist`)
+
+        const { name, surname, email } = user
+
+        return { name, surname, email }
+        })()
+    },
+
+    searchDucks(id, query) {
+        validate.arguments([
+            { name: 'id', value: id, type: 'string', notEmpty: true },
+            { name: 'query', value: query, type: 'string' }
+        ])
+
+        return (async () => { 
+        const user = await userData.retrieve(id)
+           
+        if (!user) throw new LogicError(`user with id "${id}" does not exist`)
+
+        const ducks = await duckApi.searchDucks(query)
+
+        return ducks instanceof Array ? ducks : []
+        
+    })()
+
+    },
+
+    retrieveDuck(id, duckId) {
+        validate.arguments([
+            { name: 'id', value: id, type: 'string', notEmpty: true },
+            { name: 'duckId', value: duckId, type: 'string', notEmpty: true }
+        ])
+
+        return  (async () => {
+        const user = userData.retrieve(id)
+            
+        if (!user) throw new LogicError(`user with id "${id}" does not exist`)
+
+        return await duckApi.retrieveDuck(duckId)
+        })()
+    },
+
+    toggleFavDuck(id, duckId) {
+        validate.arguments([
+            { name: 'id', value: id, type: 'string', notEmpty: true },
+            { name: 'duckId', value: duckId, type: 'string', notEmpty: true }
+        ])
+
+        return (async () => {
+        const user = await userData.retrieve(id)
+            
+        const { favs = [] } = user
+
+        const index = favs.indexOf(duckId)
+
+        if (index < 0) favs.push(duckId)
+        else favs.splice(index, 1)
+
+        return await userData.update(id, { favs })
+                    
+        })()
+    },
+
+    retrieveFavDucks(id) {
+        validate.arguments([
+            { name: 'id', value: id, type: 'string', notEmpty: true }
+        ])
+
+        return (async () => {
+        const user = await userData.retrieve(id)
+            
+        const { favs = [] } = user
+
+        if (favs.length) {
+            const calls = favs.map(fav => duckApi.retrieveDuck(fav))
+           
+        return await Promise.all(calls)
+        } else return favs
+    })()
+    }
+}
+
+module.exports = logic
