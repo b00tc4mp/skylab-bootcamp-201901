64d46179292d6b31ab7ffc228bf575f745bb3f08
Albert <albert_escoli@hotmail.com>
2019-05-23 11:54:40 +0200

    mongoose demo2

--- staff/albert-escolar/mongoose-demo/index.js
+++ staff/albert-escolar/mongoose-demo/index.js
@@ -5,8 +5,7 @@ mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB...', err))
 
-
-async function createCourse(l) {
+async function createCourse(name, author, tags, isPublished) {
     const course = new Course({
         name: 'Test Course',
         author: 'Mosh',
