a34e1357e337bbcd8ee91ca4d4358c9e99d1a938
manuelbarzi <manuelbarzi@manuelbarzis-MacBook-Pro.local>
2019-05-24 13:19:14 +0200

    update notes-api

--- staff/manuel-barzi/notes-apx/notes-api/logic/index.js
+++ staff/manuel-barzi/notes-apx/notes-api/logic/index.js
@@ -1,14 +1,74 @@
 import models from '../data/models'
+import argon2 from 'argon2'
+import LogicError from './logic-error'
 
 const { User, Note } = models
 
 const logic = {
+    registerUser(name, surname, email, password) {
+        // TODO validate inputs
+
+
+        // TODO implement logic
+        return (async () => {
+            const hash = await argon2.hash(password)
+
+            // TODO end logic, other cases, flows, states... (user already exists check, etc)
+
+            await User.create({ name, surname, email, password: hash })
+        })()
+    },
+
+    authenticateUser(email, password) {
+        // TODO validate inputs
+
+        // TODO implement logic
+        return (async () => {
+            const user = await User.findOne({ email })
+
+            if (await argon2.verify(user.password, password)) return user.id
+            else throw new LogicError('wrong credentials')
+        })()
+    },
+
+    retrieveUser(id) {
+        // TODO validate inputs
+
+        // TODO implement logic
+        return (async () => {
+            // 1
+
+            // const user = await User.findById(id).lean()
+
+            // user.id = user._id.toString()
+            // delete user._id
+
+            // delete user.password
+            // delete user.notes
+            // delete user.__v
+
+            // return user
+
+            // 2
+
+            return  await User.findById(id).select('name surname email -_id').lean()
+
+            // 3
+
+            // const { name, surname, email } = await User.findById(id)
+
+            // return { name, surname, email }
+        })()
+    },
+
     addPublicNote(userId, text) {
         // TODO validate inputs
 
         // TODO implement logic
 
-        return (async() => {
+        // return Note.create({ author: userId, text }).then(() => {})
+
+        return (async () => {
             await Note.create({ author: userId, text })
         })()
     },
--- staff/manuel-barzi/notes-apx/notes-api/logic/index.test.js
+++ staff/manuel-barzi/notes-apx/notes-api/logic/index.test.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import models from '../data/models'
 import { expect } from 'chai'
 import logic from '.';
+import argon2 from 'argon2'
 
 dotenv.config()
 
@@ -10,7 +11,7 @@ const { User, Note } = models;
 const { env: { MONGO_URL_LOGIC_TEST: url } } = process
 
 describe('logic', () => {
-    let userData
+    let name, surname, email, password
 
     before(() => mongoose.connect(url, { useNewUrlParser: true }))
 
@@ -18,25 +19,78 @@ describe('logic', () => {
         await User.deleteMany()
         await Note.deleteMany()
 
-        userData = {
-            name: `name-${Math.random()}`,
-            surname: `surname-${Math.random()}`,
-            email: `email-${Math.random()}@mail.com`,
-            password: `password-${Math.random()}`
-        }
+        name = `name-${Math.random()}`
+        surname = `surname-${Math.random()}`
+        email = `email-${Math.random()}@mail.com`
+        password = `password-${Math.random()}`
+    })
+
+    describe('register user', () => {
+        it('should succeed on correct data', async () => {
+            const res = await logic.registerUser(name, surname, email, password)
+
+            expect(res).to.be.undefined
+
+            const users = await User.find()
+
+            expect(users).to.exist
+            expect(users).to.have.lengthOf(1)
+
+            const [user] = users
+
+            expect(user.name).to.equal(name)
+            expect(user.surname).to.equal(surname)
+            expect(user.email).to.equal(email)
+
+            expect(user.password).to.exist
+
+            expect(await argon2.verify(user.password, password)).to.be.true
+        })
+    })
+
+    describe('authenticate user', () => {
+        let user
+
+        beforeEach(async () => user = await User.create({ name, surname, email, password: await argon2.hash(password) }))
+
+        it('should succeed on correct credentials', async () => {
+            const id = await logic.authenticateUser(email, password)
+
+            expect(id).to.exist
+            expect(id).to.be.a('string')
+
+            expect(id).to.equal(user.id)
+        })
+    })
+
+    describe('retrieve user', () => {
+        let user
+
+        beforeEach(async () => user = await User.create({ name, surname, email, password: await argon2.hash(password) }))
+
+        it('should succeed on correct id from existing user', async () => {
+            const _user = await logic.retrieveUser(user.id)
+
+            expect(_user.id).to.be.undefined
+            expect(_user.name).to.equal(name)
+            expect(_user.surname).to.equal(surname)
+            expect(_user.email).to.equal(email)
+
+            expect(_user.password).to.be.undefined
+        })
     })
 
     describe('add public note', () => {
         let user
 
-        beforeEach(async () => user = await User.create(userData))
+        beforeEach(async () => user = await User.create({ name, surname, email, password: await argon2.hash(password) }))
 
         it('should succeed for existing user', async () => {
             const text = 'Hola, Mundo!'
-            
-            debugger
 
-            await logic.addPublicNote(user.id, text)
+            const res = await logic.addPublicNote(user.id, text)
+
+            expect(res).to.be.undefined
 
             const notes = await Note.find()
 
