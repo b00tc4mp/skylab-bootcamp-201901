0d412c42cc5e50b1131b0dce7cee9ec38262482b
Llorenç Comellas <llorenscomellas@gmail.com>
2019-06-12 22:22:39 +0200

    add documentation

--- staff/llorenç-comellas/kaori/kaori-app/src/data/kaori-api/index.js
+++ staff/llorenç-comellas/kaori/kaori-app/src/data/kaori-api/index.js
@@ -1,9 +1,9 @@
 import { validate, call } from 'kaori-utils'
 
-const { REACT_APP_PORT } = process.env
+const { REACT_APP_PORT} = process.env
 
 const kaoriApi = {
-    __url__: `http://localhost:${REACT_APP_PORT}/api`,
+    __url__:REACT_APP_PORT,
     __timeout__: 0,
 
     registerUser(name, surname, phone, email, password) {
