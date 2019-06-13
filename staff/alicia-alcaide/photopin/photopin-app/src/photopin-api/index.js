8c10a9c56714f0c69e16b71081ff8db36f826357
alicia-alcaide <alicia.alcaide@gmail.com>
2019-06-13 12:40:42 +0200

    coment conection to heroku

--- staff/alicia-alcaide/photopin/photopin-app/src/photopin-api/index.js
+++ staff/alicia-alcaide/photopin/photopin-app/src/photopin-api/index.js
@@ -5,9 +5,9 @@ const port = process.env.REACT_APP_APP_PORT
 
 const photopinApi = {
 
-    // __url__: `http://localhost:${port}/api`,
+    __url__: `http://localhost:${port}/api`,
 
-    __url__: `https://radiant-ocean-32759.herokuapp.com/api`,
+    //__url__: `https://radiant-ocean-32759.herokuapp.com/api`,
 
     __timeout__: 0,
 
